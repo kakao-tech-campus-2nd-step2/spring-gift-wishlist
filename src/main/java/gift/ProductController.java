31fc37a03e9bb7dcbd854daf08d513d37fa04af5
stopmin <geemin0@naver.com>
2024-06-24 19:13:20 +0900

    product crud controller 생성

---
+++ src/main/java/gift/ProductController.java
@@ -0,0 +1,43 @@
+package gift;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    private final ProductService productService = new ProductService();
+
+    @GetMapping("/products")
+    public List<Product> getProducts() {
+        return productService.getProducts();
+    }
+
+    @PostMapping("/create")
+    public void addProduct(Product product) {
+        productService.addProduct(product);
+    }
+
+    @PostMapping("/delete")
+    public void deleteProduct(String name) {
+        productService.deleteProduct(name);
+    }
+
+    @PostMapping("/update")
+    public void updateProduct(String name, Product product) {
+        productService.updateProduct(name, product);
+    }
+
+    @GetMapping("/products/{name}")
+    public Product getProductByName(String name) {
+        return productService.getProductByName(name);
+    }
+
+    @GetMapping("/products/{id}")
+    public Product getProductById(Long id) {
+        return productService.getProductById(id);
+    }
+
+}
