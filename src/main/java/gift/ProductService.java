10cf63563ce101139f8f6e6dcb4d16678e055939
stopmin <geemin0@naver.com>
2024-06-24 19:13:10 +0900

    product 서비스 로직

---
+++ src/main/java/gift/ProductService.java
@@ -0,0 +1,38 @@
+package gift;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository = new ProductRepository();
+
+    public ProductService() {
+    }
+
+    public List<Product> getProducts() {
+        return productRepository.getProducts();
+    }
+
+    public void addProduct(Product product) {
+        productRepository.addProduct(product);
+    }
+
+    public void deleteProduct(String name) {
+        productRepository.deleteProduct(name);
+    }
+
+    public void updateProduct(String name, Product product) {
+        productRepository.updateProduct(name, product);
+    }
+
+    public Product getProductByName(String name) {
+        return productRepository.getProductByName(name);
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.getProductById(id);
+    }
+
+}
