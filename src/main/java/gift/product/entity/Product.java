9cae7af39a39df980f059303d1aeef7a9ab10c13
JaeBin <201924576@pusan.ac.kr>
2024-06-25 18:25:53 +0900

    fix(product): add column id into product entity

--- src/main/java/gift/product/entity/Product.java
+++ src/main/java/gift/product/entity/Product.java
@@ -1,14 +1,19 @@
 package gift.product.entity;
 
 public class Product {
+    private Long id;
     private String name;
     private int price;
     private String imageUrl;
 
-    public Product(String name, int price, String imageUrl) {
+    public Product(Long id, String name, int price, String imageUrl) {
+        this.id = id;
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
+    }
+    public Long getId() {
+        return id;
     }
 
     public String getName() {
@@ -22,4 +27,5 @@ public class Product {
     public String getImageUrl() {
         return imageUrl;
     }
+
 }
