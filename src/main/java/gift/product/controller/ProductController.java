5b377756dea992d7cfbcbfddf83e831fa44bcdd3
JaeBin <201924576@pusan.ac.kr>
2024-06-25 18:20:04 +0900

    feat(product): create product controller

--- src/main/java/gift/product/controller/ProductController.java
+++ src/main/java/gift/product/controller/ProductController.java
@@ -1,7 +1,8 @@
 package gift.product.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/api")
 public class ProductController {
 }
