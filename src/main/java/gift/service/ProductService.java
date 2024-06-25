3fc7ecc164f51d7b0cf54a8724d943b1c6aacca5
nove1080 <skwpqjq@gmail.com>
2024-06-25 15:51:28 +0900

    feat: ProductService createProduct 구현

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -1,15 +1,26 @@
 package gift.service;
 
+import gift.converter.ProductConverter;
+import gift.domain.Product;
 import gift.repository.ProductRepository;
+import gift.web.dto.request.CreateProductRequest;
+import gift.web.dto.response.CreateProductResponse;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final ProductConverter productConverter;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, ProductConverter productConverter) {
         this.productRepository = productRepository;
+        this.productConverter = productConverter;
+    }
+
+    public CreateProductResponse createProduct(CreateProductRequest request) {
+        Product product = productConverter.convertToEntity(request);
+        return new CreateProductResponse(productRepository.save(product));
     }
 
 }
