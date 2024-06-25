f376876bae07a856acea70afe751e627bbb8d372
JaeBin <201924576@pusan.ac.kr>
2024-06-25 18:26:28 +0900

    feat(product): add method get product by id in controller

--- src/main/java/gift/product/controller/ProductController.java
+++ src/main/java/gift/product/controller/ProductController.java
@@ -1,8 +1,17 @@
 package gift.product.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import gift.product.entity.Product;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController("/api")
 public class ProductController {
+    private final Map<Long, Product> productMap = new HashMap<>();
+
+    @GetMapping("/product/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return productMap.get(id);
+    }
 }
