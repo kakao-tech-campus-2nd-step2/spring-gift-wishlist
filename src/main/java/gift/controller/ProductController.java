bdf237cbb2b30814e94d0794213f2a4c27b63f55
momnpa333 <momnpa333@gmail.com>
2024-06-25 21:15:34 +0900

    feat(ProductController): controller 제작

---
+++ src/main/java/gift/controller/ProductController.java
@@ -0,0 +1,58 @@
+package gift.controller;
+
+import gift.model.Product;
+import gift.model.ProductDao;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    private final ProductDao productDao;
+
+    public ProductController(ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getProducts() {
+        var response = productDao.findAll();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable("id") Long id) {
+        var response = productDao.findById(id);
+        return ResponseEntity.ok(response.orElse(null));
+    }
+
+    @PostMapping("/products")
+    public ResponseEntity<Product> createProduct(@RequestBody ProductRequest request) {
+        var response = productDao.save(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable("id") Long id,
+        @RequestBody ProductRequest request) {
+        var response = productDao.update(id, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable("id") Long id) {
+        productDao.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
