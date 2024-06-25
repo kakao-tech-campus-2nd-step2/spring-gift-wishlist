0d0e8c0126b71f0cafcd706d8267fab5eef41dff
nove1080 <skwpqjq@gmail.com>
2024-06-25 14:44:38 +0900

    feat: ProductController 생성

---
+++ src/main/java/gift/controller/ProductController.java
@@ -0,0 +1,17 @@
+package gift.controller;
+
+import gift.repository.ProductRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+}
