223adcb44078f5538a34ce7985464b95267238e2
stopmin <geemin0@naver.com>
2024-06-26 00:24:51 +0900

    feat: thymeleaf 템플릿 추가 및 PageController 생성

---
+++ src/main/java/gift/PageController.java
@@ -0,0 +1,14 @@
+package gift;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String redirectToProducts() {
+        return "employees.html";
+    }
+
+}
