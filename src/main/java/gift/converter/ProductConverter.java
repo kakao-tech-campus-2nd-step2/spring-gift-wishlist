14a80000f9c2dacf577f32fa3733e97ff7a11c97
nove1080 <skwpqjq@gmail.com>
2024-06-25 23:13:39 +0900

    docs: ProductConverter 주석 수정

--- src/main/java/gift/converter/ProductConverter.java
+++ src/main/java/gift/converter/ProductConverter.java
@@ -5,7 +5,7 @@ import gift.web.dto.request.CreateProductRequest;
 import org.springframework.stereotype.Component;
 
 /**
- * Converts a CreateProductRequest to a Product entity.
+ * 상품 관련 요청/응답 클래스를 상품 엔티티로 변환하는 컨버터 클래스입니다.
  */
 @Component
 public class ProductConverter {
