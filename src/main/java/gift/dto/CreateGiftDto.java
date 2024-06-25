42e4815bf8be9cc54383c1f442ba8e5ff6fb2b46
sim-mer <gyu2781@gmail.com>
2024-06-25 13:43:14 +0900

    feat(dto): 상품 추가 dto 검증 로직 추가

--- src/main/java/gift/dto/CreateGiftDto.java
+++ src/main/java/gift/dto/CreateGiftDto.java
@@ -1,9 +1,26 @@
 package gift.dto;
 
 import gift.domain.Gift;
+import gift.exception.ErrorCode;
+import gift.exception.InvalidRequestException;
 
-public record CreateGiftDto (String name, Integer price, String description, String url) {
+public record CreateGiftDto (String name, Integer price, String description, String imageUrl) {
     public Gift toGift() {
-        return new Gift(name, description, price, url);
+        return new Gift(name, description, price, imageUrl);
+    }
+
+    public CreateGiftDto {
+        if (name == null || name.isBlank()) {
+            throw new InvalidRequestException(ErrorCode.INVALID_REQUEST);
+        }
+        if (price == null || price <= 0) {
+            throw new InvalidRequestException(ErrorCode.INVALID_REQUEST);
+        }
+        if (description == null || description.isBlank()) {
+            throw new InvalidRequestException(ErrorCode.INVALID_REQUEST);
+        }
+        if (imageUrl == null || imageUrl.isBlank()) {
+            throw new InvalidRequestException(ErrorCode.INVALID_REQUEST);
+        }
     }
 }
