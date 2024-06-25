7c8c8c53d4e39a949956348a3a2392a76864fb16
student-p <paulmoon00@naver.com>
2024-06-25 18:37:12 +0900

    feat: 하나의 상품 조회 기능 추가

---
+++ src/main/java/gift/controller/ProductController.java
@@ -0,0 +1,38 @@
+package gift.controller;
+
+import gift.Product;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    private final Map<Long, Product> products = new HashMap<>();
+
+    ProductController(){
+        products.put(1L, new Product(1L, "AAA", 123, "adsfiewfjo"));
+        products.put(2L, new Product(2L, "BBB", 456, "eroguhensjkn"));
+    }
+
+    // 모두 조회
+    @GetMapping("/api/products")
+    public Map<Long, Product> responseAllProducts(){
+        return products;
+    }
+
+    // 하나 조회
+    @GetMapping("/api/products/{id}")
+    public ResponseEntity<?> responseOneProduct(@PathVariable("id") long id){
+        if(products.containsKey(id)){
+            return new ResponseEntity<>(products.get(id), HttpStatus.OK);
+        }
+        return new ResponseEntity<>("ID not exists", HttpStatus.NOT_FOUND);
+    }
+
+
+}
