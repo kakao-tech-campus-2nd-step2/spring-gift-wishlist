c9b4fb530f459a6c615d3f39cfc5ef16e8de8515
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-06-25 16:00:10 +0900

    feat: 단일 상품 DB 저장 기능 구현

--- src/main/java/gift/product/model/Product.java
+++ src/main/java/gift/product/model/Product.java
@@ -1,20 +1,13 @@
 package gift.product.model;
 
-import org.springframework.lang.NonNull;
-
 public class Product {
-    @NonNull
     private Long id;        // 선물의 고유 식별자
-    @NonNull
     private String name;    // 선물의 이름
-    @NonNull
     private int price;      // 선물의 가격
     private String imageUrl; // 선물 이미지 URL
-    @NonNull
     private Boolean isActive = true; // 선물의 활성화 상태
 
     public Product(String name, int price, String imageUrl, Boolean isActive) {
-        this.id = lastId;
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
--- src/main/java/gift/product/model/ProductRepository.java
+++ src/main/java/gift/product/model/ProductRepository.java
@@ -5,10 +5,20 @@ import java.util.Map;
 
 public class ProductRepository {
     private final Map<Long, Product> products = new HashMap<>();
+    private Long nextId = 0L;
 
     public Product findProduct(Long id) throws Exception {
         validateProductId(id);
         return products.get(id); // ID가 유효한 경우, 상품 반환
+    }
+
+    public void addProduct(Product product) throws Exception {
+        product.setId(generateNextId()); // 새 ID 할당
+        products.put(product.getId(), product);
+    }
+
+    private synchronized Long generateNextId() {
+        return ++nextId;
     }
 
     // 상품 ID 유효성 검증 메서드
@@ -17,4 +27,4 @@ public class ProductRepository {
             throw new Exception("존재하지 않는 상품 ID 입니다.");
         }
     }
-}
+}ㅎ
