2b52d04de7514e70a851fc602b152d0df1ff4373
JaeBin <201924576@pusan.ac.kr>
2024-06-25 18:21:00 +0900

    feat(product): create product entity

--- src/main/java/gift/product/entity/Product.java
+++ src/main/java/gift/product/entity/Product.java
@@ -1,27 +1,14 @@
 package gift.product.entity;
 
 public class Product {
-    private Long id;
     private String name;
     private int price;
     private String imageUrl;
 
-    public Product(Long id, String name, int price, String imageUrl) {
-        this.id = id;
+    public Product(String name, int price, String imageUrl) {
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
-    }
-
-    public Product(Long id, ProductInfo productInfo) {
-        this.id = id;
-        this.name = productInfo.getName();
-        this.price = productInfo.getPrice();
-        this.imageUrl = productInfo.getImageUrl();
-    }
-
-    public Long getId() {
-        return id;
     }
 
     public String getName() {
