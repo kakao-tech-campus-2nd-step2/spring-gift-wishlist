eaa72370b041c5388b61c0dc8363cde57398c93b
xkrwjdals <developjmt@gmail.com>
2024-06-26 14:53:45 +0900

    feat(controller): Add ProductController with @PostMapping
    - Implement @PostMapping to handle POST request
    - Use @RequestBody to use parameter Product p

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,4 +32,9 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
 
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product p) {
+        Product product = productOperation.createProduct(p);
+        return ResponseEntity.status(HttpStatus.CREATED).body(product);
+    }
 }
