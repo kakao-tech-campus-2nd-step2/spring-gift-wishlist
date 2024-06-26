5d21ad18fde656c16a4ee7da158f6d3f15da2448
JaeBin <201924576@pusan.ac.kr>
2024-06-26 15:49:17 +0900

    refactor(global): combine common parts of API URLs

--- src/main/java/gift/product/controller/ProductController.java
+++ src/main/java/gift/product/controller/ProductController.java
@@ -8,28 +8,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/products")
 public class ProductController {
     private final Map<Long, Product> productMap = new HashMap<>();
 
-    @GetMapping("/products/{id}")
+    @GetMapping("/{id}")
     public Product getProductById(@PathVariable(name = "id") Long id) {
         return productMap.get(id);
     }
 
-    @PostMapping("/products")
+    @PostMapping("")
     public Product createProduct(@RequestBody ProductRequest productRequest) {
         Long id = addProduct(productRequest);
         return productMap.get(id);
     }
 
-    @PutMapping("/products/{id}")
+    @PutMapping("/{id}")
     public Product updateProduct(@PathVariable(name = "id") Long id, @RequestBody ProductRequest productRequest) {
         productMap.replace(id, new Product(id, productRequest));
         return productMap.get(id);
     }
 
-    @DeleteMapping("/products/{id}")
+    @DeleteMapping("/{id}")
     public String deleteProduct(@PathVariable(name = "id") Long id) {
         productMap.remove(id);
         return "product " + id + " is deleted";
