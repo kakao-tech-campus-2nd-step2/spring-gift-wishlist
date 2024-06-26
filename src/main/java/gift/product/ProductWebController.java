a07d8ae21ad76892e46af702b84e88f143e1a292
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-06-27 02:18:38 +0900

    feat: 상품 추가 컨트롤러 구현

--- src/main/java/gift/product/ProductWebController.java
+++ src/main/java/gift/product/ProductWebController.java
@@ -9,7 +9,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/product")
@@ -33,5 +36,11 @@ public class ProductWebController {
         return "add_product";
     }
 
+    @PostMapping("/save")
+    public String saveProduct(@ModelAttribute PostProductReq productReq, RedirectAttributes redirectAttributes) {
+        productRepository.addProduct(productReq);
+        redirectAttributes.addFlashAttribute("message", "새 상품이 추가되었습니다.");
+        return "redirect:/product";
+    }
 
 }
