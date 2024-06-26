afcf76ac3906182a0a9b8740a740dd8e8b8ae791
xkrwjdals <developjmt@gmail.com>
2024-06-26 15:55:28 +0900

    feat(controller): Add ProductController with @DeleteMapping
    - Implement @DeleteMapping for DELETE request
    - Use boolean check variable
    - Return ResponseEntity.noContent if delete succeed, else return 404

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -5,6 +5,7 @@ import javax.swing.text.html.parser.Entity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,5 +49,14 @@ public class ProductController {
             return ResponseEntity.ok(product);
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Product> deleteProduct(@PathVariable Long id) {
+        boolean check = productOperation.deleteProduct(id);
+        if (check) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 }
