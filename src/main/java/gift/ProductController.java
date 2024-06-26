bf4fe2cede7b999ce01582833246948e17de3760
xkrwjdals <developjmt@gmail.com>
2024-06-26 14:46:32 +0900

    feat(controller): Add ProductController with @GetMapping
    - Implement @GetMapping to handle GET request
    - getAllProduct method return all products
    - getProductById method return product by @RequestParam id

---
+++ src/main/java/gift/ProductController.java
@@ -0,0 +1,33 @@
+package gift;
+
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    @Autowired
+    private ProductOperation productOperation;
+
+    @GetMapping
+    public ResponseEntity<Map<Long, Product>> getAllProduct() {
+        Map<Long, Product> products = productOperation.getAllProduct();
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@RequestParam("id") Long id) {
+        Product product = productOperation.getProductById(id);
+        if (product != null) {
+            return ResponseEntity.ok(product);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    }
+
+}
