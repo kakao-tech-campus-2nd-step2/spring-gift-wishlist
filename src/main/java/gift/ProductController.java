b31f7cf89aa14b62c4f815b75e86421595ce6537
minju26 <mjalswn26@gmail.com>
2024-06-26 23:50:08 +0900

    feat: implement product addition
    - added default constructor to Product model

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -1,15 +1,17 @@
 package gift;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class ProductController {
-    Product newProduct = new Product(
+
+    private static final String ADD_SUCCESS_MSG = "상품 추가 성공";
+
+    Product initialProduct = new Product(
             8146027L,
             "아이스 카페 아메리카노 T",
             4500,
@@ -19,7 +21,7 @@ public class ProductController {
     private final Map<Long, Product> products = new HashMap<>();
 
     public ProductController() {
-        products.put(newProduct.getId(), newProduct);
+        products.put(initialProduct.getId(), initialProduct);
     }
 
     @GetMapping("/api/products/{productId}")
@@ -27,8 +29,18 @@ public class ProductController {
         return getProduct(productId);
     }
 
+    @PostMapping("/api/products")
+    public ResponseEntity<String> addProduct(@RequestBody Product product) {
+        return ResponseEntity.ok(addNewProduct(product));
+    }
+
     public Product getProduct(Long productId) {
         return products.get(productId);
+    }
+
+    public String addNewProduct(Product newProduct) {
+        products.put(newProduct.getId(), newProduct);
+        return ADD_SUCCESS_MSG;
     }
 
 }
