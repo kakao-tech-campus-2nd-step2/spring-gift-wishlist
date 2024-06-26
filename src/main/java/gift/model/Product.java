b8a1e9b85d0dc6bf9497c4aed463eaa9fb5bd92e
donghyuun <bomul0524@gmail.com>
2024-06-26 15:46:22 +0900

    feat: create Product class

---
+++ src/main/java/gift/model/Product.java
@@ -0,0 +1,55 @@
+package gift.model;
+
+public class Product {
+
+    //id 값을 위해 정적 변수 선언
+    private static Long id = 1L;
+
+    private String name;
+    private int price;
+    private String imageUrl;
+
+    public Product(Long id, String name, int price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    // id 값 증가
+    public void increase() {
+        this.id += 1;
+    }
+
+    public static Long getId() {
+        return id;
+    }
+
+    public static void setId(Long id) {
+        Product.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
