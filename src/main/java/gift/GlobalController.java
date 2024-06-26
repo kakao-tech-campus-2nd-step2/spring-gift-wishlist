10883378cb3a4d54812f064b7fa3a48addb4a952
JaeBin <201924576@pusan.ac.kr>
2024-06-26 15:33:12 +0900

    feat(global): create global controller

---
+++ src/main/java/gift/GlobalController.java
@@ -0,0 +1,12 @@
+package gift;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GlobalController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
