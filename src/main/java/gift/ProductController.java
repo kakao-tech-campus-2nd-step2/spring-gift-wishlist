5de4c1614b0ea2d3114c6d8defc3c4846b0b1df7
xkrwjdals <developjmt@gmail.com>
2024-06-26 15:30:12 +0900

    feat(controller): Add ProductController with @PutMapping
    - Implement @PutMapping for PUT request
    - Return ResponseEntity.ok if update succeed, else return 404

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -1,11 +1,14 @@
 package gift;
 
 import java.util.Map;
+import javax.swing.text.html.parser.Entity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,5 +39,14 @@ public class ProductController {
     public ResponseEntity<Product> createProduct(@RequestBody Product p) {
         Product product = productOperation.createProduct(p);
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product p) {
+        Product product = productOperation.updateProduct(id, p);
+        if (product != null) {
+            return ResponseEntity.ok(product);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
 }
