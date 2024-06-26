d0f2418133176c5a6b61639b24657930fb1c5b85
xkrwjdals <developjmt@gmail.com>
2024-06-26 14:27:53 +0900

    feat(service): add ProductService
    - Implement ProductOperation class
    - Add methods for CRUD
    - Use hashmap for storage

---
+++ src/main/java/gift/ProductOperation.java
@@ -0,0 +1,39 @@
+package gift;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductOperation {
+    private final Map<Long, Product> productMap = new HashMap<>();
+    private long Id = 1;
+
+    public Product createProduct(Product p) {
+        Product newProduct = new Product(Id++, p.name(), p.price(), p.imageUrl());
+        productMap.put(newProduct.id(), newProduct);
+        return newProduct;
+    }
+
+    public Product getProductById(long id) {
+        return productMap.get(id);
+    }
+
+    public Map<Long, Product> getAllProduct() {
+        return productMap;
+    }
+
+    public Product updateProduct(Long id, Product p) {
+        if (productMap.containsKey(id)) {
+            Product newProduct = new Product(id, p.name(), p.price(), p.imageUrl());
+            productMap.put(newProduct.id(), newProduct);
+            return newProduct;
+        }
+        return null;
+    }
+
+    public boolean deleteProduct(Long id) {
+        return productMap.remove(id) != null;
+    }
+
+}
