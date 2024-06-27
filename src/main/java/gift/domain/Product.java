c02e8644af385e9a5a960c57e9437faa6df1abfa
stopmin <geemin0@naver.com>
2024-06-27 20:14:16 +0900

    refactor: Java14 record 활용하도록 수정

--- src/main/java/gift/domain/Product.java
+++ src/main/java/gift/domain/Product.java
@@ -2,25 +2,10 @@ package gift.domain;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-public class Product {
+public record Product(Long id, String name, double price, String imageUrl) {
 
     private final static AtomicLong index = new AtomicLong();
 
-    private Long id;
-    private String name;
-    private double price;
-    private String imageUrl;
-
-
-    public Product() {
-    }
-
-    public Product(String name, String imageUrl, double price) {
-        this.id = index.incrementAndGet();
-        this.name = name;
-        this.price = price;
-        this.imageUrl = imageUrl;
-    }
 
     public String getName() {
         return name;
