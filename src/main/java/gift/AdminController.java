eab6dd5fb5b9ff4732972dfa8a6b0b8d61b5c946
xkrwjdals <developjmt@gmail.com>
2024-06-28 05:31:07 +0900

    feat: Update AdminController.java with @GetMapping("/add") and @PostMapping
    - Implement showAddPage and addProduct
    - Handle GET request for show product-add template
    - Handle POST request when click save button in product-add template

--- src/main/java/gift/AdminController.java
+++ src/main/java/gift/AdminController.java
@@ -4,8 +4,11 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/admin/product")
@@ -18,5 +21,16 @@ public class AdminController {
         Map<Long, Product> products = productOperation.getAllProduct();
         model.addAttribute("products", products);
         return "product-list";
+    }
+    @GetMapping("/add")
+    public ModelAndView showAddPage() {
+        ModelAndView modelAndView = new ModelAndView("product-add");
+        modelAndView.addObject("product", new Product(0L, "", 0L, ""));
+        return modelAndView;
+    }
+    @PostMapping("/add")
+    public String addProduct(@ModelAttribute Product p) {
+        Product product = productOperation.createProduct(p);
+        return "redirect:/admin/product/list";
     }
 }
