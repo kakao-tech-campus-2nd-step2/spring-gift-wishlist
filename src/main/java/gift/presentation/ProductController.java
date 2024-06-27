7668d37f2f52af4fa25952d639ec0b0a49de76f2
stopmin <geemin0@naver.com>
2024-06-27 20:56:41 +0900

    refactor: endpoint 수정

--- src/main/java/gift/presentation/ProductController.java
+++ src/main/java/gift/presentation/ProductController.java
@@ -2,52 +2,56 @@ package gift.presentation;
 
 import gift.application.ProductService;
 import gift.domain.Product;
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/products")
 public class ProductController {
 
-    private final ProductService productService = new ProductService();
+    private final ProductService productService;
 
-    @GetMapping("/products")
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("")
     public ResponseEntity<List<Product>> getProducts() {
         List<Product> products = productService.getProducts();
         return ResponseEntity.ok(products);
     }
 
-    @PostMapping("/create")
+    @PostMapping("")
     public ResponseEntity<Object> addProduct(@RequestBody Product product) {
         productService.addProduct(product);
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/delete")
-    public ResponseEntity<Object> deleteProduct(@RequestBody String name) {
+    @DeleteMapping("/{name}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable String name) {
         productService.deleteProduct(name);
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/update")
-    public ResponseEntity<Object> updateProduct(String name, Product product) {
+    @PutMapping("/{name}")
+    public ResponseEntity<Object> updateProduct(@PathVariable String name, @RequestBody Product product) {
         productService.updateProduct(name, product);
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/products/{name}")
-    public ResponseEntity<Product> getProductByName(String name) {
+    @GetMapping("/name/{name}")
+    public ResponseEntity<Product> getProductByName(@PathVariable String name) {
         Product product = productService.getProductByName(name);
         return ResponseEntity.ok(product);
     }
 
-    @GetMapping("/products/{id}")
-    public ResponseEntity<Product> getProductById(Long id) {
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
         Product product = productService.getProductById(id);
         return ResponseEntity.ok(product);
     }
 }
-
