7e6aae30d35c177137020e79954be80ef4bc55d3
geist9110 <geist9110@gmail.com>
2024-06-27 22:50:13 +0900

    feat(Admin): Add pagination feature

--- src/main/java/gift/AdminPageController.java
+++ src/main/java/gift/AdminPageController.java
@@ -5,14 +5,17 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class AdminPageController {
     private final ProductRepository productRepository = ProductRepository.getInstance();
+    private final Integer pageSize = 5;
 
-    @GetMapping(path = "/admin")
-    public String adminPage(Model model) {
+    @GetMapping(path = "/admin/{page}")
+    public String adminPage(Model model, @PathVariable("page") Integer page) {
         List<Product> products = productRepository.getAllProduct();
+        products = products.subList((page - 1) * pageSize, Math.min(page * pageSize, products.size()));
         model.addAttribute("products", products);
 
         return "adminPage";
