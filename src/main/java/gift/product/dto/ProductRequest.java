1345df908ce6f2e0a2a50862c7bc3fcddbfddc46
JaeBin <201924576@pusan.ac.kr>
2024-06-27 16:01:15 +0900

    feat(product): add toServiceDto method to convert ProductRequest

--- src/main/java/gift/product/dto/ProductRequest.java
+++ src/main/java/gift/product/dto/ProductRequest.java
@@ -1,10 +1,16 @@
 package gift.product.dto;
 
+import gift.product.service.ProductService;
+
 import java.util.Objects;
 
 public record ProductRequest(String name, int price, String imageUrl) {
     public ProductRequest {
         Objects.requireNonNull(name);
         Objects.requireNonNull(imageUrl);
+    }
+
+    public ServiceDto toServiceDto() {
+        return new ServiceDto(null, this.name, this.price, this.imageUrl);
     }
 }
