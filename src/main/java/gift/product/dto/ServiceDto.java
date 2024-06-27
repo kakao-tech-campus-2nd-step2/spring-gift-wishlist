605be3a7ea3f4008d8374e00e498c48183572fe2
JaeBin <201924576@pusan.ac.kr>
2024-06-27 17:52:33 +0900

    feat(product): create Service Dto

--- src/main/java/gift/product/dto/ServiceDto.java
+++ src/main/java/gift/product/dto/ServiceDto.java
@@ -1,4 +1,9 @@
 package gift.product.dto;
 
+import gift.product.entity.Product;
+
 public record ServiceDto(Long id, String name, int price, String imageUrl) {
+    public Product toProduct() {
+        return new Product(id, name, price, imageUrl);
+    }
 }
