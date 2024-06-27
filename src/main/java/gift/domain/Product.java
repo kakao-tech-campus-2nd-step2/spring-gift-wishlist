72ce24b5b70780e1f42837664ab785d1f3341160
stopmin <geemin0@naver.com>
2024-06-27 21:21:35 +0900

    feat: 임시로 시퀀스 initial num 3으로 설정해둠

--- src/main/java/gift/domain/Product.java
+++ src/main/java/gift/domain/Product.java
@@ -4,7 +4,7 @@ import java.util.concurrent.atomic.AtomicLong;
 
 public record Product(Long id, String name, double price, String imageUrl) {
 
-    private static final AtomicLong index = new AtomicLong();
+    private static final AtomicLong index = new AtomicLong(3);
 
     public Product(String name, double price, String imageUrl) {
         this(index.incrementAndGet(), name, price, imageUrl);
