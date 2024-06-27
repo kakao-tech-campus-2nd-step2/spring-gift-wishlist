d2a7a6ec76c7a9c5b16be23ec4e33141286f4222
nove1080 <skwpqjq@gmail.com>
2024-06-28 00:48:47 +0900

    rename: ProductConverter -> CreateRequestToProductConverter
    역할이 뚜렷하게 드러나도록 클래스 이름 수정

--- src/main/java/gift/converter/CreateRequestToProductConverter.java
+++ src/main/java/gift/converter/CreateRequestToProductConverter.java
@@ -4,11 +4,8 @@ import gift.domain.Product;
 import gift.web.dto.request.CreateProductRequest;
 import org.springframework.stereotype.Component;
 
-/**
- * 상품 관련 요청/응답 클래스를 상품 엔티티로 변환하는 컨버터 클래스입니다.
- */
 @Component
-public class ProductConverter {
+public class CreateRequestToProductConverter {
 
     public Product convertToEntity(CreateProductRequest request) {
         return new Product.Builder()
