ab40789ea5b3ccbada7f65f487444d8691027288
cussle <cussle@kakao.com>
2024-06-27 16:48:06 +0900

    feat(admin): 모든 상품 조회 화면 구현

---
+++ src/main/java/gift/controller/AdminController.java
@@ -0,0 +1,45 @@
+package gift.controller;
+
+import gift.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/products")
+public class AdminController {
+    private final ProductService productService;
+
+    public AdminController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public String getAllProducts(Model model) {
+        model.addAttribute("products", productService.getAllProducts());
+        return "products";
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
