e01e391d7e79ccb5dbbbd5a35308cfcbb976275a
geist9110 <geist9110@gmail.com>
2024-06-27 18:38:15 +0900

    feat(Admin): Add addProduct function

--- src/main/resources/static/adminPage.js
+++ src/main/resources/static/adminPage.js
@@ -1,17 +1,34 @@
-let modal = document.getElementById("productModal");
-let addProductbtn = document.getElementsByClassName("header-add")[0];
-let span = document.getElementsByClassName("close")[0];
+let modal = document.getElementById('productModal');
+let addProductbtn = document.getElementsByClassName('header-add')[0];
+let span = document.getElementsByClassName('close')[0];
 
 addProductbtn.onclick = function () {
-  modal.style.display = "flex";
+    modal.style.display = 'flex';
 };
 
 span.onclick = function () {
-  modal.style.display = "none";
+    modal.style.display = 'none';
 };
 
 window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
 };
+
+function addProduct() {
+    const name = document.getElementById('productName').value;
+    const price = document.getElementById('productPrice').value;
+    const imageUrl = document.getElementById('productImage').value;
+
+    fetch(`/api/products?name=${name}&price=${price}&imageurl=${imageUrl}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }).then((response) => {
+        if (response.ok) {
+            location.reload();
+        }
+    });
+}
