59b518d8711c536888263763a7701bbcfd93a276
stopmin <geemin0@naver.com>
2024-06-27 21:21:18 +0900

    feat: Customer 멤버 수정 및 포맷터 적용

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,12 +1,16 @@
-CREATE TABLE Product (
-                         id BIGINT PRIMARY KEY,
-                         name VARCHAR(255) NOT NULL,
-                         price DOUBLE NOT NULL,
-                         imageUrl VARCHAR(255) NOT NULL
+CREATE TABLE Product
+(
+    id       BIGINT PRIMARY KEY,
+    name     VARCHAR(255) NOT NULL,
+    price DOUBLE NOT NULL,
+    imageUrl VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE Customer (
-                          id BIGINT PRIMARY KEY,
-                          firstName VARCHAR(255) NOT NULL,
-                          lastName VARCHAR(255) NOT NULL
+CREATE TABLE Customer
+(
+    id      BIGINT PRIMARY KEY,
+    name    VARCHAR(255) NOT NULL,
+    email   VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    phone   VARCHAR(255) NOT NULL
 );
