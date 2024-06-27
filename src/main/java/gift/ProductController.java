64470aa9715ec2e65ea21e28fcdc88fe41fd83a1
geist9110 <geist9110@gmail.com>
2024-06-28 03:42:26 +0900

    feat(Database): Add createProductTable

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -1,42 +1,39 @@
 package gift;
 
-import java.util.List;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
-    private final ProductRepository productRepository = ProductRepository.getInstance();
+    private final ProductRepository productRepository;
 
-    @GetMapping("/api/products")
-    public List<Product> getProduct() {
-        return productRepository.getAllProduct();
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
     }
 
-    @PostMapping("/api/products")
-    public Product addProduct(
-            @RequestParam("name") String name,
-            @RequestParam("price") Integer price,
-            @RequestParam("imageurl") String imageUrl) {
-        return productRepository.addProduct(name, price, imageUrl);
-    }
+    // @GetMapping("/api/products")
+    // public List<Product> getProduct() {
+    // return productRepository.getAllProduct();
+    // }
 
-    @PatchMapping("/api/products")
-    public Product updateProduct(
-            @RequestParam("id") Long id,
-            @RequestParam("name") String name,
-            @RequestParam("price") Integer price,
-            @RequestParam("imageurl") String imageUrl) {
-        return productRepository.updateProduct(id, name, price, imageUrl);
-    }
+    // @PostMapping("/api/products")
+    // public Product addProduct(
+    // @RequestParam("name") String name,
+    // @RequestParam("price") Integer price,
+    // @RequestParam("imageurl") String imageUrl) {
+    // return productRepository.addProduct(name, price, imageUrl);
+    // }
 
-    @DeleteMapping("/api/products")
-    public Product deleteProduct(@RequestParam("id") Long id) {
-        return productRepository.deleteProduct(id);
-    }
+    // @PatchMapping("/api/products")
+    // public Product updateProduct(
+    // @RequestParam("id") Long id,
+    // @RequestParam("name") String name,
+    // @RequestParam("price") Integer price,
+    // @RequestParam("imageurl") String imageUrl) {
+    // return productRepository.updateProduct(id, name, price, imageUrl);
+    // }
+
+    // @DeleteMapping("/api/products")
+    // public Product deleteProduct(@RequestParam("id") Long id) {
+    // return productRepository.deleteProduct(id);
+    // }
 }
--- src/main/java/gift/ProductRepository.java
+++ src/main/java/gift/ProductRepository.java
@@ -1,38 +1,43 @@
 package gift;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ProductRepository {
-    private static ProductRepository instance;
-    private final Map<Long, Product> productsMap = new HashMap<>();
-    private Long idGenerator = 0L;
+    private final JdbcTemplate jdbcTemplate;
 
-    private ProductRepository() {
+    public ProductRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        createProductTable();
     }
 
-    public static ProductRepository getInstance() {
-        if (instance == null) {
-            instance = new ProductRepository();
-        }
-        return instance;
+    private void createProductTable() {
+        jdbcTemplate.execute("""
+                    CREATE TABLE products(
+                        id SERIAL PRIMARY KEY,
+                        name VARCHAR(255),
+                        price INT,
+                        imageUrl VARCHAR(255))
+                """);
     }
 
-    public Product addProduct(String name, Integer price, String imageUrl) {
-        productsMap.put(idGenerator, new Product(idGenerator, name, price, imageUrl));
-        return productsMap.get(idGenerator++);
-    }
+    // public Product addProduct(String name, Integer price, String imageUrl) {
+    // productsMap.put(idGenerator, new Product(idGenerator, name, price,
+    // imageUrl));
+    // return productsMap.get(idGenerator++);
+    // }
 
-    public List<Product> getAllProduct() {
-        return productsMap.values().stream().toList();
-    }
+    // public List<Product> getAllProduct() {
+    // return productsMap.values().stream().toList();
+    // }
 
-    public Product updateProduct(Long id, String name, Integer price, String imageUrl) {
-        return productsMap.put(id, new Product(id, name, price, imageUrl));
-    }
+    // public Product updateProduct(Long id, String name, Integer price, String
+    // imageUrl) {
+    // return productsMap.put(id, new Product(id, name, price, imageUrl));
+    // }
 
-    public Product deleteProduct(Long id) {
-        return productsMap.remove(id);
-    }
+    // public Product deleteProduct(Long id) {
+    // return productsMap.remove(id);
+    // }
 }
