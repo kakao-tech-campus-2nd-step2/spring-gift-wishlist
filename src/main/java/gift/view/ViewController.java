bf7afcd32ead59e8d707d95aa3bcce1eebf6cfed
JaeBin <201924576@pusan.ac.kr>
2024-06-27 14:56:47 +0900

    chore(view): delete unnecssary parameter

--- src/main/java/gift/view/ViewController.java
+++ src/main/java/gift/view/ViewController.java
@@ -1,13 +1,13 @@
 package gift.view;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
+
     @GetMapping("/")
-    public String index(Model model) {
+    public String index() {
         return "index";
     }
 }
