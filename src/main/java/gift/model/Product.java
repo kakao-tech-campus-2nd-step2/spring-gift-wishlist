ff0e72829a6aa79bd41418b7f21b308bf41581c9
momnpa333 <momnpa333@gmail.com>
2024-06-27 18:03:21 +0900

    refact(Product.java): Product 생성자 제거

--- src/main/java/gift/model/Product.java
+++ src/main/java/gift/model/Product.java
@@ -9,13 +9,6 @@ public class Product {
     private Integer price;
     private String imageUrl;
 
-    private Product(String name, Integer price, String imageUrl) {
-        this.id = UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
-        this.name = name;
-        this.price = price;
-        this.imageUrl = imageUrl;
-    }
-
     private Product(Long id, String name, Integer price, String imageUrl) {
         this.id = id;
         this.name = name;
@@ -39,11 +32,8 @@ public class Product {
         return imageUrl;
     }
 
-    public static Product create(String name, Integer price, String imageUrl) {
-        return new Product(name, price, imageUrl);
-    }
-
-    public static Product update(Long id, String name, Integer price, String imageUrl) {
+    public static Product create(Long id, String name, Integer price, String imageUrl) {
         return new Product(id, name, price, imageUrl);
     }
+
 }
