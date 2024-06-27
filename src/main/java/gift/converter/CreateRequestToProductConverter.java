f226a64de4a19481ecdf20e440d655e90f013912
nove1080 <skwpqjq@gmail.com>
2024-06-28 00:49:32 +0900

    feat: CreateRequestToProductConverter converter 인터페이스 구현

--- src/main/java/gift/converter/CreateRequestToProductConverter.java
+++ src/main/java/gift/converter/CreateRequestToProductConverter.java
@@ -2,12 +2,14 @@ package gift.converter;
 
 import gift.domain.Product;
 import gift.web.dto.request.CreateProductRequest;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CreateRequestToProductConverter {
+public class CreateRequestToProductConverter implements Converter<CreateProductRequest, Product> {
 
-    public Product convertToEntity(CreateProductRequest request) {
+    @Override
+    public Product convert(CreateProductRequest request) {
         return new Product.Builder()
             .name(request.getName())
             .price(request.getPrice())
