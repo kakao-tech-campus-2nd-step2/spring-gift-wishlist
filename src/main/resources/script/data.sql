3ffda723fb851cb12ff6aa96c9ba756125715720
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-06-28 05:19:53 +0900

    feat: 초기 데이터 추가 스키마  추가

---
+++ src/main/resources/script/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO products (name, price, image_url, is_active)
+VALUES ('상품 1', 100000, 'http://example.com/image1.jpg'),
+       ('상품 2', 20000, 'http://example.com/image2.jpg'),
+       ('상품 3', 1500, NULL);
