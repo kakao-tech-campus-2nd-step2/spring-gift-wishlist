9410cc5b3146fd7ffcf4f95bca9a78e28b6e407e
JaeBin <201924576@pusan.ac.kr>
2024-06-28 13:14:16 +0900

    fix(global): change result code status type String to int

--- src/main/java/gift/global/response/ResultCode.java
+++ src/main/java/gift/global/response/ResultCode.java
@@ -2,26 +2,26 @@ package gift.global.response;
 
 public enum ResultCode {
     // Product
-    GET_ALL_PRODUCTS_SUCCESS("200", "P001", "모든 제품 조회 성공"),
-    GET_PRODUCT_BY_ID_SUCCESS("200", "P002", "단일 제품 조회 성공"),
-    CREATE_PRODUCT_SUCCESS("200", "P003", "제품 추가 성공"),
-    UPDATE_PRODUCT_SUCCESS("200", "P002", "제품 수정 성공"),
-    DELETE_PRODUCT_SUCCESS("200", "P002", "제품 삭제 성공"),
+    GET_ALL_PRODUCTS_SUCCESS(200, "P001", "모든 제품 조회 성공"),
+    GET_PRODUCT_BY_ID_SUCCESS(200, "P002", "단일 제품 조회 성공"),
+    CREATE_PRODUCT_SUCCESS(200, "P003", "제품 추가 성공"),
+    UPDATE_PRODUCT_SUCCESS(200, "P002", "제품 수정 성공"),
+    DELETE_PRODUCT_SUCCESS(200, "P002", "제품 삭제 성공"),
 
     ;
 
-    // status 를 HttpStatus 로 관리하는 것이 좋을까, 아니면 String으로 관리하는 것이 좋을까?
-    private final String status;
+    // status 를 HttpStatus 로 관리하는 것이 좋을까, 아니면 int로 관리하는 것이 좋을까?
+    private final int status;
     private final String resultCode;
     private final String message;
 
-    ResultCode(String status, String resultCode, String message) {
+    ResultCode(int status, String resultCode, String message) {
         this.status = status;
         this.resultCode = resultCode;
         this.message = message;
     }
 
-    public String getStatus() {
+    public int getStatus() {
         return status;
     }
 
