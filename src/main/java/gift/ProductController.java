8f92b7bcc26867ea66f43cf1a3a326c9af40c938
minju26 <mjalswn26@gmail.com>
2024-06-28 14:28:02 +0900

    feat: add ProductService class
    - move CURD method from ProductController to ProductService

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -3,80 +3,32 @@ package gift;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
-
-    private static final String ADD_SUCCESS_MSG = "상품 추가 성공";
-    private static final String UPDATE_SUCCESS_MSG = "상품 수정 성공";
-    private static final String DELETE_SUCCESS_MSG = "상품 삭제 성공";
-
-
-    Product initialProduct = new Product(
-            8146027L,
-            "아이스 카페 아메리카노 T",
-            4500,
-            "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg"
-    );
-
-    private final Map<Long, Product> products = new HashMap<>();
-
-    public ProductController() {
-        products.put(initialProduct.getId(), initialProduct);
+    private final ProductService productService;
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
 
     @GetMapping("/{productId}")
     public Product retrieveProduct(@PathVariable("productId") Long productId) {
-        return getProduct(productId);
-    }
-
-    public Product getProduct(Long productId) {
-        return products.get(productId);
+        return productService.getProduct(productId);
     }
 
     @PostMapping
     public ResponseEntity<String> addProduct(@RequestBody Product product) {
-        return ResponseEntity.ok(addNewProduct(product));
-    }
-
-    public String addNewProduct(Product newProduct) {
-        products.put(newProduct.getId(), newProduct);
-        return ADD_SUCCESS_MSG;
+        return ResponseEntity.ok(productService.addNewProduct(product));
     }
 
     @PutMapping("/{productId}")
     public ResponseEntity<String> updateProduct(@PathVariable("productId") Long productId, @RequestBody Product product) {
-        return ResponseEntity.ok(updateProductInfo(productId, product));
-    }
-
-    public String updateProductInfo(Long productId, Product product) {
-
-        Product productToUpdate = products.get(productId);
-
-        if (product.getName() != null) {
-            productToUpdate.setName(product.getName());
-        }
-        if (product.getPrice() > 0) {
-            productToUpdate.setPrice(product.getPrice());
-        }
-        if (product.getImageUrl() != null) {
-            productToUpdate.setImageUrl(product.getImageUrl());
-        }
-
-        return UPDATE_SUCCESS_MSG;
+        return ResponseEntity.ok(productService.updateProductInfo(productId, product));
     }
 
     @DeleteMapping("/{productId}")
     public ResponseEntity<String> deleteProduct(@PathVariable("productId") Long productId) {
-        return ResponseEntity.ok(deleteTheProduct(productId));
-    }
-
-    public String deleteTheProduct(Long productId) {
-        products.remove(productId);
-        return DELETE_SUCCESS_MSG;
+        return ResponseEntity.ok(productService.deleteTheProduct(productId));
     }
 
 }
---
+++ src/main/java/gift/ProductService.java
@@ -0,0 +1,47 @@
+package gift;
+
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class ProductService {
+
+    private static final String ADD_SUCCESS_MSG = "상품 추가 성공";
+    private static final String UPDATE_SUCCESS_MSG = "상품 수정 성공";
+    private static final String DELETE_SUCCESS_MSG = "상품 삭제 성공";
+
+    private final Map<Long, Product> products = new HashMap<>();
+    public Product getProduct(Long productId) {
+        return products.get(productId);
+    }
+
+    public String addNewProduct(Product newProduct) {
+        products.put(newProduct.getId(), newProduct);
+        return ADD_SUCCESS_MSG;
+    }
+
+    public String updateProductInfo(Long productId, Product product) {
+
+        Product productToUpdate = products.get(productId);
+
+        if (product.getName() != null) {
+            productToUpdate.setName(product.getName());
+        }
+        if (product.getPrice() > 0) {
+            productToUpdate.setPrice(product.getPrice());
+        }
+        if (product.getImageUrl() != null) {
+            productToUpdate.setImageUrl(product.getImageUrl());
+        }
+
+        return UPDATE_SUCCESS_MSG;
+    }
+
+    public String deleteTheProduct(Long productId) {
+        products.remove(productId);
+        return DELETE_SUCCESS_MSG;
+    }
+
+}
