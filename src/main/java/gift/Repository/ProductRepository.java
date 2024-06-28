b99a57cb6374b395eb9afd260bacc777c0c9f318
donghyuun <bomul0524@gmail.com>
2024-06-28 14:25:14 +0900

    refactor: controller 내 상품 저장소 -> Repository 클래스 생성

--- src/main/java/gift/Repository/ProductRepository.java
+++ src/main/java/gift/Repository/ProductRepository.java
@@ -1,2 +1,24 @@
-package gift.Repository;public class ProductRepository {
+package gift.Repository;
+
+import gift.Service.ProductService;
+import gift.model.Product;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductRepository {
+
+    // 상품 저장소
+    public static final Map<Long, Product> products = new HashMap<>();
+
+    public ProductRepository() {
+        Product product1 = new Product("아이스 아메리카노 T", 5500, "testImageUrl.com");
+        Product product2 = new Product("아이스 카푸치노 M", 5200, "testImageUrl.com");
+        Product product3 = new Product("핫 초코 프라푸치노 M", 6000, "testImageUrl.com");
+
+        products.put(product1.getId(), product1);
+        products.put(product2.getId(), product2);
+        products.put(product3.getId(), product3);
+    }
 }
