a91017206f73ef757938f410506d8f3053031be0
momnpa333 <momnpa333@gmail.com>
2024-06-28 15:39:55 +0900

    feat(ProductController.java): ProductResponse 반영 및 예외처리

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -34,15 +34,20 @@ public class ProductController {
     }
 
     @GetMapping("/products")
-    public ResponseEntity<List<Product>> getProducts() {
-        var response = productDao.findAll();
+    public ResponseEntity<List<ProductResponse>> getProducts() {
+        var products = productDao.findAll();
+        var response = products.stream()
+            .map(ProductResponse::from)
+            .toList();
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/products/{id}")
-    public ResponseEntity<Product> getProduct(@PathVariable("id") Long id) {
-        var response = productDao.findById(id);
-        return ResponseEntity.ok(response.orElse(null));
+    public ResponseEntity<ProductResponse> getProduct(@PathVariable("id") Long id) {
+        var product = productDao.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("해당 상품이 존재하지 않습니다."));
+        var response = ProductResponse.from(product);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/products")
@@ -55,12 +60,16 @@ public class ProductController {
     @PutMapping("/products/{id}")
     public ResponseEntity<String> updateProduct(@PathVariable("id") Long id,
         @RequestBody ProductRequest request) {
-        productDao.update(id, request.toEntity(id));
+        productDao.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("해당 상품이 존재하지 않습니다."));
+        productDao.update(request.toEntity(id));
         return ResponseEntity.ok().body("Product updated successfully.");
     }
 
     @DeleteMapping("/products/{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable("id") Long id) {
+        productDao.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("해당 상품이 존재하지 않습니다."));
         productDao.deleteById(id);
         return ResponseEntity.noContent().build();
     }
