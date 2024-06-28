7bac451fdbcc97d298a1058aa4303560af030133
JaeBin <201924576@pusan.ac.kr>
2024-06-28 13:22:12 +0900

    chore(global): change ResultResponse name to ResultResponseDto

--- src/main/java/gift/global/response/ResultResponseDto.java
+++ src/main/java/gift/global/response/ResultResponseDto.java
@@ -1,8 +1,8 @@
 package gift.global.response;
 
-public record ResultResponse<T>(String code, String message, T data) {
+public record ResultResponseDto<T>(String code, String message, T data) {
     // 자바 record 에서 생성자를 만들 경우, 반드시 canonical(표준) 생성자를 사용해야 한다!!
-    public ResultResponse(ResultCode resultCode, T data) {
+    public ResultResponseDto(ResultCode resultCode, T data) {
         this(resultCode.getResultCode(), resultCode.getMessage(), data);
     }
 }
