fd8961965d7987d0a0c3a5a301a54172dd902f90
JaeBin <201924576@pusan.ac.kr>
2024-06-28 14:53:46 +0900

    refactor(product): apply ResponseHelper for response creation in ProductController

--- src/main/java/gift/product/controller/ProductController.java
+++ src/main/java/gift/product/controller/ProductController.java
@@ -3,6 +3,7 @@ package gift.product.controller;
 import gift.global.response.ResultCode;
 import gift.global.response.ResultResponseDto;
 import gift.global.response.SimpleResultResponseDto;
+import gift.global.utils.ResponseHelper;
 import gift.product.dto.ProductRequest;
 import gift.product.domain.Product;
 import gift.product.service.ProductService;
@@ -23,42 +24,30 @@ public class ProductController {
     @GetMapping("")
     public ResponseEntity<ResultResponseDto<List<Product>>> getAllProducts() {
         List<Product> products = productService.getAllProducts();
-        return createResponse(ResultCode.GET_ALL_PRODUCTS_SUCCESS, products);
+        return ResponseHelper.createResponse(ResultCode.GET_ALL_PRODUCTS_SUCCESS, products);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ResultResponseDto<Product>> getProductById(@PathVariable(name = "id") Long id) {
         Product product = productService.getProductById(id);
-        return createResponse(ResultCode.GET_PRODUCT_BY_ID_SUCCESS, product);
+        return ResponseHelper.createResponse(ResultCode.GET_PRODUCT_BY_ID_SUCCESS, product);
     }
 
     @PostMapping("")
     public ResponseEntity<SimpleResultResponseDto> createProduct(@RequestBody ProductRequest productRequest) {
         productService.createProduct(productRequest.toServiceDto());
-        return createSimpleResponse(ResultCode.CREATE_PRODUCT_SUCCESS);
+        return ResponseHelper.createSimpleResponse(ResultCode.CREATE_PRODUCT_SUCCESS);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<SimpleResultResponseDto> updateProduct(@PathVariable(name = "id") Long id, @RequestBody ProductRequest productRequest) {
         productService.updateProduct(productRequest.toServiceDto(id));
-        return createSimpleResponse(ResultCode.UPDATE_PRODUCT_SUCCESS);
+        return ResponseHelper.createSimpleResponse(ResultCode.UPDATE_PRODUCT_SUCCESS);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<SimpleResultResponseDto> deleteProduct(@PathVariable(name = "id") Long id) {
         productService.deleteProduct(id);
-        return createSimpleResponse(ResultCode.DELETE_PRODUCT_SUCCESS);
-    }
-
-    private <T> ResponseEntity<ResultResponseDto<T>> createResponse(ResultCode resultCode, T data) {
-        ResultResponseDto<T> resultResponseDto = new ResultResponseDto<>(resultCode, data);
-        return ResponseEntity.status(resultCode.getStatus())
-                .body(resultResponseDto);
-    }
-
-    private ResponseEntity<SimpleResultResponseDto> createSimpleResponse(ResultCode resultCode) {
-        var resultResponseDto = new SimpleResultResponseDto(resultCode);
-        return ResponseEntity.status(resultCode.getStatus())
-                .body(resultResponseDto);
+        return ResponseHelper.createSimpleResponse(ResultCode.DELETE_PRODUCT_SUCCESS);
     }
 }
