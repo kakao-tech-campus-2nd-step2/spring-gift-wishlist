17a98ddfbd218f7116f49e64083c202f97006c7b
student-p <paulmoon00@naver.com>
2024-06-28 11:36:01 +0900

    refactor: Product클래스 재구성
    form데이터가 정상적으로 받아지지 않아서
    record에서 일반 클래스로 변경

--- src/main/java/gift/Product.java
+++ src/main/java/gift/Product.java
@@ -1,8 +1,63 @@
 package gift;
 
-public record Product (long id, String name, int price, String imageUrl){
 
-    public Product(String name, int price, String imageUrl){
-        this(-1,name,price,imageUrl);
+public class Product {
+    private long id;
+    private String name;
+    private int price;
+    private String imageUrl;
+
+    public Product() {
+        this.id = -1;
+        this.name = "";
+        this.price = 0;
+        this.imageUrl = "";
+    }
+
+    public Product(long id, String name, int price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public Product(String name, int price, String imageUrl) {
+        this.id = -1;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 }
+
