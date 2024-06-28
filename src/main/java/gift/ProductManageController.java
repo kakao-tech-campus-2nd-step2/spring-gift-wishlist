763eec87bc7759dc84b52fc656bf584720ca1580
minju26 <mjalswn26@gmail.com>
2024-06-28 14:45:47 +0900

    feat: add ProductManageController

---
+++ src/main/java/gift/ProductManageController.java
@@ -0,0 +1,24 @@
+package gift;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/api/manage/products")
+public class ProductManageController {
+
+    private final ProductService productService;
+    public ProductManageController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public String retrieveProduct(Model model) {
+        model.addAttribute("products", productService.getProduct());
+        return "manage-products";
+    }
+
+}
