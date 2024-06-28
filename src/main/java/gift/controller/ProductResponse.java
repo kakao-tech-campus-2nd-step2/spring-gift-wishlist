7f1e12db729d074e8fff9c387d22b820dc4dcaa7
momnpa333 <momnpa333@gmail.com>
2024-06-28 15:39:25 +0900

    chore(ProductResponse.java): ResponseDto 제작

---
+++ src/main/java/gift/controller/ProductResponse.java
@@ -0,0 +1,16 @@
+package gift.controller;
+
+import gift.model.Product;
+
+public record ProductResponse(
+    Long id,
+    String name,
+    Integer price,
+    String imageUrl
+) {
+
+    public static ProductResponse from(Product product) {
+        return new ProductResponse(product.getId(), product.getName(), product.getPrice(),
+            product.getImageUrl());
+    }
+}
