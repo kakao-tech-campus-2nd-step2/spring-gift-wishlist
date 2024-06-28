77076beddfd3b85c5b78b8fa9eb4c299833af336
JaeBin <201924576@pusan.ac.kr>
2024-06-28 14:51:42 +0900

    feat(global): create response helper util

---
+++ src/main/java/gift/global/utils/ResponseHelper.java
@@ -0,0 +1,20 @@
+package gift.global.utils;
+
+import gift.global.response.ResultCode;
+import gift.global.response.ResultResponseDto;
+import gift.global.response.SimpleResultResponseDto;
+import org.springframework.http.ResponseEntity;
+
+public class ResponseHelper {
+    public static <T> ResponseEntity<ResultResponseDto<T>> createResponse(ResultCode resultCode, T data) {
+        ResultResponseDto<T> resultResponseDto = new ResultResponseDto<>(resultCode, data);
+        return org.springframework.http.ResponseEntity.status(resultCode.getStatus())
+                .body(resultResponseDto);
+    }
+
+    public static ResponseEntity<SimpleResultResponseDto> createSimpleResponse(ResultCode resultCode) {
+        var resultResponseDto = new SimpleResultResponseDto(resultCode);
+        return ResponseEntity.status(resultCode.getStatus())
+                .body(resultResponseDto);
+    }
+}
