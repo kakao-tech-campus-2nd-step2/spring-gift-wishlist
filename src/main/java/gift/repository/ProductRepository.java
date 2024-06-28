7e73783d1f7041ac9f97517225e99f068e792edf
nove1080 <skwpqjq@gmail.com>
2024-06-28 15:01:14 +0900

    feat: ProductRepository JdbcTemplate 추가
    메모리 대신 H2DB 적용을 위함

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -6,12 +6,19 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ProductRepository {
 
+    //todo remove
     private final Map<Long, Product> products = new HashMap<>();
+    private final JdbcTemplate jdbcTemplate;
+
+    public ProductRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     public Long save(Product product) {
         products.put(product.getId(), product);
