b388095bdffbdc129d0985828661b88fe99c1734
JaeBin <201924576@pusan.ac.kr>
2024-06-28 13:05:10 +0900

    feat(product): set result code of product api

--- src/main/java/gift/global/response/ResultCode.java
+++ src/main/java/gift/global/response/ResultCode.java
@@ -1,20 +1,35 @@
 package gift.global.response;
 
-import org.springframework.http.HttpStatusCode;
-
 public enum ResultCode {
     // Product
-
+    GET_ALL_PRODUCTS_SUCCESS("200", "P001", "모든 제품 조회 성공"),
+    GET_PRODUCT_BY_ID_SUCCESS("200", "P002", "단일 제품 조회 성공"),
+    CREATE_PRODUCT_SUCCESS("200", "P003", "제품 추가 성공"),
+    UPDATE_PRODUCT_SUCCESS("200", "P002", "제품 수정 성공"),
+    DELETE_PRODUCT_SUCCESS("200", "P002", "제품 삭제 성공"),
 
     ;
-    // HttpStatusCode 로 관리하는 것이 좋을까, 아니면 String으로 관리하는 것이 좋을까?
-    private final HttpStatusCode status;
+
+    // status 를 HttpStatus 로 관리하는 것이 좋을까, 아니면 String으로 관리하는 것이 좋을까?
+    private final String status;
     private final String resultCode;
     private final String message;
 
-    ResultCode(HttpStatusCode status, String resultCode, String message) {
+    ResultCode(String status, String resultCode, String message) {
         this.status = status;
         this.resultCode = resultCode;
         this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getResultCode() {
+        return resultCode;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
