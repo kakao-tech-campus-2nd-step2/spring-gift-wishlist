bedd0c2b6de5930cf7622f99abae04543b5014b0
JaeBin <201924576@pusan.ac.kr>
2024-06-28 15:23:28 +0900

    feat(global): create ErrorCode enum

---
+++ src/main/java/gift/global/response/ErrorCode.java
@@ -0,0 +1,28 @@
+package gift.global.response;
+
+public enum ErrorCode {
+    // Product
+    PRODUCT_NOT_FOUND_ERROR(400, "EP001", "Product Not Found Error"),
+    ;
+    private final int status;
+    private final String code;
+    private final String message;
+
+    ErrorCode(int status, String code, String message) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
