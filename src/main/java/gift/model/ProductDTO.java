2044ddb7992af59c8999d91830fd790b62f3896f
xkrwjdals <developjmt@gmail.com>
2024-06-30 19:13:19 +0900

    Refactor: Delete unused methods

--- src/main/java/gift/model/ProductDTO.java
+++ src/main/java/gift/model/ProductDTO.java
@@ -10,31 +10,16 @@ public class ProductDTO {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public Long getPrice() {
         return price;
     }
 
-    public void setPrice(Long price) {
-        this.price = price;
-    }
-
     public String getImageUrl() {
         return imageUrl;
     }
 
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
 }
