c35f50862792b1515940ed138a02b5acf027e68c
Yongjin An <yongjin5352@gmail.com>
2024-07-01 19:42:57 +0900

    feat: set auto schema initialization using schema.sql

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -7,17 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @Autowired
     JdbcTemplate jdbcTemplate;
-
-    @Override
-    public void run(String... args) throws Exception {
-        jdbcTemplate.execute("DROP TABLE products IF EXISTS");
-        jdbcTemplate.execute("CREATE TABLE products(id SERIAL, name VARCHAR(255), price INT, imageUrl TEXT(65535))");
-    }
 }
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS products;
+CREATE TABLE products(id long, name VARCHAR(255), price INT, imageUrl TEXT(65535));
