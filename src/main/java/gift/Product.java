c21be01d5cbb2ee4f5b8c85e569d190c449c674f
amm0124 <108533909+amm0124@users.noreply.github.com>
2024-07-01 21:20:35 +0900

    FEAT : 1주차 복사 후 step0 완료 (#56)
    2주차 화이팅 입니다!!

---
+++ src/main/java/gift/Product.java
@@ -0,0 +1,3 @@
+package gift;
+
+public record Product(long id, String name, int price, String imageUrl) {}
---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,5 @@
+-- data.sql
+
+INSERT INTO products (name, price, imageUrl) VALUES ('Laptop', 1200000, 'https://example.com/laptop.jpg');
+INSERT INTO products (name, price, imageUrl) VALUES ('Smartphone', 800000, 'https://example.com/smartphone.jpg');
+INSERT INTO products (name, price, imageUrl) VALUES ('Tablet', 500000, 'https://example.com/tablet.jpg');
