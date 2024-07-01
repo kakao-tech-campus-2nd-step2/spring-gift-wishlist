e45fad9b320d421379bc2c69d8e1272cbf92c572
nove1080 <skwpqjq@gmail.com>
2024-07-01 22:53:32 +0900

    refactor: ProductApiController 리소스 수정 요청을 patch -> put 변경

--- src/main/java/gift/web/controller/api/ProductApiController.java
+++ src/main/java/gift/web/controller/api/ProductApiController.java
@@ -12,9 +12,9 @@ import java.util.NoSuchElementException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,7 +44,7 @@ public class ProductApiController {
         return ResponseEntity.ok(response);
     }
 
-    @PatchMapping("/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<UpdateProductResponse> updateProduct(@PathVariable Long id, @RequestBody UpdateProductRequest request) {
         UpdateProductResponse response;
         try {
