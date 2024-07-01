2f9152c1de3fd324e850995d45f644f16aa73226
donghyeon <thornappl2@gmail.com>
2024-07-01 15:17:15 +0900

    feat: step0 - 기본 코드 준비

---
+++ src/main/java/gift/model/Product.java
@@ -0,0 +1,49 @@
+package gift.model;
+
+public class Product {
+    private long id;
+    private String name;
+    private int price;
+    private String imageUrl;
+
+    public Product(){}
+
+    public Product(long id, String name, int price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public long getId(){
+        return id;
+    }
+
+    public void setId(long id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+
+    public void setPrice(int price){
+        this.price = price;
+    }
+
+    public String getImageUrl(){
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl){
+        this.imageUrl = imageUrl;
+    }
+}
