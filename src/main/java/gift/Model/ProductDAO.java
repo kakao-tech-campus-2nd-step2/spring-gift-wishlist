13311b86e2d15c4f7d1c57fc456c052bdfe3f72b
kmj452 <111856852+kmj452@users.noreply.github.com>
2024-07-01 23:47:55 +0900

    전남대 BE_김명진 2주차 과제 (step0) (#42)
    * docs: 이전 과제 가져오기
    
    * refactor: new와 edit 수행할 form 분리, put 메서드 추가 위한 의존성 추가
    
    * refactor: AdminController 로직 변경
    
    * refactor: html 파일들 이름 수정 및 url 재설정

---
+++ src/main/java/gift/Model/ProductDAO.java
@@ -0,0 +1,40 @@
+package gift.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ProductDAO {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public List<ProductModel> getAllProducts() {
+        String sql = "SELECT * FROM product";
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(ProductModel.class));
+    }
+
+    public ProductModel getProductById(Long id) {
+        String sql = "SELECT * FROM product WHERE id = ?";
+        return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(ProductModel.class), id);
+    }
+
+    public void saveProduct(ProductModel product) {
+        String sql = "INSERT INTO product (name, price, image_url) VALUES (?, ?, ?)";
+        jdbcTemplate.update(sql, product.getName(), product.getPrice(), product.getImageURL());
+    }
+
+    public void updateProduct(ProductModel product) {
+        String sql = "UPDATE product SET name = ?, price = ?, image_url = ? WHERE id = ?";
+        jdbcTemplate.update(sql, product.getName(), product.getPrice(), product.getImageURL(), product.getId());
+    }
+
+    public void deleteProduct(Long id) {
+        String sql = "DELETE FROM product WHERE id = ?";
+        jdbcTemplate.update(sql, id);
+    }
+}
+
