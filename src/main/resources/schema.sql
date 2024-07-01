50ebd991af985663348cd5901f3b17b9037ad589
J-1ac <tkdwns26@naver.com>
2024-07-01 23:51:40 +0900

    feat: DB 스키마 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS products;
 
 CREATE TABLE products
 (
-    id       BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id       BIGINT PRIMARY KEY,
     name     VARCHAR(255)  NOT NULL,
     price    BIGINT        NOT NULL,
     imageurl VARCHAR(2083) NOT NULL
