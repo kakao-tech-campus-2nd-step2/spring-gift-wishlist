80191c35e29aec9c0faee3e0ddbd572797a67d9e
배민수 <duecost@gmail.com>
2024-07-01 16:20:52 +0900

    step0 수행 : 저번 주차 과제 백업

---
+++ src/main/java/gift/product/controller/AdminController.java
@@ -0,0 +1,63 @@
+package gift.product.controller;
+
+import gift.product.dto.ProductDto;
+import gift.product.model.Product;
+import gift.product.repository.ProductRepository;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/products")
+public class AdminController {
+
+    public static final String REDIRECT_ADMIN_PRODUCTS = "redirect:/admin/products";
+    private final ProductRepository productRepository;
+
+    public AdminController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping
+    public String products(Model model) {
+        List<Product> products = productRepository.findAll();
+        model.addAttribute("products", products);
+        return "admin/products";
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable(name = "id") Long productId) {
+        productRepository.delete(productId);
+        return REDIRECT_ADMIN_PRODUCTS;
+    }
+
+    @GetMapping("/insert")
+    public String insertForm() {
+        return "admin/insertForm";
+    }
+
+    @PostMapping("/insert")
+    public String insertProduct(ProductDto productDto) {
+        productRepository.save(new Product(productDto.name(), productDto.price(), productDto.imageUrl()));
+        return REDIRECT_ADMIN_PRODUCTS;
+    }
+
+    @GetMapping("/update/{id}")
+    public String updateForm(@PathVariable(name = "id") Long productId, Model model) {
+        Product product = productRepository.findById(productId);
+        model.addAttribute("product", product);
+        return "admin/updateForm";
+    }
+
+    @PutMapping("/update/{id}")
+    public String updateForm(@PathVariable(name = "id") Long productId, ProductDto productDto) {
+        productRepository.update(new Product(productId, productDto.name(), productDto.price(), productDto.imageUrl()));
+        return REDIRECT_ADMIN_PRODUCTS;
+    }
+}
---
+++ src/main/java/gift/product/controller/ProductController.java
@@ -0,0 +1,59 @@
+package gift.product.controller;
+
+import gift.product.dto.ProductDto;
+import gift.product.model.Product;
+import gift.product.service.ProductService;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping("/insert")
+    public ResponseEntity<Product> insertProduct(@RequestBody ProductDto productDto) {
+        Product responseProduct = productService.insertProduct(productDto);
+
+        return ResponseEntity.ok(responseProduct);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getProductAll() {
+        List<Product> productAll = productService.getProductAll();
+        return ResponseEntity.ok(productAll);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable(name = "id") Long id) {
+        Product product = productService.getProduct(id);
+        return ResponseEntity.ok(product);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable(name = "id") Long id,
+        @RequestBody ProductDto productDto) {
+        Product product = productService.updateProduct(id, productDto);
+        return ResponseEntity.ok(product);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable(name = "id") Long id) {
+        productService.deleteProduct(id);
+
+        return ResponseEntity.ok().build();
+    }
+}
