9fffd6f1fb26d98ed924c38be72d4958f9f3d78b
HSHwan <tnghks2866@gmail.com>
2024-07-01 17:23:14 +0900

    feat: 상품 이름과 가격 유효성 검사 추가

--- src/main/java/gift/dto/ProductRequest.java
+++ src/main/java/gift/dto/ProductRequest.java
@@ -1,5 +1,13 @@
 package gift.dto;
 
-public record ProductRequest(String name,
-                             int price,
-                             String imageUrl) { }
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record ProductRequest(
+        @NotNull
+        @Pattern(regexp = "^[\\w\\d\\s\\(\\)\\[\\]\\+\\-\\&\\/\\_\\uAC00-\\uD7A3]{0,15}$")
+        String name,
+        @Min(0)
+        int price,
+        String imageUrl) { }
