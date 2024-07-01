34ddd31d546b9127756f21a19260197693234e40
us4c0d3 <jwseok312@gmail.com>
2024-07-01 14:16:58 +0900

    chore: 1주차 소스코드 로드

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -1,11 +1,36 @@
 package gift;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
+
+    @Override
+    public void run(String... args) {
+        jdbcTemplate.execute("DROP TABLE products IF EXISTS");
+        jdbcTemplate.execute("CREATE TABLE products ("
+            + "id LONG,"
+            + " name VARCHAR(255),"
+            + " price INT,"
+            + " imageUrl VARCHAR(255),"
+            + " PRIMARY KEY (id))"
+        );
     }
 }
