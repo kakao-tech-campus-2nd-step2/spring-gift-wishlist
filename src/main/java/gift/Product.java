4b1c297b52bacab9129aad93cd2a01d7e508f305
seoyoung-dev <seoyoung.dev@gmail.com>
2024-07-01 19:33:21 +0900

    project setting

---
+++ src/main/java/gift/Product.java
@@ -0,0 +1,51 @@
+package gift;
+
+public class Product {
+    private Long id;
+    private String name;
+    private double price;
+    private String imageUrl;
+
+    public Product(Long id, String name, double price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public Product() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE product (
+                         id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                         name VARCHAR(255) NOT NULL,
+                         price DOUBLE NOT NULL,
+                         image_url VARCHAR(255)
+);
