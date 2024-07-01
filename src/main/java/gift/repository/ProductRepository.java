4611425004fc1b69e44f33e31ac265ee48e933a4
Kang Sumin <neoramiregi@gmail.com>
2024-07-02 00:15:43 +0900

    부산대 BE_강수민 2주차 과제 (0단계) (#78)
    step1 시작하실 땐 resourece 디렉터리에 파일도 옮겨주세요.
    일단 merge 하겠습니다!
    2주차도 화이팅 입니다.

---
+++ src/main/java/gift/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package gift.repository;
+
+import gift.domain.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+    Long save(Product product);
+    Optional<Product> findById(Long id);
+    List<Product> findAll();
+    Long delete(Long id);
+    Product update(Long id, Product product);
+}
