09692ad536c861deda7337a913da954917531968
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-07-02 01:10:09 +0900

    feat: 존재하지 않는 객체에 대한 접근 예외 처리
    존재하지 않는 객체에 대한 접근 예외 처리

--- src/main/java/gift/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -12,4 +12,9 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> priceLessThanZeroExceptionHandling(){
         return new ResponseEntity<>("금액(비용)은 0원보다 작을 수 없습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    @ExceptionHandler(value = NotFoundElementException.class)
+    public ResponseEntity<String> notFoundElementExceptionHandling(){
+        return new ResponseEntity<>("존재하지 않는 리소스에 대한 접근입니다.", HttpStatus.NOT_FOUND);
+    }
 }
