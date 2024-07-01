368be951795c65e136489510a844ce70aeb76909
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-07-02 02:21:01 +0900

    refactor: api 버전 명시 및 requestmapping 적용

--- src/main/java/gift/controller/AdminController.java
+++ src/main/java/gift/controller/AdminController.java
@@ -14,12 +14,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AdminController {
     private ProductDao productDao;
-    @Autowired
+
     AdminController(ProductDao productDao) {
         this.productDao = productDao;
     }
--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -13,42 +13,44 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class ProductController {
 
     private ProductDao productDao;
-    @Autowired
+
     ProductController(ProductDao productDao) {
         this.productDao = productDao;
     }
 
-    @PostMapping("/api/product")
+    @PostMapping("/product")
     public ProductResponse registerProduct(@RequestBody ProductRequest productRequest) {
         Product product = productDao.save(productRequest);
         return ProductResponse.from(product);
     }
 
-    @GetMapping("/api/products")
+    @GetMapping("/products")
     public List<ProductResponse> getAllProducts() {
         List<Product> productList = productDao.findAll();
         return productList.stream().map(ProductResponse::from).collect(Collectors.toList());
     }
 
-    @GetMapping("/api/product/{id}")
+    @GetMapping("/product/{id}")
     public ProductResponse getProduct(@PathVariable("id") Long id) {
         Product product = productDao.findById(id);
         return ProductResponse.from(product);
     }
 
-    @PutMapping("/api/product/{id}")
+    @PutMapping("/product/{id}")
     public ProductResponse updateProduct(@PathVariable("id") Long id, @RequestBody ProductRequest productRequest) {
         Product product = productDao.update(id, productRequest);
         return ProductResponse.from(product);
     }
 
-    @DeleteMapping("/api/product/{id}")
+    @DeleteMapping("/product/{id}")
     public void deleteProduct(@PathVariable("id") Long id) {
         productDao.delete(id);
     }
