f9583756d6731f83a7367baca732a9914bbb03f9
hyunseo <wndlthsk@naver.com>
2024-07-01 19:16:28 +0900

    refactor : id AUTO_INCREMENT 설정 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,1 @@
-INSERT INTO product (id, name, price, imageUrl) VALUES (1, 'iced americano', 4500, 'https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg');
+INSERT INTO product (name, price, imageUrl) VALUES ('iced americano', 4500, 'https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE product (
-                         id BIGINT PRIMARY KEY,
+                         id BIGINT PRIMARY KEY AUTO_INCREMENT,
                          name VARCHAR(255) NOT NULL,
                          price INT NOT NULL,
                          imageUrl VARCHAR(255)
