231d42e20949752defb619cbc716c43fa7958dd3
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-01 19:19:08 +0900

    chore : 기존 코드 copy

---
+++ src/main/java/gift/controller/ProductController.java
@@ -0,0 +1,62 @@
+package gift.controller;
+
+
+import gift.domain.Product;
+import gift.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    // 모든 상품 조회
+    @GetMapping
+    public String getAllProducts(Model model) {
+        model.addAttribute("products", productService.getAllProducts());
+        return "products";
+    }
+
+    // 상품 추가하는 창
+    @GetMapping("/add")
+    public String showAddProductForm(Model model) {
+        model.addAttribute("product", new Product());
+        return "product-form";
+    }
+
+    // 상품 추가 후 홈으로 이동
+    @PostMapping("/add")
+    public String addProduct(@ModelAttribute Product product) {
+        productService.createProduct(product);
+        return "redirect:/products";
+    }
+
+    // 등록된 상품 수정하는 창
+    @GetMapping("/edit/{id}")
+    public String showEditProductForm(@PathVariable Long id, Model model) {
+        model.addAttribute("product", productService.getProductById(id));
+        return "product-form";
+    }
+
+    // 등록된 상품을 수정하는 기능
+    @PutMapping("/edit/{id}")
+    public String editProduct(@PathVariable("id") Long id, @ModelAttribute Product product) {
+        product.setId(id);
+        productService.updateProduct(id, product);
+        return "redirect:/products";
+    }
+
+    // 등록된 상품을 삭제하는 기능
+    @DeleteMapping ("/delete/{id}")
+    public String deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return "redirect:/products";
+    }
+}
