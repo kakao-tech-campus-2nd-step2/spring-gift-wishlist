097405cd76407ea81899120454ffa0c528ef8771
pswaao88 <pswaao88@gmail.com>
2024-07-01 13:41:11 +0900

    Init: 프로젝트 생성

---
+++ src/main/java/gift/Repository/ProductRepository.java
@@ -0,0 +1,43 @@
+package gift.Repository;
+
+import gift.Model.Product;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public ProductRepository(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private final RowMapper<Product> rowMapper = new BeanPropertyRowMapper<>(Product.class);
+
+    public int add(Product product){
+        return jdbcTemplate.update("INSERT INTO product (name, price, imageUrl) VALUES (?,?,?)",product.getName(),product.getPrice(),product.getImageUrl());
+    }
+
+    public List<Product> findAll() {
+        return jdbcTemplate.query("SELECT * FROM product", rowMapper);
+    }
+
+    public Product findById(Long id){
+        return jdbcTemplate.queryForObject("SELECT * FROM product WHERE id = ?", rowMapper, id);
+    }
+
+    public int update(Product product){
+        return jdbcTemplate.update("UPDATE product SET name = ?, price = ?, imageUrl = ? WHERE id = ?", product.getName(), product.getPrice(), product.getImageUrl(),product.getId());
+    }
+
+    public int delete(Long id){
+        return jdbcTemplate.update("DELETE FROM product WHERE id = ?", id);
+    }
+}
