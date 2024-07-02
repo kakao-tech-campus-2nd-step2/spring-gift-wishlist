7ccc00e36ce8c4722f353ba0a8ae2a7ccfd8289e
dong_yxxn <g0326kdy@gmail.com>
2024-07-02 22:15:44 +0900

    refactor(ProductNameValidator): 이름 검증 추가

--- src/main/java/gift/validation/ProductNameValidator.java
+++ src/main/java/gift/validation/ProductNameValidator.java
@@ -13,6 +13,10 @@ public class ProductNameValidator implements ConstraintValidator<ValidProductNam
     }
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
+        if(value.length()>15){
+            throw new NotValidProductNameException("이름은 공백 포함 최대 15자입니다.");
+        }
+
         if (value.contains("카카오")) {
             throw new NotValidProductNameException("'카카오'가 포함된 문구는 담당 MD와 협의한 경우만 가능합니다.");
         }
