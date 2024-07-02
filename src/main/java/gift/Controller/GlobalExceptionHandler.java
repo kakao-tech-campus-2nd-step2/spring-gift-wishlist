03c7db78c14e2b62be576ec4537dd2d3825cb3c1
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-07-02 19:48:38 +0900

    feat : MethodArgumentNotValidException 예외 처리

--- src/main/java/gift/Controller/GlobalExceptionHandler.java
+++ src/main/java/gift/Controller/GlobalExceptionHandler.java
@@ -1,5 +1,23 @@
 package gift.Controller;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
 }
