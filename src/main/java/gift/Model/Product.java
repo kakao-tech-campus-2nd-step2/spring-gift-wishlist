8029c1d35d806d57d53385f7eec2b8b1c64ceae4
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-07-02 19:15:02 +0900

    feat : 상품 가격 검사 @Pattern을 사용했습니다. 상품 이름에 (), [], +, -, &, /, _ 외 특수 문자는 사용 불가능하므로 사용 가능한 문자는 regexp에 넣습니다.

--- src/main/java/gift/Model/Product.java
+++ src/main/java/gift/Model/Product.java
@@ -2,12 +2,15 @@ package gift.Model;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 
 public record Product(Long id,
                       @NotBlank(message = "상품 이름은 최소 1자 이상이어야 합니다.")
                       @Size(max = 15, message = "상품 이름은 공백 포함 최대 15자까지 입력할 수 있습니다.")
                       @Pattern(regexp = "^[a-zA-Z0-9가-힣\\s\\(\\)\\[\\]\\+\\-\\&\\/\\_]*$", message = "상품 이름에 (), [], +, -, &, /, _ 외 특수 문자는 사용할 수 없습니다.")
-                      String name, long price, String imageUrl) {
+                      String name,
+                      @PositiveOrZero(message = "가격은 음수가 될 수 없습니다.")
+                      long price, String imageUrl) {
 
 }
