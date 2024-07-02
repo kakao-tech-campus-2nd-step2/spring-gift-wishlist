979082bd9ef5ccf77b68d5d8e65c2bf4265f8e04
noojmogstel <goaudgo@naver.com>
2024-07-02 14:45:10 +0900

    feat(exception/GlobalControllerException): Add  ValidationException Method

--- src/main/java/gift/exception/GlobalControllerExceptionHandler.java
+++ src/main/java/gift/exception/GlobalControllerExceptionHandler.java
@@ -1,10 +1,28 @@
 package gift.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalControllerExceptionHandler {
 
 
+    //@Valid에서 발생한 예외 처리
+    @ExceptionHandler(value= MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex){
+        Map<String,String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName,errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
 
 }
