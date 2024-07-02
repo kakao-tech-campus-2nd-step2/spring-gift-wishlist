e17691f8b977b41ff9243488c41479daca531f34
SeongHun <tjdgns5506@gmail.com>
2024-07-02 18:51:49 +0900

    refactor: product_id -> productId java convention

---
+++ src/main/java/gift/response/ResponseProductId.java
@@ -0,0 +1,15 @@
+package gift.response;
+
+public class ResponseProductId {
+
+    private Long productId;
+
+    public ResponseProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+}
