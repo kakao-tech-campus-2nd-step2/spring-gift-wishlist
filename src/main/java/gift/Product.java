14413150f216d0518ab9ce977e9e4e6dbb9decb6
us4c0d3 <jwseok312@gmail.com>
2024-07-03 01:17:47 +0900

    remove(Product): 무의미한 주석 삭제
    ID 입력에 관한 검증 로직
    TODO: Controller에 검증 로직 추가 필요

--- src/main/java/gift/Product.java
+++ src/main/java/gift/Product.java
@@ -1,10 +1,5 @@
 package gift;
 
 public record Product(Long id, String name, Integer price, String imageUrl) {
-    public Product {
-        // 빈 객체 전달을 위한 검증 임시 주석처리
-//        if(id == null) {
-//            throw new IllegalArgumentException("[ERROR] ID는 비워둘 수 없습니다.");
-//        }
-    }
+
 }
