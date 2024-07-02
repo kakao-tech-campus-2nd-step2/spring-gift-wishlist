6468a39c22f8273ff37d786478caf5df4a3b0e74
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-07-03 02:04:17 +0900

    refactor: UpdateRequest 수정 - update시 모든 필드값을 보내줘야함

--- src/main/java/gift/product/model/dto/UpdateProductRequest.java
+++ src/main/java/gift/product/model/dto/UpdateProductRequest.java
@@ -9,11 +9,11 @@ import org.springframework.lang.Nullable;
 public class UpdateProductRequest {
     @NotNull
     private Long id;
-    @Nullable
+    @NotNull
     @Size(max = 15, message = "상품 이름은 15자 이하로 입력해주세요.")
     @Pattern(regexp = "^[a-zA-Z0-9 ()\\[\\]\\+\\-&/_]*$", message = "사용할 수 없는 특수문자가 포함되어 있습니다.")
     private String name;
-    @Nullable
+    @NotNull
     @Min(value = 0, message = "상품 가격은 0 이상의 정수로 입력해주세요.")
     private int price = 0;
     @Nullable
