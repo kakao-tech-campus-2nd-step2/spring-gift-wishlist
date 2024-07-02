bc1368fd1c4853107b87b288035da050dc79640d
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-07-02 17:04:56 +0900

    test: 상품 옵션 입력 검증 테스트 수행
    상품 옵션 입력 검증 테스트 수행

--- src/test/java/gift/controller/ProductOptionControllerTest.java
+++ src/test/java/gift/controller/ProductOptionControllerTest.java
@@ -2,6 +2,10 @@ package gift.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gift.dto.ProductOptionRequest;
+import gift.dto.ProductRequest;
+import gift.model.Product;
+import gift.service.ProductService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,13 +30,45 @@ class ProductOptionControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private ProductService productService;
+
+    private Product product;
+
+    @BeforeEach
+    @DisplayName("옵션에 대한 작업을 수행하기 위한 상품 추가 작업")
+    void setUp() {
+        ProductRequest productRequest = new ProductRequest("아이폰16pro", 1800000, "https://image.zdnet.co.kr/2024/03/21/29acda4f841885d2122750fbff5cbd9d.jpg");
+        product = productService.addProduct(productRequest);
+    }
+
     @Test
-    @DisplayName("불량 옵션 추가하기")
+    @DisplayName("잘못된 가격으로 된 오류 상품 옵션 생성하기")
     void failOptionAdd() throws Exception {
         ResultActions result = mockMvc.perform(post("/api/options/add")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(new ProductOptionRequest(1L, "기본", -1000))));
+                .content(objectMapper.writeValueAsString(new ProductOptionRequest(product.getId(), "기본", -1000))));
 
-        result.andExpect(status().isBadRequest()).andExpect(content().string("잘못된 입력입니다."));
+        result.andExpect(status().isBadRequest()).andExpect(content().string("추가 금액은 0보다 크거나 같아야 합니다."));
+    }
+
+    @Test
+    @DisplayName("빈 이름을 가진 오류 상품 옵션 생성하기")
+    void failOptionAddWithEmptyName() throws Exception {
+        ResultActions result = mockMvc.perform(post("/api/options/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new ProductOptionRequest(product.getId(), "", 1000))));
+
+        result.andExpect(status().isBadRequest()).andExpect(content().string("이름의 길이는 최소 1자 이상이어야 합니다."));
+    }
+
+    @Test
+    @DisplayName("정상 상품 옵션 생성하기")
+    void failOptionSuccess() throws Exception {
+        ResultActions result = mockMvc.perform(post("/api/options/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new ProductOptionRequest(product.getId(), "Large", 1500))));
+
+        result.andExpect(status().isCreated());
     }
 }
