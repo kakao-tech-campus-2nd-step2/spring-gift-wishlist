d4f48f342f6a5baa7afe28cf1fb7a9f41982377b
SeongHun <tjdgns5506@gmail.com>
2024-07-02 12:39:03 +0900

    feat: API 응답용 클래스 생성

---
+++ src/main/java/gift/response/ResponseProductId.java
@@ -0,0 +1,15 @@
+package gift.response;
+
+public class ResponseProductId {
+    
+    private Long id;
+
+    public ResponseProductId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+}
