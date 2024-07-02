e62c98abb7962e243520347c1a213acfe1950f1b
김수랑 <we6610@gmail.com>
2024-07-02 16:54:55 +0900

    :sparkles: feat: FE에서 제품명 유효성 검사 기능 추가

--- src/main/resources/static/js/getProducts.js
+++ src/main/resources/static/js/getProducts.js
@@ -10,15 +10,48 @@ function addProductRow(element) {
   const saveCell = newRow.insertCell(4);
   const cancelCell = newRow.insertCell(5);
 
-  newNameCell.innerHTML = '<input type="text" id="newName">';
+  newNameCell.innerHTML = '<input type="text" id="newName" oninput= "validateName()"> <span class="message"></span>';
   newPriceCell.innerHTML = '<input type="text" id="newPrice">';
   newImageCell.innerHTML = '<input type="text" id="newImage">';
-  saveCell.innerHTML = '<img src="/image/save.png" alt="save" style="width:100px;height: auto" onclick="saveAddProduct()">';
+  saveCell.innerHTML = '<img src="/image/save.png" alt="save" id="saveButton" style="width:100px;height: auto" onclick="saveAddProduct()">';
   cancelCell.innerHTML = '<img src="/image/cancel.png" alt="cancel" style="width:100px;height: auto" onclick="cancelProductEditing()">';
 
   // 추가 버튼 비활성화
   element.style.pointerEvents = 'none';
   element.style.opacity = '0.3';
+}
+
+function validateName() {
+  const inputName = document.getElementById('newName').value;
+  const inputMessage = document.getElementsByClassName('message')[0];
+  const pattern1 = /^[a-zA-Z0-9ㄱ-ㅎ가-힣()\[\]+\-&/_ ]+$/;
+  const pattern2 = /^((?!카카오).)*$/;
+
+  const saveButton = document.getElementById("saveButton");
+
+  if(inputName.length < 1 || inputName.length > 15) {
+    inputMessage.textContent = "제품명 길이는 1~15자만 가능합니다."
+    inputMessage.style.color = "red";
+    saveButton.style.pointerEvents = 'none';
+    saveButton.style.opacity = '0.3';
+  }
+  else if (pattern1.test(inputName) && pattern2.test(inputName)) {
+    inputMessage.textContent = "올바른 이름입니다.";
+    inputMessage.style.color = "green";
+    saveButton.style.pointerEvents = 'auto';
+    saveButton.style.opacity = '1';
+  } else if (pattern1.test(inputName) && !pattern2.test(inputName)) {
+    inputMessage.textContent = "카카오가 포함된 문구는 담당 MD와 협의한 후에 사용해주시기 바랍니다."
+    inputMessage.style.color = "red";
+    saveButton.style.pointerEvents = 'none';
+    saveButton.style.opacity = '0.3';
+  } else {
+    inputMessage.textContent = "( ), [ ], +, -, &, /, _을 제외한 특수문자는 입력할 수 없습니다."
+    inputMessage.style.color = "red";
+    saveButton.style.pointerEvents = 'none';
+    saveButton.style.opacity = '0.3';
+  }
+
 }
 
 function saveAddProduct() {
@@ -73,7 +106,7 @@ function removeProductRow(button) {
     url: `/api/products/${productId}`,
     contentType: 'application/json; charset=utf-8',
     success: function () {
-        alert('상품 삭제를 성공하였습니다.');
+      alert('상품 삭제를 성공하였습니다.');
       window.location.href = '/api/products';
     },
     error: function (xhr) {
