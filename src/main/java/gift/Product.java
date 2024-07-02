b73671b1c44fff622759e519646cff5e410a32aa
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-07-03 00:25:21 +0900

    fix : Product record record에서 class로 수정

--- src/main/java/gift/Product.java
+++ src/main/java/gift/Product.java
@@ -1,5 +1,37 @@
 package gift;
 
-public record Product(Long id, String name, Long price, String imageUrl) {
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
+public class Product {
+    Long id;
+    String name;
+    Long price;
+    String imageUrl;
+
+    public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public Product(Long id, String name, Long price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
 }
