f3c691ea5d3ef777d23eef00a2ac8877f8508642
anacnoda77 <sm1108shin@naver.com>
2024-07-02 23:30:48 +0900

    feat: 상품명 검증 로직 작성
    Form 객체에서 한 인스턴스에 2개의 @Pattern 어노테이션이 잘 작동하지 않아, '카카오'라는 문구가 들어간 상품명을 금지하는 검증 로직은 별도의 애노테이션으로 구현하였음

--- src/main/java/gift/form/ProductAddForm.java
+++ src/main/java/gift/form/ProductAddForm.java
@@ -1,6 +1,41 @@
 package gift.form;
 
+import gift.validation.NoKakao;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
+
 public class ProductAddForm {
 
+    @NotBlank(message = "상품명을 입력해주세요.")
+    @Length(min=1, max=15, message = "1~15자 사이로 입력해주세요.")
+    @Pattern(regexp = "^[a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣0-9()+&/_ \\[\\]-]*$", message = "특수 문자는 '(, ), [, ], +, -, &, /, - 만 입력 가능합니다.'")
+    @NoKakao
+    private String name;
+
+    private int price;
+
+    private String imageUrl;
+
+    public ProductAddForm() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public ProductAddForm(String name, int price, String imageUrl) {
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
 
 }
---
+++ src/main/java/gift/validation/NoKakaoValidator.java
@@ -0,0 +1,20 @@
+package gift.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class NoKakaoValidator implements ConstraintValidator<NoKakao, String> {
+
+    @Override
+    public void initialize(NoKakao constraintAnnotation) {
+
+    }
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+       if(s == null)
+           return false;
+
+       return !s.contains("카카오");
+    }
+}
