59d3dab7dfeda3863456140113ebdef7a9841f9e
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-07-03 01:59:45 +0900

    feat: 입력값 기본 어노테이션 유효성 검사 - postReq 유효성 검사 추가 - patchReq 유효성 검사 추가

--- src/main/java/gift/product/model/dto/PatchProductReq.java
+++ src/main/java/gift/product/model/dto/PatchProductReq.java
@@ -1,14 +1,20 @@
 package gift.product.model.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import org.springframework.lang.Nullable;
 
 public class PatchProductReq {
     @NotNull
     private Long id;
     @Nullable
+    @Size(max = 15, message = "상품 이름은 15자 이하로 입력해주세요.")
+    @Pattern(regexp = "^[a-zA-Z0-9 ()\\[\\]\\+\\-&/_]*$", message = "사용할 수 없는 특수문자가 포함되어 있습니다.")
     private String name;
     @Nullable
+    @Min(value = 0, message = "상품 가격은 0 이상의 정수로 입력해주세요.")
     private int price = 0;
     @Nullable
     private String imageUrl;
--- src/main/java/gift/product/model/dto/PostProductReq.java
+++ src/main/java/gift/product/model/dto/PostProductReq.java
@@ -2,13 +2,17 @@ package gift.product.model.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import org.springframework.lang.Nullable;
 
 public class PostProductReq {
     @NotNull
+    @Size(max = 15, message = "상품 이름은 15자 이하로 입력해주세요.")
+    @Pattern(regexp = "^[a-zA-Z0-9 ()\\[\\]\\+\\-&/_]*$", message = "사용할 수 없는 특수문자가 포함되어 있습니다.")
     private String name;
     @NotNull
-    @Min(0)
+    @Min(value = 0, message = "상품 가격은 0 이상의 정수로 입력해주세요.")
     private int price = 0;
     @Nullable
     private String imageUrl;
