831b5eeb36d364ec71398247432f3ed3e501f03c
suhyeon7497 <nucc1186@gmail.com>
2024-07-02 19:26:37 +0900

    feat: imageUrl에 @NotBlank 추가

--- src/main/java/gift/controller/dto/ProductRequestDto.java
+++ src/main/java/gift/controller/dto/ProductRequestDto.java
@@ -3,7 +3,6 @@ package gift.controller.dto;
 import gift.model.Product;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
@@ -19,6 +18,7 @@ public class ProductRequestDto {
     @Min(value = 0, message ="가격은 0원 이상입니다.")
     private int price;
 
+    @NotBlank
     private String imageUrl;
 
     public ProductRequestDto(){}
