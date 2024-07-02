823064cffb3366de1f5da92b542fcd250d2ef017
안원모 <tommy45231@naver.com>
2024-07-02 16:15:24 +0900

    Feat : 제품 관리자 페이지 , HTTP API 에서의 CRUD 기능 구현

---
+++ src/main/java/gift/ProductController.java
@@ -0,0 +1,43 @@
+package gift;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getProducts() {
+        return productService.findAllProducts();
+    }
+
+    @PostMapping
+    public Product postProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
+    }
+
+    @PutMapping("/{id}")
+    public Product putProduct(@PathVariable Long id, @RequestBody Product product) {
+        return productService.updateProduct(id, product);
+    }
+
+    @DeleteMapping("/{id}")
+    public Long deleteProduct(@PathVariable Long id) {
+        return productService.deleteProduct(id);
+    }
+}
