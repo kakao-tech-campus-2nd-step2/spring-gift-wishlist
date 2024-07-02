6f0578e97562be056464f95867247f7cdacb9717
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-07-02 19:10:23 +0900

    feat : 상품 이름 길이 검사 상품 이름은 최소 1자 이상이어야 하므로 @NotBlank를 사용했습니다. 그리고 공백 포함 최대 15자까지 입력 가능하므로 @Size(max=15)를 사용했습니다.

--- src/main/java/gift/Model/Product.java
+++ src/main/java/gift/Model/Product.java
@@ -1,5 +1,11 @@
 package gift.Model;
 
-public record Product(Long id, String name, long price, String imageUrl) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record Product(Long id,
+                      @NotBlank(message = "상품 이름은 최소 1자 이상이어야 합니다.")
+                      @Size(max = 15, message = "상품 이름은 공백 포함 최대 15자까지 입력할 수 있습니다.")
+                      String name, long price, String imageUrl) {
 
 }
