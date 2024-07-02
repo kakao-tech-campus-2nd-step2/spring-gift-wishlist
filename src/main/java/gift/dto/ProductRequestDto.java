6481137a2349b1bf93f548a653b46538b420c221
wvssm <neoramiregi@gmail.com>
2024-07-02 15:31:24 +0900

    feat(ProductRequestDto): 상품 특수문자 포함 검사 로직 추가

--- src/main/java/gift/dto/ProductRequestDto.java
+++ src/main/java/gift/dto/ProductRequestDto.java
@@ -8,6 +8,7 @@ import jakarta.validation.constraints.Size;
 public class ProductRequestDto {
     @NotBlank(message = "상품 이름은 공백일 수 없습니다.")
     @Size(max = 15, message = "상품 이름은 공백을 포함하여 최대 15자까지 입력할 수 있습니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣 ()\\[\\]+\\-&/_]*$", message = "상품 이름에 허용되지 않은 특수 문자가 포함되어 있습니다.")
     private String name;
     private int price;
     private String imageUrl;
