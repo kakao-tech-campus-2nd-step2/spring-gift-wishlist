fcafd1d467fbc85fb599f3bf5dbc0ed00aa29042
sim-mer <gyu2781@gmail.com>
2024-07-02 16:36:32 +0900

    refactor(): ProductDeleteDto 변경 - RequestProductIdsDto로 rename - Valid 어노테이션 추가

--- src/main/java/gift/dto/ProductDeleteDto.java
+++
@@ -1,8 +0,0 @@
-package gift.dto;
-
-import java.util.List;
-
-public record ProductDeleteDto(
-        List<Long> productIds
-) {
-}
---
+++ src/main/java/gift/dto/RequestProductIdsDto.java
@@ -0,0 +1,13 @@
+package gift.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+public record RequestProductIdsDto(
+        @NotEmpty
+        @Size(min = 1, max = 10)
+        List<Long> productIds
+) {
+}
