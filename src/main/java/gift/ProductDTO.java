fa0a41a07b6a286b664ce4b297677adf8b6fcd57
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-07-03 00:26:08 +0900

    feat : ProductDTO Bean Validation ProductDTO의 필드에 조건과 오류메시지 추가

--- src/main/java/gift/ProductDTO.java
+++ src/main/java/gift/ProductDTO.java
@@ -1,8 +1,16 @@
 package gift;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 public class ProductDTO {
+    @NotBlank(message = "상품명은 공백으로 둘 수 없습니다.")
+    @Pattern(regexp = "^[a-zA-Zㄱ-ㅎ가-힣0-9\\(\\)\\[\\]\\+\\-&/\\_ ]{1,16}$", message = "상품명은 공백을 포함하여 최대 15자까지 입력할 수 있습니다. 특수문자는 ( ) [ ] + - & / _ 만 사용 가능합니다.")
     String name;
+    @NotNull(message = "가격은 공백으로 둘 수 없습니다.")
     Long price;
+    @NotBlank(message = "이미지 URL은 공백으로 둘 수 없습니다.")
     String imageUrl;
 
     public ProductDTO() {
