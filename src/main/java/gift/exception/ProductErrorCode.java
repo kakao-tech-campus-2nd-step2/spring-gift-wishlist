09c7172a6e441f417bd293f6c0ce35a85ce6efe4
suhyeon7497 <nucc1186@gmail.com>
2024-07-02 19:44:43 +0900

    feat: ProductException 추가

---
+++ src/main/java/gift/exception/ProductErrorCode.java
@@ -0,0 +1,15 @@
+package gift.exception;
+
+public enum ProductErrorCode {
+    HAS_KAKAO_WORD("\"kakao\"가 포함된 문구는 담당자와 협의 후 사용할 수 있습니다.");
+
+    private final String message;
+
+    private ProductErrorCode(String message){
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
