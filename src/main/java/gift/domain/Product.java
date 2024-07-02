6ef0fc4a3edfb1a3a352705636ade345d8d1adfe
noojmogstel <goaudgo@naver.com>
2024-07-02 14:36:00 +0900

    feat(domain/Product): 어노테이션 추가

--- src/main/java/gift/domain/Product.java
+++ src/main/java/gift/domain/Product.java
@@ -1,32 +1,40 @@
 package gift.domain;
 
+import gift.annotation.ProductName;
+import jakarta.validation.constraints.NotNull;
+
 public class Product {
+
+    @NotNull
     private Long id;
+    @ProductName
     private String name;
     private int price;
     private String imageUrl;
 
-    public Product(){}
-    public Product(Long id, String name, int price, String imageUrl){
-        this.id=id;
-        this.name=name;
-        this.price=price;
-        this.imageUrl=imageUrl;
+    public Product() {
     }
 
-    public Long getId(){
+    public Product(Long id, String name, int price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getId() {
         return id;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public int getPrice(){
+    public int getPrice() {
         return price;
     }
 
-    public String getImageUrl(){
+    public String getImageUrl() {
         return imageUrl;
     }
 
--- src/main/java/gift/exception/GlobalControllerExceptionHandler.java
+++ src/main/java/gift/exception/GlobalControllerExceptionHandler.java
@@ -5,4 +5,5 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalControllerExceptionHandler {
 
+
 }
