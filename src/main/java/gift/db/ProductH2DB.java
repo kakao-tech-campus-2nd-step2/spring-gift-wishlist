89cf5b886a2b5c39016326fd3611bb2ba73c354b
SeongHun <tjdgns5506@gmail.com>
2024-07-02 12:16:08 +0900

    refactor: Product의 Key Id를 클라이언트가 정하는게 아니라 서버가 결정하도록 변경. auto increment 적용

--- src/main/java/gift/db/ProductH2DB.java
+++ src/main/java/gift/db/ProductH2DB.java
@@ -20,16 +20,16 @@ public class ProductH2DB {
     @PostConstruct
     public void initialize() {
         createProductTable();
-        addProduct(new Product(1L, "Americano", 4500, "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg"));
-        addProduct(new Product(2L, "Latte", 5500, "https://cdn.pixabay.com/photo/2023/07/08/13/17/coffee-8114518_1280.png"));
-        addProduct(new Product(3L, "Sandwich", 7700, "https://cdn.pixabay.com/photo/2023/08/12/02/58/sandwich-8184642_1280.png"));
-        addProduct(new Product(4L, "Cup Cake", 10000, "https://cdn.pixabay.com/photo/2023/05/31/14/41/ai-generated-8031574_1280.png"));
+        addProduct(new Product("Americano", 4500, "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg"));
+        addProduct(new Product("Latte", 5500, "https://cdn.pixabay.com/photo/2023/07/08/13/17/coffee-8114518_1280.png"));
+        addProduct(new Product("Sandwich", 7700, "https://cdn.pixabay.com/photo/2023/08/12/02/58/sandwich-8184642_1280.png"));
+        addProduct(new Product("Cup Cake", 10000, "https://cdn.pixabay.com/photo/2023/05/31/14/41/ai-generated-8031574_1280.png"));
     }
 
     private void createProductTable() {
         var sql = """
                 create table product (
-                  id bigint,
+                  id bigint AUTO_INCREMENT,
                   name varchar(255),
                   price int,
                   imageUrl varchar(255),
@@ -40,8 +40,8 @@ public class ProductH2DB {
     }
 
     public boolean addProduct(Product product) {
-        String sql = "INSERT INTO product (id, name, price, imageUrl) VALUES (?, ?, ?, ?)";
-        int rowsAffected = jdbcTemplate.update(sql, product.getId(), product.getName(), product.getPrice(), product.getImageUrl());
+        String sql = "INSERT INTO product (name, price, imageUrl) VALUES ( ?, ?, ?)";
+        int rowsAffected = jdbcTemplate.update(sql, product.getName(), product.getPrice(), product.getImageUrl());
         return rowsAffected > 0;
     }
 
