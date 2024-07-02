6bda645847ea937d4e625ebb6448c762a9b57238
Profile-exe <kang67572346@gmail.com>
2024-07-03 03:16:19 +0900

    refactor(ProductController): 상품 API에 대한 검증 적용 및 커스텀 예외를 던지는 로직 추가

--- src/main/java/gift/product/controller/ProductController.java
+++ src/main/java/gift/product/controller/ProductController.java
@@ -1,6 +1,17 @@
-package gift.product;
+package gift.product.controller;
 
+import gift.product.Product;
+import gift.product.exception.ProductCreateException;
+import gift.product.exception.ProductDeleteException;
+import gift.product.exception.ProductErrorCode;
+import gift.product.exception.ProductUpdateException;
+import gift.product.message.ProductInfo;
+import gift.product.dto.ProductReqDto;
+import gift.product.dto.ProductResDto;
+import gift.product.repository.ProductRepository;
+import jakarta.validation.Valid;
 import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,30 +47,34 @@ public class ProductController {
 
     @GetMapping("/products/{productId}")
     public ResponseEntity<ProductResDto> getProduct(@PathVariable Long productId) {
-        Product product = getProductById(productId);
+        Product product = productRepository.findProductById(productId);;
 
         return ResponseEntity.ok(new ProductResDto(product));
     }
 
     @PostMapping("/products")
-    public ResponseEntity<ProductResDto> addProduct(@RequestBody ProductReqDto productReqDto) {
+    public ResponseEntity<ProductResDto> addProduct(@Valid @RequestBody ProductReqDto productReqDto) {
         Long productId = productRepository.addProduct(productReqDto);
+
+        if (productId == null || productId == 0L) {
+            throw new ProductCreateException(ProductErrorCode.PRODUCT_CREATE_FAILED);
+        }
 
         // 저장된 상품 가져오기
         Product newProduct = productRepository.findProductById(productId);
 
-        return ResponseEntity.ok(new ProductResDto(newProduct));
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ProductResDto(newProduct));
     }
 
     @PutMapping("/products/{productId}")
-    public ResponseEntity<String> updateProduct(@PathVariable Long productId, @RequestBody ProductReqDto productReqDto) {
+    public ResponseEntity<String> updateProduct(@PathVariable Long productId, @Valid @RequestBody ProductReqDto productReqDto) {
         Integer noOfRowsAffected = productRepository.updateProductById(productId, productReqDto);
 
         if (NO_OF_ROWS_AFFECTED.equals(noOfRowsAffected)) {     // 수정된 행의 개수 반환 - 1이면 성공, 0이면 실패
             return ResponseEntity.ok(ProductInfo.PRODUCT_UPDATE_SUCCESS);
         }
 
-        return ResponseEntity.badRequest().body(ProductInfo.PRODUCT_UPDATE_FAIL);
+        throw new ProductUpdateException(ProductErrorCode.PRODUCT_UPDATE_FAILED);
     }
 
     @DeleteMapping("/products/{productId}")
@@ -70,10 +85,6 @@ public class ProductController {
             return ResponseEntity.ok(ProductInfo.PRODUCT_DELETE_SUCCESS);
         }
 
-        return ResponseEntity.badRequest().body(ProductInfo.PRODUCT_DELETE_FAIL);
-    }
-
-    private Product getProductById(Long productId) {
-        return productRepository.findProductById(productId);
+        throw new ProductDeleteException(ProductErrorCode.PRODUCT_DELETE_FAILED);
     }
 }
