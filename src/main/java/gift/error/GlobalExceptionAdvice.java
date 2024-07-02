189cd04277c65e38ffa36ae4d3b8ab0b1da430d3
HSHwan <tnghks2866@gmail.com>
2024-07-02 15:06:54 +0900

    fix: 상품 API Request 유효성 검사 수정

---
+++ src/main/java/gift/error/GlobalExceptionAdvice.java
@@ -0,0 +1,29 @@
+package gift.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationExceptions(
+            MethodArgumentNotValidException exception) {
+        BindingResult bindingResult = exception.getBindingResult();
+
+        StringBuilder builder = new StringBuilder();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            builder.append(fieldError.getField());
+            builder.append("(은)는 ");
+            builder.append(fieldError.getDefaultMessage());
+        }
+
+        return new ResponseEntity<>(builder.toString(), HttpStatus.BAD_REQUEST);
+    }
+
+}
