bf3a82074efc376375fdc292450d15e451f2fe6d
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-07-02 19:52:41 +0900

    feat : @ResponseStatus 사용 @ResponseStatus 사용하여 Http Status Code를 반환합니다.

--- src/main/java/gift/Controller/GlobalExceptionHandler.java
+++ src/main/java/gift/Controller/GlobalExceptionHandler.java
@@ -2,14 +2,17 @@ package gift.Controller;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
@@ -21,6 +24,7 @@ public class GlobalExceptionHandler {
         return errors;
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
     public Map<String, String> handleIllegalArgumentException(IllegalArgumentException ex) {
         Map<String, String> errors = new HashMap<>();
