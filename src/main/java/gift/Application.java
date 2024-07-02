7fb9fc4c216ae9bf6c4307e1f45d6f20e29021c6
Yongjin An <yongjin5352@gmail.com>
2024-07-02 10:55:24 +0900

    refactor: Remove unused fields

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -8,10 +8,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-
     @Autowired
     JdbcTemplate jdbcTemplate;
 }
