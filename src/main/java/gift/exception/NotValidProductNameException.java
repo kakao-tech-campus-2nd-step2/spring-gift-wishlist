fcf9a2b3f2fd943b3b2bbc10e709ea2e69b10ec9
dong_yxxn <g0326kdy@gmail.com>
2024-07-02 21:32:32 +0900

    feat(NotValidProductNameException): 이름 입력 에러 정의

---
+++ src/main/java/gift/exception/NotValidProductNameException.java
@@ -0,0 +1,7 @@
+package gift.exception;
+
+public class NotValidProductNameException extends RuntimeException {
+    public NotValidProductNameException(String message) {
+        super(message);
+    }
+}
