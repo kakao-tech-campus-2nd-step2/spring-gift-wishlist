450591e4d8591c172106b811001d026c8bc3dddb
pswaao88 <pswaao88@gmail.com>
2024-07-02 15:06:07 +0900

    Feat: ErrorCode클래스 작성

--- src/main/java/gift/Exception/ErrorCode.java
+++ src/main/java/gift/Exception/ErrorCode.java
@@ -1,5 +1,24 @@
 package gift.Exception;
 
 public enum ErrorCode {
+    INVALID_NAME_LENGTH(400, "길이가 15를 넘습니다."),
+    INVALID_NAME_SPECIAL_SYMBOL(400, "( ), [ ], +, -, &, /, _ 외의 특수문자가 포함됐습니다."),
+    INVALID_NAME_KAKAO(400, "( ), [ ], +, -, &, /, _ 외의 특수문자가 포함됐습니다."),
+    INVALID_PRICE(400, "가격은 0이하가 될 수 없습니다.");
 
+    private final int status;
+    private final String message;
+
+    ErrorCode(int status, String message){
+        this.status = status;
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
