0c86ee8307aff4e817bee45d3c4d4f74ecd75dec
us4c0d3 <jwseok312@gmail.com>
2024-07-02 15:21:11 +0900

    chore: 미사용 import 삭제

--- src/main/java/gift/ViewController.java
+++ src/main/java/gift/ViewController.java
@@ -2,6 +2,7 @@ package gift;
 
 import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("/products")
--- src/test/java/gift/ProductServiceTest.java
+++ src/test/java/gift/ProductServiceTest.java
@@ -1,7 +1,6 @@
 package gift;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
