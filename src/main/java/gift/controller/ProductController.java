e0188304b61eb9983f887d9e749090d4b78f5ef7
GitJIHO <jiho99322@gmail.com>
2024-07-02 17:31:20 +0900

    refactor: 사용하지 않는 import 제거

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -3,7 +3,6 @@ package gift.controller;
 import gift.dto.Product;
 import gift.service.ProductService;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
