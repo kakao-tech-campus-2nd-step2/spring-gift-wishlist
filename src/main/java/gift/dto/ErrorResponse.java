4943a5112d9792dddf2d1c84becb83df844b4d1c
wvssm <neoramiregi@gmail.com>
2024-07-02 16:13:29 +0900

    refactor: 글로벌 예외 처리기 생성

--- src/main/java/gift/dto/ErrorResponse.java
+++ src/main/java/gift/dto/ErrorResponse.java
@@ -1,7 +1,23 @@
 package gift.dto;
 
+import java.util.Map;
+
 public class ErrorResponse {
     private String message;
+    private Map<String, String> fieldErrors;
+
+    public ErrorResponse(String message, Map<String, String> fieldErrors) {
+        this.message = message;
+        this.fieldErrors = fieldErrors;
+    }
+
+    public Map<String, String> getFieldErrors() {
+        return fieldErrors;
+    }
+
+    public void setFieldErrors(Map<String, String> fieldErrors) {
+        this.fieldErrors = fieldErrors;
+    }
 
     public ErrorResponse(String message) {
         this.message = message;
---
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package gift.exception;
+
+import gift.dto.ErrorResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleProductNotFoundException(ProductNotFoundException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex) {
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        ErrorResponse errorResponse = new ErrorResponse("이름 검증에 실패했습니다.", errors);
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+}
