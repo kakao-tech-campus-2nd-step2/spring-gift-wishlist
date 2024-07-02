e0335402777f74dab1fa2fa51b25080f7f05f2dd
alsswl <alswl4223@naver.com>
2024-07-03 01:51:43 +0900

    feat : 이름의 적절성 판단하는 메서드 작성

--- src/main/java/gift/controller/MenuController.java
+++ src/main/java/gift/controller/MenuController.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.regex.Pattern;
+
 import org.springframework.ui.Model;
 
 @Controller
@@ -14,10 +17,23 @@ import org.springframework.ui.Model;
 public class MenuController {
 
     private final MenuService menuService;
+    Pattern signPattern = Pattern.compile("[!@#$%^*,.?\":{}|<>\\\\]");
+    Pattern kakaoPattern = Pattern.compile("카카오");
     public MenuController(MenuService menuService){
         this.menuService = menuService;
     }
 
+    private void checkName(String name){
+        if(name.length() > 15){
+            throw new NoSuchElementException("상품명은 15자 이내로 작성하여야 합니다.");
+        }
+        else if(signPattern.matcher(name).find()){
+            throw new NoSuchElementException(" ( ), [ ], +, -, &, /, _ 이외의 특수문자는 사용이 불가능합니다");
+        }
+        else if(kakaoPattern.matcher(name).find()){
+            throw new NoSuchElementException("\"카카오\"가 포함된 상품명은 담당 MD와 협의해주세요");
+        }
+    }
     @PostMapping
     public String save(
             @ModelAttribute MenuRequest request
