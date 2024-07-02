5f05b2aa777f9ec309fa5cf09a72f4558d906e56
hyeonseo <69136047+hxunsc@users.noreply.github.com>
2024-07-03 01:24:54 +0900

    feat: 상품 이름에 사용 불가 특수문자 유효성 검사

--- src/main/java/gift/validator/ProductNameValidator.java
+++ src/main/java/gift/validator/ProductNameValidator.java
@@ -8,6 +8,8 @@ import org.springframework.validation.Validator;
 
 @Component
 public class ProductNameValidator implements Validator {
+    private static final Pattern ALLOWED_PATTERN = Pattern.compile("^[\\w\\s\\(\\)\\[\\]\\+\\-\\&\\/\\_]*$");
+
     @Override
     public boolean supports(Class<?> clazz) {
         return Product.class.isAssignableFrom(clazz);
@@ -19,6 +21,10 @@ public class ProductNameValidator implements Validator {
         String name = product.getName();
         if (name == null || name.length() > 15) {
             errors.rejectValue("name", "name.size", "상품의 이름은 최대 15자까지 입력할 수 있습니다.");
+        }
+
+        if (!ALLOWED_PATTERN.matcher(name).matches()) {
+            errors.rejectValue("name", "invalid.name", "상품의 이름에 사용 불가한 문자가 포함되어 있습니다.");
         }
 
     }
