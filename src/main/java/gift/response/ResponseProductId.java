3ac3f46e4df2fe73c06cd0ec2ba4e4433bf91907
SeongHun <tjdgns5506@gmail.com>
2024-07-02 13:48:20 +0900

    fix: no acceptable representation 에러 ResponseProductId의 getter추가하니 해결

--- src/main/java/gift/response/ResponseProductId.java
+++ src/main/java/gift/response/ResponseProductId.java
@@ -2,14 +2,14 @@ package gift.response;
 
 public class ResponseProductId {
     
-    private Long id;
+    private Long product_id;
 
-    public ResponseProductId(Long id) {
-        this.id = id;
+    public ResponseProductId(Long product_id) {
+        this.product_id = product_id;
     }
 
-    public Long getId() {
-        return id;
+    public Long getProduct_id() {
+        return product_id;
     }
 
 }
