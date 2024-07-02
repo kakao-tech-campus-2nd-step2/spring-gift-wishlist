4597cdb5b0db351675c6cf4be89c04dcc915fd3e
lja3723 <lja3723@gmail.com>
2024-07-03 00:21:34 +0900

    chore: fix config & move Application to root package

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -1,9 +1,9 @@
-package gift.global;
+package gift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "gift")
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
