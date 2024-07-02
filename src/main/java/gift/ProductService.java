f5b70d32e340798937da73e68a62e4d88d1f6aee
hyeonseo <69136047+hxunsc@users.noreply.github.com>
2024-07-02 11:45:54 +0900

    feat: Add ProductService

---
+++ src/main/java/gift/ProductService.java
@@ -0,0 +1,30 @@
+package gift;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+    private ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> findById(long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteById(long id) {
+        productRepository.deleteById(id);
+    }
+
+}
