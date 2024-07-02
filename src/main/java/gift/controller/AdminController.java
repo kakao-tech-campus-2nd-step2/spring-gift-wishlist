d293183c183b53a052eaa9c46cfbb3d76572da1e
hjinshin <gudwls818@knu.ac.kr>
2024-07-02 23:25:27 +0900

    경북대_BE 신형진 2주차 과제(step0) (#33)
    * feat: 상품 관리 코드 복사
    
    * refactor: restController와 controller 분리
    
    * refactor: url 컨벤션 통일
    
    * refactor: 스테레오타입 어노테이션 변경
    
    @Component -> @Repository
    
    * refactor: 검증 어노테이션 추가
    
    * refactor: create sql에 not null 추가

---
+++ src/main/java/gift/controller/AdminController.java
@@ -0,0 +1,60 @@
+package gift.controller;
+
+import gift.controller.dto.ProductRequest;
+import gift.model.Product;
+import gift.model.ProductDao;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+public class AdminController {
+
+    private final ProductDao productDao;
+
+    public AdminController(ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
+    @GetMapping("")
+    public String index(Model model) {
+        List<Product> products = productDao.findAll();
+        model.addAttribute("products", products);
+        return "index";
+    }
+
+    @GetMapping("admin/product/new")
+    public String newProduct() {
+        return "new";
+    }
+
+    @GetMapping("admin/product/{id}")
+    public String updateProduct(@PathVariable("id") @NotNull @Min(1) Long id, Model model) {
+        Product product = productDao.findById(id);
+        model.addAttribute("product", product);
+        return "edit";
+    }
+
+    @PostMapping("admin/product")
+    public String newProduct(@ModelAttribute ProductRequest request) {
+        productDao.save(request);
+        return "redirect:/";
+    }
+
+    @PutMapping("admin/product/{id}")
+    public String updateProduct(@PathVariable("id") @NotNull @Min(1) Long id,
+                                @ModelAttribute ProductRequest request) {
+        productDao.updateById(id, request);
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/admin/product/{id}")
+    public String delete(@PathVariable("id") @NotNull @Min(1) Long id) {
+        productDao.deleteById(id);
+        return "redirect:/";
+    }
+}
