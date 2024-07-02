1cc1cba6d3d121a798bec6a3375004374a204567
HSHwan <tnghks2866@gmail.com>
2024-07-02 16:13:13 +0900

    fix: 상품 Request 유효성 검사 및 상품 edit 폼 수정

--- src/main/java/gift/dto/ProductRequest.java
+++ src/main/java/gift/dto/ProductRequest.java
@@ -1,13 +1,13 @@
 package gift.dto;
 
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record ProductRequest(
-        @NotBlank(message = "반드시 값이 존재해야 합니다.")
-        @Pattern(regexp = "^[\\w\\d\\s\\(\\)\\[\\]\\+\\-\\&\\/\\_\\uAC00-\\uD7A3]*$", message = "특수 문자를 제외한 문자열을 입력해야 합니다.")
+        @NotEmpty(message = "반드시 값이 존재해야 합니다.")
+        @Pattern(regexp = "[\\w\\d\\s\\(\\)\\[\\]\\+\\-\\&\\/\\_\\uAC00-\\uD7A3]*", message = "특수 문자를 제외한 문자열을 입력해야 합니다.")
         @Size(max = 15, message = "길이가 15 이하여야 합니다.")
         String name,
         @Min(value = 0, message = "0 이상의 정수를 입력해야 합니다.")
