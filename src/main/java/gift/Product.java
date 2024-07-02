eefb0f83ad636a246e6d905a5599799faf932c53
안원모 <tommy45231@naver.com>
2024-07-02 16:06:01 +0900

    Feat : Product 도메인 모델 구현

---
+++ src/main/java/gift/Product.java
@@ -0,0 +1,62 @@
+package gift;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    private String imageUrl;
+    private String description;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, BigDecimal price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
