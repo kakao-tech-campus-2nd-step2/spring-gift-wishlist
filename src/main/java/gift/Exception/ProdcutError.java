862f7f6d943dfc996c8e2701962a7922c50da3a7
pswaao88 <pswaao88@gmail.com>
2024-07-02 15:06:39 +0900

    Feat: ProductError 클래스 작성

--- src/main/java/gift/Exception/ProdcutError.java
+++ src/main/java/gift/Exception/ProdcutError.java
@@ -1,5 +1,27 @@
 package gift.Exception;
 
 public class ProdcutError {
+    private int status;
+    private String message;
 
+    public ProdcutError(ErrorCode errorCode){
+        this.status = errorCode.getStatus();
+        this.message = errorCode.getMessage();
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
