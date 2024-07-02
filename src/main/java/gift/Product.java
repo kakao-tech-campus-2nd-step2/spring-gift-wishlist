d240567f04565cb494cb155e1935e6b43c3b7150
hyyyh0x <hyyyh0x@gmail.com>
2024-07-03 00:24:53 +0900

    refactor: 카카오 유효성 검사 고침

--- src/main/java/gift/Product.java
+++ src/main/java/gift/Product.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.*;
 public record Product(Long id,
                       @Pattern(regexp = "^[a-zA-Z0-9가-힣ㄱ-ㅎ ()\\[\\]+\\-&/_]*$",
                           message="'( ), [ ], +, -, &, /, _'외의 특수문자는 사용할 수 없습니다.")
-                      @Pattern(regexp = "^(?!.*카카오*)",
+                      @Pattern(regexp = "^(?!.*카카오).*",
                           message="\"카카오\"가 포함된 문구는 담당 MD와 협의한 경우에만 사용할 수 있습니다.")
                       @Size(max=15, message="공백을 포함하여 15자 이내의 이름을 입력해주세요.")
                       @NotBlank(message="이름을 입력하지 않았습니다.")
