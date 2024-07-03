1997aef70a5931f62565713b39bbb9cc051c9571
xkrwjdals <developjmt@gmail.com>
2024-07-03 18:20:06 +0900

    fix: Delete HiddenHttpMethodFilter Bean
    - Activate hidden method filter in application.properties

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -10,9 +10,4 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-    @Bean
-    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
-        return new HiddenHttpMethodFilter();
-    }
 }
