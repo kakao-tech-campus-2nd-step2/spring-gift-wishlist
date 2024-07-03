518579f4743d9a4272d958ac2df4069a9c2ab34c
Yongjin An <yongjin5352@gmail.com>
2024-07-03 17:18:44 +0900

    feat: Add exception handler for MethodArgumentNotValidException

--- src/main/java/gift/model/ProductRecord.java
+++ src/main/java/gift/model/ProductRecord.java
@@ -8,6 +8,7 @@ public record ProductRecord(
 
         @Pattern(regexp =  "^[a-zA-Z0-9가-힣 ()\\[\\]+\\-&/_]{1,15}$", message = "이름은 1 ~ 15자 사이여야 하며, 특수 문자는 허용된 특수 문자만 가능합니다. " +
                 "\n허용된 특수문자 : [, ], (, ), +, -, &, /, _ ")
+        @Pattern(regexp = "^(?!.*카카오).*$", message = "'카카오'가 포함된 상품은 md만 추가할 수 있습니다. 담당 md와 협의해주세요.")
         String name,
 
         @Max(value = Integer.MAX_VALUE, message = "가격은 " + Integer.MAX_VALUE + "를 넘을 수 없습니다.")
