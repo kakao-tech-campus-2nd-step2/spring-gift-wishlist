07ae0cf0f69e16e7d461279447cf90d12c0d6750
seolbin96 <jsb3038@jnu.ac.kr>
2024-07-03 15:30:14 +0900

    feat : 어노테이션 메세지 수정

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -43,16 +43,16 @@ public class ProductController {
 
         String productName = product.name();
         if (productName.length() > 15) {
-            return new ResponseEntity<>("Name must be less than or equal to 15 characters", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("상품 이름은 15자 이내여야 합니다.", HttpStatus.BAD_REQUEST);
         }
 
         Pattern specialCharPattern = Pattern.compile("[^ㄱ-ㅎ가-힣a-zA-Z0-9\\s\\(\\)\\[\\]+&\\-/_]");
         if (specialCharPattern.matcher(productName).find()) {
-            return new ResponseEntity<>("Invalid characters in name", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("상품 이름에 유효하지 않은 문자가 포함되어 있습니다.", HttpStatus.BAD_REQUEST);
         }
 
         if (productName.contains("카카오")) {
-            return new ResponseEntity<>("Name contains restricted word '카카오'", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("'카카오'를 이름에 포함하려면 담당 MD와 협의해주세요.", HttpStatus.BAD_REQUEST);
         }
 
         Product newProduct = productRepository.save(product);
@@ -67,16 +67,16 @@ public class ProductController {
 
         String productName = product.name();
         if (productName.length() > 15) {
-            return new ResponseEntity<>("Name must be less than or equal to 15 characters", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("상품 이름은 15자 이내여야 합니다.", HttpStatus.BAD_REQUEST);
         }
 
         Pattern specialCharPattern = Pattern.compile("[^ㄱ-ㅎ가-힣a-zA-Z0-9\\s\\(\\)\\[\\]+&\\-/_]");
         if (specialCharPattern.matcher(productName).find()) {
-            return new ResponseEntity<>("Invalid characters in name", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("상품 이름에 유효하지 않은 문자가 포함되어 있습니다.", HttpStatus.BAD_REQUEST);
         }
 
         if (productName.contains("카카오")) {
-            return new ResponseEntity<>("Name contains restricted word '카카오'", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("'카카오'를 이름에 포함하려면 담당 MD와 협의해주세요.", HttpStatus.BAD_REQUEST);
         }
 
         Product updatedProduct = new Product(id, product.name(), product.price(), product.imageUrl());
