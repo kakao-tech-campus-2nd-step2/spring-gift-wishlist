d246a37178da07602ed4bb0e7265ce8807a51208
yooonwodyd <dbswodyd00@naver.com>
2024-07-03 15:09:09 +0900

    [feat] 검증 로직 추가.

--- src/main/java/gift/dto/ProductRequestDto.java
+++ src/main/java/gift/dto/ProductRequestDto.java
@@ -1,14 +1,25 @@
 package gift.dto;
 
 import gift.domain.Product;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record ProductRequestDto(
 	Long id,
+
+	@Pattern(
+		regexp = "^(?!.*카카오).*$",
+		message = "Product name cannot contain '카카오'"
+	)
+	@Size(max = 15, message = "Product name must be at most 15 characters long")
+	@Pattern(
+		regexp = "^[a-zA-Z0-9가-힣 \\(\\)\\[\\]\\+\\-\\&\\/\\_]*$",
+		message = "Product name contains invalid characters"
+	)
 	String name,
 	Long price,
 	String imageUrl
 ) {
-
 	public Product toEntity() {
 		return Product.of(id, name, price, imageUrl);
 	}
