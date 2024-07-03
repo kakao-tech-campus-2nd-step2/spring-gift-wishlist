188a75c6ae40ce3e753a78b62c74cd17704098b5
geist9110 <geist9110@gmail.com>
2024-07-03 15:15:20 +0900

    feat(validation): Add productDTO's length validation message

--- src/main/java/gift/Product/ProductDTO.java
+++ src/main/java/gift/Product/ProductDTO.java
@@ -6,7 +6,11 @@ import gift.Validation.KakaoStringValidation;
 import gift.Validation.SpecialCharacterValidation;
 
 public record ProductDTO(
-        @Length(min = 1, max = 15) @SpecialCharacterValidation @KakaoStringValidation String name,
-        Integer price,
-        String imageUrl) {
+    @Length(min = 1, max = 15, message = "product name's length must be between 1 and 15")
+    @SpecialCharacterValidation
+    @KakaoStringValidation
+    String name,
+    
+    Integer price,
+    String imageUrl) {
 }
