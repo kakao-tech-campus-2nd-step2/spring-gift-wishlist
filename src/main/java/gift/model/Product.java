87a9bd457e9f383a7d9556c76e4a7e906e3b01ed
seolbin96 <jsb3038@jnu.ac.kr>
2024-07-03 15:20:51 +0900

    feat : 불필요한 메소드 삭제

--- src/main/java/gift/model/Product.java
+++ src/main/java/gift/model/Product.java
@@ -1,6 +1,5 @@
 package gift.model;
 
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -8,15 +7,12 @@ import javax.validation.constraints.Size;
 public record Product(
         Long id,
 
-        @NotBlank(message = "Name is mandatory")
-        @Size(max = 15, message = "Name must be less than or equal to 15 characters")
-        @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9\\s\\(\\)\\[\\]+&\\-/_]*$", message = "Invalid characters in name")
+        @NotBlank(message = "이름은 필수 항목입니다.")
+        @Size(max = 15, message = "이름은 15자 이내여야 합니다.")
+        @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9\\s\\(\\)\\[\\]+&\\-/_]*$", message = "이름에 유효하지 않은 문자가 포함되어 있습니다.")
         String name,
 
-        @Min(value = 0, message = "Price must be greater than or equal to 0")
         int price,
-
-        @NotBlank(message = "Image URL is mandatory")
         String imageUrl
 ) {
 }
