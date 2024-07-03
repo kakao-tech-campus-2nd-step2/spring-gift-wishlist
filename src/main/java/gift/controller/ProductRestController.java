809fa49c4c311bfdc5b324e14f539ad7c4930d2d
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-03 15:29:53 +0900

    refactor : @Valid 추가

--- src/main/java/gift/controller/ProductRestController.java
+++ src/main/java/gift/controller/ProductRestController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,12 +51,16 @@ public class ProductRestController {
         return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
     }
 
-    // 상품 수정
-//        @PutMapping("/{id}")
-//        public ResponseEntity<Product> updateProduct(@PathVariable Long id, @Valid @RequestBody Product product) {
-//            product.setId(id);
-//            productService.updateProduct(id, product);
-//        }
+//     상품 수정
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateProduct(@PathVariable Long id, @Valid @RequestBody Product product) {
+        try {
+            productService.updateProduct(id, product);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 
     // 상품 삭제
     @DeleteMapping("/{id}")
