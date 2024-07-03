0a2f54e1a701ae4862006b036b48ec9be55910e5
sim-mer <gyu2781@gmail.com>
2024-07-03 11:01:38 +0900

    feat() : 상품 이름에 문자 필터링 추가

--- src/main/java/gift/dto/RequestProductDto.java
+++ src/main/java/gift/dto/RequestProductDto.java
@@ -1,14 +1,16 @@
 package gift.dto;
 
 import gift.domain.Product;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import org.hibernate.validator.constraints.URL;
 
 public record RequestProductDto(
-        @NotBlank String name,
+        @NotBlank
+        @Size(max = 15)
+        @Pattern(regexp = "^[a-zA-Z0-9ㄱ-ㅎ가-힣 ()\\[\\]+\\-&/_]*$",
+                message = "오직 문자, 공백 그리고 특수문자 (),[],+,&,-,/,_만 허용됩니다."
+        )
+        String name,
         @NotNull @Min(0) Integer price,
         @Size(max = 255) String description,
         @URL String imageUrl
