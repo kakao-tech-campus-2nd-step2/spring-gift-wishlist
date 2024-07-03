ad92d5e3d262a5f78551476c12675641ee775545
Geon Kim <kky8586696@naver.com>
2024-07-03 15:17:24 +0900

    feat : ProtectedController 추가

---
+++ src/main/java/gift/controller/ProtectedController.java
@@ -0,0 +1,29 @@
+package gift.controller;
+
+import gift.model.User;
+import gift.model.UserRepository;
+import gift.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/protected")
+public class ProtectedController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<String> getProtectedResource(@RequestHeader("Authorization") String authHeader) {
+        String token = authHeader.replace("Bearer ", "");
+        if (userService.validateToken(token)) {
+            return ResponseEntity.ok("Protected resource accessed!");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token");
+        }
+    }
+}
