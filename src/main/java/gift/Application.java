edeeb0a12fe8c2b6858da363b77db52a06381340
pinkbeenyeti <rbgusdn12@gmail.com>
2024-07-03 23:07:23 +0900

    fix: 스키마 스크립트를 사용하도록 Application 클래스 수정

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -1,25 +1,13 @@
 package gift;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Autowired
-    JdbcTemplate jdbcTemplate;
-
-    @Override
-    public void run(String... args) throws Exception {
-        jdbcTemplate.execute("DROP TABLE products IF EXISTS");
-        jdbcTemplate.execute("CREATE TABLE products(id SERIAL PRIMARY KEY, name VARCHAR(255), price INT, imageurl VARCHAR(255))");
     }
 }
