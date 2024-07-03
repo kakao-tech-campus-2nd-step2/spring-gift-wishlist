f78012c82fd87f00d131424697e98fc37953af66
HaegyeongKim01 <haegyeong.kim01@gmail.com>
2024-07-03 15:06:35 +0900

    feat: 가격 필드 유효성 검사

--- src/main/java/gift/dto/ProductUpdateDto.java
+++ src/main/java/gift/dto/ProductUpdateDto.java
@@ -3,12 +3,14 @@ package gift.dto;
 import gift.validation.ValidName;
 import gift.vo.Product;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 public record ProductUpdateDto (
         Long id,
         @Size(max=15) @ValidName @NotEmpty(message = "상품명을 입력해주세요.")
         String name,
+        @Positive(message = "가격을 입력해 주세요(0보다 커야 합니다.)")
         int price,
         String imageUrl
 ) {
