f77ce4bfc33f4dad7edb24cbaae13131b955e1b7
geist9110 <geist9110@gmail.com>
2024-07-03 15:09:32 +0900

    feat(validation): Add MethodArgumentNotValidException error handler

---
+++ src/main/java/gift/ExceptionHandler/ValidationExcpetionHandler.java
@@ -0,0 +1,17 @@
+package gift.ExceptionHandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ValidationExcpetionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+    }
+}
