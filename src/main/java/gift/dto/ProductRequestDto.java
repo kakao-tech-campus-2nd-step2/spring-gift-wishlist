9344bf26732b707197e1267c9f25c2674d705fb8
wvssm <neoramiregi@gmail.com>
2024-07-03 14:18:21 +0900

    feat(ProductRequestDto): MD 동의에 따른 `카카오` 이름 포함 검증 추가

--- src/main/java/gift/dto/ProductRequestDto.java
+++ src/main/java/gift/dto/ProductRequestDto.java
@@ -9,6 +9,7 @@ public class ProductRequestDto {
     @NotBlank(message = "상품 이름은 공백일 수 없습니다.")
     @Size(max = 15, message = "상품 이름은 공백을 포함하여 최대 15자까지 입력할 수 있습니다.")
     @Pattern(regexp = "^[a-zA-Z0-9가-힣 ()\\[\\]+\\-&/_]*$", message = "상품 이름에 허용되지 않은 특수 문자가 포함되어 있습니다.")
+    @Pattern(regexp = "^(?!.*카카오).*$", message = "'카카오'가 포함된 상품 이름은 담당 MD와 협의한 후에 사용할 수 있습니다.")
     private String name;
     private int price;
     private String imageUrl;
