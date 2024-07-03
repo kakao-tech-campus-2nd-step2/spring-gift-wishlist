2565eae8000745663dec0cc6dbfffd1c0a19d8c8
GamJaDo <kim0615157@gmail.com>
2024-07-04 03:11:32 +0900

    fix(Product.java)
    충돌 수정

--- src/main/java/gift/model/Product.java
+++ src/main/java/gift/model/Product.java
@@ -1,7 +1,6 @@
 package gift.model;
 
 import java.beans.ConstructorProperties;
-
 import gift.exception.InvalidProductException;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
