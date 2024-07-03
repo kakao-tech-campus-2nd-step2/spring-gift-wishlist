5240ea4e189da75758d472620991dd6eb4a32700
nove1080 <skwpqjq@gmail.com>
2024-07-03 20:56:00 +0900

    feat: StringUtils containsAnySubstring() 추가

--- src/main/java/gift/utils/StringUtils.java
+++ src/main/java/gift/utils/StringUtils.java
@@ -6,6 +6,17 @@ import java.util.stream.Collectors;
 public abstract class StringUtils {
 
     /**
+     * 문자열과 문자열 집합을 전달받아, 집합에 포함된 어떤 문자열이라도 입력 문자열에 포함되어 있는지 검사합니다.
+     *
+     * @param input 검사할 문자열
+     * @param substrings 검사할 문자열 집합
+     * @return 집합에 포함된 어떤 문자열이라도 입력 문자열에 포함되어 있으면 true, 그렇지 않으면 false
+     */
+    public static boolean containsAnySubstring(String input, Set<String> substrings) {
+        return substrings.stream().anyMatch(input::contains);
+    }
+
+    /**
      * 문자열과 허용할 특수문자의 목록을 받아서 문자열이 허용할 특수문자 외의 특수문자가 있는지 검사합니다.
      *
      * @param input 문자열 입력
