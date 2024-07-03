c4982b223f5521ef19ccdcfd01b5ae9b23b08fde
us4c0d3 <jwseok312@gmail.com>
2024-07-03 22:04:33 +0900

    feat(Product): 상품 이름 validation exception message 수정

--- src/main/java/gift/Product.java
+++ src/main/java/gift/Product.java
@@ -11,7 +11,8 @@ public record Product(
 
     @NotBlank(message = "상품 이름은 비워둘 수 없습니다.")
     @Size(max = 15, message = "상품 이름은 공백을 포함하여 최대 15자까지 입력할 수 있습니다.")
-    @Pattern(regexp = "^[a-zA-Z가-힣0-9 ()\\[\\]+\\-&/_]*$", message = "특수문자는 ( ), [ ], +, -, &, /, _만 사용 가능합니다.")
+    @Pattern(regexp = "^[a-zA-Z가-힣0-9 ()\\[\\]+\\-&/_]*$",
+        message = "상품 이름은 특수문자는 ( ), [ ], +, -, &, /, _만 사용 가능하며, 한글, 영어, 숫자만 입력할 수 있습니다.")
     String name,
 
     @Positive Integer price,
