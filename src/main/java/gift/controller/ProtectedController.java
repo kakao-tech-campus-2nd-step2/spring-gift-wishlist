7a8bcac162544d515e72c755a8af3bdc73eeeada
Geon Kim <kky8586696@naver.com>
2024-07-03 19:32:50 +0900

    feat : Controller 위시리스트에 추가하는 메서드추가

--- src/main/java/gift/controller/ProtectedController.java
+++ src/main/java/gift/controller/ProtectedController.java
@@ -1,29 +1,57 @@
 package gift.controller;
 
-import gift.model.User;
-import gift.model.UserRepository;
+import gift.model.*;
+import gift.service.GiftService;
 import gift.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/protected")
 public class ProtectedController {
 
-    @Autowired
+
     private UserService userService;
 
-    @GetMapping
-    public ResponseEntity<String> getProtectedResource(@RequestHeader("Authorization") String authHeader) {
+    private GiftService giftService;
+
+    @Autowired
+    public ProtectedController(UserService userService, GiftService giftService) {
+        this.userService = userService;
+        this.giftService = giftService;
+    }
+    @GetMapping("/gifts")
+    public ResponseEntity<List<GiftResponse>> getGiftList(@RequestHeader("Authorization") String authHeader) {
         String token = authHeader.replace("Bearer ", "").trim();
         if (userService.validateToken(token)) {
-            return ResponseEntity.ok("Protected resource accessed!");
+            List<GiftResponse> gifts = giftService.getAllGifts();
+            return ResponseEntity.ok(gifts);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
+    }
+    @PostMapping("/gifts/{giftId}")
+    public ResponseEntity<String> addGiftToCart(
+            @RequestHeader("Authorization") String authHeader,
+            @PathVariable Long giftId) {
+        String token = authHeader.replace("Bearer ", "").trim();
+        if (userService.validateToken(token)) {
+            Optional<User> user = userService.getUserByToken(token);
+            if (user.isPresent()) {
+                userService.addGiftToUser(user.get().getId(), giftId);
+                return ResponseEntity.ok("위시리스트에 상품이 추가되었습니다.");
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("유저정보가 없습니다.");
+            }
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token");
         }
     }
+
+
 }
