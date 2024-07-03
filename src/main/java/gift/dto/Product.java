84ec2d4f07a65d9096c61d029a2448e7f29a26f2
GitJIHO <jiho99322@gmail.com>
2024-07-03 14:11:05 +0900

    refactor: NoKakao 어노테이션 제거 후 Pattern 추가

--- src/main/java/gift/dto/Product.java
+++ src/main/java/gift/dto/Product.java
@@ -1,7 +1,6 @@
 package gift.dto;
 
 
-import gift.validation.NoKakao;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
@@ -10,7 +9,7 @@ public class Product {
     private Long id;
     @Size(max = 15, message = "상품 이름은 공백을 포함하여 최대 15자까지 입력할 수 있습니다.")
     @Pattern(regexp = "^[a-zA-Z0-9가-힣 ()\\[\\]+\\-&/_]*$", message = "(),[],+,-,&,/,_를 제외한 특수 문자 사용은 불가능합니다.")
-    @NoKakao
+    @Pattern(regexp = "^(?!.*카카오).*$", message = "카카오가 포함된 문구는 담당 MD와 협의한 경우에만 사용할 수 있습니다.")
     private String name;
     private int price;
     private String img;
