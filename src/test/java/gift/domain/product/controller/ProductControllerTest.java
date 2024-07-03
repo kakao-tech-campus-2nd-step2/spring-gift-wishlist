815f1981b5d9bca514264e690242a9a35f028a72
hyunseo Jeong <wjdgustj2877@naver.com>
2024-07-04 01:08:01 +0900

    test: 상품 가격이 범위를 벗어나는 경우 테스트

--- src/test/java/gift/domain/product/controller/ProductControllerTest.java
+++ src/test/java/gift/domain/product/controller/ProductControllerTest.java
@@ -33,10 +33,22 @@ class ProductControllerTest {
 
     @Test
     @DisplayName("상품 생성 시 이름이 NULL인 경우")
-    void create_fail_type_error() throws Exception {
+    void create_fail_null_error() throws Exception {
         mockMvc.perform(post("/products")
             .contentType(MediaType.APPLICATION_FORM_URLENCODED)
             .param("price", "3500")
+            .param("imageUrl", "https://image.istarbucks.co.kr/upload/store/skuimg/2023/09/[9300000004823]_20230911131337469.jpg"))
+            .andExpect(status().isFound())
+            .andExpect(redirectedUrl("/products/new"))
+            .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    @DisplayName("상품 생성 시 가격이 범위를 초과한 경우")
+    void create_fail_price_range_error() throws Exception {
+        mockMvc.perform(post("/products")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("price", "-123")
             .param("imageUrl", "https://image.istarbucks.co.kr/upload/store/skuimg/2023/09/[9300000004823]_20230911131337469.jpg"))
             .andExpect(status().isFound())
             .andExpect(redirectedUrl("/products/new"))
@@ -58,13 +70,25 @@ class ProductControllerTest {
 
     @Test
     @DisplayName("상품 수정 시 이름이 NULL인 경우")
-    void update_fail_type_error() throws Exception {
+    void update_fail_null_error() throws Exception {
         mockMvc.perform(put("/products/1")
             .contentType(MediaType.APPLICATION_FORM_URLENCODED)
             .param("price", "3500")
             .param("imageUrl", "https://image.istarbucks.co.kr/upload/store/skuimg/2023/09/[9300000004823]_20230911131337469.jpg"))
             .andExpect(status().isFound())
             .andExpect(redirectedUrl("/products/edit/1"))
+            .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    @DisplayName("상품 수정 시 가격이 범위를 초과한 경우")
+    void update_fail_price_range_error() throws Exception {
+        mockMvc.perform(post("/products")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("price", "-123")
+                .param("imageUrl", "https://image.istarbucks.co.kr/upload/store/skuimg/2023/09/[9300000004823]_20230911131337469.jpg"))
+            .andExpect(status().isFound())
+            .andExpect(redirectedUrl("/products/new"))
             .andExpect(status().is3xxRedirection());
     }
 }
