0d24bf73a8fe87c8fdc2decc39e6d30fcead6fe8
UiChan Lee <junglkls1234@gmail.com>
2024-07-03 20:36:33 +0900

    Fix: 오류 컨트롤러

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -3,7 +3,6 @@ package gift;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.http.HttpStatus;
@@ -13,9 +12,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
 
 @RequestMapping("/api/products")
 @Controller
@@ -40,11 +36,13 @@ public class ProductController {
     }
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleConstraintViolationException(MethodArgumentNotValidException ex) {
-        FieldError fieldError = ex.getBindingResult().getFieldError();
-        String errorMessage = fieldError.getDefaultMessage();
-        Locale currentLocale = LocaleContextHolder.getLocale();
-        String localizedErrorMessage = messageSource.getMessage(errorMessage, null, currentLocale);
-        return ResponseEntity.badRequest().body(localizedErrorMessage);
+        FieldError firstError = (FieldError) ex.getBindingResult().getAllErrors().stream().findFirst().orElse(null);
+        String errorMessage = "Unknown error";
+        if (firstError != null) {
+            String fieldName = firstError.getField();
+            errorMessage = firstError.getDefaultMessage();
+        }
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 
     @PutMapping("/{id}")
