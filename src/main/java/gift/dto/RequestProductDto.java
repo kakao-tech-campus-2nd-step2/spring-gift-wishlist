ad0c4430ceb4e35c2bcc7ad544750d4ee465add4
sim-mer <gyu2781@gmail.com>
2024-07-03 11:14:40 +0900

    feat() : 상품 이름에 '카카오' 필터링 추가

--- src/main/java/gift/dto/RequestProductDto.java
+++ src/main/java/gift/dto/RequestProductDto.java
@@ -7,8 +7,13 @@ import org.hibernate.validator.constraints.URL;
 public record RequestProductDto(
         @NotBlank
         @Size(max = 15)
-        @Pattern(regexp = "^[a-zA-Z0-9ㄱ-ㅎ가-힣 ()\\[\\]+\\-&/_]*$",
+        @Pattern(
+                regexp = "^[a-zA-Z0-9ㄱ-ㅎ가-힣 ()\\[\\]+\\-&/_]*$",
                 message = "오직 문자, 공백 그리고 특수문자 (),[],+,&,-,/,_만 허용됩니다."
+        )
+        @Pattern(
+                regexp = "(?!.*카카오).*",
+                message = "카카오가 포함된 문구는 담당 MD와 협의한 경우에만 사용할 수 있습니다."
         )
         String name,
         @NotNull @Min(0) Integer price,
