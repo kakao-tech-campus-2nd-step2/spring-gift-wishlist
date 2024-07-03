98bf44807a91f08b36110d076b33ecc40af2af2a
lja3723 <lja3723@gmail.com>
2024-07-03 21:43:09 +0900

    fix: allow usage of hangul
    한글을 이름으로 할 수 있도록 수정

--- src/main/java/gift/domain/product/dto/ProductRequestDto.java
+++ src/main/java/gift/domain/product/dto/ProductRequestDto.java
@@ -9,7 +9,7 @@ public record ProductRequestDto(
     @NotNull
     @Size(min = 1, max = 15,
         message = "상품 이름은 공백을 포함하여 최대 15자까지 입력할 수 있습니다.")
-    @Pattern(regexp = "^[\\w\\s\\[\\]()\\+\\-&/_]*$",
+    @Pattern(regexp = "^[\\w\\s\\[\\]()\\+\\-&/_가-힣]*$",
         message = "사용 가능한 특수 문자는 (), [], +, -, &, /, _ 입니다.")
     @Pattern(regexp = "^(?!.*카카오).*$",
         message = "'카카오'가 포함된 문구는 사용할 수 없습니다.")
