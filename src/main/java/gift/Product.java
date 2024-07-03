a39cb10748ce227719f693ec754037c2d5cdb08b
박준형 <jun@bagjunhyeong-ui-MacBookAir.local>
2024-07-03 15:22:20 +0900

    fix : 한글이 허용되게끔 어노테이션의 정규식 수정

--- src/main/java/gift/Product.java
+++ src/main/java/gift/Product.java
@@ -8,7 +8,7 @@ public record Product(
     long id,
 
     @Size(max=15, message="Name is too long!")
-    @Pattern(regexp = "^[a-zA-Z0-9 ()\\[\\]+\\-\\&\\/\\_]*$", message = "Name has invalid chracter")
+    @Pattern(regexp = "^[a-zA-Z0-9 ()\\[\\]+\\-\\&\\/\\_가-힣]*$", message = "Name has invalid chracter")
     String name,
     int price,
     String imageUrl
