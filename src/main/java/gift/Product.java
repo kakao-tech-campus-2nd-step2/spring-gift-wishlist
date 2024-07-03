7616d056cc4cfa47367acd0b737a0ed3f716f36a
seojiu <seojiu5598@gmail.com>
2024-07-03 15:14:32 +0900

    fix(Product.java): 오류 메세지 수정

--- src/main/java/gift/Product.java
+++ src/main/java/gift/Product.java
@@ -4,16 +4,15 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 
 public class Product {
     private Long id;
     @NotBlank(message = "이름은 필수 입력 값입니다.")
-    @Size(max=15, message = "공백을 포함하여 최대 15자까지 입력 가능합니다.")
+    @Size(max=15, message = "이름은 공백을 포함하여 최대 15자까지 입력 가능합니다.")
     @Pattern(regexp = "^[a-zA-Z0-9가-힣_()+&/ ]*$",
-            message = "특수 문자는 (, ), [, ], +, -, &, /, _ 만 사용 가능합니다.")
+            message = "이름에 특수 문자는 (, ), [, ], +, -, &, /, _ 만 사용 가능합니다.")
     private String name;
-    @Min(value = 0, message = "0이상의 가격을 입력하세요.")
+    @Min(value = 0, message = "가격은 0원 이상의 값만 가능합니다.")
     private int price;
     private String imageUrl;
 
