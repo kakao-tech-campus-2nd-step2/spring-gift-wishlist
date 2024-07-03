bd8f5461e9ba9504ccdbc6c9453fe6a276961779
Geon Kim <kky8586696@naver.com>
2024-07-03 15:12:00 +0900

    feat : 유저 정보를 담는 User 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,15 @@
 drop table gift if exists;
+drop table users if exists;
 create table gift(
     `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
     `name` varchar(255) not null,
     `price` INT not null,
     `imageUrl` varchar(255) not null
 
+);
+CREATE TABLE users (
+    `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `token` VARCHAR(255)
 );
