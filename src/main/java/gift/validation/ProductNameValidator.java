4907a82cc84962a0a4e82f90206e39ed866d1058
dong_yxxn <g0326kdy@gmail.com>
2024-07-03 17:00:17 +0900

    refactor(ProductNameValidator): Exception 던질 때 product 객체도 포함되도록 수정

--- src/main/java/gift/validation/ProductNameValidator.java
+++ src/main/java/gift/validation/ProductNameValidator.java
@@ -1,10 +1,11 @@
 package gift.validation;
 
 import gift.exception.NotValidProductNameException;
+import gift.model.Product;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-public class ProductNameValidator implements ConstraintValidator<ValidProductName, String> {
+public class ProductNameValidator implements ConstraintValidator<ValidProductName, Product> {
 
     private static final String VALID_PATTERN = "^[A-Za-z0-9 ()\\[\\]+\\-&/_가-힣]+$";
 
@@ -12,16 +13,17 @@ public class ProductNameValidator implements ConstraintValidator<ValidProductNam
     public void initialize(ValidProductName constraintAnnotation) {
     }
     @Override
-    public boolean isValid(String value, ConstraintValidatorContext context) {
-        if(value.length()>15){
-            throw new NotValidProductNameException("이름은 공백 포함 최대 15자입니다.");
-        }
+    public boolean isValid(Product product, ConstraintValidatorContext context) {
+        String value = product.getName();
 
+        if(value.length()>15){
+            throw new NotValidProductNameException("이름은 공백 포함 최대 15자입니다.", product);
+        }
         if (value.contains("카카오")) {
-            throw new NotValidProductNameException("'카카오'가 포함된 문구는 담당 MD와 협의한 경우만 가능합니다.");
+            throw new NotValidProductNameException("'카카오'가 포함된 문구는 담당 MD와 협의한 경우만 가능합니다.", product);
         }
         if(!value.matches(VALID_PATTERN)){
-            throw new NotValidProductNameException("특수문자는 (),[],+,-,&,/,_만 사용가능합니다.");
+            throw new NotValidProductNameException("특수문자는 (),[],+,-,&,/,_만 사용가능합니다.", product);
         }
         return true;
     }
