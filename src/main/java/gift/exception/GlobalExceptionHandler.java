70122a016007cfa065d26d9be6e8f69b9802fc54
hyunseo Jeong <wjdgustj2877@naver.com>
2024-07-04 01:19:02 +0900

    feat: 상품 가격에 int형으로 치환 불가능한 값이 입력된 경우 처리

--- src/main/java/gift/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -1,6 +1,10 @@
 package gift.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.Locale;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,6 +16,12 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private final MessageSource messageSource;
+
+    public GlobalExceptionHandler(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public String handleMethodArgumentNotValidException(MethodArgumentNotValidException ex,
                                                         RedirectAttributes redirectAttributes,
@@ -19,8 +29,10 @@ public class GlobalExceptionHandler {
                                                         HttpServletRequest request)
     {
         BindingResult bindingResult = ex.getBindingResult();
+        Locale locale = LocaleContextHolder.getLocale();
         for (FieldError fieldError : bindingResult.getFieldErrors()) {
-            redirectAttributes.addFlashAttribute(fieldError.getField() + "Error", fieldError.getDefaultMessage());
+            String errorMessage = messageSource.getMessage(fieldError, locale);
+            redirectAttributes.addFlashAttribute(fieldError.getField() + "Error", errorMessage);
         }
 
         String redirectUrl = "/products/new";
