d3f17e528783a7470e98e232ff74561accc22864
Geon Kim <kky8586696@naver.com>
2024-07-03 15:13:34 +0900

    feat : UserService 추가

---
+++ src/main/java/gift/service/UserService.java
@@ -0,0 +1,39 @@
+package gift.service;
+import gift.model.User;
+import gift.model.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    private Map<String, String> tokenStore = new ConcurrentHashMap<>();
+    public boolean register(User user) {
+        if (userRepository.findByEmail(user.getEmail()).isEmpty()) {
+            userRepository.save(user);
+            return true;
+        }
+        return false;
+    }
+
+    public Optional<String> login(String email, String password) {
+        Optional<User> user = userRepository.findByEmail(email);
+        if (user.isPresent() && user.get().getPassword().equals(password)) {
+            String token = UUID.randomUUID().toString();
+            tokenStore.put(token, email);
+            return Optional.of(token);
+        }
+        return Optional.empty();
+    }
+
+    public boolean validateToken(String token) {
+        return tokenStore.containsKey(token);
+    }
+}
