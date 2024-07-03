374d07045603d8418ea6c61539ef341a48f5dee3
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-03 12:21:18 +0900

    feat : @valid 추가

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -3,6 +3,12 @@ package gift.controller;
 
 import gift.domain.Product;
 import gift.service.ProductService;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Valid;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -33,7 +39,7 @@ public class ProductController {
 
     // 상품 추가 후 홈으로 이동
     @PostMapping("/add")
-    public String addProduct(@ModelAttribute Product product) {
+    public String addProduct(@Valid @ModelAttribute Product product) {
         productService.createProduct(product);
         return "redirect:/products";
     }
@@ -47,7 +53,7 @@ public class ProductController {
 
     // 등록된 상품을 수정하는 기능
     @PutMapping("/edit/{id}")
-    public String editProduct(@PathVariable("id") Long id, @ModelAttribute Product product) {
+    public String editProduct(@PathVariable("id") Long id, @Valid @ModelAttribute Product product) {
         product.setId(id);
         productService.updateProduct(id, product);
         return "redirect:/products";
@@ -58,5 +64,14 @@ public class ProductController {
     public String deleteProduct(@PathVariable Long id) {
         productService.deleteProduct(id);
         return "redirect:/products";
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(ConstraintViolationException e) {
+        Map<String, String> error = e.getConstraintViolations().stream()
+            .collect(Collectors.toMap(
+                violation -> violation.getPropertyPath().toString(), violation -> violation.getMessage()
+            ));
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
