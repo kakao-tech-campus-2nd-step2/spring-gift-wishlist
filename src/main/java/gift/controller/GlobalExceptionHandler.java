d6c58874c42055496d42519309de474c32383cb2
SeongHun <tjdgns5506@gmail.com>
2024-07-03 12:50:21 +0900

    feat: MethodArgumentNotValidException 처리위한 ExceptionHandler구현, ProblemDetail 반환 - 발생한 필드 에러의 메세지를 추출해서 ProblemDetail property 에 set하여 api 사용자에게 구체적인 문제사항 반환

--- src/main/java/gift/controller/GlobalExceptionHandler.java
+++ src/main/java/gift/controller/GlobalExceptionHandler.java
@@ -1,18 +1,30 @@
 package gift.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleValidationExceptions() {
-        return "version-SSR/product-error";
+    public ProblemDetail handleValidationExceptions(MethodArgumentNotValidException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        problemDetail.setTitle("상품 유효성 검사 실패");
+
+        List<String> problemMessageList = e.getBindingResult().getFieldErrors().stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        problemDetail.setProperty("problemMessages", problemMessageList);
+
+        return problemDetail;
     }
 
 }
