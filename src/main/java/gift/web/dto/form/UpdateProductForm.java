7ed046cb2b3788d42d407fc3383a4c5c19834b25
nove1080 <skwpqjq@gmail.com>
2024-07-03 23:25:11 +0900

    rename: UpdateProductFormDto -> UpdateProductForm

--- src/main/java/gift/web/dto/form/UpdateProductForm.java
+++ src/main/java/gift/web/dto/form/UpdateProductForm.java
@@ -3,22 +3,22 @@ package gift.web.dto.form;
 import gift.domain.Product;
 import java.net.URL;
 
-public class UpdateProductFormDto {
+public class UpdateProductForm {
 
     private final Long id;
     private final String name;
     private final Integer price;
     private final URL imageUrl;
 
-    private UpdateProductFormDto(Long id, String name, Integer price, URL imageUrl) {
+    private UpdateProductForm(Long id, String name, Integer price, URL imageUrl) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
     }
 
-    public static UpdateProductFormDto fromEntity(Product product) {
-        return new UpdateProductFormDto(product.getId(), product.getName(), product.getPrice(), product.getImageUrl());
+    public static UpdateProductForm fromEntity(Product product) {
+        return new UpdateProductForm(product.getId(), product.getName(), product.getPrice(), product.getImageUrl());
     }
 
     public Long getId() {
