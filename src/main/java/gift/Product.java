1d7053658655ae40f681bb88a820c6bda87ec038
us4c0d3 <jwseok312@gmail.com>
2024-07-03 21:59:47 +0900

    feat(Product): Validation Exception Message 추가

--- src/main/java/gift/Product.java
+++ src/main/java/gift/Product.java
@@ -9,9 +9,9 @@ import jakarta.validation.constraints.Size;
 public record Product(
     @NotNull Long id,
 
-    @NotBlank
-    @Size(max = 15)
-    @Pattern(regexp = "^[a-zA-Z가-힣0-9 ()\\[\\]+\\-&/_]*$")
+    @NotBlank(message = "상품 이름은 비워둘 수 없습니다.")
+    @Size(max = 15, message = "상품 이름은 공백을 포함하여 최대 15자까지 입력할 수 있습니다.")
+    @Pattern(regexp = "^[a-zA-Z가-힣0-9 ()\\[\\]+\\-&/_]*$", message = "특수문자는 ( ), [ ], +, -, &, /, _만 사용 가능합니다.")
     String name,
 
     @Positive Integer price,
