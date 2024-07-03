a4b79c435b5bddfec0abf7f8b39e55eebd8af76c
Gyumin Bae <bgmsound0528@gmail.com>
2024-07-03 14:40:41 +0900

    style: 주석 제거

--- src/main/java/gift/product/restapi/validator/NotContainingKaKao.java
+++ src/main/java/gift/product/restapi/validator/NotContainingKaKao.java
@@ -7,11 +7,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.FIELD) // 1
-@Retention(RetentionPolicy.RUNTIME) // 2
-@Constraint(validatedBy = NotContainingKaKaoValidator.class) // 3
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = NotContainingKaKaoValidator.class)
 public @interface NotContainingKaKao {
-    String message() default "\"카카오\"가 포함된 문구는 담당 MD와 협의한 경우에만 사용할 수 있습니다."; // 4
+    String message() default "\"카카오\"가 포함된 문구는 담당 MD와 협의한 경우에만 사용할 수 있습니다.";
     Class[] groups() default {};
     Class[] payload() default {};
 }
