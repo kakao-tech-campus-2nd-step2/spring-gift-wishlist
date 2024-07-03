0783076b740c827d1b59ef0d32f1360bd710b6fa
J-1ac <tkdwns26@naver.com>
2024-07-03 22:25:44 +0900

    feat: ProductDto 상품 이름 유효성 검사 추가

--- src/main/java/gift/domain/model/ProductDto.java
+++ src/main/java/gift/domain/model/ProductDto.java
@@ -1,10 +1,18 @@
 package gift.domain.model;
 
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public class ProductDto {
 
     Long id;
+
+    @Size(max = 15, message = "상품 이름은 최대 15자까지 입력할 수 있습니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣\\s()\\[\\]+\\-&/_]*$", message = "허용되지 않은 특수문자가 포함되어 있습니다.")
     String name;
+
     Long price;
+
     String imageUrl;
 
     public ProductDto(Long id, String name, Long price, String imageUrl) {
