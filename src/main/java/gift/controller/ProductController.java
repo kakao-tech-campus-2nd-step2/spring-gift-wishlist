596e2c3a22e4fd7bbeb9417ee67a0835c2d4367b
J-1ac <tkdwns26@naver.com>
2024-07-04 02:58:51 +0900

    feat: 상품 추가, 수정 시 유효성 검사 추가

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 @RequestMapping("/api/products")
+@Validated
 public class ProductController {
 
     private final ProductService productService;
@@ -30,7 +32,7 @@ public class ProductController {
         model.addAttribute("products", products);
     }
 
-//    id로 상품 조회
+    //    id로 상품 조회
     @GetMapping()
     @ResponseStatus(HttpStatus.OK)
     public void getProduct(@RequestParam Long id, Model model) {
@@ -42,7 +44,7 @@ public class ProductController {
         }
     }
 
-//    전체 상품 조회
+    //    전체 상품 조회
     @GetMapping("/all")
     public String getAllProduct(Model model) {
         try {
@@ -54,7 +56,7 @@ public class ProductController {
         return "index";
     }
 
-//    상품 추가
+    //    상품 추가
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
     public void addProduct(@RequestBody ProductDto productDto, Model model) {
@@ -66,7 +68,7 @@ public class ProductController {
         }
     }
 
-//    상품 수정
+    //    상품 수정
     @PutMapping()
     @ResponseStatus(HttpStatus.OK)
     public void updateProduct(@RequestBody ProductDto productDto, Model model) {
@@ -78,7 +80,7 @@ public class ProductController {
         }
     }
 
-//    상품 삭제
+    //    상품 삭제
     @DeleteMapping()
     @ResponseStatus(HttpStatus.OK)
     public void deleteProduct(@RequestParam Long id, Model model) {
