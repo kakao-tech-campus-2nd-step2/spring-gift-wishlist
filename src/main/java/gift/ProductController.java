c0ac5c1b9cded805fb53bcd468039c441eaf8c21
doyooon <ian661087@gmail.com>
2024-07-03 14:08:51 +0900

    refactor ProductController.java, home.html :  POST, GET으로 update, delete 요청을 보내던 것을 PUT, DELETE로 보내도록 수정

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -1,12 +1,17 @@
 package gift;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -25,23 +30,27 @@ public class ProductController {
         return "home"; // Thymeleaf 템플릿 이름
     }
 
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public ResponseEntity<String> handleNullPointerException(IllegalArgumentException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
     @PostMapping("/post")
     public String createProduct(@ModelAttribute Product newProduct){
         productRepository.save(newProduct);
         return "redirect:/products";
     }
 
-    @PostMapping ("/update")
+    @PutMapping("/update")
     public String updateProduct(@ModelAttribute Product changeProduct){
         productRepository.update(changeProduct);
 
         return "redirect:/products";
     }
 
-    @GetMapping ("/delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public String deleteProduct(@PathVariable Long id){
         productRepository.deleteById(id);
-
         return "redirect:/products";
     }
 
