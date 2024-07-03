ad93ae629d2f118ea0596546c9dad46f5a057831
seoyoung-dev <seoyoung.dev@gmail.com>
2024-07-03 21:21:13 +0900

    style: 사용되지 않는 import문 삭제

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -7,13 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.PutMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/products")
