549ba304317a958825d84d2e4ced436523564bb6
seolbin96 <jsb3038@jnu.ac.kr>
2024-07-03 15:29:45 +0900

    feat : 어노테이션 메세지 수정

--- src/main/java/gift/model/Product.java
+++ src/main/java/gift/model/Product.java
@@ -7,9 +7,9 @@ import javax.validation.constraints.Size;
 public record Product(
         Long id,
 
-        @NotBlank(message = "이름은 필수 항목입니다.")
-        @Size(max = 15, message = "이름은 15자 이내여야 합니다.")
-        @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9\\s\\(\\)\\[\\]+&\\-/_]*$", message = "이름에 유효하지 않은 문자가 포함되어 있습니다.")
+        @NotBlank(message = "상품 이름을 입력해주세요.")
+        @Size(max = 15, message = "상품 이름은 15자 이내여야 합니다.")
+        @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9\\s\\(\\)\\[\\]+&\\-/_]*$", message = "상품 이름에 유효하지 않은 문자가 포함되어 있습니다.")
         String name,
 
         int price,
