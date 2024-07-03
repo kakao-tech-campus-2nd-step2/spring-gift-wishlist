342bbbaea694d4fc3831fa3c31aea3a7ad5da1c1
JO SEOYOUNG <seoyoung.dev@gmail.com>
2024-07-04 01:14:06 +0900

    강원대 BE_조서영 2주차 과제 (1단계) (#174)
    * docs: step1 기능 구현 리스트
    
    * feat: spring-boot-starter-validation 의존성 추가
    
    * feat: Product 클래스에 유효성 검사 어노테이션 추가
    
    * feat: ProductController 수정
    
    * style: 사용되지 않는 import문 삭제
    
    * feat: NoKakao 커스텀 어노테이션 생성 및 적용
    
    * feat: PUT 요청에 대한 유효성 검증
    
    * docs: step1 개발 완료된 내용 반영

--- src/main/java/gift/Product.java
+++ src/main/java/gift/Product.java
@@ -1,7 +1,16 @@
 package gift;
 
+import gift.exception.NoKakao;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+
 public class Product {
     private Long id;
+
+    @NotEmpty(message = "상품 이름은 필수 입력 값입니다.")
+    @jakarta.validation.constraints.Size(max = 15, message = "상품 이름은 공백 포함 15자 이하로 입력해야 합니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣\\(\\)\\[\\]\\+\\-&/_ ]*$", message = "[ ] ( ), [ ], +, -, &, /, _ 의 특수문자만 사용가능합니다.")
+    @NoKakao
     private String name;
     private double price;
     private String imageUrl;
