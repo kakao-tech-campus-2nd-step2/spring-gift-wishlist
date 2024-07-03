b239e185253fd21ea42670b97450118cc51c9847
anacnoda77 <sm1108shin@naver.com>
2024-07-03 16:45:14 +0900

    refactor: 검증 오류 메시지 수정

--- src/main/java/gift/form/ProductAddForm.java
+++ src/main/java/gift/form/ProductAddForm.java
@@ -11,7 +11,7 @@ public class ProductAddForm {
 
     @NotBlank(message = "상품명을 입력해주세요.")
     @Length(min=1, max=15, message = "1~15자 사이로 입력해주세요.")
-    @Pattern(regexp = "^[a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣0-9()+&/_ \\[\\]-]*$", message = "특수 문자는 '(, ), [, ], +, -, &, /, -' 만 입력 가능합니다.")
+    @Pattern(regexp = "^[a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣0-9()+&/_ \\[\\]-]*$", message = "상품명에 특수 문자는 '(, ), [, ], +, -, &, /, -' 만 입력 가능합니다.")
     @NoKakao
     private String name;
 
