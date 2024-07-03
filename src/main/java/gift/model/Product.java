6af0a5cb61df4e5598d9c1e1734fd31b937e797f
Jeje <jjh4450jjh4450@gmail.com>
2024-07-04 01:30:41 +0900

    feat(Product.java): 검증 어노테이션을 Product model에 추가함

--- src/main/java/gift/model/Product.java
+++ src/main/java/gift/model/Product.java
@@ -1,5 +1,25 @@
 package gift.model;
 
-public record Product(long id, String name, long price, String imageUrl) {
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
-}
+/**
+ * 제품의 세부 사항을 나타내는 클래스. ID, 이름, 가격, 이미지 URL을 포함한다.
+ */
+public record Product(
+    @NotNull
+    long id,
+
+    @NotNull
+    @Size(min = 1, max = 15)
+    @Pattern(regexp = "^[a-zA-Z0-9\\s\\(\\)\\[\\]\\+\\-\\&\\/\\_]*$")
+    String name,
+
+    @NotNull
+    long price,
+
+    @NotNull
+    @Pattern(regexp = "https?://.*")
+    String imageUrl
+) {}
