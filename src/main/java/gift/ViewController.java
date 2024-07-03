1f09879bb10d155aa28f24568af5ca600a45f782
us4c0d3 <jwseok312@gmail.com>
2024-07-03 21:31:02 +0900

    refactor(product_list): product_list.html에서 상품 삭제를 ProductController에 직접 요청하도록 수정

--- src/main/java/gift/ViewController.java
+++ src/main/java/gift/ViewController.java
@@ -3,7 +3,6 @@ package gift;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,9 +37,4 @@ public class ViewController {
         return "product_edit_form";
     }
 
-    @DeleteMapping("/delete/{id}")
-    public String deleteProduct(@PathVariable Long id) {
-        productService.deleteProduct(id);
-        return "redirect:/products";
-    }
 }
