eb981ef3834260ef839991cc6c94e7a709b6e96c
pinkbeenyeti <rbgusdn12@gmail.com>
2024-07-03 23:17:16 +0900

    feat: 전역에서 발생한 예외를 처리하는 컨트롤러 글자 수, 특수문자, 카카오 문구 포함 시 발생하는 예외를 검출하고 응답한다. 해당 예외 발생 시 400 상태 코드와 에러 메시지를 클라이언트에 전송한다.

--- src/main/java/gift/controller/GlobalExceptionHandler.java
+++ src/main/java/gift/controller/GlobalExceptionHandler.java
@@ -1,4 +1,44 @@
 package gift.controller;
 
-public class GlobalExceptrionHandler {
+import gift.exceptions.KakaoContainException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(KakaoContainException.class)
+    public ResponseEntity<Map<String, String>> handleKakaoContainException(KakaoContainException kex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", kex.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleInvalidNameExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        // 첫 번째 오류 메시지를 사용
+        String firstErrorMessage = errors.values().iterator().next();
+        Map<String, String> response = new HashMap<>();
+        response.put("message", firstErrorMessage);
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
 }
+
+
