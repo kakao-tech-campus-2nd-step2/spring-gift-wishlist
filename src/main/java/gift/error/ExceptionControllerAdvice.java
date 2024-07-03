9f13f9a6da0ef7e8bc643923c8eaac69cb52fff4
JunHyeongChoi <ajfeh22@gmail.com>
2024-07-03 13:32:49 +0900

    feat: 전역 예외 처리 추가

--- src/main/java/gift/error/ExceptionControllerAdvice.java
+++ src/main/java/gift/error/ExceptionControllerAdvice.java
@@ -1,5 +1,6 @@
 package gift.error;
 
+import gift.error.exception.ProductNotFoundException;
 import gift.error.exception.ValidationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,9 +10,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ExceptionControllerAdvice {
 
     @ExceptionHandler(ValidationException.class)
-    public ResponseEntity<ErrorResponse> validation(ValidationException e) {
+    public ResponseEntity<ErrorResponse> validate(ValidationException e) {
         return ResponseEntity.badRequest()
             .body(new ErrorResponse(e.getMessage(), e.getErrors()));
+    }
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<ErrorResponse> notfound(ProductNotFoundException e) {
+        return ResponseEntity.badRequest()
+            .body(new ErrorResponse(e.getMessage(), null));
     }
 
 }
