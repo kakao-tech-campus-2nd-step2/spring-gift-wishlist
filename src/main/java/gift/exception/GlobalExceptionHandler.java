fc02541db62b8d569d64623cd6e42c448db1576e
GitJIHO <jiho99322@gmail.com>
2024-07-03 14:51:32 +0900

    refactor: HashMap대신 ErrorDTO 생성 후 이를 이용해 반환하는 방식으로 변경 및 조건에 맞게 리팩토링

--- src/main/java/gift/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -1,27 +1,41 @@
 package gift.exception;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(
-        MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<List<ErrorDTO>> handleValidationExceptions(
+        MethodArgumentNotValidException exception) {
+        List<ErrorDTO> fieldErrorDTOList = createErrorDTOList(exception);
+        return new ResponseEntity<>(fieldErrorDTOList, HttpStatus.BAD_REQUEST);
+    }
+
+    private List<ErrorDTO> createErrorDTOList(MethodArgumentNotValidException exception) {
+        List<ErrorDTO> fieldErrorDTOList = new ArrayList<>();
+
+        BindingResult bindingResult = exception.getBindingResult();
+        List<ObjectError> allErrors = bindingResult.getAllErrors();
+
+        for (ObjectError objectError : allErrors) {
+            FieldError fieldError = (FieldError) objectError;
+            String fieldName = fieldError.getField();
+            String errorMessage = fieldError.getDefaultMessage();
+            ErrorDTO errorDTO = new ErrorDTO(fieldName, errorMessage);
+            fieldErrorDTOList.add(errorDTO);
+        }
+
+        return fieldErrorDTOList;
     }
 
     @ExceptionHandler(RuntimeException.class)
