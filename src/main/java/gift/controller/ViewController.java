50213ac2b796420a5fcd349883d74fcfb0a8f648
Kangboom <jkl6147@naver.com>
2024-07-03 12:46:00 +0900

    feat(ViewController): add ExceptionHandler to ViewController

--- src/main/java/gift/controller/ViewController.java
+++ src/main/java/gift/controller/ViewController.java
@@ -1,22 +1,31 @@
 package gift.controller;
 
 
+import gift.ExceptionService;
+import gift.NameException;
 import gift.Product;
 import gift.ProductRepository;
 import gift.ProductDto;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class ViewController {
     private final ProductRepository productRepository;
-    public ViewController(ProductRepository productRepository) {
+    private final ExceptionService exceptionService;
+    public ViewController(ProductRepository productRepository, ExceptionService exceptionService) {
         this.productRepository = productRepository;
+        this.exceptionService = exceptionService;
     }
     @GetMapping("/")
     public String index(Model model){
@@ -32,6 +41,7 @@ public class ViewController {
 
     @PostMapping("/create-product")
     public String create(@ModelAttribute ProductDto productDto){
+        exceptionService.findNameException(productDto.getName());
         productRepository.save(productDto);
 
         return "redirect:/";
@@ -45,6 +55,7 @@ public class ViewController {
 
     @PostMapping("/update-product/{id}")
     public String update(@PathVariable("id") Long id, @ModelAttribute ProductDto productDto){
+        exceptionService.findNameException(productDto.getName());
         productRepository.update(id, productDto);
         return "redirect:/";
     }
@@ -55,4 +66,10 @@ public class ViewController {
         return "redirect:/";
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = NameException.class)
+    public String handleNameException(NameException e, Model model){
+        model.addAttribute("error", e.getMessage());
+        return "error";
+    }
 }
