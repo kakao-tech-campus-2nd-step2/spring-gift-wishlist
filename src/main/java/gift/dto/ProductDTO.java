a9b765ed0b912cfcae391b1bdf008eadd608a8ee
pinkbeenyeti <rbgusdn12@gmail.com>
2024-07-03 23:14:04 +0900

    feat: 예외 검출, validation 이용 Validation을 이용하여 객체인 ProductDTO의 필드인 name에서 길이 제한과 정규식을 이용한 제한을 두어 예외를 던지도록 설정하였다.

--- src/main/java/gift/dto/ProductDTO.java
+++ src/main/java/gift/dto/ProductDTO.java
@@ -1,6 +1,14 @@
 package gift.dto;
 
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public record ProductDTO(Long id,
-                       String name,
-                       Integer price,
-                       String imageUrl) { }
+                         @Size (max=15, message = "이름이 15글자를 초과하였습니다.")
+                         @Pattern(
+                                 regexp = "^[a-zA-Z0-9-ㄱ-하-ㅣ()\\[\\]+\\-\\&/_\\s]*$",
+                                 message = "이름에 허용되지 않은 특수 문자가 포함되었습니다."
+                         )
+                         String name,
+                         Integer price,
+                         String imageUrl) { }
