36fd88a506a6f460e30c5a40e582afa0264f6d07
Gahyeon Lee <best11gh@naver.com>
2024-07-03 18:22:27 +0900

    feat: 관리자 화면 붙이기

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,7 +1,7 @@
 function deleteProduct(element) {
   if (confirm('이 제품을 삭제하시겠습니까?')) {
     const productId = element.getAttribute('data-id');
-    fetch(`/products/${productId}`, {
+    fetch(`/admin/${productId}`, {
       method: 'DELETE'
     })
     .then(response => response.text())
@@ -9,7 +9,7 @@ function deleteProduct(element) {
       if (result === 'success') {
         window.location.reload();
       } else {
-        alert('Error deleting product');
+        alert('제품을 삭제하는데 실패하였습니다.');
       }
     });
   }
