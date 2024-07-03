a3df04b4be6db1586d87d693ee3962335d1b7c76
lja3723 <lja3723@gmail.com>
2024-07-03 15:22:05 +0900

    feat: add validate condition to Product model
    Product 모델에 검증 조건 추가

--- src/main/java/gift/domain/product/Product.java
+++ src/main/java/gift/domain/product/Product.java
@@ -1,5 +1,20 @@
 package gift.domain.product;
 
-public record Product(Long id, String name, Long price, String imageUrl) {
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record Product(
+    @NotNull Long id,
+    @NotNull
+    @Size(min = 1, max = 15,
+        message = "상품 이름은 공백을 포함하여 최대 15자까지 입력할 수 있습니다.")
+    @Pattern(regexp = "^[\\w\\s\\[\\]()\\+\\-&/_]*$",
+        message = "사용 가능한 특수 문자는 (), [], +, -, &, /, _ 입니다.")
+    @Pattern(regexp = "^(?!.*카카오).*$",
+        message = "'카카오'가 포함된 문구는 사용할 수 없습니다.")
+    String name,
+    @NotNull Long price,
+    @NotNull String imageUrl) {
 
 }
