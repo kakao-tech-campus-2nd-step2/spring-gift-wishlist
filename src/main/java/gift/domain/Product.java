904bc7d273d2d9990ebd4265e05cd73f0af16edb
youcaslte03 <luckthb325@naver.com>
2024-07-03 17:20:41 +0900

    feat: Product검증 추가

--- src/main/java/gift/domain/Product.java
+++ src/main/java/gift/domain/Product.java
@@ -1,6 +1,5 @@
 package gift.domain;
 
-import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
