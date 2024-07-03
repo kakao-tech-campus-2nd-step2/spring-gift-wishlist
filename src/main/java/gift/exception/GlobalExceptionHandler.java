9c8d2f244609821b5409cf61a147febdcf767a14
GamJaDo <kim0615157@gmail.com>
2024-07-04 03:04:31 +0900

    feat(GlobalExceptionHandler.java)
    예외 처리 핸들러 작성

---
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package gift.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(InvalidProductException.class)
+    public ResponseEntity<String> handleInvalidProductException(InvalidProductException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
