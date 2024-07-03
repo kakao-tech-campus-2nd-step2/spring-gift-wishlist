c54110bc8fd618c8a4a48e878bcd243168081104
us4c0d3 <jwseok312@gmail.com>
2024-07-03 11:22:36 +0900

    feat(Product): Product 레코드에 validation 추가

--- src/main/java/gift/Product.java
+++ src/main/java/gift/Product.java
@@ -1,5 +1,18 @@
 package gift;
 
-public record Product(Long id, String name, Integer price, String imageUrl) {
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
-}
+public record Product(
+    @NotNull Long id,
+
+    @Size(min = 1, max = 15)
+    @Pattern(regexp = "^[a-zA-Z가-힣0-9 ()\\[\\]+\\-&/_]*$")
+    String name,
+
+    @Positive Integer price,
+
+    String imageUrl
+) {}
