ce8420128c38971fe9d759fce0dc73a1fc167743
amm0124 <amm012450@gmail.com>
2024-07-04 15:14:27 +0900

    REFACTORING : service logic 구현

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -1,8 +1,12 @@
 package gift.service;
 
+import gift.Product;
 import gift.repository.JdbcProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -14,5 +18,39 @@ public class ProductService {
         this.repository = repository;
     }
 
+    public boolean saveProduct(Product product) {
+        long productId = product.id();
+        if(!repository.findById(productId).isPresent()) {
+            repository.save(product);
+            return true;
+        }
+
+        return false;
+    }
+
+    public Optional<Product> getProduct(long productId) {
+        return repository.findById(productId);
+    }
+
+    public List<Product> getAllProducts() {
+        return repository.findAll();
+    }
+
+    public boolean deleteProduct(long productId) {
+        if(repository.findById(productId).isPresent()) {
+            repository.deleteById(productId);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean updateProduct(long id, Product product) {
+        long productId = product.id();
+        if(repository.findById(productId).isPresent()) {
+            repository.updateById(productId, product);
+            return true;
+        }
+        return false;
+    }
 
 }
