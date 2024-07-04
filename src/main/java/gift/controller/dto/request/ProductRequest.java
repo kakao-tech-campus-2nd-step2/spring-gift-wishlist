c837f13f2c249ca2f27929ba68f8e821567a33e1
sanghee0820 <lsh277604@gmail.com>
2024-07-04 20:47:24 +0900

    feat: ProductNamePattern annotation 추가

--- src/main/java/gift/controller/dto/request/ProductRequest.java
+++ src/main/java/gift/controller/dto/request/ProductRequest.java
@@ -1,20 +1,17 @@
 package gift.controller.dto.request;
 
 import gift.common.exception.ValidateErrorMessage;
+import gift.common.validation.ProductNamePattern;
 import gift.model.Product;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.Range;
 import org.hibernate.validator.constraints.URL;
 
 public record ProductRequest(
         @NotBlank(message = ValidateErrorMessage.INVALID_PRODUCT_NAME_NULL)
-        @Pattern(
-                regexp = "^[a-zA-Z0-9ㄱ-ㅎ가-힣 ()\\[\\]+\\-&/_]*$",
-                message = ValidateErrorMessage.INVALID_PRODUCT_NAME_PATTERN
-        )
+        @ProductNamePattern
         @Length(max = 15, message = ValidateErrorMessage.INVALID_PRODUCT_NAME_LENGTH)
         String name,
         @NotNull(message = ValidateErrorMessage.INVALID_PRODUCT_PRICE_NULL)
