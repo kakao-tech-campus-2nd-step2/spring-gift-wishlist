5c8fbf353c063aa51bc9cfe2d2ca880ddaef289a
GitJIHO <jiho99322@gmail.com>
2024-07-04 14:29:56 +0900

    feat: 보일러 플레이트를 피하기 위해 Interceptor 생성 및 구현

---
+++ src/main/java/gift/Interceptor/TokenInterceptor.java
@@ -0,0 +1,39 @@
+package gift.Interceptor;
+
+import gift.service.TokenService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class TokenInterceptor implements HandlerInterceptor {
+
+    private final TokenService tokenService;
+
+    public TokenInterceptor(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+        Object handler) throws Exception {
+        final String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return false;
+        }
+
+        String token = authorizationHeader.substring(7);
+        String email = tokenService.getEmailFromToken(token);
+
+        if (email == null || !tokenService.validateToken(token, email)) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return false;
+        }
+
+        request.setAttribute("token", token);
+        return true;
+    }
+}
