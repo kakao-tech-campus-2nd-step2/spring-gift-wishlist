0143e139a2def8620e5af9009ae643645ec10665
geist9110 <geist9110@gmail.com>
2024-07-04 20:55:59 +0900

    feat(authorize): Add user controller layer

---
+++ src/main/java/gift/user/UserController.java
@@ -0,0 +1,23 @@
+package gift.user;
+
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public void addUser(@Valid @RequestBody User user) {
+        userService.addUser(user);
+    }
+}
