945ace373e95ccc8f3a36d9cad6b164e2850c750
rladbrua0207 <48901587+rladbrua0207@users.noreply.github.com>
2024-07-04 21:26:33 +0900

    feat: Service 예외처리 추가

--- src/main/java/gift/product/application/ProductService.java
+++ src/main/java/gift/product/application/ProductService.java
@@ -1,12 +1,12 @@
 package gift.product.application;
 
+import gift.exception.type.NotFoundException;
 import gift.product.application.command.ProductCreateCommand;
 import gift.product.application.command.ProductUpdateCommand;
 import gift.product.domain.ProductRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @Service
 public class ProductService {
@@ -24,7 +24,7 @@ public class ProductService {
     public ProductResponse findById(Long productId) {
         return productRepository.findById(productId)
                 .map(ProductResponse::from)
-                .orElseThrow(() -> new NoSuchElementException("Product not found"));
+                .orElseThrow(() -> new NotFoundException("해당 상품이 존재하지 않습니다."));
     }
 
     public void add(ProductCreateCommand product) {
@@ -33,7 +33,7 @@ public class ProductService {
 
     public void update(ProductUpdateCommand command) {
         productRepository.findById(command.productId())
-                .orElseThrow(() -> new NoSuchElementException("Product not found"));
+                .orElseThrow(() -> new NotFoundException("해당 상품이 존재하지 않습니다."));
 
         productRepository.updateProduct(command);
     }
