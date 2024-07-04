243db6c365fa38a4f852933e03c799cb6cebd983
rdme0 <we6610@gmail.com>
2024-07-04 19:06:26 +0900

    :recycle: refactor: 비밀번호 검증 더 엄격하게 리팩터링

--- src/main/java/gift/dto/UserDTO.java
+++ src/main/java/gift/dto/UserDTO.java
@@ -1,13 +1,14 @@
 package gift.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 
 public record UserDTO(
-        @NotNull(message = "비밀번호를 입력해주세요.")
-        String password,
-
         @Email(message = "올바른 이메일 형식이 아닙니다.")
-        String email
+        String email,
+
+        @NotBlank(message = "비밀번호를 입력해주세요.")
+        String password
 ) {
+
 }
