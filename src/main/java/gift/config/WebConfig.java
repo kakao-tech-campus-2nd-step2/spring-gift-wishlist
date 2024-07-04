ea2ec995dcac4ac0696ba08842284c3850333372
GitJIHO <jiho99322@gmail.com>
2024-07-04 16:21:20 +0900

    fix: WebConfig excludePath 링크 수정

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -17,6 +17,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(tokenInterceptor)
             .addPathPatterns("/**")
-            .excludePathPatterns("login/token");
+            .excludePathPatterns("/login/token");
     }
 }
