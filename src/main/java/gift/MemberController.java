d8758ebe6bfdf4d2332b71e30f9e4ddb845d0fbe
SeoYeongU <westzeroright@gmail.com>
2024-07-05 03:25:28 +0900

    feat(MemberController): 회원가입 기능
    이메일과 비밀번호를 DB에 저장하고 access token 발급

--- src/main/java/gift/MemberController.java
+++ src/main/java/gift/MemberController.java
@@ -1,5 +1,7 @@
 package gift;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/member")
 public class MemberController {
 
+    private final MemberDao memberDao;
+    private final JwtTokenUtil jwtTokenUtil;
+
+    public MemberController(MemberDao memberDao, JwtTokenUtil jwtTokenUtil) {
+        this.memberDao = memberDao;
+        this.jwtTokenUtil = jwtTokenUtil;
+    }
+
     @PostMapping("/register")
-    public Member registerMember(@RequestBody Member member) {
-        Member newMember = new Member(
-            member.getEmail(),
-            member.getPassword()
-        );
-        return newMember;
+    public ResponseEntity<?> registerMember(@RequestBody Member member) {
+        memberDao.insertMember(member);
+        String token = jwtTokenUtil.generateToken(member.getEmail());
+        return ResponseEntity.ok(new TokenResponseDto(token));
     }
 }
