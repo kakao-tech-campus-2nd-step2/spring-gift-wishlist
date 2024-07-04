2446e25230a9766277a9cf22bcb5ac8bdfd7555e
Kiwoong Kim <gw150311@naver.com>
2024-07-05 02:11:45 +0900

    fix(SignupController): signup 매서드 리턴을 템플릿에서 ResponseEntity로 변경
    -SignupViewController 분리
    -SignupResponse 클래스 생성 (DTO)

--- src/main/java/gift/controller/SignupController.java
+++ src/main/java/gift/controller/SignupController.java
@@ -1,16 +1,17 @@
 package gift.controller;
 
 import gift.DTO.SignupRequest;
+import gift.DTO.SignupResponse;
 import gift.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/signup")
 public class SignupController {
 
@@ -21,20 +22,11 @@ public class SignupController {
         this.userService = userService;
     }
 
-    @GetMapping
-    public String showSignupForm(Model model) {
-        model.addAttribute("signupRequest", new SignupRequest());
-        return "signup";
-    }
-
     @PostMapping
-    public String signup(@ModelAttribute SignupRequest signupRequest, Model model) {
-        try {
-            userService.registerUser(signupRequest);
-            return "redirect:/login"; // 회원가입 성공 시 로그인 페이지로 리디렉션
-        } catch (Exception e) {
-            model.addAttribute("error", "Registration failed: " + e.getMessage());
-            return "signup"; // 회원가입 실패 시 다시 회원가입 페이지로
-        }
+    public ResponseEntity<SignupResponse> signup(@RequestBody SignupRequest signupRequest,
+        Model model) {
+        String token = userService.registerUser(signupRequest);
+        SignupResponse response = new SignupResponse(token);
+        return ResponseEntity.ok().body(response);
     }
 }
