04e28382cb5b7b0c735e21fed48db6f7d8e56bd2
pswaao88 <pswaao88@gmail.com>
2024-07-04 14:48:26 +0900

    Feat: MemberService클래스 작성

--- src/main/java/gift/Service/MemberService.java
+++ src/main/java/gift/Service/MemberService.java
@@ -1,6 +1,19 @@
 package gift.Service;
 
-public class MemberService {
+import gift.Model.Member;
+import gift.Repository.MemberRepository;
+import org.springframework.stereotype.Service;
 
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
+
+    public Member getMemberByEmail(String email){
+        return memberRepository.findByEmail(email);
+    }
 
 }
