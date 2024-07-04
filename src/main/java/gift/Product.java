be11d668307bd52d2abe18cb0d5a77aafec8327b
ariimo <159447466+ariimo@users.noreply.github.com>
2024-07-04 17:03:15 +0900

    feat: week1 코드 수정 및 복사

---
+++ src/main/java/gift/Product.java
@@ -0,0 +1,57 @@
+package gift;
+
+public class Product {
+    private Long id;
+    private String name;
+    private double price;
+    private String imageUrl;
+
+    public Product(Long id, String name, double price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", price=" + price +
+            ", imageUrl='" + imageUrl + '\'' +
+            '}';
+    }
+}
