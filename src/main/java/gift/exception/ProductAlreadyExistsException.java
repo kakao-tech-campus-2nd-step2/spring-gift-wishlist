80d7b4f9196c36113d336df8ee91e7e2a25accb3
Kim Sanghae <pelikan@pusan.ac.kr>
2024-07-04 15:24:07 +0900

    chore: 파일 삭제

--- src/main/java/gift/exception/ProductAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package gift.exception;
-
-public class ProductAlreadyExistsException extends RuntimeException {
-
-    public ProductAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- src/main/java/gift/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package gift.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
