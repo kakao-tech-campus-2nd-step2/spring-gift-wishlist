9e7a738869b96f2f39dc0a36eab9bbec931ba8db
hyunseo Jeong <wjdgustj2877@naver.com>
2024-07-04 18:05:21 +0900

    feat: RestController 구현

--- src/main/java/gift/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -1,16 +1,17 @@
 package gift.exception;
 
-import jakarta.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.method.HandlerMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -22,17 +23,22 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public String handleMethodArgumentNotValidException(MethodArgumentNotValidException ex,
-                                                        RedirectAttributes redirectAttributes,
-                                                        HttpServletRequest request)
-    {
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
         BindingResult bindingResult = ex.getBindingResult();
         Locale locale = LocaleContextHolder.getLocale();
-        for (FieldError fieldError : bindingResult.getFieldErrors()) {
-            String errorMessage = messageSource.getMessage(fieldError, locale);
-            redirectAttributes.addFlashAttribute(fieldError.getField() + "Error", errorMessage);
-        }
 
-        return "redirect:" + request.getHeader("Referer");
+        bindingResult.getFieldErrors().forEach(fieldError -> {
+            errors.put(fieldError.getField(), messageSource.getMessage(fieldError, locale));
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, String>> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("price", "잘못된 형식입니다. 상품 가격을 숫자로 입력해주세요.");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
