056bd3907a151bdd8ea401bef862116c37b15c21
Hyun-Seo Jeong <90139789+hynseoj@users.noreply.github.com>
2024-07-04 15:38:58 +0900

    충남대 BE_정현서 2주차 과제 (1단계) (#189)
    * init: 기본 코드 준비
    
    * docs: 기능 요구사항 목록 작성
    
    * feat: 입력이 null인 경우 처리
    
    * test: 입력이 null인 경우 테스트
    
    * feat: 상품 가격이 범위를 벗어나는 경우 처리
    
    * test: 상품 가격이 범위를 벗어나는 경우 테스트
    
    * feat: 상품 가격에 int형으로 치환 불가능한 값이 입력된 경우 처리
    
    * test: 상품 가격에 int형으로 치환 불가능한 값이 입력된 경우 테스트
    
    * feat: 형식에 맞지 않는 URL이 입력된 경우 처리
    
    * test: 형식에 맞지 않는 URL이 입력된 경우 테스트
    
    * feat: 상품 이름 15자 초과 처리
    
    * test: 상품 이름 15자 초과 처리 테스트 및 잘못된 코드 수정
    
    * feat: 불가능한 특수 문자 처리
    
    * test: 불가능한 특수 문자 처리 테스트
    
    * feat: 상품 이름에 "카카오"가 포함된 경우 처리
    
    * test: 상품 이름에 "카카오"가 포함된 경우 처리 테스트
    
    * test: 오류 메시지까지 테스트하도록 코드 수정
    
    * refactor: 상품 이름에 "카카오"가 포함된 경우 처리 방법 변경
    
    * refactor: 오류 발생 시 redirect url을 referer 헤더를 사용하는 방식으로 변경
    
    * refactor: 웹 테스트 완료로 input type 복구
    
    * refactor: 테스트 중복 코드 메서드로 추출

---
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package gift.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Locale;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private final MessageSource messageSource;
+
+    public GlobalExceptionHandler(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public String handleMethodArgumentNotValidException(MethodArgumentNotValidException ex,
+                                                        RedirectAttributes redirectAttributes,
+                                                        HttpServletRequest request)
+    {
+        BindingResult bindingResult = ex.getBindingResult();
+        Locale locale = LocaleContextHolder.getLocale();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            String errorMessage = messageSource.getMessage(fieldError, locale);
+            redirectAttributes.addFlashAttribute(fieldError.getField() + "Error", errorMessage);
+        }
+
+        return "redirect:" + request.getHeader("Referer");
+    }
+}
