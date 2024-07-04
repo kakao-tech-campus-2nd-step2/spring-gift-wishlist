99c03062dea2446a17800d70660f392ca17d8452
kangwlgns <kangji0615@gmail.com>
2024-07-04 14:48:22 +0900

    docs(ValidationException): write a comment which is the reason of exception's name

--- src/main/java/gift/exception/ValidationException.java
+++ src/main/java/gift/exception/ValidationException.java
@@ -1,6 +1,6 @@
 package gift.exception;
 
-// 추가한 예외
+// 추가한 예외. 잘못된 형식이라는 느낌을 직관적으로 주기 위해 validation이라는 단어를 선택
 public class ValidationException extends IllegalArgumentException {
     public ValidationException(String message) {
         super(message);
