c7dce6457f4f030f29c69e32c5e757eb8604d465
pswaao88 <pswaao88@gmail.com>
2024-07-04 17:45:03 +0900

    Refactor: jwt토큰 조금더 이해가 쉬운 형태로 변경

--- src/main/java/gift/Service/MemberAccessTokenProvider.java
+++ src/main/java/gift/Service/MemberAccessTokenProvider.java
@@ -8,13 +8,11 @@ import java.util.Date;
 public class MemberAccessTokenProvider {
 
     public static String createJwt(String email){
-        Date now = new Date();
+        String secretKey = "21312BHV48DX21421C25F215NJN2DF12141JJ214GVSQ12429858236H7998325A72";
         return Jwts.builder()
             .setHeaderParam("type","jwt")
             .claim("email", email)
-            .setIssuedAt(now)
-            .setExpiration(new Date(System.currentTimeMillis()+1*(1000*60*60*24*365)))
-            .signWith(SignatureAlgorithm.HS256, SecretKey.JWT_SECRET_KEY)
+            .signWith(SignatureAlgorithm.HS256, secretKey)
             .compact();
     }
 }
