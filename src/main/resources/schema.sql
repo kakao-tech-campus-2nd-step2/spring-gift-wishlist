3ec24879689e7abbfd7f1d84f9c0ca5e3a5702b3
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-05 01:20:50 +0900

    feat : users table 추가 sql 작성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,12 @@
-drop table if exists product;
+DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS users;
 CREATE TABLE product (
     id LONG AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
     price INT,
     imageUrl VARCHAR(255)
+);
+CREATE TABLE users (
+    email VARCHAR(255) PRIMARY KEY ,
+    password VARCHAR(255)
 );
