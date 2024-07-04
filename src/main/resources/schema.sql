38ca51aaacc3209fdd96e09f5cc1c8a9c7d4c260
JunHyeongChoi <ajfeh22@gmail.com>
2024-07-04 13:28:51 +0900

    feat: 회원 테이블 생성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,3 +4,9 @@ CREATE TABLE product (
     price integer NOT NULL,
     image_url VARCHAR(255)
 );
+
+CREATE TABLE member (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
