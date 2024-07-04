404548466b9485f12c73760a9f776144eaa4cf4c
dkswoals <a4445656@gmail.com>
2024-07-05 00:17:17 +0900

    feat: GlobalExceptionHandler 수정

--- src/main/java/wishlist/exception/GlobalExceptionHandler.java
+++ src/main/java/wishlist/exception/GlobalExceptionHandler.java
@@ -1,45 +1,34 @@
 package wishlist.exception;
 
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.Map;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
+import org.springframework.ui.Model;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ItemNotFoundException.class)
-    public ResponseEntity<ErrorResponseDTO> handleItemNotFoundException(ItemNotFoundException e) {
-        return handleException(e.getErrorCode(), null);
+    private String handleItemNotFoundException(ItemNotFoundException e, Model model) {
+        return handleException(e.getErrorCode(), Collections.emptyMap(),model);
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public ResponseEntity<ErrorResponseDTO> handleHttpRequestMethodNotSupportedException(
-        HttpRequestMethodNotSupportedException e) {
-        return handleException(ErrorCode.METHOD_NOT_ALLOWED, null);
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponseDTO> handleMethodArgumentNotValidException(
-        MethodArgumentNotValidException e) {
-        Map<String, String> errors = new HashMap<>();
-        for (FieldError fieldError : e.getFieldErrors()) {
-            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
-        }
-        return handleException(ErrorCode.INVALID_INPUT, errors);
+    private String handleHttpRequestMethodNotSupportedException(
+        HttpRequestMethodNotSupportedException e, Model model) {
+        return handleException(ErrorCode.METHOD_NOT_ALLOWED, Collections.emptyMap(),model);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponseDTO> handleCommonException(Exception e) {
-        return handleException(ErrorCode.BAD_REQUEST, null);
+    private String handleCommonException(Exception e, Model model) {
+        return handleException(ErrorCode.BAD_REQUEST, Collections.emptyMap(),model);
     }
 
-    public ResponseEntity<ErrorResponseDTO> handleException(ErrorCode errorCode,
-        Map<String, String> errors) {
-        return new ResponseEntity<>(new ErrorResponseDTO(errorCode, errors), errorCode.getStatus());
+    private String handleException(ErrorCode errorCode,
+        Map<String, String> errors,Model model) {
+            model.addAttribute("errorResponse",new ErrorResponseDTO(errorCode,errors));
+        return "error-page";
     }
 }
