b6f905aebaf45157e9e14b36b2fca8c669bb18cd
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-07-05 03:09:14 +0900

    feat: forbidden 커스텀 exception 구현

---
+++ src/main/java/gift/user/exception/UserExceptionHandler.java
@@ -0,0 +1,14 @@
+package gift.user.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice(basePackages = {"gift.user"})
+public class UserExceptionHandler {
+    @ExceptionHandler(ForbiddenException.class)
+    public ResponseEntity<String> handleForbiddenException(ForbiddenException ex) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.getMessage());
+    }
+}
