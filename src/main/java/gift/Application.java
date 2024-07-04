5f9c048a93001acb31c803b1a22225d3cf57527a
pswaao88 <66404498+pswaao88@users.noreply.github.com>
2024-07-04 13:39:12 +0900

    충남대 BE_박상우 2주차 과제 (0단계)  (#94)
    * Init: 프로젝트 생성
    
    * DOcs: ReadMe 파일 작성
    
    * Init: Error, ProductExceptionHandler 클래스 생성 및 프로젝트 구조 완성
    
    * Feat: InputValidity 클래스 생성
    
    * Feat: InputValidity 메소드 작성
    
    이름 검사 일부와 가격 체크 메소드 작성
    
    * Feat: InpuValidity 메소드 작성
    
    특수문자 검사 메소드 작성
    
    * Reafactor: 브랜치 변경

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -3,9 +3,10 @@ package gift;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Application {
+@SpringBootApplication()
+public class Application   {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
 }
---
+++ src/main/java/gift/Model/Product.java
@@ -0,0 +1,43 @@
+package gift.Model;
+
+public class Product {
+    private Long id;
+    private String name;
+    private int price;
+    private String imageUrl;
+
+    public Product(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
