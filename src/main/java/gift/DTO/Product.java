71d3bc21d1c35403ab453610e64210e18dc9107f
unknown <tnsgh3691@gmail.com>
2024-07-04 20:22:56 +0900

    fix: PR충돌로 인한 브랜치 새로 파기

--- src/main/java/gift/DTO/Product.java
+++ src/main/java/gift/DTO/Product.java
@@ -1,12 +1,24 @@
-package gift;
+package gift.DTO;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 
 public class Product {
     private Long id;
+
+    @Size(max = 15, message = "상품 이름은 공백을 포함하여 최대 15자까지 입력할 수 있습니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣 ()\\[\\]+\\-&/_]*$", message = "(),[],+,-,&,/,_를 제외한 특수 문자 사용은 불가능합니다.")
+    @Pattern(regexp = "^(?!.*카카오).*$", message = "카카오가 포함된 문구는 담당 MD와 협의한 경우에만 사용할 수 있습니다.")
     private String name;
     private int price;
     private String imageUrl;
 
-    public Product() {}
+    public Product(long l, String product1, double v) {}
+
+    public Product() {
+    }
 
     public Product(Long id, String name, int price, String imageUrl) {
         this.id = id;
