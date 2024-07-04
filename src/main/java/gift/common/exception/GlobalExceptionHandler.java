bbd34041d12227d79b0f73938f70917d1fdddc89
hyeonseo <69136047+hxunsc@users.noreply.github.com>
2024-07-05 02:44:14 +0900

    feat: 이메일 중복 및 값  찾을 수 없는 에러 처리

--- src/main/java/gift/common/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package gift.common.exception;
 
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -22,6 +24,16 @@ public class GlobalExceptionHandler {
             errors.put(fieldName, errorMessage);
         });
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DuplicateKeyException.class)
+    public ResponseEntity<?> handleDuplicateKeyException(DuplicateKeyException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("이미 존재하는 이메일입니다.");
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<?> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("찾을 수 없습니다.");
     }
 
     @ExceptionHandler(Exception.class)
