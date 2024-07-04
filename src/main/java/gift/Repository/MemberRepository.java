5a087482c34012f36168db201f77ba09f757aca7
pswaao88 <pswaao88@gmail.com>
2024-07-04 14:47:57 +0900

    Feat: MemberRepository클래스 작성

--- src/main/java/gift/Repository/MemberRepository.java
+++ src/main/java/gift/Repository/MemberRepository.java
@@ -1,6 +1,25 @@
 package gift.Repository;
 
+import gift.Model.Member;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class MemberRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public MemberRepository(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private final RowMapper<Member> rowMapper = new BeanPropertyRowMapper<>(Member.class);
+
+    public Member findByEmail(String email){
+        return jdbcTemplate.queryForObject("SELECT * FROM member WHERE email = ?",rowMapper,email);
+    }
 
 }
 
