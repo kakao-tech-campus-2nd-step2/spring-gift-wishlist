6822410b014d45bbe694bfd5fa90495d236b1e32
seojiu <seojiu5598@gmail.com>
2024-07-05 08:31:28 +0900

    feat(MemberController.java): 회원 가입 기능 구현

--- src/main/java/gift/MemberController.java
+++ src/main/java/gift/MemberController.java
@@ -1,9 +1,26 @@
 package gift;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/members")
 public class MemberController {
     private final MemberService memberService;
 
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody Member member) {
+        String token = memberService.register(member);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+        return ResponseEntity.ok().headers(headers).body("{\"token\": \"" + token + "\"}");
     }
 }
