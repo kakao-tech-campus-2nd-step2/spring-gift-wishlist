4e45fa9218f94e1a2a932359c6f8cb4bca1c541a
sanghee0820 <lsh277604@gmail.com>
2024-07-04 21:24:12 +0900

    feat: User 클래스 추가

---
+++ src/main/java/gift/model/User.java
@@ -0,0 +1,37 @@
+package gift.model;
+
+public class User {
+    private Long id;
+    private String username;
+    private String password;
+    private Role role;
+
+    public User(Long id, String username, String password, Role role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public User(String username, String password, Role role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+}
