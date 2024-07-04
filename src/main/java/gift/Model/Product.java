69403c75c5092f71f3a2e3a8bf5edc0d0038c902
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-07-05 03:35:04 +0900

    feat : price 검증을 위해 타입 변경 price 검증을 위해 long에서 String으로 변경하여 @Pattern을 사용해 숫자만 입력 가능하도록 변경했습니다. 그리고 그에 맞게 메서드를 작성 및 수정했습니다.

--- src/main/java/gift/Model/Product.java
+++ src/main/java/gift/Model/Product.java
@@ -2,7 +2,6 @@ package gift.Model;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 
 public record Product(Long id,
@@ -11,10 +10,14 @@ public record Product(Long id,
                       @Pattern(regexp = "^[a-zA-Z0-9가-힣ㄱ-ㅎㅏ-ㅣ\\s\\(\\)\\[\\]\\+\\-\\&\\/\\_]*$", message = "상품 이름에 (), [], +, -, &, /, _ 외 특수 문자는 사용할 수 없습니다.")
                       @Pattern(regexp = "^(?!.*카카오).*$", message = "'카카오'가 포함된 문구는 담당 MD와 협의 후 사용 바랍니다.")
                       String name,
-                      @PositiveOrZero(message = "가격은 음수가 될 수 없습니다.")
-                      long price,
+                      @NotBlank(message = "가격을 입력해야 합니다.")
+                      @Pattern(regexp = "^\\d+$", message = "가격은 0이상의 숫자만 입력 가능합니다.")
+                      String price,
                       @NotBlank(message = "이미지 URL을 입력해야 합니다.")
                       @Pattern(regexp = "^(http|https)://.*$", message = "유효한 이미지 URL을 입력해야 합니다.")
                       String imageUrl) {
+    public static ProductDTO toDTO(Product product) {
+        return new ProductDTO(product.name(), product.price(), product.imageUrl());
+    }
 
 }
