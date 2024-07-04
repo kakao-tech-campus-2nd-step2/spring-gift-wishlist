e1b3b4319c39bfa5c039a435d331645b51420db7
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-07-05 04:12:39 +0900

    feat: product.name 기본 어노테이션 유효성 검사 추가

--- src/main/java/gift/product/model/dto/valid/ExcludeKeyword.java
+++ src/main/java/gift/product/model/dto/valid/ExcludeKeyword.java
@@ -9,10 +9,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Documented
-@Constraint(validatedBy = ProductNameValidator.class)
+@Constraint(validatedBy = ExcludeKeywordValidator.class)
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface ValidProductName {
+public @interface ExcludeKeyword {
     String message() default "상품 이름 형식이 올바르지 않습니다.";
 
     Class<?>[] groups() default {};
--- src/main/java/gift/product/model/dto/valid/ExcludeKeywordValidator.java
+++ src/main/java/gift/product/model/dto/valid/ExcludeKeywordValidator.java
@@ -3,11 +3,11 @@ package gift.product.model.dto.valid;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-public class ProductNameValidator implements ConstraintValidator<ValidProductName, String> {
+public class ExcludeKeywordValidator implements ConstraintValidator<ExcludeKeyword, String> {
     private String excludeKeyword;
 
     @Override
-    public void initialize(ValidProductName constraintAnnotation) {
+    public void initialize(ExcludeKeyword constraintAnnotation) {
         this.excludeKeyword = constraintAnnotation.excludeKeyword();
     }
 
@@ -15,7 +15,7 @@ public class ProductNameValidator implements ConstraintValidator<ValidProductNam
     public boolean isValid(String name, ConstraintValidatorContext context) {
         if (!excludeKeyword.isEmpty() && name.contains(excludeKeyword)) {
             context.disableDefaultConstraintViolation();
-            context.buildConstraintViolationWithTemplate("상품 이름에 포함할 수 없는 키워드가 포함되어 있습니다: " + excludeKeyword)
+            context.buildConstraintViolationWithTemplate("상품 이름에 '카카오'를 포함할 수 없습니다. 담당 MD와 협의 후 사용해주세요.")
                     .addConstraintViolation();
             return false;
         }
