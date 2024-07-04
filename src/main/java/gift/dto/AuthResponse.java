cb531d7cd1bdcc16a8c6606c5e305e0b6b3f0b17
GitJIHO <jiho99322@gmail.com>
2024-07-04 13:49:01 +0900

    feat: 인증 Request, Response DTO 생성 및 구현

---
+++ src/main/java/gift/dto/AuthResponse.java
@@ -0,0 +1,18 @@
+package gift.dto;
+
+public class AuthResponse {
+
+    private String accessToken;
+
+    public AuthResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
