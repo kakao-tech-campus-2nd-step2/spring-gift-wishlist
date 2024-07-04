b200556b95fc7fae964364d1390d6cb07301dd00
김수랑 <we6610@gmail.com>
2024-07-04 16:52:49 +0900

    :sparkles: feat: UserService의 회원가입 하는 부분 스켈레톤 코드 추가

--- src/main/java/gift/service/UserService.java
+++ src/main/java/gift/service/UserService.java
@@ -1,5 +1,24 @@
 package gift.service;
 
-public class UserService {
+import gift.dao.ProductDao;
+import gift.dao.UserDao;
+import gift.dto.UserDTO;
+import gift.entity.User;
+import org.springframework.stereotype.Service;
 
+@Service
+public class UserService {
+    private final UserDao userDao;
+
+    public UserService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public void signUp(UserDTO userDto){
+        try{
+            userDao.insertUser(new User(userDto));
+        } catch (Exception e){
+
+        }
+    }
 }
