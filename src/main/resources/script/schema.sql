151613e4aeda4fdd9c89f84067b39f39b871d906
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-07-04 16:12:15 +0900

    feat: user 테이블 생성 sql 추가

--- src/main/resources/script/schema.sql
+++ src/main/resources/script/schema.sql
@@ -1,7 +1,16 @@
-CREATE TABLE product (
+CREATE TABLE Product (
                           id BIGINT AUTO_INCREMENT PRIMARY KEY,
                           name VARCHAR(255) NOT NULL,
                           price INT NOT NULL CHECK (price >= 0),
                           image_url TEXT,
                           is_active BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE User (
+                      id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                      name VARCHAR(100) NOT NULL,
+                      email VARCHAR(255) NOT NULL UNIQUE,
+                      password VARCHAR(100) NOT NULL,
+                      status BOOLEAN NOT NULL DEFAULT TRUE,
+                      role VARCHAR(50) NOT NULL DEFAULT 'USER'
 );
