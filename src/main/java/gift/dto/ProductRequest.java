9738fc1b8b1dcb4b6cb142d3d36086b176c01a41
NextrPlue <p.plue1881@gmail.com>
2024-07-04 12:00:43 +0900

    fix(dto): update product name validation to include additional Korean characters

--- src/main/java/gift/dto/ProductRequest.java
+++ src/main/java/gift/dto/ProductRequest.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 public record ProductRequest(
         @NotBlank(message = "상품 이름은 공백일 수 없습니다.")
         @Size(max = 15, message = "상품 이름은 공백을 포함하여 최대 15자까지 입력할 수 있습니다.")
-        @Pattern(regexp = "^[a-zA-Z0-9 ()\\[\\]+\\-&/_\\uAC00-\\uD7A3]*$", message = "상품 이름이 유효하지 않은 문자를 포함하고 있습니다.")
+        @Pattern(regexp = "^[a-zA-Z0-9 ()\\[\\]+\\-&/_\\uAC00-\\uD7A3\\u3131-\\u3163]*$", message = "상품 이름이 유효하지 않은 문자를 포함하고 있습니다.")
         @KakaoNotAllowed(message = "\"카카오\"가 포함된 문구는 담당 MD와 협의한 경우에만 사용할 수 있습니다.")
         String name,
 
