c5b067453628bba29129f2c2537dcc91a2b8654d
HSHwan <tnghks2866@gmail.com>
2024-07-04 20:55:22 +0900

    feat: users 테이블 구축

--- src/main/resources/script/schema.sql
+++ src/main/resources/script/schema.sql
@@ -1,7 +1,14 @@
 DROP TABLE IF EXISTS product;
 CREATE TABLE product (
-                         id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                         name VARCHAR(15) NOT NULL,
-                         price INT NOT NULL CHECK (price >= 0),
-                         image_url VARCHAR(255)
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(15) NOT NULL,
+    price INT NOT NULL CHECK (price >= 0),
+    image_url VARCHAR(255)
+);
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(50) NOT NULL
 );
