25ab305f05f3b21e35601d4b8f6a024a0b6d11b6
anacnoda77 <sm1108shin@naver.com>
2024-07-04 15:19:15 +0900

    feat: Member 테이블 스키마/데모 데이터  작성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,8 @@ INSERT INTO PRODUCTS(name,price,image_url) VALUES ('productA', 100, 'https://url
 INSERT INTO PRODUCTS(name,price,image_url) VALUES ('productB', 5000, 'https://url2.com');
 INSERT INTO PRODUCTS(name,price,image_url) VALUES ('productC', 500000, 'https://url3.com');
 INSERT INTO PRODUCTS(name,price,image_url) VALUES ('productD', 1000000, 'https://url4.com');
+
+INSERT INTO MEMBER(email,password) VALUES ('aaa123@a.com');
+INSERT INTO MEMBER(email,password) VALUES ('bbb123@b.com');
+INSERT INTO MEMBER(email,password) VALUES ('ccc123@c.com');
+INSERT INTO MEMBER(email,password) VALUES ('ddd123@d.com');
