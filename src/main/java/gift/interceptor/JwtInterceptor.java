0c1bc49d96be506be4dd07ee9890e19a346f3939
rdme0 <we6610@gmail.com>
2024-07-05 01:46:56 +0900

    :recycle: refactor: 상품을 Read만 할 경우 토큰 필요없게 리팩터링

--- src/main/java/gift/interceptor/JwtInterceptor.java
+++ src/main/java/gift/interceptor/JwtInterceptor.java
@@ -9,10 +9,17 @@ public class JwtInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String token = request.getHeader("Authorization");
+        System.out.println("token = " + token);
+        if ("GET".equalsIgnoreCase(request.getMethod()) && "/api/products".equals(request.getRequestURI())) {
+            return true;
+        }
+        System.out.println("token = " + token);
         if(token == null || !JwtUtil.validateToken(token)){
+            response.addHeader("WWW-Authenticate", "Bearear");
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
             return false;
         }
+
         return true;
     }
 }
