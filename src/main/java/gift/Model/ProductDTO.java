3f8d6bb329760f4a984ed795c8b6b2bab5774b4d
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-07-05 02:38:05 +0900

    refactor : ProductDTO 수정 화면에서 입력 받을 때 id를 사용하지 않으므로 id를 삭제했습니다.

--- src/main/java/gift/Model/ProductDTO.java
+++ src/main/java/gift/Model/ProductDTO.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 
-public record ProductDTO(Long id,
+public record ProductDTO(
                          @NotBlank(message = "상품 이름은 최소 1자 이상이어야 합니다.")
                          @Size(max = 15, message = "상품 이름은 공백 포함 최대 15자까지 입력할 수 있습니다.")
                          @Pattern(regexp = "^[a-zA-Z0-9가-힣ㄱ-ㅎㅏ-ㅣ\\s\\(\\)\\[\\]\\+\\-\\&\\/\\_]*$", message = "상품 이름에 (), [], +, -, &, /, _ 외 특수 문자는 사용할 수 없습니다.")
