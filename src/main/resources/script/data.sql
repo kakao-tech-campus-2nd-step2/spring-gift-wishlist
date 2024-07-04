65d30293e3bc0a4fc10e899168f5f0fca4f3dc83
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-07-04 16:14:23 +0900

    feat: user 초기 데이터 추가  sql 구현

--- src/main/resources/script/data.sql
+++ src/main/resources/script/data.sql
@@ -2,3 +2,8 @@ INSERT INTO product (name, price, image_url)
 VALUES ('상품 1', 100000, 'http://example.com/image1.jpg'),
        ('상품 2', 20000, 'http://example.com/image2.jpg'),
        ('상품 3', 1500, NULL);
+
+INSERT INTO User (name, email, password, status, role)
+VALUES ('yoo', 'yoo@example.com', '1234','ADMIN'),
+       ('kyeong', 'kyeong@example.com', '1234', 'USER'),
+       ('miiii', 'miiii@example.com', '1234', 'USER');
