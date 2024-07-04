d6a132ca3eb937ef961da45aede3b0ce81e0fe86
Giheon Do <dgh0001@naver.com>
2024-07-04 23:19:53 +0900

    add: add account table scheme

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,8 +1,16 @@
 drop table if exists product CASCADE;
 create table product
 (
-  id bigint AUTO_INCREMENT PRIMARY KEY,
-  name varchar(255),
-  price int,
-  imageUrl varchar(255)
+    id       bigint AUTO_INCREMENT PRIMARY KEY,
+    name     varchar(255),
+    price    int,
+    imageUrl varchar(255)
+);
+
+drop table if exists account CASCADE;
+create table account
+(
+    id       bigint AUTO_INCREMENT PRIMARY KEY,
+    email    varchar(255),
+    password varchar(255)
 );
