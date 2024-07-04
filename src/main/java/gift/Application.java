e223eedcb161a3373fdff9a1b574dfa95eb8fbf6
Awhn <ahnshwan@outlook.com>
2024-07-04 10:49:35 +0900

    fix : 불필요한 의존성 제거

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -1,7 +1,5 @@
 package gift;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +8,6 @@ import java.sql.DriverManager;
 
 @SpringBootApplication
 public class Application {
-    @Autowired
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
         final Connection connection = getConnection();
