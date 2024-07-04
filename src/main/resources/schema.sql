246a115e3d70ca9f4a73487573a98dd6646ea024
배민수 <duecost@gmail.com>
2024-07-04 20:17:20 +0900

    refactor: schema.sql 회원 테이블 추가, 상품 테이블 리팩토링

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,18 @@
 create table Product
 (
     id bigint not null auto_increment,
+    member_id bigint not null,
     name varchar(255) not null,
     price bigint not null,
     imageUrl varchar(255) not null,
-    primary key (id)
+    primary key (id, member_id),
+    foreign key (member_id) references Member(member_id)
+);
+
+create table Member
+(
+    member_id bigint not null auto_increment,
+    email varchar(255) not null,
+    password varchar(255) not null
+    primary key (member_id)
 );
