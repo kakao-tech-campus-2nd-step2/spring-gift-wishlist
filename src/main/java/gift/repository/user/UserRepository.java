b2cd319bfdcb5d07d5d1be4be990d08e7bf49c1c
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-05 01:19:42 +0900

    feat : UserRepository 추가

--- src/main/java/gift/repository/user/UserRepository.java
+++ src/main/java/gift/repository/user/UserRepository.java
@@ -1,2 +1,34 @@
-package gift.repository.user;public class UserRepository {
+package gift.repository.user;
+
+import gift.domain.Product;
+import gift.domain.User;
+import java.util.List;
+import org.springframework.boot.autoconfigure.batch.BatchProperties.Jdbc;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+    private final JdbcTemplate jdbcTemplate;
+    public UserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public User findByEmail(String email) {
+        var sql = "select * from user where email = ?";
+        List<User> users = jdbcTemplate.query(sql, userRowMapper, email);
+        return users.isEmpty() ? null : users.get(0);
+    }
+
+    public void save(User user) {
+        var sql = "insert into user (email, password) values (?, ?)";
+        jdbcTemplate.update(sql, user.getEmail(), user.getPassword());
+    }
+
+    private final RowMapper<User> userRowMapper = (rs, rowNum) -> new User(
+        rs.getString("email"),
+        rs.getString("password")
+    );
+
 }
