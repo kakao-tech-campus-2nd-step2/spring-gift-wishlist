2f1669344c04f06d42ad2d48bc40a6127f31f76a
geist9110 <geist9110@gmail.com>
2024-07-04 20:56:37 +0900

    feat(authorize): Add user exception handler when user email already exist

---
+++ src/main/java/gift/exceptionHandler/UserExceptionHandler.java
@@ -0,0 +1,17 @@
+package gift.exceptionHandler;
+
+import gift.user.UserController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(assignableTypes = UserController.class)
+public class UserExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ex.getMessage();
+    }
+}
