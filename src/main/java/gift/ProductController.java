fa5b7c0ef7cf8b085c523fefef80cc38dc544ec9
안원모 <tommy45231@naver.com>
2024-07-04 14:33:12 +0900

    Refactor : 불필요한 어노테이션 삭제

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -15,8 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/products")
 public class ProductController {
     private final ProductService productService;
-
-    @Autowired
+    
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
