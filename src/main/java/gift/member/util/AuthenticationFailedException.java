2ac2ab098a9d0cd1f5fa5836e321ada83a6fd83a
sunandrabbit <sunandrabbit@gamil.com>
2024-07-04 23:53:58 +0900

    feat : 로그인과 회원 가입 시 발생하는 Exception 정의

--- src/main/java/gift/member/util/AuthenticationFailedException.java
+++
@@ -1,7 +0,0 @@
-package gift.member.util;
-
-public class AuthenticationFailedException extends RuntimeException {
-    public AuthenticationFailedException(String message) {
-        super(message);
-    }
-}
