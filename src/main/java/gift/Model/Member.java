a9d7963249c9861c38a920909b880e6b8359ddd7
pswaao88 <pswaao88@gmail.com>
2024-07-04 15:57:50 +0900

    Refactor: Validation 추가

--- src/main/java/gift/Model/Member.java
+++ src/main/java/gift/Model/Member.java
@@ -1,9 +1,13 @@
 package gift.Model;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 public class Member {
+    @NotBlank(message = "입력은 공백일 수 없습니다.")
     private String password;
+    @NotBlank(message = "입력은 공백일 수 없습니다.")
+    @Email(message = "이메일 형식이어야 합니다.")
     private String email;
 
     public Member(){
