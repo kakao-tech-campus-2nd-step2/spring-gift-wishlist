9d2085de93f15183265e9d1473bca05b99685391
ajy4304 <ajy4304@naver.com>
2024-07-04 18:36:56 +0900

    feat(UserService): User에 관한 서비스 클래스 생성

--- src/main/java/gift/service/UserService.java
+++ src/main/java/gift/service/UserService.java
@@ -1,2 +1,29 @@
-package gift.service;public class UserService {
+package gift.service;
+
+import gift.dto.UserRequestDTO;
+import gift.model.User;
+import gift.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void register(UserRequestDTO userRequest) {
+        User user = new User(userRequest.getEmail(), userRequest.getPassword());
+        userRepository.save(user);
+    }
+
+    public String authenticate(UserRequestDTO userRequest) {
+        Optional<User> userOpt = userRepository.findByEmail(userRequest.getEmail());
+        if (userOpt.isEmpty() || !userOpt.get().checkPassword(userRequest.getPassword())) {
+            return null;
+        }
+        return "access-token";
+    }
 }
