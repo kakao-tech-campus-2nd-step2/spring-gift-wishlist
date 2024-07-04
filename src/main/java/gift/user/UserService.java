b59170ae55c346e969b208ef0e332ad3c4086350
geist9110 <geist9110@gmail.com>
2024-07-04 22:37:58 +0900

    feat(authorize): Add login method in service layer

--- src/main/java/gift/user/UserService.java
+++ src/main/java/gift/user/UserService.java
@@ -1,5 +1,6 @@
 package gift.user;
 
+import com.github.dockerjava.api.exception.UnauthorizedException;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,5 +17,18 @@ public class UserService {
             throw new IllegalArgumentException("User already exists");
         }
         userRepository.addUser(user);
+    }
+
+    public String login(User user){
+        if (!userRepository.existUserByEmail(user.email())) {
+            throw new UnauthorizedException("User does not exist");
+        }
+        User findUser = userRepository.findUserByEmail(user.email());
+        if (!findUser.password().equals(user.password())){
+            throw new UnauthorizedException("Wrong password");
+        }
+
+        // TODO Token 생성 로직 추가 및 TOKEN 생성 결과값 리턴
+        return "token";
     }
 }
