b6e3a3fb714be97d4ea9bb9308df62e0cd452fb7
anacnoda77 <sm1108shin@naver.com>
2024-07-04 21:49:48 +0900

    feat: AuthInterceptor 작성

---
+++ src/main/java/gift/auth/AuthInterceptor.java
@@ -0,0 +1,33 @@
+package gift.auth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class AuthInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+        Object handler) throws Exception {
+
+        HttpSession session = request.getSession(false);
+
+        if (session == null || session.getAttribute("member") == null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+        ModelAndView modelAndView) throws Exception {
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+        Object handler, Exception ex) throws Exception {
+    }
+}
