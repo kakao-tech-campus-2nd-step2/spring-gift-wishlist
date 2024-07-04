bcba8d01843dbf1caab7c6801d03df596315e1a2
ajy4304 <ajy4304@naver.com>
2024-07-04 18:37:24 +0900

    feat(UserController): User에 관한 컨트롤러 생성
    회원가입과 로그인하면 accessToken이라는 문구를 넘겨주는 메서드들로 구성

--- src/main/java/gift/controller/UserController.java
+++ src/main/java/gift/controller/UserController.java
@@ -1,2 +1,36 @@
-package gift.controller;public class UserController {
+package gift.controller;
+
+import gift.dto.UserRequestDTO;
+import gift.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> register(@RequestBody UserRequestDTO userRequest) {
+        userService.register(userRequest);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+    // 사용자 로그인
+    @PostMapping("/login/token")
+    public ResponseEntity<Map<String, String>> login(@RequestBody UserRequestDTO userRequest) {
+        String token = userService.authenticate(userRequest);
+        if (token == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        Map<String, String> response = new HashMap<>();
+        response.put("accessToken", token);
+        return ResponseEntity.ok(response);
+    }
 }
