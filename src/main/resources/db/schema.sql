8bfcbb2b188f245b3fc13319213f908475b4ddfe
hjinshin <gudwls818@gmail.com>
2024-07-04 23:50:39 +0900

    feat: wish 테이블 생성

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,15 +1,27 @@
 drop table if exists product;
 create table product(
-                        id bigint auto_increment primary key,
-                        name varchar(255) not null ,
-                        price int not null ,
-                        imageUrl varchar(1000) not null
+    id bigint auto_increment primary key,
+    name varchar(255) not null ,
+    price int not null ,
+    imageUrl varchar(1000) not null
 );
 
 drop table if exists member;
 create table member(
-                        id bigint auto_increment primary key,
-                        email varchar(50) unique not null ,
-                        password varchar(50) not null
+    id bigint auto_increment primary key,
+    email varchar(50) unique not null,
+    password varchar(50) not null
 );
+
+drop table if exists wish;
+create table wish(
+    id bigint auto_increment primary key,
+    product_id bigint not null,
+    member_id bigint not null,
+    createdAt DATETIME default current_timestamp,
+    updatedAt DATETIME default current_timestamp on update current_timestamp,
+    foreign key (product_id) references product(id) on delete cascade on update cascade,
+    foreign key (member_id) references member(id) on delete cascade on update cascade
+);
+CREATE INDEX idx_member_id ON wish(member_id);
 
