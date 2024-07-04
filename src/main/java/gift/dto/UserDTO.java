e3c09db1e1578dfccf2be0c9473ab09c587d219e
김수랑 <we6610@gmail.com>
2024-07-04 16:51:20 +0900

    :sparkles: feat: UserDTO의 기본적인 Validation 부분 추가

--- src/main/java/gift/dto/UserDTO.java
+++ src/main/java/gift/dto/UserDTO.java
@@ -1,10 +1,13 @@
 package gift.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 
 public record UserDTO(
+        @NotNull(message = "비밀번호를 입력해주세요.")
         String password,
-        @Email
+
+        @Email(message = "올바른 이메일 형식이 아닙니다.")
         String email
 ) {
 }
