c659e32d23c5818df2aafc397d64c2bde275becb
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-05 01:20:21 +0900

    feat : JwtUtil 추가

--- src/main/java/gift/utils/JwtUtil.java
+++ src/main/java/gift/utils/JwtUtil.java
@@ -1,2 +1,53 @@
-package gift.utils;public class JwtUtil {
+package gift.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    // 사용자 이름 추출
+    public String extractUsername(String token) {
+        return Jwts.parser()
+            .setSigningKey(secret)
+            .parseClaimsJws(token)
+            .getBody()
+            .getSubject();
+    }
+
+    // 토큰 만료 여부 확인
+    private Boolean isTokenExpired(String token) {
+        return Jwts.parser()
+            .setSigningKey(secret)
+            .parseClaimsJws(token)
+            .getBody()
+            .getExpiration()
+            .before(new Date());
+    }
+
+    // 토큰 생성
+    public String generateToken(String username) {
+        return Jwts.builder()
+            .setSubject(username)
+            .setIssuedAt(new Date())
+            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))  // 10시간
+            .signWith(SignatureAlgorithm.HS256, secret)
+            .compact();
+    }
+
+    // 토큰 검증
+    public Boolean validateToken(String token, String username) {
+        final String extractedUsername = extractUsername(token);
+        return (extractedUsername.equals(username) && !isTokenExpired(token));
+    }
 }
+
+
