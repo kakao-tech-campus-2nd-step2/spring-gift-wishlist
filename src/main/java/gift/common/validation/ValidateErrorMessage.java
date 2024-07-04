9d9349e25d768bd53312628222bd49e9cb9292d7
sanghee0820 <lsh277604@gmail.com>
2024-07-04 23:31:49 +0900

    feat: UserRequest 관련 errorMessage 추가

--- src/main/java/gift/common/validation/ValidateErrorMessage.java
+++ src/main/java/gift/common/validation/ValidateErrorMessage.java
@@ -8,4 +8,8 @@ public class ValidateErrorMessage {
     public static final String INVALID_PRODUCT_PRICE_RANGE = "가격은 1원 이상, 21억원 이하이어야 합니다.";
     public static final String INVALID_PRODUCT_IMG_URL_NULL = "이미지 URL은 필수 입력값입니다.";
     public static final String INVALID_PRODUCT_IMG_URL_FORMAT = "이미지 URL 형식이 올바르지 않습니다.";
+
+    public static final String INVALID_USER_NAME_NULL = "사용자 이름은 필수 입력값입니다.";
+    public static final String INVALID_USER_NAME_PATTERN = "사용자 이름은 이메일 형식이어야 합니다.";
+    public static final String INVALID_USER_PASSWORD_NULL = "비밀번호는 필수 입력값입니다.";
 }
