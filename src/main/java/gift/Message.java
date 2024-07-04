2e77b0f2ffec15e151b15896cb33a62468a40dac
minju26 <mjalswn26@gmail.com>
2024-07-04 22:32:51 +0900

    feat: Error message 추가

--- src/main/java/gift/Message.java
+++ src/main/java/gift/Message.java
@@ -1,7 +1,14 @@
 package gift;
 
 public class Message {
+
+    /*SUCCESS*/
     public static final String ADD_SUCCESS_MSG = "상품 추가 성공";
     public static final String UPDATE_SUCCESS_MSG = "상품 수정 성공";
     public static final String DELETE_SUCCESS_MSG = "상품 삭제 성공";
+
+    /*ERROR*/
+    public static final String LENGTH_ERROR_MSG = "상품 이름은 공백 포함 15자를 초과할 수 없습니다.";
+    public static final String SPECIAL_CHAR_ERROR_MSG = "특수 문자는 (), [], +, -, &, /, _ 외에는 사용할 수 없습니다.";
+    public static final String KAKAO_CONTAIN_ERROR_MSG = "상품 이름에 \"카카오\"가 포함된 경우에는 담당 MD와 협의가 필요합니다.";
 }
