095253998bab7e96b025209a5608f85803225e12
anacnoda77 <sm1108shin@naver.com>
2024-07-04 15:14:08 +0900

    feat: Member 도메인 객체 생성

---
+++ src/main/java/gift/model/Member.java
@@ -0,0 +1,35 @@
+package gift.model;
+
+public class Member {
+    private Long id;
+    private String email;
+    private String password;
+
+    public Member() {
+    }
+
+    public Member(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
