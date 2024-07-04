93f7ea60f1cee915568145fc67eab5537b0c943f
Seunghyun Ko <chris00825@naver.com>
2024-07-04 22:02:55 +0900

    feat: init member object

--- src/main/java/gift/product/controller/MemberController.java
+++ src/main/java/gift/product/controller/MemberController.java
@@ -1,2 +1,11 @@
-package gift.product.controller;public class MemberController {
+package gift.product.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/member")
+public class MemberController {
 }
---
+++ src/main/java/gift/product/model/Member.java
@@ -0,0 +1,25 @@
+package gift.product.model;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class Member {
+
+    @NotBlank(message = "이메일은 필수 입력 요소입니다.")
+    private String email;
+
+    @NotBlank(message = "비밀번호는 필수 입력 요소입니다.")
+    private String password;
+
+    public Member(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
