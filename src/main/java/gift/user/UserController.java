7c4764f592545d175f329f0145e33f54e238a2d4
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-07-05 01:10:31 +0900

    fix : UserController API 뷰 매핑 삭제. API 구현으로 수정

--- src/main/java/gift/user/UserController.java
+++ src/main/java/gift/user/UserController.java
@@ -2,20 +2,24 @@ package gift.user;
 
 
 import gift.ProductRepository;
+import gift.ResponseDTO;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@Controller
-@RequestMapping("/user")
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/members")
 public class UserController {
+
     private final ProductRepository productRepository;
     private final UserService userService;
     private final JWTService jwtService;
@@ -25,60 +29,56 @@ public class UserController {
         this.jwtService = jwtService;
     }
 
-    //로그인 화면
-    @GetMapping("/login")
-    public String loginView(Model model){
-        model.addAttribute("loginDTO", new LoginDTO());
-        return "Login";
-    }
-
-    //회원 가입 화면
-    @GetMapping("/signup")
-    public String signupView(Model model){
-        model.addAttribute("userDTO", new UserDTO());
-        return "SignUp";
-    }
-
-    //메인 화면
-    @GetMapping("/main")
-    public String mainView(Model model){
-        model.addAttribute("products", productRepository.selectProducts());
-        return "MainView";
-    }
-
     //로그인 API
     @PostMapping("/login")
-    public String loginUser(@Valid @ModelAttribute("loginDTO") LoginDTO loginDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes){
+    public ResponseEntity<?> loginUser(@RequestBody @Valid LoginDTO loginDTO, BindingResult bindingResult){
         if(!bindingResult.hasErrors() && !userService.validateUser(loginDTO)){
-
             bindingResult.addError(new FieldError("loginDTO", "password", "이메일이 존재하지 않거나 비밀번호가 일치하지 않습니다"));
         }
 
         if(bindingResult.hasErrors()){
-            return "Login";
-        }
+            HashMap<String, String> map = new HashMap<>();
+            bindingResult.getFieldErrors().forEach(err -> {
+                map.put(err.getField(), err.getDefaultMessage());
+            });
 
+            ResponseDTO responseDTO = new ResponseDTO(
+                    HttpStatus.FORBIDDEN,
+                    "Validation Error",
+                    map
+            );
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
 
         UserDTO userDTO = userService.getUserDTOByLoginDTO(loginDTO);
         String accessToken = jwtService.generateAccessToken(userDTO);
-        redirectAttributes.addAttribute("accessToken", accessToken);
-        return "redirect:/user/main";
+
+        return ResponseEntity.ok(new Token(accessToken));
     }
 
     //회원 가입 API
     @PostMapping("/signup")
-    public String signupUser(@Valid @ModelAttribute("userDTO") UserDTO userDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes){
+    public ResponseEntity<?> signupUser(@RequestBody @Valid UserDTO userDTO, BindingResult bindingResult){
         if(userService.checkIfDuplicatedEmail(userDTO.email)){
             bindingResult.addError(new FieldError("userDTO", "email", "이미 등록된 이메일입니다."));
         }
 
         if(bindingResult.hasErrors()){
-            return "SignUp";
+            HashMap<String, String> map = new HashMap<>();
+            bindingResult.getFieldErrors().forEach(err -> {
+                map.put(err.getField(), err.getDefaultMessage());
+            });
+
+            ResponseDTO responseDTO = new ResponseDTO(
+                    HttpStatus.FORBIDDEN,
+                    "Validation Error",
+                    map
+            );
+            return ResponseEntity.badRequest().body(responseDTO);
         }
 
         userService.registerUser(userDTO);
         String accessToken = jwtService.generateAccessToken(userDTO);
-        redirectAttributes.addAttribute("accessToken", accessToken);
-        return "redirect:/user/main";
+        return ResponseEntity.ok(new Token(accessToken));
     }
 }
