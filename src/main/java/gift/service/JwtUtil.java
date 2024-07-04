cfbcc530fdfec75906a1f1d3d4caacc97a30583e
wvssm <neoramiregi@gmail.com>
2024-07-04 19:00:50 +0900

    feat(JwtUtil): 토큰 생성 기능 구현

---
+++ src/main/java/gift/service/JwtUtil.java
@@ -0,0 +1,21 @@
+package gift.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
+public class JwtUtil {
+    private static final String SECRET_KEY = "jinyoung";
+    private static final long EXPIRATION_TIME = 86400000;
+
+    // JWT 생성
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .compact();
+    }
+}
