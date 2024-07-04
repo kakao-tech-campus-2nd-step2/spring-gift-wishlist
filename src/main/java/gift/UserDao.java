5c3ea500ff5883d566e6c14d3a658c56fa8e278f
hyyyh0x <hyyyh0x@gmail.com>
2024-07-05 00:12:30 +0900

    feat: UserDao file

---
+++ src/main/java/gift/UserDao.java
@@ -0,0 +1,33 @@
+package gift;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void insertUser(User user) {
+        String sql = "INSERT INTO users (email, password) VALUES (?, ?)";
+        jdbcTemplate.update(sql, user.getEmail(), user.getPassword());
+    }
+
+    public User selectUser(String email) {
+        String sql = "SELECT id, email, password FROM users WHERE email = ?";
+        try {
+            return jdbcTemplate.queryForObject(sql, new Object[]{email}, (resultSet, rowNum) ->
+                new User(
+                    resultSet.getString("email"),
+                    resultSet.getString("password")
+                ));
+        } catch (EmptyResultDataAccessException ex) {
+            return null;
+        }
+    }
+}
