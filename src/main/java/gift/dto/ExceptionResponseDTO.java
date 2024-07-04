8fb79979e835e3c5b4a5a83a2dfae88fbc959589
mac <20j.code@gmail.com>
2024-07-05 06:30:58 +0900

    chore : 가독성을 위한 정렬

--- src/main/java/gift/dto/ExceptionResponseDTO.java
+++ src/main/java/gift/dto/ExceptionResponseDTO.java
@@ -1,13 +1,11 @@
 package gift.dto;
 
 public class ExceptionResponseDTO {
-
     private String message;
 
     public ExceptionResponseDTO(String message) {
         this.message = message;
     }
-
     public String getMessage() {
         return message;
     }
--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -12,14 +12,19 @@ public interface ProductService {
 
     //상품 리스트 전체 조회
     List<ProductDTO> readAll();
+
     //새 상품 생성
     void create(ProductDTO prod);
+
     //상품 이름 변경
-    void updateName(long id,String name);
+    void updateName(long id, String name);
+
     //상품 가격 변경
-    void updatePrice(long id,int price);
+    void updatePrice(long id, int price);
+
     //상품 이미지 변경
-    void updateImageUrl(long id,String url);
+    void updateImageUrl(long id, String url);
+
     //상품 삭제
     void delete(long id);
 
