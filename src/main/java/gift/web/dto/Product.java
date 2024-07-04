45bdaf113692bc491e1da16309b4f347136aad4f
yoong-saks <wjdghtjd06@gmail.com>
2024-07-05 01:13:31 +0900

    feat: 상품명 유효성 검사

--- src/main/java/gift/web/dto/Product.java
+++ src/main/java/gift/web/dto/Product.java
@@ -1,4 +1,16 @@
 package gift.web.dto;
 
-public record Product(Long id, String name, Long price, String imageUrl) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record Product(
+    Long id,
+    @NotBlank
+    @Size(max=15)
+    @Pattern(regexp="^[가-힣 \\w\\(\\)\\[\\]\\+\\-\\&\\/]*$",
+             message = "( ), [ ], +, -, &, /, _의 특수문자만 사용 가능합니다.")
+    String name,
+    Long price,
+    String imageUrl) {
 }
