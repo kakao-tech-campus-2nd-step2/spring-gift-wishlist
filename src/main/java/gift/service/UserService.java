84d1227f7bc0f21c0fc23eabc5a615413edf69f8
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-05 01:29:58 +0900

    feat : UserService 추가

--- src/main/java/gift/service/UserService.java
+++ src/main/java/gift/service/UserService.java
@@ -1,2 +1,22 @@
-package gift.service;public class UserService {
+package gift.service;
+
+import gift.domain.User;
+import gift.repository.user.UserRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
 }
