6b84b57c7f753656fdf2be580142abe002312680
us4c0d3 <jwseok312@gmail.com>
2024-07-05 04:42:54 +0900

    refactor(Application): 필드 주입에서 생성자 주입으로 변경

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -1,6 +1,5 @@
 package gift;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,8 +9,11 @@ import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
+
+    public Application (JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
