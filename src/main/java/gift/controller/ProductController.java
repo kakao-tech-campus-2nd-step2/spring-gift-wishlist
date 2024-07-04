6bfa3e5ccbbe95117c22fa5ad5da1374de1663b9
mac <20j.code@gmail.com>
2024-07-04 15:43:23 +0900

    refact : api request를 위한 공통 경로 정리

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  * <p>
  * $/api/products
  */
+@RequestMapping("/api")
 @RestController
 public class ProductController {
 
@@ -28,49 +30,52 @@ public class ProductController {
 
     /**
      * 상품 전체 목록 반환
+     *
      * @return 상품 DTO
      */
-    @GetMapping("/api/products")
-    public List<ProductDTO> getList(){
+    @GetMapping("/products")
+    public List<ProductDTO> getList() {
         List<ProductDTO> dto = pm.readAll();
         return dto;
     }
 
     /**
      * 새로운 상품 생성
+     *
      * @param dto id가 존재하는 상태로 입력되더라도 무시됨.
      */
-    @PostMapping("/api/products")
-    public void add(@RequestBody ProductDTO dto){
+    @PostMapping("/products")
+    public void add(@RequestBody ProductDTO dto) {
         pm.create(dto);
     }
 
     /**
      * 기존 상품 수정
-     * @param id 수정하고자 하는 상품의 id
+     *
+     * @param id  수정하고자 하는 상품의 id
      * @param dto 수정하고자 하는 값 이외 null로 지정
      */
-    @PutMapping("/api/products")
-    public void update(@RequestParam("id") Long id, @RequestBody ProductDTO dto){
-        if(id==null){
+    @PutMapping("/products")
+    public void update(@RequestParam("id") Long id, @RequestBody ProductDTO dto) {
+        if (id == null) {
             throw new IllegalArgumentException("id를 입력해주세요");
         }
         changeCheckAndUpdate(id, dto);
     }
 
-    @DeleteMapping("/api/products")
-    public void delete(@RequestParam("id") Long id){
+    @DeleteMapping("/products")
+    public void delete(@RequestParam("id") Long id) {
         pm.delete(id);
     }
 
     private void changeCheckAndUpdate(Long id, ProductDTO dto) {
-        if (dto.getName()!=null){
+        if (dto.getName() != null) {
             pm.updateName(id, dto.getName());
         }
-        if (dto.getPrice()!=null){
+        if (dto.getPrice() != null) {
             pm.updatePrice(id, dto.getPrice());
         }
-        if (dto.getImageUrl()!=null){
+        if (dto.getImageUrl() != null) {
             pm.updateImageUrl(id, dto.getImageUrl());
         }
     }
