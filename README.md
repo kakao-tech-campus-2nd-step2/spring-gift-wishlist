35d0c77aac32edde17e8545cf09a55452ee27477
mogld <143364802+mogld@users.noreply.github.com>
2024-07-04 09:49:17 +0900

    부산대 BE_이진솔 2주차 과제 (1단계) (#162)
    * docs: update README
    
    * feat: add Exception
    
    * feat: add ProductService
    
    * feat: view for ErrorMessage
    
    * test: WebControllerTest

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # spring-gift-wishlist
+
+제품 입력에 대한 유효성 검사와 잘못된 입력에 대한 상세 오류 메세지를 클라이언트에게 제공한다.
+
+1. Product: 유효성 검사를 위한 annotation 사용 (null x, 최대 15자, 특수문자 제한, 카카오 x)
+2. GlobalExceptionHandler : 예외처리 발생시 핸들링
+3. ProductService: ProductController에서 비즈니스 로직과 서비스 호출 역할을 분리
--- src/main/java/gift/Product.java
+++ src/main/java/gift/Product.java
@@ -3,8 +3,18 @@ package gift;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+
 public class Product {
     private Long id;
+
+    @NotBlank(message = "상품을 입력해주세요")
+    @Size(max = 15, message = "상품 이름은 최대 15자까지 입력할 수 있습니다")
+    @Pattern(regexp = "^[가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9\\s\\(\\)\\[\\]\\+\\-\\&\\/\\_]+$", message = "특수문자는 (),[],+,-,&,/,_만 가능합니다")
+    @Pattern(regexp = "^(?!.*카카오).*$", message = "상품 이름에 '카카오'를 포함할 수 없습니다. 관리자와의 협의가 필요합니다.")
     private String name;
     private Integer price;
     private String imageUrl;
