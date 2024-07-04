839a3eb6ebad438fc679f2912bcbd86b215dcf86
5win <woozzoo7@gmail.com>
2024-07-05 03:47:33 +0900

    feat: Add logout function

--- src/main/resources/static/js/memberReq.js
+++ src/main/resources/static/js/memberReq.js
@@ -23,6 +23,16 @@ function register() {
     });
 }
 
+
+$(document).ready(function() {
+    var token = localStorage.getItem("token");
+    if (token) {
+        updateUI(true);
+    } else {
+        updateUI(false);
+    }
+});
+
 function login() {
 
     event.preventDefault();
@@ -38,12 +48,31 @@ function login() {
         data: JSON.stringify(formData),
         contentType: 'application/json',
         processData: false,
-        success: function (response) {
-            alert(response);
+        success: function (data, status, jqXHR) {
+            const token = jqXHR.getResponseHeader('Token');
+            localStorage.setItem("token", token);
+            console.log(token);
+            alert(data);
             location.href = '/';
+            updateUI(true);
         },
         error: function (request, status, error) {
             alert(request.responseText);
         }
     });
+}
+
+function logout() {
+    localStorage.removeItem('token');
+    updateUI(false);
+}
+
+function updateUI(isLoggedIn) {
+    if (isLoggedIn) {
+        $('#login-button').hide(); 
+        $('#logout-button').show();
+    } else {
+        $('#login-button').show();
+        $('#logout-button').hide(); 
+    }
 }
