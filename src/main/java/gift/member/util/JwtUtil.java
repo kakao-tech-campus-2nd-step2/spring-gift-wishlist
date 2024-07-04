c07e1d59f348cf3a7e01915fa8d2968a37232b69
sunandrabbit <sunandrabbit@gamil.com>
2024-07-04 16:36:10 +0900

    refactor : key와 expiration 정보를 application.properties에 설정함으로 정보 숨김

--- src/main/java/gift/member/util/JwtUtil.java
+++ src/main/java/gift/member/util/JwtUtil.java
@@ -2,22 +2,27 @@ package gift.member.util;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 import java.util.Date;
 
 import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtUtil {
 
-    private final SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long validityInMilliseconds;
 
     public String generateToken(String email) {
         return Jwts.builder()
             .setSubject(email)
             .setIssuedAt(new Date())
-            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+            .setExpiration(new Date(System.currentTimeMillis() + validityInMilliseconds))
+            .signWith(SignatureAlgorithm.HS256, secretKey)
             .compact();
     }
 
