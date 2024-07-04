907837f6cd78ea42efc694a7a73ddedd177c077f
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-07-04 15:36:37 +0900

    feat : UserController view mapping 뷰와 URL 매핑

---
+++ src/main/java/gift/user/UserController.java
@@ -0,0 +1,49 @@
+package gift.user;
+
+
+import gift.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    private final ProductRepository productRepository;
+    private final UserService userService;
+    private final JWTService jwtService;
+    public UserController(ProductRepository productRepository, UserService userService, JWTService jwtService) {
+        this.productRepository = productRepository;
+        this.userService = userService;
+        this.jwtService = jwtService;
+    }
+
+    //로그인 화면
+    @GetMapping("/login")
+    public String loginView(Model model){
+        model.addAttribute("loginDTO", new LoginDTO());
+        return "Login";
+    }
+
+    //회원 가입 화면
+    @GetMapping("/signup")
+    public String signupView(Model model){
+        model.addAttribute("userDTO", new UserDTO());
+        return "SignUp";
+    }
+
+    //메인 화면
+    @GetMapping("/main")
+    public String mainView(Model model){
+        model.addAttribute("products", productRepository.selectProducts());
+        return "MainView";
+    }
+
+}
