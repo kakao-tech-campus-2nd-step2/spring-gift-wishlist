6d4032cfa00ba6dbd7cf6ae72cc9567dc84ffdc0
wvssm <neoramiregi@gmail.com>
2024-07-04 18:52:10 +0900

    feat(CommonResponse): 일반 message 전달 객체 생성

---
+++ src/main/java/gift/dto/CommonResponse.java
@@ -0,0 +1,17 @@
+package gift.dto;
+
+public class CommonResponse {
+    private String message;
+
+    public CommonResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
