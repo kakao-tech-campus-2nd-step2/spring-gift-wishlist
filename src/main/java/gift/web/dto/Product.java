6ecd46a30aba9e3f075440955b54a7fa6059c21b
yoong-saks <wjdghtjd06@gmail.com>
2024-07-05 07:23:57 +0900

    feat: '카카오' 문구에 대한 유효성 검증 추가

--- src/main/java/gift/web/dto/Product.java
+++ src/main/java/gift/web/dto/Product.java
@@ -11,6 +11,8 @@ public record Product(
     @Size(max=15)
     @Pattern(regexp="^[가-힣 \\w\\(\\)\\[\\]\\+\\-\\&\\/]*$",
              message = "( ), [ ], +, -, &, /, _의 특수문자만 사용 가능합니다.")
+    @Pattern(regexp="^(?!.*카카오).*$",
+             message = "\"카카오\"가 포함된 문구는 담당 MD와 협의한 경우에만 사용할 수 있습니다.")
     String name,
     @PositiveOrZero(message = "가격은 0원 이상이어야 합니다.")
     Long price,
