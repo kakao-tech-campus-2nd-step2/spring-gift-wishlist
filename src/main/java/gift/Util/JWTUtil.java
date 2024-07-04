c957340baf36ec466b9ea547f1bc503d5c0f9199
canyos <4581974@naver.com>
2024-07-04 23:12:59 +0900

    token 생성, login구현

--- src/main/java/gift/Util/JWTUtil.java
+++ src/main/java/gift/Util/JWTUtil.java
@@ -4,28 +4,30 @@ import gift.entity.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
+
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 public class JWTUtil {
-    // 설정파일로 빼서 환경변수로 사용하는 것을 권장
-    private final String SECRET = "secret";
 
+    private static final SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
     /**
      * 토큰 생성
      */
-    public String generateToken(User user) {
+    public static String generateToken(User user) {
         Claims claims = Jwts.claims();
         claims.put("username", user.getUserId());
-        return createToken(claims, user.getUserId()); // username을 subject로 해서 token 생성
+        return createToken(claims, user.getUserId());
     }
 
-    private String createToken(Claims claims, String subject) {
+    private static String createToken(Claims claims, String subject) {
         return Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 1시간
-                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256, key)
                 .compact();
     }
 
@@ -44,7 +46,7 @@ public class JWTUtil {
     private Claims getAllClaims(String token) {
         //log.info("getAllClaims token = {}", token);
         return Jwts.parser()
-                .setSigningKey(SECRET)
+                .setSigningKey(key)
                 .parseClaimsJws(token)
                 .getBody();
     }
--- src/main/java/gift/service/UserService.java
+++ src/main/java/gift/service/UserService.java
@@ -1,19 +1,36 @@
 package gift.service;
 
+import gift.Util.JWTUtil;
+import gift.entity.Token;
 import gift.entity.User;
 import gift.exception.*;
 import gift.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class UserService {
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
-    public void signUp(String userId, String password) {
-        if(userRepository.isExistAccount(userId))
-            throw new Exception404("이미 존재하는 계정");
-        userRepository.saveUser(new User(userId, password));
+    public void signUp(String email, String password) {
+        if(userRepository.isExistAccount(email))
+            throw new Exception400("이미 존재하는 계정");
+        userRepository.saveUser(new User(email, password));
+    }
+
+    public Token signIn(String email, String password){
+        User user = userRepository.findUserbyID(email);
+        if(!userRepository.isExistAccount(email))
+            throw new Exception404("존재하지 않는 계정");
+        if (!passwordEncoder.matches(password, user.getUserPw()))
+            throw new Exception400("비밀번호가 일치하지 않습니다.");
+
+        return new Token(JWTUtil.generateToken(user));
     }
 }
