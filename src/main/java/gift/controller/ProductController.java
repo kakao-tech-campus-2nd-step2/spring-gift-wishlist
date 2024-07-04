2be8146409dc30d70a8552652d05edad11d3242a
seolbin96 <jsb3038@jnu.ac.kr>
2024-07-04 14:21:09 +0900

    fix : 불필요한 코드 삭제

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -66,14 +66,6 @@ public class ProductController {
         }
 
         String productName = product.name();
-        if (productName.length() > 15) {
-            return new ResponseEntity<>("상품 이름은 15자 이내여야 합니다.", HttpStatus.BAD_REQUEST);
-        }
-
-        Pattern specialCharPattern = Pattern.compile("[^ㄱ-ㅎ가-힣a-zA-Z0-9\\s\\(\\)\\[\\]+&\\-/_]");
-        if (specialCharPattern.matcher(productName).find()) {
-            return new ResponseEntity<>("상품 이름에 유효하지 않은 문자가 포함되어 있습니다.", HttpStatus.BAD_REQUEST);
-        }
 
         if (productName.contains("카카오")) {
             return new ResponseEntity<>("'카카오'를 이름에 포함하려면 담당 MD와 협의해주세요.", HttpStatus.BAD_REQUEST);
