da265adb05be20de8c2d3ef720e77a5021456a85
kangwlgns <kangji0615@gmail.com>
2024-07-05 03:26:58 +0900

    feat: add registration controller and view

---
+++ src/main/java/gift/controller/RegistrationPageController.java
@@ -0,0 +1,23 @@
+package gift.controller;
+
+import gift.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RegistrationPageController {
+
+    private final UserService userService;
+    private static final String REGISTRATION_PAGE = "html/registration";
+
+    @Autowired
+    public RegistrationPageController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users/registration-page")
+    public String getRegistrationPage() {
+        return REGISTRATION_PAGE;
+    }
+}
---
+++ src/main/java/gift/service/UserService.java
@@ -0,0 +1,8 @@
+package gift.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
