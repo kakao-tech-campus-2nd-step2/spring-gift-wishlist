5c0cf25c4e003d3babd9d87bff3b339c266205de
seolbin96 <jsb3038@jnu.ac.kr>
2024-07-05 00:47:48 +0900

    feat : application.properties, data.sql, schema.sql 업데이트

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,15 @@
 DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE products (
                           id BIGINT AUTO_INCREMENT PRIMARY KEY,
                           name VARCHAR(255) NOT NULL,
                           price INT NOT NULL,
                           imageUrl VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE users (
+                       id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                       email VARCHAR(255) NOT NULL UNIQUE,
+                       password VARCHAR(255) NOT NULL
 );
