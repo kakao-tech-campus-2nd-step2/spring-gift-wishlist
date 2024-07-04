722cb2db8ecd9800e6f07e5bfc1de485a2a945c7
pswaao88 <pswaao88@gmail.com>
2024-07-04 14:46:24 +0900

    Feat: MemberAccessToken클래스 작성

--- src/main/java/gift/Model/MemberAccessToken.java
+++ src/main/java/gift/Model/MemberAccessToken.java
@@ -1,5 +1,12 @@
 package gift.Model;
 
 public class MemberAccessToken {
+    private final boolean accessToken;
+    public MemberAccessToken(boolean accessToken){
+        this.accessToken = accessToken;
+    }
 
+    public boolean getAccessToken() {
+        return accessToken;
+    }
 }
