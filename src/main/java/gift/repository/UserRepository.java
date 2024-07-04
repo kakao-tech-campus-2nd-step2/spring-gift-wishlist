ee7f9dc921fc1664f01d003eb58129e74d79ac6f
Kiwoong Kim <gw150311@naver.com>
2024-07-05 00:08:52 +0900

    feat(UserService): 회원가입 기능 구현 중 - registerUser 메서드 구현 - UserRepository 클래스 생성

---
+++ src/main/java/gift/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package gift.repository;
+
+import gift.DTO.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public UserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void addUser(User user) {
+
+    }
+}
--- src/main/java/gift/service/UserService.java
+++ src/main/java/gift/service/UserService.java
@@ -1,11 +1,23 @@
 package gift.service;
 
 import gift.DTO.SignupRequest;
+import gift.DTO.User;
+import gift.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public void registerUser(SignupRequest signupRequest) {
+        User user = new User(signupRequest.getEmail(), signupRequest.getPassword());
+        userRepository.addUser(user);
     }
 }
