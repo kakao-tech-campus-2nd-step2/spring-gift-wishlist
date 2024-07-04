9443772dba0b64bbda9adef0f24f023b97473b4b
kanguk <lm0828@naver.com>
2024-07-04 22:22:51 +0900

    feat : 로그인, 회원가입 페이지 구현
    feat : 로그인, 회원가입 페이지 구현
    
    로그인, 회원가입 페이지를 구현한다. 각각 /users/register, /users/login 엔드포인트이다.

---
+++ src/main/java/gift/controller/UserViewController.java
@@ -0,0 +1,20 @@
+package gift.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserViewController {
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String showRegisterForm() {
+        return "register";
+    }
+}
