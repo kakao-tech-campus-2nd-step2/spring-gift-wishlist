515c83f6e56358f268aee8d70b89164e8b931c59
seojiu <seojiu5598@gmail.com>
2024-07-05 08:08:53 +0900

    feat(MemberService.java): MemberService 클래스 생성

---
+++ src/main/java/gift/MemberService.java
@@ -0,0 +1,14 @@
+package gift;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository;
+    private final JwtUtil jwtUtil;
+
+    public MemberService(MemberRepository memberRepository, JwtUtil jwtUtil) {
+        this.memberRepository = memberRepository;
+        this.jwtUtil = jwtUtil;
+    }
+}
