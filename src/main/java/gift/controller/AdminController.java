59b8c34f6f43fc351d63767557f869ddb17aaca8
sanghee0820 <lsh277604@gmail.com>
2024-07-04 17:33:29 +0900

    style: 코드 포메팅

--- src/main/java/gift/controller/AdminController.java
+++ src/main/java/gift/controller/AdminController.java
@@ -4,12 +4,17 @@ import gift.common.exception.ProductNotFoundException;
 import gift.controller.dto.request.ProductRequest;
 import gift.model.Product;
 import gift.model.repository.ProductRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
@@ -22,19 +27,14 @@ public class AdminController {
     }
 
     @GetMapping()
-    public String listProducts(
-            Model model
-    ) {
+    public String listProducts(Model model) {
         List<Product> products = productRepository.findAll().stream().toList();
         model.addAttribute("products", products);
         return "admin/list";
     }
 
     @GetMapping("/products/{id}")
-    public String showEditProductForm(
-            @PathVariable("id") Long id,
-            Model model
-    ) {
+    public String showEditProductForm(@PathVariable("id") Long id, Model model) {
         Product product = productRepository.find(id)
                 .orElseThrow(() -> ProductNotFoundException.of(id));
 
@@ -44,19 +44,14 @@ public class AdminController {
     }
 
     @PostMapping("/products")
-    public String productSave(
-            @RequestBody ProductRequest newProduct
-    ) {
+    public String productSave(@RequestBody ProductRequest newProduct) {
         productRepository.save(newProduct.toModel());
 
         return "admin/list";
     }
 
     @PatchMapping("/products/{id}")
-    public String productModify(
-            @PathVariable("id") Long id,
-            @RequestBody ProductRequest modifyProduct
-    ) {
+    public String productModify(@PathVariable("id") Long id, @RequestBody ProductRequest modifyProduct) {
         Product findedProduct = productRepository.find(id)
                 .orElseThrow(() -> ProductNotFoundException.of(id));
 
@@ -66,9 +61,7 @@ public class AdminController {
     }
 
     @DeleteMapping("/products/{id}")
-    public String productDelete(
-            @PathVariable("id") Long id
-    ) {
+    public String productDelete(@PathVariable("id") Long id) {
         Product findedProduct = productRepository.find(id)
                 .orElseThrow(() -> ProductNotFoundException.of(id));
 
