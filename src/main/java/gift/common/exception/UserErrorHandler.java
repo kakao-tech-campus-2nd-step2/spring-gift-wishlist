96bae96127025ae13ed99ee9f4508d6111fbe095
sanghee0820 <lsh277604@gmail.com>
2024-07-05 00:00:11 +0900

    refactor: 반환값을 responseEntity로 변경

--- src/main/java/gift/common/exception/UserErrorHandler.java
+++ src/main/java/gift/common/exception/UserErrorHandler.java
@@ -2,19 +2,23 @@ package gift.common.exception;
 
 import gift.common.util.ApiResponse;
 import gift.controller.UserController;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice(basePackageClasses = UserController.class)
 public class UserErrorHandler {
-
     @ExceptionHandler(UserAlreadyExistsException.class)
-    public ApiResponse<String> handleUserAlreadyExistsException(UserAlreadyExistsException e) {
-        return ApiResponse.error(e.getStatus(), e.getMessage());
+    public ResponseEntity<ApiResponse<String>> handleUserAlreadyExistsException(UserAlreadyExistsException e) {
+        return ResponseEntity
+                .status(e.getStatus())
+                .body(ApiResponse.error(e.getStatus(), e.getMessage()));
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ApiResponse<String> handleUserNotFoundException(UserNotFoundException e) {
-        return ApiResponse.error(e.getStatus(), e.getMessage());
+    public ResponseEntity<ApiResponse<String>> handleUserNotFoundException(UserNotFoundException e) {
+        return ResponseEntity
+                .status(e.getStatus())
+                .body(ApiResponse.error(e.getStatus(), e.getMessage()));
     }
 }
