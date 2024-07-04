5f6019b5f102ea01d63a57d4164282884c46065b
seoyoung-dev <seoyoung.dev@gmail.com>
2024-07-05 02:38:40 +0900

    style: schema.sql 띄어쓰기 제거

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE product (
-                         id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                         name VARCHAR(255) NOT NULL,
-                         price DOUBLE NOT NULL,
-                         image_url VARCHAR(255)
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price DOUBLE NOT NULL,
+    image_url VARCHAR(255)
 );
 
 CREATE TABLE members (
