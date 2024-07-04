914492e4c1ceef4c3bbc103414da682172c75e97
nove1080 <skwpqjq@gmail.com>
2024-07-04 17:12:46 +0900

    feat: MemberRepository 추가

---
+++ src/main/java/gift/repository/MemberRepository.java
@@ -0,0 +1,55 @@
+package gift.repository;
+
+import gift.domain.Member;
+import gift.domain.Member.Builder;
+import gift.domain.vo.Email;
+import gift.domain.vo.Password;
+import java.sql.PreparedStatement;
+import java.util.Objects;
+import java.util.Optional;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public MemberRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Long save(Member member) {
+        String sql = "insert into member (email, password, name) values (?, ?, ?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
+            ps.setString(1, member.getName());
+            ps.setString(2, member.getEmail().getEmailValue());
+            ps.setString(3, member.getPassword().getPasswordValue());
+            return ps;
+        }, keyHolder);
+
+        return Objects.requireNonNull(keyHolder.getKey()).longValue();
+    }
+
+    public Optional<Member> findById(Long id) {
+        String sql = "select id, email, password, name from member where id = ?";
+        Member findMember = jdbcTemplate.queryForObject(sql, getMemberRowMapper(), id);
+
+        return Optional.ofNullable(findMember);
+    }
+
+    private RowMapper<Member> getMemberRowMapper() {
+        return (rs, rowNum) -> new Builder()
+            .id(rs.getLong("id"))
+            .name(rs.getString("name"))
+            .password(Password.from(rs.getString("password")))
+            .email(Email.from(rs.getString("email")))
+            .build();
+    }
+}
