e6d251288beffe0e02ba8795a093ffc4e43543fd
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-07-05 03:28:36 +0900

    test: 회원가입 후 로그인 및 삭제하는 테스트 추가
    회원가입 후 로그인 및 삭제하는 테스트 추가

--- src/test/java/gift/controller/MemberControllerTest.java
+++ src/test/java/gift/controller/MemberControllerTest.java
@@ -1,7 +1,10 @@
 package gift.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import gift.dto.LoginRequest;
 import gift.dto.RegisterRequest;
+import gift.util.AuthServiceUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -66,5 +70,63 @@ class MemberControllerTest {
 
         result.andExpect(status().isBadRequest())
                 .andExpect(content().string("허용되지 않은 형식의 패스워드입니다."));
+    }
+
+    @Test
+    @DisplayName("허용되지 않는 형식의 이메일로 로그인 요청하기")
+    void loginFailWithEmail() throws Exception {
+        var result = mockMvc.perform(post("/api/members/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new LoginRequest("test@hello", "testPassword"))));
+
+        result.andExpect(status().isBadRequest())
+                .andExpect(content().string("허용되지 않은 형식의 이메일입니다."));
+    }
+
+    @Test
+    @DisplayName("허용되지 않는 형식의 패스워드로 로그인 요청하기")
+    void loginFailWithPassword() throws Exception {
+        var result = mockMvc.perform(post("/api/members/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new LoginRequest("test@naver.com", "잘못된패스워드"))));
+
+        result.andExpect(status().isBadRequest())
+                .andExpect(content().string("허용되지 않은 형식의 패스워드입니다."));
+    }
+
+    @Test
+    @DisplayName("정상적으로 회원가입 후 로그인 요청하기")
+    void registerAndLoginSuccess() throws Exception {
+        var register = mockMvc.perform(post("/api/members/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new RegisterRequest("테스트", "test@naver.com", "testPassword"))));
+
+        register.andExpect(status().isOk());
+
+        var login = mockMvc.perform(post("/api/members/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new LoginRequest("test@naver.com", "testPassword"))));
+
+        login.andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("정상적으로 회원가입 후 삭제하기")
+    void registerAndDeleteSuccess() throws Exception {
+        var register = mockMvc.perform(post("/api/members/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new RegisterRequest("테스트", "test@naver.com", "testPassword"))));
+
+        var result = register.andExpect(status().isOk()).andReturn();
+        var responseContent = result.getResponse().getContentAsString();
+        var token = JsonPath.parse(responseContent).read("$.token");
+
+        var id = AuthServiceUtils.getMemberIdWithToken(token.toString());
+
+        var deleted = mockMvc.perform(delete("/api/members/"+id)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization","Bearer "+token));
+
+        deleted.andExpect(status().isNoContent());
     }
 }
