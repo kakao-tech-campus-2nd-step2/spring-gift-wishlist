936720bc7156902d395aecc46eb67aebdd5e0ddb
sanghee0820 <lsh277604@gmail.com>
2024-07-04 23:30:48 +0900

    feat: User 관련 Dao 추가

---
+++ src/main/java/gift/model/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package gift.model.repository;
+
+import gift.model.User;
+import java.util.Optional;
+
+public interface UserRepository extends RepositoryInterface<User, Long> {
+    Optional<User> findByUsernameAndPassword(String username, String password);
+}
