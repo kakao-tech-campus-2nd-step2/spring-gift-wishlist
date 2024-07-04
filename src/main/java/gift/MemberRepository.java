0bea54a255349ddccab1d848e963f10097681cfa
seojiu <seojiu5598@gmail.com>
2024-07-05 08:03:24 +0900

    feat(MemberRepository.java): Resultset을 Member 객체로 변환해주는 메서드

--- src/main/java/gift/MemberRepository.java
+++ src/main/java/gift/MemberRepository.java
@@ -1,8 +1,10 @@
 package gift;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+
 import javax.sql.DataSource;
 
 @Repository
@@ -16,4 +18,12 @@ public class MemberRepository {
                 .withTableName("members")
                 .usingGeneratedKeyColumns("id");
     }
+
+    private final RowMapper<Member> MemberRowMapper = (Resultset, rowNum) -> {
+        Member member = new Member();
+        member.setId(Resultset.getLong("id"));
+        member.setEmail(Resultset.getString("email"));
+        member.setPassword(Resultset.getString("password"));
+        return member;
+    };
 }
