f400a52b1d23026cf232395c8d0874d3abda33d7
Profile-exe <kang67572346@gmail.com>
2024-07-05 01:29:27 +0900

    feat(ProductService): 비즈니스 로직을 수행하는 서비스 계층 구현

--- src/main/java/gift/product/controller/ProductController.java
+++ src/main/java/gift/product/controller/ProductController.java
@@ -1,11 +1,9 @@
 package gift.product.controller;
 
-import gift.product.Product;
 import gift.product.dto.ProductReqDto;
 import gift.product.dto.ProductResDto;
-import gift.product.exception.ProductNotFoundException;
 import gift.product.message.ProductInfo;
-import gift.product.repository.ProductRepository;
+import gift.product.service.ProductService;
 import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.http.HttpStatus;
@@ -23,63 +21,44 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class ProductController {
 
-    private final ProductRepository productRepository;
+    private final ProductService productService;
 
-    public ProductController(ProductRepository productRepository) {
-        this.productRepository = productRepository;
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
 
     @GetMapping("/products")
     public ResponseEntity<List<ProductResDto>> getProducts() {
-        List<Product> products = productRepository.findProducts();
-
-        List<ProductResDto> productResDtos = products.stream()
-                .map(ProductResDto::new)
-                .toList();
+        List<ProductResDto> productResDtos = productService.getProducts();
 
         return ResponseEntity.ok(productResDtos);
     }
 
     @GetMapping("/products/{productId}")
     public ResponseEntity<ProductResDto> getProduct(@PathVariable Long productId) {
-        Product product = productRepository.findProductByIdOrThrow(productId);
+        ProductResDto productResDto = productService.getProduct(productId);
 
-        return ResponseEntity.ok(new ProductResDto(product));
+        return ResponseEntity.ok(productResDto);
     }
 
     @PostMapping("/products")
     public ResponseEntity<ProductResDto> addProduct(@Valid @RequestBody ProductReqDto productReqDto) {
-        Long productId = productRepository.addProduct(productReqDto);
+        ProductResDto productResDto = productService.addProduct(productReqDto);
 
-        // 저장된 상품 가져오기
-        Product newProduct = productRepository.findProductByIdOrThrow(productId);
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(new ProductResDto(newProduct));
+        return ResponseEntity.status(HttpStatus.CREATED).body(productResDto);
     }
 
     @PutMapping("/products/{productId}")
     public ResponseEntity<String> updateProduct(@PathVariable Long productId, @Valid @RequestBody ProductReqDto productReqDto) {
-        validateProductExists(productId);
-
-        productRepository.updateProductById(productId, productReqDto);
+        productService.updateProduct(productId, productReqDto);
 
         return ResponseEntity.ok(ProductInfo.PRODUCT_UPDATE_SUCCESS);
     }
 
     @DeleteMapping("/products/{productId}")
     public ResponseEntity<String> deleteProduct(@PathVariable Long productId) {
-        validateProductExists(productId);
-
-        productRepository.deleteProductById(productId);
+        productService.deleteProduct(productId);
 
         return ResponseEntity.ok(ProductInfo.PRODUCT_DELETE_SUCCESS);
-    }
-
-    private void validateProductExists(Long productId) {
-        boolean isExist = productRepository.isProductExistById(productId);
-
-        if (!isExist) {
-            throw ProductNotFoundException.EXCEPTION;
-        }
     }
 }
