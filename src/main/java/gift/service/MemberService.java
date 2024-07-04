5a395515a3b726570397de5e463686414db9244a
alsswl <alswl4223@naver.com>
2024-07-05 02:15:30 +0900

    feat : MemberService에 login추가

--- src/main/java/gift/service/MemberService.java
+++ src/main/java/gift/service/MemberService.java
@@ -1,9 +1,12 @@
 package gift.service;
 
 import gift.controller.MemberController;
+import gift.domain.Member;
 import gift.domain.MemberRequest;
 import gift.repository.MemberRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class MemberService {
@@ -16,5 +19,9 @@ public class MemberService {
 
     public void join(MemberRequest memberRequest) {
         memberRepository.save(memberRequest);
+    }
+
+    public MemberRequest login(MemberRequest memberRequest) {
+        return memberRepository.findById(memberRequest.id());
     }
 }
