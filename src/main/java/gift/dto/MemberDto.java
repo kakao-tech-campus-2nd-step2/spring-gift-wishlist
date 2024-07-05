50f2c3c578a3502390ba56ef3a86d8f0d5cb2124
Kangboom <jkl6147@naver.com>
2024-07-05 13:09:19 +0900

    feat(MemberDto): add MemberDto

---
+++ src/main/java/gift/dto/MemberDto.java
@@ -0,0 +1,23 @@
+package gift.dto;
+
+public class MemberDto {
+    String email;
+    String password;
+
+    public MemberDto(){}
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
