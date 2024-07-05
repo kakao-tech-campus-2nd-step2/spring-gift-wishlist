7b76ee56e898b6f95670c7e214c4dc8482b0c877
rdme0 <we6610@gmail.com>
2024-07-06 01:27:31 +0900

    :sparkles: feat: 인터셉터 Config 추가

--- src/main/java/gift/config/WebMvcConfig.java
+++ src/main/java/gift/config/WebMvcConfig.java
@@ -1,15 +1,34 @@
 package gift.config;
 
+import gift.controller.LoginMemberArgumentResolver;
 import gift.interceptor.JwtInterceptor;
+import gift.service.UserService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    UserService userService;
+
+    @Autowired
+    WebMvcConfig(UserService userService) {
+        this.userService = userService;
+    }
+
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new JwtInterceptor())
                 .addPathPatterns("/api/products/**");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new LoginMemberArgumentResolver(userService));
     }
 }
