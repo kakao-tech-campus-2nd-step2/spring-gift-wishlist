b510fc90c64dae0c84d19e4fdeb8ad54f4a30bfc
pinkbeenyeti <rbgusdn12@gmail.com>
2024-07-05 16:23:45 +0900

    feat: users db와 실제 상호작용하는 계층

--- src/main/java/gift/repository/UserRepository.java
+++ src/main/java/gift/repository/UserRepository.java
@@ -1,2 +1,44 @@
-package gift.reporsitory;public class UserReporsitory {
+package gift.repository;
+
+import gift.dto.UserDTO;
+import gift.model.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class UserRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void insertUser(User user) {
+        var sql = "INSERT INTO USERS (email, password) VALUES (?, ?)";
+        jdbcTemplate.update(sql, user.getEmail(), user.getPassword());
+    }
+
+    public UserDTO selectUser(String email) {
+        String sql = "select email, password from USERS where email = ?";
+        User user = jdbcTemplate.queryForObject(
+                sql, new Object[]{email}, (resultSet, rowNum) -> {
+                    User userEntity = new User(
+                            resultSet.getString("email"),
+                            resultSet.getString("password")
+                    );
+                    return userEntity;
+                });
+
+        return new UserDTO(user.getEmail(), user.getPassword());
+    }
+
+    public Optional<Integer> countUsers(String email) {
+        String sql = "select count(*) from USERS where email = ?";
+        Integer count = jdbcTemplate.queryForObject(sql, new Object[]{email}, Integer.class);
+
+        return Optional.ofNullable(count);
+    }
+
 }
