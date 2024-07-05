cb4c648b2a756530ced32663b2ad78b183d457e0
Hyewon Seok <hws2008@naver.com>
2024-07-05 10:47:10 +0900

    fix: modify login to respond token in header

--- src/main/java/gift/api/member/MemberController.java
+++ src/main/java/gift/api/member/MemberController.java
@@ -2,6 +2,7 @@ package gift.api.member;
 
 import jakarta.validation.Valid;
 import java.util.Base64;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,14 +22,17 @@ public class MemberController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<MemberResponseDto> register(@RequestBody @Valid MemberRequestDto memberRequestDto) {
+    public ResponseEntity<Void> register(@RequestBody @Valid MemberRequestDto memberRequestDto) {
         if (memberDao.hasMemberByEmail(memberRequestDto.getEmail())) {
             throw new EmailAlreadyExistsException("Email already exists.");
         }
         memberDao.insert(memberRequestDto);
         var credentials = memberRequestDto.getEmail() + ":" + memberRequestDto.getPassword();
-        return ResponseEntity.ok().body(new MemberResponseDto(Base64.getEncoder()
-                                                                    .encodeToString(credentials.getBytes())));
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Authorization", "Basic " + Base64.getEncoder().encodeToString(credentials.getBytes()));
+        return ResponseEntity.ok()
+                        .headers(responseHeaders)
+                        .build();
     }
 
     @PostMapping("/login")
