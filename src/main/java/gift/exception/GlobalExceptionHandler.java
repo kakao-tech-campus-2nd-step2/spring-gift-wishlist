0f74c6f1bec82d842bd33ad4d450e3845c875caf
minjae <minjae4650@gmail.com>
2024-07-05 14:18:48 +0900

    feat: GlobalExceptionHandler 추가 및 유효성 검사 예외 처리
    - 전역 예외 처리 관리를 위해 GlobalExceptionHandler 클래스를 추가
    - ProductNotFoundException 예외를 처리하여 404 NOT FOUND 상태 코드를 반환하도록 설정했습니다.
    - MethodArgumentNotValidException 예외를 처리하여 입력값 유효성 검사 실패 시 400 BAD REQUEST 상태 코드를 반환하도록 설정

---
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package gift.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleProductNotFoundException(ProductNotFoundException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+}
