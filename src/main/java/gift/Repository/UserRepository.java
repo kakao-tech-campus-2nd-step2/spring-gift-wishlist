d49e376819d5cde95140f246e9d08813dea8d62f
pswaao88 <pswaao88@gmail.com>
2024-07-05 15:20:22 +0900

    Feat: UserRepository 클래스 작성

--- src/main/java/gift/Repository/UserRepository.java
+++ src/main/java/gift/Repository/UserRepository.java
@@ -1,5 +1,25 @@
 package gift.Repository;
 
-public class UserRepository {
+import gift.Model.Member;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public class UserRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserRepository(JdbcTemplate jdbcTemplate){
+            this.jdbcTemplate = jdbcTemplate;
+    }
+    private final RowMapper<String> rowMapper = new BeanPropertyRowMapper<>(String.class);
+
+    public int addUser(Member member){
+        return jdbcTemplate.update("INSERT INTO wishuser (email) VALUES (?)",member.getEmail());
+    }
+    public String findUser(String email){
+        //있나 없나 확인
+        return jdbcTemplate.queryForObject("SELECT * FROM wishuser WHERE email = ?", rowMapper, email);
+    }
 }
