394a429e5d068434567bc23bc10239b224d69fb0
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-07-05 15:49:53 +0900

    feat : JwtService tokenValidateCheck 토큰 유효성 검사 기능 + 키 Base64 사용

--- src/main/java/gift/user/JWTService.java
+++ src/main/java/gift/user/JWTService.java
@@ -1,32 +1,62 @@
 package gift.user;
 
-import io.jsonwebtoken.Header;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
-import java.security.Key;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.Date;
 
 @Service
 public class JWTService {
-    private Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+
+    private String secretKey = "";
 
     public String generateAccessToken(UserDTO userDTO){
         Date now = new Date();
-
+        String encodeString = userDTO.getEmail() + ":" + userDTO.getPassword();
+        secretKey = Base64.getEncoder().encodeToString(encodeString.getBytes(StandardCharsets.UTF_8));
+        System.out.println(secretKey);
         return Jwts.builder()
-                    .setHeaderParam(Header.TYPE,Header.JWT_TYPE)
-                    .setIssuedAt(now)
-                    .setExpiration(createExpiredDate(now))
-                    .signWith(key)
-                    .compact();
+                .subject(userDTO.getNickname())
+                .claim("email", userDTO.email)
+                .issuedAt(now).expiration(createExpiredDate(now))
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
+                .compact();
         }
 
 
     private Date createExpiredDate(Date now) {
         return new Date(now.getTime() + (60 * (60 * 1000)));
+    }
+
+    public Jws<Claims> getClaims(String jwt) {
+        try{
+            return Jwts.parser().verifyWith(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8))).build().parseSignedClaims(jwt);
+        }
+        catch (SignatureException e){
+            return null;
+        }
+    }
+
+    public boolean tokenValidCheck(String jwt) {
+        try{
+            Jws<Claims> claims = getClaims(jwt);
+            return true;
+        }
+        catch (ExpiredJwtException e){
+            throw new JwtException("토큰이 만료되었습니다");
+        }
+        catch (JwtException e){
+            throw new JwtException(e.getMessage());
+        }
+        catch (NullPointerException e){
+            throw new JwtException("토큰이 비어있습니다");
+        }
+        catch (Exception e){
+            throw new JwtException(e.getMessage());
+        }
     }
 
 }
