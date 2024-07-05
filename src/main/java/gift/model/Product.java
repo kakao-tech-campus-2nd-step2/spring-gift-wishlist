0985569c8951de339265e5bc3069f53bce448692
peacefullyquietly <152265512+peacefullyquietly@users.noreply.github.com>
2024-07-06 00:04:32 +0900

    강원대 BE_박지희 2주차 과제(1단계) (#205)
    * Initial commit
    
    * refactor : 불필요한 주석 제거
    
    * docs : README.md 작성
    
    * chore : validation 의존성 추가
    
    * chore : 불필요한 주석 제거
    
    * feat : 유효성 검사 어노테이션 추가
    
    * feat : 유효성 검사 어노테이션 및 상태코드 반환
    
    * feat : CSS 추가 및 유효성 검사 결과 출력
    
    * refactor : 코드 리뷰 반영 - price, imageUrl 유효성 검증 추가

--- src/main/java/gift/model/Product.java
+++ src/main/java/gift/model/Product.java
@@ -1,10 +1,25 @@
 package gift.model;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+
 public class Product {
     // 필드 생성
     private Long id;
+
+    @NotNull
+    @Size(min = 1, max = 15, message = "상품의 이름은 1자 15자 이내로 작성해주세요.")
+    @Pattern(regexp = "^[가-힣a-zA-Z0-9()\\[\\]+\\-\\/&_\\s]*$", message = "한글, 영문자, 숫자, 특수 기호 ()[],+-&/_ 만 입력 가능합니다.")
+    @Pattern(regexp = "^(?!.*카카오).*$", message = "'카카오'가 포함된 문구는 담당 MD와 협의 후 사용 바랍니다.")
     private String name;
+
+
+    @PositiveOrZero(message = "가격은 0 이상의 숫자를 입력해 주세요.")
     private int price;
+
+    @Pattern(regexp = "^https?://.*$", message = "올바른 이미지 URL 형식으로 입력해 주세요")
     private String imageUrl;
 
     public Product(){}
