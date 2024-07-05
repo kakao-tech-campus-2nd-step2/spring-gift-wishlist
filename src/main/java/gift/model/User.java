8dfd6b8ea9cdbe7ef17e5a999385853b9e5e6a66
pinkbeenyeti <rbgusdn12@gmail.com>
2024-07-05 16:24:48 +0900

    feat: users DB와 실제 상호작용을 하기 위한 객체

--- src/main/java/gift/model/User.java
+++ src/main/java/gift/model/User.java
@@ -1,2 +1,27 @@
-package gift.model;public class User {
+package gift.model;
+
+public class User {
+    private String email;
+    private String password;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
