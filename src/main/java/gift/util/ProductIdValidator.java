b74eae826f892f5a113016e7313ab6223562f72a
Giheon Do <dgh0001@naver.com>
2024-07-05 16:06:37 +0900

    feat: add productId constraint for wishlist

--- src/main/java/gift/util/ProductIdValidator.java
+++ src/main/java/gift/util/ProductIdValidator.java
@@ -1,2 +1,34 @@
-package gift.util;public class ProductIdValidator {
+package gift.util;
+
+import gift.model.ProductDAO;
+import gift.repository.ProductRepository;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class ProductIdValidator implements ConstraintValidator<ProductIdConstraint, Long> {
+
+    private final ProductRepository productRepository;
+
+    public ProductIdValidator(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public void initialize(ProductIdConstraint constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(Long productId, ConstraintValidatorContext cxt) {
+        ProductDAO product = productRepository.findById(productId);
+        if (product == null) return returnValidationResult("Such product doesn't exist", cxt);
+        return true;
+    }
+
+    private static boolean returnValidationResult(String errorMsg, ConstraintValidatorContext cxt) {
+        cxt.disableDefaultConstraintViolation();
+        cxt.buildConstraintViolationWithTemplate(errorMsg)
+                .addConstraintViolation();
+        return false;
+    }
 }
