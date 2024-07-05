1fae7b2f8e73e5e462c44f14111bb32e2c65fe2f
UiChan Lee <junglkls1234@gmail.com>
2024-07-05 12:02:09 +0900

    Feat: productDto 추가

---
+++ src/main/java/gift/model/ProductName.java
@@ -0,0 +1,27 @@
+package gift.model;
+
+import java.util.Objects;
+
+public class ProductName {
+    private final String name;
+
+    public ProductName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        ProductName that = (ProductName) object;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
