be2f2e4e62474868e6e6a571f3a9e3bae5ec47b2
sim-mer <gyu2781@gmail.com>
2024-07-05 14:33:59 +0900

    feat(resources) : Member 스키마 작성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,3 +5,9 @@ CREATE TABLE product (
     price INT,
     url VARCHAR(1024)
 );
+
+CREATE TABLE member (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
