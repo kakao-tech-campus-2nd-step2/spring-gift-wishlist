82eb65f204c7ec762a6900a0db2a877e27d5cff0
nove1080 <skwpqjq@gmail.com>
2024-07-05 17:37:08 +0900

    feat: WebConfig 추가

---
+++ src/main/java/gift/config/WebConfig.java
@@ -0,0 +1,22 @@
+package gift.config;
+
+import gift.authentication.resolver.LoginMemberArgumentResolver;
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginMemberArgumentResolver loginUserArgumentResolver;
+
+    public WebConfig(LoginMemberArgumentResolver loginUserArgumentResolver) {
+        this.loginUserArgumentResolver = loginUserArgumentResolver;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserArgumentResolver);
+    }
+
+}
