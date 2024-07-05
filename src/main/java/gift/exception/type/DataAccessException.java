2eed8831d160acc80bd1d9d826c6d99535fb0f19
rladbrua0207 <48901587+rladbrua0207@users.noreply.github.com>
2024-07-05 12:17:39 +0900

    refact: 상태코드 하드코딩에서 변수로 변경

--- src/main/java/gift/exception/type/DataAccessException.java
+++ src/main/java/gift/exception/type/DataAccessException.java
@@ -1,10 +1,11 @@
 package gift.exception.type;
 
 import gift.exception.ApplicationException;
+import org.springframework.http.HttpStatus;
 
 public class DataAccessException extends ApplicationException {
 
     public DataAccessException(String message) {
-        super(message, 500);
+        super(message, HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 }
--- src/main/java/gift/exception/type/NotFoundException.java
+++ src/main/java/gift/exception/type/NotFoundException.java
@@ -1,10 +1,11 @@
 package gift.exception.type;
 
 import gift.exception.ApplicationException;
+import org.springframework.http.HttpStatus;
 
 public class NotFoundException extends ApplicationException {
 
     public NotFoundException(String message) {
-        super(message, 404);
+        super(message, HttpStatus.NOT_FOUND.value());
     }
 }
