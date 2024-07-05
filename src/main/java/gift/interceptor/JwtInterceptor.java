4a495af2dbae6391cf1d8be66f08f35ca96a9cbd
5win <woozzoo7@gmail.com>
2024-07-05 16:46:27 +0900

    feat: Add interceptor to verify token - WebConfig : 인터셉터를 추가 - JwtInterceptor : JWT를 검증하기 위한 인터셉터

---
+++ src/main/java/gift/interceptor/JwtInterceptor.java
@@ -0,0 +1,42 @@
+package gift.interceptor;
+
+import gift.jwt.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class JwtInterceptor implements HandlerInterceptor {
+
+    JwtUtil jwtUtil;
+
+    public JwtInterceptor(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+        Object handler) throws Exception {
+
+        String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        String token = null;
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            token = authorizationHeader.substring(7);
+        }
+
+        if (token != null) {
+            try {
+                String email = jwtUtil.getEmail(token);
+                request.setAttribute("email", email);
+            } catch (Exception e) {
+                response.setStatus(HttpStatus.FORBIDDEN.value());
+                return false;
+            }
+        }
+        return true;
+    }
+}
