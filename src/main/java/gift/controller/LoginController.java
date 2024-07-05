e16eb51f38efb17d72a7860b95f26e5f6cf05862
Kiwoong Kim <gw150311@naver.com>
2024-07-05 15:02:04 +0900

    chore(LoginController): 필요 없는 파라미터 삭제

--- src/main/java/gift/controller/LoginController.java
+++ src/main/java/gift/controller/LoginController.java
@@ -6,7 +6,6 @@ import gift.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,14 +23,12 @@ public class LoginController {
     }
 
     @PostMapping
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest,
-        Model model) {
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
         try {
             String token = userService.loginUser(loginRequest);
             LoginResponse loginResponse = new LoginResponse(token);
             return ResponseEntity.ok().body(loginResponse);
         } catch (Exception e) {
-            model.addAttribute("error", "Invalid email or password");
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
     }
