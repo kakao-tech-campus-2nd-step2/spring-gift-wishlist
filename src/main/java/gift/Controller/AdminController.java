3d9259300a665cb66610e6bf395ac1859dd603bc
2iedo <162278702+2iedo@users.noreply.github.com>
2024-07-05 14:55:08 +0900

    경북대 BE_이도훈 2주차 과제(1단계) (#100)
    * feat : 1주차 과제 불러오기
    
    * docs : step-1 구현사항 추가
    
    * feat : 유효성 검사 어노테이션 추가
    
    * feat : 유효성 검사 기능 추가
    추가/수정 시 유효성 검사에서 에러가 발생할 경우 데이터가 입력되지 않게 하였다.
    
    * feat : 유효성 검사 메시지 추가
    유효성 검사에서 에러 발생 시 에러 메시지를 출력한다.
    
    * feat : 유효성 검사 메시지 추가
    유효성 검사에서 에러 발생 시 에러 메시지를 출력한다.
    
    * feat : 에러 메시지 스타일 추가
    
    * feat : 유효성 검사 수정
    
    * feat : 유효성 검사 수정
    
    * feat : 유효성 검사 추가
    
    '카카오'가 포함된 단어 검사

--- src/main/java/gift/Controller/AdminController.java
+++ src/main/java/gift/Controller/AdminController.java
@@ -3,23 +3,33 @@ package gift.Controller;
 import gift.Model.Product;
 import gift.Model.ProductDAO;
 import java.util.List;
+
+import gift.Valid.NameValidator;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/admin/products")
 public class AdminController {
     private ProductDAO productDAO;
+    private NameValidator nameValidator;
 
     @Autowired
     public void setProductDao(ProductDAO productDao){
         this.productDAO = productDao;
+    }
+
+    @Autowired
+    public void setNameValidator(NameValidator nameValidator){this.nameValidator = nameValidator;}
+
+    @InitBinder
+    protected void initBinder(WebDataBinder binder) {
+        binder.addValidators(nameValidator);
     }
 
     @GetMapping
@@ -31,12 +41,15 @@ public class AdminController {
 
     @GetMapping("/add")
     public String addProductForm(Model model){
-        model.addAttribute("product", new Product(0,"",0,""));
+        model.addAttribute("product", new Product(1,"",0,""));
         return "add";
     }
 
     @PostMapping("/add")
-    public String addProduct(@ModelAttribute Product product){
+    public String addProduct(@ModelAttribute @Valid Product product, BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()) {
+            return "add";
+        }
         productDAO.insertProduct(product);
         return "redirect:/admin/products";
     }
@@ -48,7 +61,11 @@ public class AdminController {
     }
 
     @PostMapping("edit/{id}")
-    public String updateProduct(@PathVariable("id") Long id, @ModelAttribute Product product){
+    public String updateProduct(@PathVariable("id") Long id, @ModelAttribute @Valid Product product, BindingResult bindingResult){
+        if(bindingResult.hasErrors()) {
+            return "edit";
+        }
+
         productDAO.updateProduct(id, product);
         return "redirect:/admin/products";
     }
--- src/main/java/gift/Model/Product.java
+++ src/main/java/gift/Model/Product.java
@@ -1,6 +1,15 @@
 package gift.Model;
 
-public record Product(long id, String name, int price, String imageUrl){
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record Product(long id,
+                      @Size(max=15, message = "글자의 길이는 15를 넘을 수 없습니다.")
+                      @Pattern(regexp = "^[a-zA-Z0-9가-힣 ()\\[\\]+\\-&/_]*$", message = "허용되지 않은 특수 기호((),[],+,-,&,/,_ 이외)가 있습니다.")
+                      String name,
+                      int price,
+                      String imageUrl)
+{
     @Override
     public String toString() {
         return "id : " + id + ", name : " + name + ", price : " + price + ", imageUrl : " + imageUrl;
