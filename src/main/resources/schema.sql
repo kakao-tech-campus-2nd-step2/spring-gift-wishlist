f4cfc414680ca11d0723ca879ce69cbb78b57b51
youcaslte03 <luckthb325@naver.com>
2024-07-05 14:17:25 +0900

    config:application.properties및 schema,data sql수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,3 +5,8 @@ CREATE TABLE products
     price    INT,
     imageUrl VARCHAR(255)
 );
+CREATE TABLE users(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) UNIQUE  NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
