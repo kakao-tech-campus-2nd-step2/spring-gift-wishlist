a6e0f2171caba4aa9170b7f914273799d7e87688
GitJIHO <jiho99322@gmail.com>
2024-07-05 15:28:36 +0900

    feat: wish의 request를 다룰 DTO 생성 및 구현

---
+++ src/main/java/gift/dto/WishRequest.java
@@ -0,0 +1,14 @@
+package gift.dto;
+
+public class WishRequest {
+
+    private Long productId;
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+}
