19b1d140933272eb50f56efd861f679a11b19cc8
Lucerna00 <pjs3859811@naver.com>
2024-07-05 17:24:49 +0900

    feat: add wished product dummy data

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,3 +9,12 @@ CREATE TABLE MEMBER (
     email VARCHAR(255) PRIMARY KEY,
     password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE WISHED_PRODUCT (
+    member_email VARCHAR(255),
+    product_id BIGINT,
+    amount INT NOT NULL,
+    PRIMARY KEY(member_email, product_id),
+    FOREIGN KEY(member_email) REFERENCES MEMBER(email),
+    FOREIGN KEY(product_id) REFERENCES PRODUCT(id)
+);
