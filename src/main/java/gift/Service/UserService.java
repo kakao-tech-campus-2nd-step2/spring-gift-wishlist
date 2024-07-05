73629d42a41255917f1978c6e52531731189f05c
pswaao88 <pswaao88@gmail.com>
2024-07-05 15:20:52 +0900

    Feat: UserService클래스 작성

--- src/main/java/gift/Service/UserService.java
+++ src/main/java/gift/Service/UserService.java
@@ -1,5 +1,24 @@
 package gift.Service;
 
-public class UserService {
+import gift.Model.Member;
+import gift.Model.Product;
+import gift.Repository.ProductRepository;
+import gift.Repository.UserRepository;
+import org.springframework.stereotype.Service;
 
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public boolean checkUserByMemberEmail(String email){
+        return userRepository.findUser(email) != null;
+    }
+
+    public void addUser(Member member){
+        userRepository.addUser(member);
+    }
 }
