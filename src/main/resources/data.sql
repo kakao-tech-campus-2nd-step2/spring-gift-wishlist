4c1d7550090f08ccb20d373df879b4f1c5cc8678
seolbin96 <jsb3038@jnu.ac.kr>
2024-07-05 15:51:26 +0900

    feat : users 초기값 설정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,3 @@
 INSERT INTO products (name, price, imageUrl) VALUES ('Sample Product', 1000, 'https://via.placeholder.com/150');
+
+INSERT INTO users (email, password) VALUES ('test@example.com', 'password123');
