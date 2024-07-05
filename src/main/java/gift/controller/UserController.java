e8ef38bd0967a86553c272f365be671819e92f0d
seolbin96 <jsb3038@jnu.ac.kr>
2024-07-05 16:01:40 +0900

    feat : UserController 생성

---
+++ src/main/java/gift/controller/UserController.java
@@ -0,0 +1,47 @@
+package gift.controller;
+
+import gift.model.User;
+import gift.service.UserService;
+import gift.dto.TokenResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/members")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<TokenResponse> register(@RequestBody User user) {
+        try {
+            userService.createUser(user);
+            String token = userService.makeToken(user);
+            TokenResponse tokenResponse = new TokenResponse(token);
+            return new ResponseEntity<>(tokenResponse, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenResponse> login(@RequestBody User user) {
+        try {
+            User savedUser = userService.loadOneUser(user.getEmail());
+            if (savedUser != null && savedUser.getPassword().equals(user.getPassword())) {
+                String token = userService.makeToken(savedUser);
+                TokenResponse tokenResponse = new TokenResponse(token);
+                return new ResponseEntity<>(tokenResponse, HttpStatus.OK);
+            }
+            return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
