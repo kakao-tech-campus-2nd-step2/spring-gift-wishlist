d578ee041867caac88ee844998d80ae5a78caccd
GamJaDo <kim0615157@gmail.com>
2024-07-06 07:18:24 +0900

    feat(AuthService.java)
    컨트롤러에 있던 토큰 발급을 서비스로 옮김
    토큰을 파싱하여 이메일을 찾는 메서드 작성
    유저를 찾을 때 해당 유저가 없을 시 null 반환을 예외 처리로 변경

--- src/main/java/gift/service/AuthService.java
+++ src/main/java/gift/service/AuthService.java
@@ -1,9 +1,13 @@
 package gift.service;
 
+import java.nio.charset.StandardCharsets;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -12,7 +16,11 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Service;
 
+import gift.exception.UnauthorizedException;
+import gift.exception.UserNotFoundException;
 import gift.model.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.annotation.PostConstruct;
 
 @Service
@@ -22,6 +30,9 @@ public class AuthService {
 	private JdbcTemplate jdbcTemplate;
 	
 	private SimpleJdbcInsert jdbcInsert;
+	
+	private String secret = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
+	private SecretKey secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
 	
 	@PostConstruct
 	public void setup() {
@@ -51,12 +62,35 @@ public class AuthService {
 		return user;
 	}
 	
-	public User getUser(String email) {
+	public User getUser(String token) {
+        try {
+            String email = Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(token.replace("Bearer ", ""))
+                    .getPayload()
+                    .get("email", String.class);
+
+            return searchUser(email);
+        } catch(Exception e) {
+            throw new UnauthorizedException("Invalid or expired token");
+        }
+    }
+	
+	public User searchUser(String email) {
 		String sql = "SELECT * FROM users WHERE email = ?";
         try {
             return jdbcTemplate.queryForObject(sql, new Object[]{email}, new UserMapper());
         } catch (EmptyResultDataAccessException e) {
-            return null;
+        	throw new UserNotFoundException("User with email " + email + " not found");
         }
+	}
+	
+	public String grantAccessToken(User user) {
+		return Jwts.builder()
+		    .setSubject(user.getId().toString())
+		    .claim("email", user.getEmail())
+		    .signWith(secretKey, SignatureAlgorithm.HS256)
+		    .compact();
 	}
 }
