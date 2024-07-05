d0ee5d75d0f62b00aabcd13b72a9fe466c6b015b
youcastle03 <107834288+youcastle03@users.noreply.github.com>
2024-07-05 12:57:15 +0900

    강원대 BE_최유성 2주차 과제 (1단계) (#167)
    * feat: 의존성 추가
    
    * feat: Product검증 추가
    
    * feat: AdminController 검증 추가
    
    * feat: ProductController 검증 추가
    
    * fix: ProductRepository 객체 없으면 빈 객체 반환하게 수정
    
    * refactor: 프론트에서 검증 오류 확인할수 있도록 처리
    
    * test:ProductServiceTest
    
    * docs:README.md
    
    * feat:ProductService

---
+++ src/test/java/gift/ProductServiceTest.java
@@ -0,0 +1,83 @@
+package gift;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import gift.domain.Product;
+import gift.service.ProductService;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class ProductServiceTest {
+
+    @Autowired
+    private ProductService productService;
+
+    @BeforeEach
+    public void setUp() {
+        List<Product> products = productService.findAll();
+        for (Product product : products) {
+            productService.deleteProduct(product.getId());
+        }
+    }
+
+    @Test
+    @DisplayName("전건조회 테스트")
+    public void findAllTest() {
+        Product chicken = new Product(1L, "치킨", 20000, "Chicken.com");
+        Product pizza = new Product(2L, "피자", 30000, "Pizza.com");
+
+        productService.addProduct(chicken);
+        productService.addProduct(pizza);
+
+        List<Product> products = productService.findAll();
+
+        assertThat(products).hasSize(2);
+        assertThat(products).extracting(Product::getId)
+            .contains(chicken.getId(), pizza.getId());
+    }
+
+    @Test
+    @DisplayName("상품추가 테스트")
+    public void addProductTest() {
+        Product chicken = new Product(1L, "치킨", 20000, "Chicken.com");
+
+        Long productId = productService.addProduct(chicken);
+
+        assertThat(productId).isEqualTo(chicken.getId());
+    }
+
+    @Test
+    @DisplayName("상품수정 테스트")
+    public void updateProductTest() {
+        Product chicken = new Product(1L, "치킨", 20000, "Chicken.com");
+        productService.addProduct(chicken);
+        chicken.setName("수정치킨");
+        Long productId = productService.updateProduct(chicken);
+
+        assertThat(productId).isEqualTo(chicken.getId());
+        assertThat(productService.getProduct(chicken.getId()).getName()).isEqualTo("수정치킨");
+
+    }
+
+    @Test
+    @DisplayName("상품삭제 테스트")
+    public void deleteProductTest() {
+        Product chicken = new Product(1L, "치킨", 20000, "Chicken.com");
+        productService.addProduct(chicken);
+        Long productId = productService.deleteProduct(1L);
+
+        assertThat(productId).isEqualTo(1L);
+        assertThat(productService.getProduct(1L)).isNull();
+    }
+
+
+
+
+}
