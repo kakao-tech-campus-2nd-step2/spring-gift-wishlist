ce8a14134bb86f7cef3a9b867880b246fdce4376
noojmogstel <goaudgo@naver.com>
2024-07-05 16:15:00 +0900

    feat(dao): ProductWithQuantityDao 클래스 생성

---
+++ src/main/java/gift/dao/ProductWithQuantityDao.java
@@ -0,0 +1,34 @@
+package gift.dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductWithQuantityDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public ProductWithQuantityDao(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate=jdbcTemplate;
+    }
+
+    public void createProductWithQuantityTable(){
+        String sql = """
+            CREATE TABLE ProductWithQuantity(
+            id bigint AUTO_INCREMENT,
+            product_id bigint,
+            quantity int,
+            wishlist_id bigint,
+            PRIMARY KEY(id),
+            FOREIGN KEY(wishlist_id) REFERENCES Wishlist(id)
+            );
+            """;
+        jdbcTemplate.execute(sql);
+    }
+
+    public void saveProductWithQuantity(long productId, int quantity,long wishlistId){
+        String sql="INSERT INTO ProductWithQuantity(product_id,quantity,wishlist_id) values(?,?,?)";
+        jdbcTemplate.update(sql,productId,quantity,wishlistId);
+    }
+
+}
