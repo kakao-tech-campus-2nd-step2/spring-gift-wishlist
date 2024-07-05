29166144b38149193df81a43556d6b5c6c4666c1
ez23re <ez23re@gmail.com>
2024-07-05 17:51:27 +0900

    feat : 위시리스트 컨트롤러 (아직 미구현)

---
+++ src/main/java/gift/controller/WishListController.java
@@ -0,0 +1,13 @@
+package gift.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/wishlist")
+public class WishListController {
+
+}
