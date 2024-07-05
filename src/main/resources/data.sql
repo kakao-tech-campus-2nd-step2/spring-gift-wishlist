0f492d893347059a602a8797c34e4e493d1804d5
dkswoals <a4445656@gmail.com>
2024-07-05 19:02:34 +0900

    chore: User 테스트용 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,6 @@ INSERT INTO Item (name, price, imgurl)
 values ('샐러드',
         20000,
         'https://media.istockphoto.com/id/183752521/ko/%EC%82%AC%EC%A7%84/%EB%B9%84%EB%B9%84%EB%B0%A5.jpg?s=2048x2048&w=is&k=20&c=f5FX83thuFkWNXmo-k7q5zD3xoEdO9oTFBSPEzBxMR0=');
+
+insert into users (email,password) values ('123@123','123');
+insert into users (email,password) values ('test@test','test');
