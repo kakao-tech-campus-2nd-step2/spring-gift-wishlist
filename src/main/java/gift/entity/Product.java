191bb95398a1e0be04b420d4c7e0176a7220a0a0
rdme0 <we6610@gmail.com>
2024-07-06 01:28:52 +0900

    :sparkles: feat: Product 엔티티에 quantity 필드 추가

--- src/main/java/gift/entity/Product.java
+++ src/main/java/gift/entity/Product.java
@@ -2,6 +2,7 @@ package gift.entity;
 
 import gift.dto.ProductDTO;
 import gift.exception.BadRequestExceptions.BlankContentException;
+import java.util.Objects;
 
 public class Product {
 
@@ -13,22 +14,28 @@ public class Product {
 
     private String imageUrl;
 
-    public Product(){}
+    private Integer quantity;
+
+    public Product() {
+    }
 
     public Product(Integer id, String name, Integer price, String imageUrl) {
+        this(id, name, price, imageUrl, 0);
+    }
+
+    public Product(ProductDTO productDTO) {
+        this(productDTO.id(), productDTO.name(), productDTO.price(), productDTO.imageUrl(), 0);
+        blankCatch(productDTO);
+    }
+
+    public Product(Integer id, String name, Integer price, String imageUrl, Integer quantity) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
+        this.quantity = quantity;
     }
 
-    public Product(ProductDTO productDTO) {
-        blankCatch(productDTO);
-        this.id = productDTO.id();
-        this.name = productDTO.name();
-        this.price = productDTO.price();
-        this.imageUrl = productDTO.imageUrl();
-    }
 
     public Integer getId() {
         return id;
@@ -46,10 +53,52 @@ public class Product {
         return imageUrl;
     }
 
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public ProductDTO convertToProductDTO(Product product) {
+        return new ProductDTO(product.getId(), product.getName(), product.getPrice(), product.getImageUrl(), product.getQuantity());
+    }
+
     private void blankCatch(ProductDTO productDTO) throws BlankContentException {
         if (productDTO.name().isBlank() || productDTO.price() == null || productDTO.imageUrl()
                 .isBlank()) {
             throw new BlankContentException("입력 값에 빈 곳이 있습니다. 다시 입력해주세요");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return Objects.equals(id, product.id) &&
+                Objects.equals(name, product.name) &&
+                Objects.equals(price, product.price) &&
+                Objects.equals(imageUrl, product.imageUrl);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
     }
 }
