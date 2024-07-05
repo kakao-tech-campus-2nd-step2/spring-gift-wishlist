282fbab726af16492497e7a03bbe2ffe1ad1fb5c
kanguk <lm0828@naver.com>
2024-07-05 18:24:18 +0900

    refactor : TokenInterceptor http status 관리 리팩토링
    refactor : TokenInterceptor http status 관리 리팩토링
    
    이미 error코드에서 관리되고 있으므로 map을 제거한다.

--- src/main/java/gift/filter/TokenInterceptor.java
+++ src/main/java/gift/filter/TokenInterceptor.java
@@ -1,7 +1,6 @@
 package gift.filter;
 
 import gift.exception.BusinessException;
-import gift.exception.ErrorCode;
 import gift.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -11,28 +10,12 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 public class TokenInterceptor implements HandlerInterceptor {
 
     @Autowired
     private TokenService tokenService;
-
-    private static final Map<ErrorCode, HttpStatus> errorCodeToStatusMap = new HashMap<>() {{
-        put(ErrorCode.INVALID_NAME_SIZE, HttpStatus.BAD_REQUEST);
-        put(ErrorCode.INVALID_NAME_PATTERN, HttpStatus.BAD_REQUEST);
-        put(ErrorCode.KAKAO_NAME_NOT_ALLOWED, HttpStatus.BAD_REQUEST);
-        put(ErrorCode.PRODUCT_NOT_FOUND, HttpStatus.NOT_FOUND);
-        put(ErrorCode.EMAIL_ALREADY_EXISTS, HttpStatus.BAD_REQUEST);
-        put(ErrorCode.INVALID_CREDENTIALS, HttpStatus.FORBIDDEN);
-        put(ErrorCode.USER_NOT_FOUND, HttpStatus.NOT_FOUND);
-        put(ErrorCode.INVALID_TOKEN, HttpStatus.UNAUTHORIZED);
-        put(ErrorCode.UNAUTHORIZED_REQUEST, HttpStatus.UNAUTHORIZED);
-        put(ErrorCode.INVALID_PRICE, HttpStatus.BAD_REQUEST);
-        put(ErrorCode.INVALID_IMAGE_URL, HttpStatus.BAD_REQUEST);
-    }};
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -48,8 +31,7 @@ public class TokenInterceptor implements HandlerInterceptor {
         try {
             tokenService.validateToken(token);
         } catch (BusinessException e) {
-            HttpStatus status = errorCodeToStatusMap.getOrDefault(e.getErrorCode(), HttpStatus.INTERNAL_SERVER_ERROR);
-            response.setStatus(status.value());
+            response.setStatus(e.getErrorCode().getStatus().value());
             return false;
         }
 
