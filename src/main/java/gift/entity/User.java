51babecddc9b166b5687ec936de42ac265d3661d
GitJIHO <jiho99322@gmail.com>
2024-07-05 15:40:23 +0900

    feat: wish 리스트를 다룰 컨트롤러 생성 구현

--- src/main/java/gift/entity/User.java
+++ src/main/java/gift/entity/User.java
@@ -2,9 +2,26 @@ package gift.entity;
 
 public class User {
 
+    private Long id;
     private String email;
     private String password;
 
+    public User() {
+    }
+
+    public User(Long id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getEmail() {
         return email;
