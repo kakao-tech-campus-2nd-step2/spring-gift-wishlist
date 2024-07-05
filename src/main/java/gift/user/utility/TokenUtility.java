6bbff30e474a4aecd9b96f0015efd8e8adac0e13
kangwlgns <kangji0615@gmail.com>
2024-07-05 14:38:21 +0900

    feat: add a dao for the users db

--- src/main/java/gift/user/utility/TokenUtility.java
+++ src/main/java/gift/user/utility/TokenUtility.java
@@ -2,6 +2,7 @@ package gift.user.utility;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import java.util.Date;
 
 // JWT 토큰을 생성해주는 utility 클래스
 public class SecurityUtility {
@@ -11,10 +12,21 @@ public class SecurityUtility {
 
     // 입력한 정보를 토대로 토큰을 반환하는 함수
     public static String getToken(String email, String password) {
+        long currentTime = System.currentTimeMillis();
         return Jwts.builder()
-            .setSubject(email.toString())
+            .subject(email.toString())
             .claim("password", password)
+            .issuedAt(new Date(currentTime))
+            // 유효기간은 60분
+            .expiration(new Date(currentTime + minuteToMillis(60)))
             .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
             .compact();
+    }
+
+    public static 
+
+    // minute을 넣으면 밀리초로 반환하는 메서드
+    private static long minuteToMillis(int minute) {
+        return minute * 60L * 1000;
     }
 }
