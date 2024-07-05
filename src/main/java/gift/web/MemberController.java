01c7d1896e1ea1215b209e34e40af63ec63a4f7d
yoong-saks <wjdghtjd06@gmail.com>
2024-07-05 17:52:37 +0900

    feat: MemberController 구현

---
+++ src/main/java/gift/web/MemberController.java
@@ -0,0 +1,35 @@
+package gift.web;
+
+import gift.web.dto.Member;
+import gift.web.dto.Token;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/members")
+public class MemberController {
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody Member member) {
+        HttpHeaders headers = new HttpHeaders();
+        Token token = memberService.createJWT(member);
+        headers.add("Authorization", "Bearer " + token);
+
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(token);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Member member) {
+
+    }
+}
