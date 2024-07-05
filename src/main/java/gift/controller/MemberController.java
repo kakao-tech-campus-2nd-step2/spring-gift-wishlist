49a5485219b95964614fa947922486bfa5bd9eab
Kangboom <jkl6147@naver.com>
2024-07-05 22:59:48 +0900

    refactor(MemberController): change methods name

--- src/main/java/gift/controller/MemberController.java
+++ src/main/java/gift/controller/MemberController.java
@@ -30,7 +30,7 @@ public class MemberController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<JwtDto> checkToken(@RequestBody MemberDto memberDto){
+    public ResponseEntity<JwtDto> login(@RequestBody MemberDto memberDto){
 
         JwtDto token = memberService.login(memberDto.getEmail(), memberDto.getPassword());
 
