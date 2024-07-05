d30b477a9329becfe011217d0c0825021c211917
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-07-05 15:56:19 +0900

    fix : UserController Exception Handler를 Controller Advice로 이동

--- src/main/java/gift/user/UserController.java
+++ src/main/java/gift/user/UserController.java
@@ -22,43 +22,6 @@ public class UserController {
         this.jwtService = jwtService;
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> handleValidationException(MethodArgumentNotValidException ex){
-        HashMap<String, String> map = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(err -> {
-            map.put(err.getField(), err.getDefaultMessage());
-        });
-
-        ResponseDTO responseDTO = new ResponseDTO(
-                HttpStatus.BAD_REQUEST,
-                "Validation Error",
-                map
-        );
-        return ResponseEntity.badRequest().body(responseDTO);
-    }
-
-    @ExceptionHandler(JwtException.class)
-    public ResponseEntity<?> handleAuthenticationException(JwtException ex){
-        ResponseDTO responseDTO = new ResponseDTO(
-                HttpStatus.UNAUTHORIZED,
-                "JwtException",
-                ex.getMessage()
-        );
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseDTO);
-
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<?> handleArgumentException(IllegalArgumentException ex){
-        ResponseDTO responseDTO = new ResponseDTO(
-                HttpStatus.BAD_REQUEST,
-                "IllegalArgumentException",
-                ex.getMessage()
-        );
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseDTO);
-
-    }
-
     //로그인 API
     @PostMapping("/login")
     public ResponseEntity<?> loginUser(@RequestBody @Valid LoginDTO loginDTO) throws Exception {
