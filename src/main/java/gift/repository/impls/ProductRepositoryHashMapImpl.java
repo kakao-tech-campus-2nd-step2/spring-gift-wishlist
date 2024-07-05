7c11db46ffc6e0a0d0fa3cf456fb0f2cd0626a14
wvssm <neoramiregi@gmail.com>
2024-07-05 16:15:17 +0900

    refactor(ProductRepositoryHashMapImpl): ProductRepository 상속 없앰, 메서드 추가로 충돌 발생

--- src/main/java/gift/repository/impls/ProductRepositoryHashMapImpl.java
+++ src/main/java/gift/repository/impls/ProductRepositoryHashMapImpl.java
@@ -5,11 +5,10 @@ import gift.repository.ProductRepository;
 
 import java.util.*;
 
-public class ProductRepositoryHashMapImpl implements ProductRepository {
+public class ProductRepositoryHashMapImpl  {
     private final Map<Long, Product> products = new HashMap<>();
     private static Long id = 0L;
 
-    @Override
     public Long save(Product product){
         id += 1;
         Product newProduct = new Product(id,product.getName(),product.getPrice(),product.getImageUrl());
@@ -17,23 +16,19 @@ public class ProductRepositoryHashMapImpl implements ProductRepository {
         return id;
     }
 
-    @Override
     public Optional<Product> findById(Long id){
        return Optional.ofNullable(products.get(id));
     }
 
-    @Override
     public List<Product> findAll(){
         return new ArrayList<>(products.values());
     }
 
-    @Override
     public Long delete(Long id){
         products.remove(id);
         return id;
     }
 
-    @Override
     public Product update(Long id, Product product){
         products.remove(id);
         Product editProduct = new Product(id,product.getName(), product.getPrice(), product.getImageUrl());
