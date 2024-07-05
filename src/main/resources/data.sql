0e04545754f01cb0aa663f4a8fc3ab3997ab2d6b
sunandrabbit <sunandrabbit@gamil.com>
2024-07-05 11:53:49 +0900

    change : 사전 입력 데이터 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,5 @@ INSERT INTO products (name, price, imageUrl) VALUES ('Sample Product 1', 1000, '
 INSERT INTO products (name, price, imageUrl) VALUES ('Sample Product 2', 2000, 'http://example.com/image2.jpg');
 INSERT INTO products (name, price, imageUrl) VALUES ('Sample Product 3', 3000, 'http://example.com/image3.jpg');
 
-INSERT INTO members (email, password) VALUES ('user1', '1234');
+INSERT INTO members (email, password) VALUES ('admin@example.com', '1111');
+INSERT INTO members (email, password) VALUES ('user@example.com', '2222');
