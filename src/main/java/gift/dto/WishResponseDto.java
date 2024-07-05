01ded9c76ca26b9739751fa3275bca52ecf535a5
wvssm <neoramiregi@gmail.com>
2024-07-05 16:17:44 +0900

    refactor(WishResponseDto): WishResponseDto에 getter, setter 추가

--- src/main/java/gift/dto/WishResponseDto.java
+++ src/main/java/gift/dto/WishResponseDto.java
@@ -16,4 +16,52 @@ public class WishResponseDto {
         this.productImageUrl = productImageUrl;
         this.quantity = quantity;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(int productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getProductImageUrl() {
+        return productImageUrl;
+    }
+
+    public void setProductImageUrl(String productImageUrl) {
+        this.productImageUrl = productImageUrl;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
