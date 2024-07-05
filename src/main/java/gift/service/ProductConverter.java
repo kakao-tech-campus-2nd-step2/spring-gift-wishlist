a81dcf53d09f837b0e7f0aee3639f58f4a987567
rdme0 <we6610@gmail.com>
2024-07-06 01:32:39 +0900

    :recycle: refactor: Product의 늘어난 파라미터에 대응하여 리팩터링

--- src/main/java/gift/service/ProductConverter.java
+++ src/main/java/gift/service/ProductConverter.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 public class ProductConverter {
     public static List<ProductDTO> convertToProductDTO(List<Product> productList) throws BadRequestException {
         return productList.stream().map(product -> {
-            return new ProductDTO(product.getId(), product.getName(), product.getPrice(), product.getImageUrl());
+            return new ProductDTO(product.getId(), product.getName(), product.getPrice(), product.getImageUrl(), 0);
         }).collect(Collectors.toList());
     }
 }
