9c25feaf35feffb37b4efb96ba19e548d1b88669
bokyeong <wntjdals0412@gmail.com>
2024-07-05 15:03:57 +0900

    Feat: 멤버 로그인 기능

--- src/main/java/gift/controller/WebController.java
+++ src/main/java/gift/controller/WebController.java
@@ -1,6 +1,6 @@
 package gift.controller;
 
-import gift.ProductService;
+import gift.service.ProductService;
 import gift.dto.ProductRequestDto;
 import gift.dto.ProductResponseDto;
 import org.springframework.stereotype.Controller;
