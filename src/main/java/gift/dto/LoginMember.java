1f7d0903a7d40574f5cbf228cd5775a1c7c862b3
minju26 <mjalswn26@gmail.com>
2024-07-05 18:16:03 +0900

    feat: LoginMember, WishProduct dto 추가

---
+++ src/main/java/gift/dto/LoginMember.java
@@ -0,0 +1,17 @@
+package gift.dto;
+
+public class LoginMember {
+    private String email;
+
+    public LoginMember(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
