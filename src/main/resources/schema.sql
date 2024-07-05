d500b806b00bc8a562273bd69beea0f69ff97e0a
jinhwa-park <parkjinhwa3914@gmail.com>
2024-07-05 16:59:46 +0900

    feat: add member table in schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,3 +4,9 @@ CREATE TABLE product (
     price INT NOT NULL,
     imageUrl VARCHAR(1000) NOT NULL
 );
+
+CREATE TABLE member {
+    id BIGINT PRIMARY KEY AUTO_INCREMENT
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+};
