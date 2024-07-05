eecd96d3a25bf375017cefaf375358b2dc59dc49
안원모 <tommy45231@naver.com>
2024-07-05 15:13:52 +0900

    MemberService 생성

---
+++ src/main/java/gift/MemberService.java
@@ -0,0 +1,13 @@
+package gift;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+}
