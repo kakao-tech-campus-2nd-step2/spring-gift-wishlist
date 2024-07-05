92e276965bad6bd740da5378718c4bdf2dbbb76b
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-05 16:35:13 +0900

    feat : wishlist controller 추가

--- src/main/java/gift/controller/WishListController.java
+++ src/main/java/gift/controller/WishListController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/wish")
+@RequestMapping("/api/wish")
 public class WishListController {
     private final WishListService wishListService;
     private final JwtUtil jwtUtil;
