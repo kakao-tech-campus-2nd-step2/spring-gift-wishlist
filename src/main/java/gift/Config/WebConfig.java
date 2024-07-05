de9573421a51f5878d0afe308acf3c6aae5efe32
suno-boy <tnsgh3691@gmail.com>
2024-07-05 12:24:22 +0900

    feat: Config구현
    - 인터셉터를 등록하고 구성하는 config구현
    - 인터셉터를 모든 경로(/**)에 적용하되, 특정 경로("/members/**")는 제외

---
+++ src/main/java/gift/Config/WebConfig.java
@@ -0,0 +1,23 @@
+package gift.Config;
+
+import gift.Config.InterceptorOfToken;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final InterceptorOfToken interceptorOfToken;
+
+    public WebConfig(InterceptorOfToken interceptorOfToken) {
+        this.interceptorOfToken = interceptorOfToken;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptorOfToken)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/members/**");
+    }
+}
