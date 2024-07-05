25ca7ab1293e242668fc471a16f165591f8f4475
momnpa333 <momnpa333@gmail.com>
2024-07-05 16:54:40 +0900

    feat(AuthInterceptor): auth를 처리하는 Interceptor 제작

---
+++ src/main/java/gift/auth/interceptor/AuthInterceptor.java
@@ -0,0 +1,77 @@
+package gift.auth.interceptor;
+
+import gift.auth.Auth;
+import gift.auth.jwt.JwtProvider;
+import gift.model.user.Role;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
+
+@Component
+public class AuthInterceptor implements HandlerInterceptor {
+
+    private final JwtProvider jwtProvider;
+
+    public AuthInterceptor(JwtProvider jwtProvider) {
+        this.jwtProvider = jwtProvider;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+        Object handler) throws Exception {
+        if (handler instanceof HandlerMethod) {
+            return handleHandler(request, response,
+                ((HandlerMethod) handler).getMethodAnnotation(Auth.class));
+        } else if (handler instanceof ResourceHttpRequestHandler) {
+            return handleHandler(request, response, handler.getClass().getAnnotation(Auth.class));
+        }
+        return true;
+    }
+
+    private boolean handleHandler(HttpServletRequest request, HttpServletResponse response,
+        Auth auth) throws Exception {
+        if (!checkAuthorization(request, response, auth)) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkAuthorization(HttpServletRequest request, HttpServletResponse response,
+        Auth auth) throws Exception {
+        if (auth == null) {
+            return true;
+        }
+
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !jwtProvider.validateToken(authHeader)) {
+            sendUnauthorizedError(response, "Unauthorized");
+            return false;
+        }
+
+        Claims claims = jwtProvider.getClaims(authHeader);
+        Role requiredRole = auth.role();
+        Role userRole = Role.valueOf(claims.get("roles", String.class));
+
+        if (userRole == Role.ADMIN || requiredRole == userRole) {
+            request.setAttribute("userId", String.valueOf(claims.getSubject()));
+            request.setAttribute("name", claims.get("name", String.class));
+            return true;
+        }
+
+        sendForbiddenError(response, "Forbidden");
+        return false;
+    }
+
+    private void sendUnauthorizedError(HttpServletResponse response, String message)
+        throws Exception {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, message);
+    }
+
+    private void sendForbiddenError(HttpServletResponse response, String message) throws Exception {
+        response.sendError(HttpServletResponse.SC_FORBIDDEN, message);
+    }
+}
