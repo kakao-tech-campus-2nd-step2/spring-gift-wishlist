7b8bbbb3cbf8b01d2beaf2643dc9f56b78decc41
ajy4304 <ajy4304@naver.com>
2024-07-05 16:26:36 +0900

    feat(WishList): 3단계 요구사항에 맞는 WishList 추가

--- src/main/java/gift/model/WishList.java
+++ src/main/java/gift/model/WishList.java
@@ -1,2 +1,23 @@
-package gift.model;public class WishList {
+package gift.model;
+
+public class WishList {
+    private String userEmail;
+    private Long productId;
+
+    // Getters and Setters
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
 }
