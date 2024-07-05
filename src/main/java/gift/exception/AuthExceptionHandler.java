63067990f115f873caa768c5fd28718fe33855d3
EunKyung Lee <2dmsrud2002@naver.com>
2024-07-05 11:27:27 +0900

    Fix: 비밀번호 틀릴 시 forbidden 반환으로 수정

--- src/main/java/gift/exception/AuthExceptionHandler.java
+++ src/main/java/gift/exception/AuthExceptionHandler.java
@@ -1,17 +1,17 @@
 package gift.exception;
 
 import gift.controller.AuthController;
-import gift.controller.UserController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice(assignableTypes = UserController.class)
-public class UserExceptionHandler {
+@RestControllerAdvice(assignableTypes = AuthController.class)
+public class AuthExceptionHandler {
+
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
     }
 
     @ExceptionHandler(UserAlreadyExistsException.class)
