51db05500489194a1b8696a0a65056373f0cc503
suhyeon7497 <nucc1186@gmail.com>
2024-07-05 21:42:20 +0900

    feat: 상품 예시 5개 insert하는 data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,10 @@ INSERT INTO users (email, password, name, role) VALUES ('user2@example.com', 'pa
 INSERT INTO users (email, password, name, role) VALUES ('user3@example.com', 'password3', 'User Three', 'ROLE_ADMIN');
 INSERT INTO users (email, password, name, role) VALUES ('user4@example.com', 'password4', 'User Four', 'ROLE_USER');
 INSERT INTO users (email, password, name, role) VALUES ('user5@example.com', 'password5', 'User Five', 'ROLE_MODERATOR');
+
+
+INSERT INTO products (name, price, image_url) VALUES ('감자', 1500, 'https://example.com/gamja.jpg');
+INSERT INTO products (name, price, image_url) VALUES ('고구마', 2000, 'https://example.com/goguma.jpg');
+INSERT INTO products (name, price, image_url) VALUES ('김치', 3000, 'https://example.com/kimchi.jpg');
+INSERT INTO products (name, price, image_url) VALUES ('사과', 2500, 'https://example.com/apple.jpg');
+INSERT INTO products (name, price, image_url) VALUES ('바나나', 1800, 'https://example.com/banana.jpg');
