35dfe5709ef5b132f69a5db441fd30d5c4f8db89
Kangboom <jkl6147@naver.com>
2024-07-05 13:12:31 +0900

    refactor(repository): move ProductDtoRepository files to repository directory

---
+++ src/main/java/gift/repository/ProductRepository.java
@@ -0,0 +1,54 @@
+package gift.repository;
+
+import gift.domain.Product;
+import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public ProductRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Product findById(Long id) {
+        String sql = "SELECT * FROM product WHERE id = ?";
+        return jdbcTemplate.queryForObject(sql, new Object[]{id}, productRowMapper());
+    }
+
+    public Product findByName(String name) {
+        String sql = "SELECT * FROM product WHERE name = ?";
+        return jdbcTemplate.queryForObject(sql, new Object[]{name}, productRowMapper());
+    }
+
+    public List<Product> findAll(){
+        return jdbcTemplate.query("select * from product", productRowMapper());
+    }
+
+    public void save(Product product) {
+        String sql = "INSERT INTO product (name, price, imageUrl) VALUES (?, ?, ?)";
+        jdbcTemplate.update(sql, product.getName(), product.getPrice(), product.getImageUrl());
+    }
+
+    public void update(Long id, Product product){
+        String sql = "UPDATE product SET name = ?, price = ?, imageUrl = ? WHERE id = ?";
+        jdbcTemplate.update(sql, product.getName(), product.getPrice(), product.getImageUrl(), id);
+    }
+    public void delete(Long id){
+        jdbcTemplate.update("DELETE FROM product WHERE id = ?", id);
+    }
+
+    private RowMapper<Product> productRowMapper() {
+        return (rs, rowNum) -> {
+            Product product = new Product();
+            product.setId(rs.getLong("id"));
+            product.setName(rs.getString("name"));
+            product.setPrice(rs.getInt("price"));
+            product.setImageUrl(rs.getString("imageUrl"));
+            return product;
+        };
+    }
+}
