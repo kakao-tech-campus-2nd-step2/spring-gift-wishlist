aacbbb65eda72c994114612b4fb12f4c59049670
박민규 <pmq0101@naver.com>
2024-07-05 14:41:47 +0900

    fix(ProductRequestDTO): 한글이 입력되지 않는 버그 수정

--- src/main/java/gift/dto/requestDTO/ProductRequestDTO.java
+++ src/main/java/gift/dto/requestDTO/ProductRequestDTO.java
@@ -7,8 +7,7 @@ import jakarta.validation.constraints.Size;
 
 public record ProductRequestDTO(
     @Size(max = 15, message = "최대 15자까지 입력할 수 있습니다.")
-    @Pattern(regexp = "^(?!.*카카오)[a-zA-Z0-9()\\[\\]+\\-&/_]*$", message = "특수문자는 (), [], +, -, &, /, _ 만 가능합니다\n\"카카오\"가 포함된 문구는 담당 MD와 협의한 경우에만 사용할 수 있습니다\"")
-//    @Pattern(regexp = "^(?!.*카카오).*", message = "\"카카오\"가 포함된 문구는 담당 MD와 협의한 경우에만 사용할 수 있습니다")
+    @Pattern(regexp = "^(?!.*카카오)[\\w가-힣ㄱ-ㅎㅏ-ㅣ()\\[\\]+\\-&/_]*$" , message = "특수문자는 (), [], +, -, &, /, _ 만 가능합니다\n\"카카오\"가 포함된 문구는 담당 MD와 협의한 경우에만 사용할 수 있습니다\"")
     @NotBlank
     String name,
     @Min(1)
