1810828ec25ed470db59628c045ca516a570547b
ajy4304 <ajy4304@naver.com>
2024-07-05 16:27:09 +0900

    feat(schema.sql): wishlist 테이블 쿼리문 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,3 +8,10 @@ CREATE TABLE users (
     email VARCHAR(255) PRIMARY KEY,
     password VARCHAR(255) NOT NULL
 );
+CREATE TABLE IF NOT EXISTS wishlist (
+    user_email VARCHAR(255) NOT NULL,
+    product_id BIGINT NOT NULL,
+    PRIMARY KEY (user_email, product_id),
+    FOREIGN KEY (user_email) REFERENCES users(email),
+    FOREIGN KEY (product_id) REFERENCES product(id)
+);
