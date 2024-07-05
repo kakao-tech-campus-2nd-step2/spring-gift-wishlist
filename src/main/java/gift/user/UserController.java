1e84587d87d8ebb16efda56393304e67ca39cae6
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-07-05 14:13:44 +0900

    refactor : UserController ExceptionHandler Exception Handler를 사용하도록 변경

--- src/main/java/gift/user/UserController.java
+++ src/main/java/gift/user/UserController.java
@@ -11,43 +11,55 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.security.sasl.AuthenticationException;
 import java.util.HashMap;
 
 @RestController
 @RequestMapping("/members")
 public class UserController {
 
-    private final ProductRepository productRepository;
     private final UserService userService;
     private final JWTService jwtService;
-    public UserController(ProductRepository productRepository, UserService userService, JWTService jwtService) {
-        this.productRepository = productRepository;
+    public UserController(UserService userService, JWTService jwtService) {
         this.userService = userService;
         this.jwtService = jwtService;
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationException(MethodArgumentNotValidException ex){
+        HashMap<String, String> map = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(err -> {
+            map.put(err.getField(), err.getDefaultMessage());
+        });
+
+        ResponseDTO responseDTO = new ResponseDTO(
+                HttpStatus.BAD_REQUEST,
+                "Validation Error",
+                map
+        );
+        return ResponseEntity.badRequest().body(responseDTO);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> handleAuthenticationException(IllegalArgumentException ex){
+        ResponseDTO responseDTO = new ResponseDTO(
+                HttpStatus.BAD_REQUEST,
+                "IllegalArgumentException",
+                ex.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseDTO);
+
+    }
+
     //로그인 API
     @PostMapping("/login")
-    public ResponseEntity<?> loginUser(@RequestBody @Valid LoginDTO loginDTO, BindingResult bindingResult){
-        if(!bindingResult.hasErrors() && !userService.validateUser(loginDTO)){
-            bindingResult.addError(new FieldError("loginDTO", "password", "이메일이 존재하지 않거나 비밀번호가 일치하지 않습니다"));
-        }
-
-        if(bindingResult.hasErrors()){
-            HashMap<String, String> map = new HashMap<>();
-            bindingResult.getFieldErrors().forEach(err -> {
-                map.put(err.getField(), err.getDefaultMessage());
-            });
-
-            ResponseDTO responseDTO = new ResponseDTO(
-                    HttpStatus.FORBIDDEN,
-                    "Validation Error",
-                    map
-            );
-            return ResponseEntity.badRequest().body(responseDTO);
+    public ResponseEntity<?> loginUser(@RequestBody @Valid LoginDTO loginDTO) throws Exception {
+        if(!userService.validateUser(loginDTO)){
+            throw new IllegalArgumentException("이메일이 존재하지 않거나 비밀번호가 일치하지 않습니다.");
         }
 
         UserDTO userDTO = userService.getUserDTOByLoginDTO(loginDTO);
@@ -58,23 +70,9 @@ public class UserController {
 
     //회원 가입 API
     @PostMapping("/signup")
-    public ResponseEntity<?> signupUser(@RequestBody @Valid UserDTO userDTO, BindingResult bindingResult){
+    public ResponseEntity<?> signupUser(@RequestBody @Valid UserDTO userDTO){
         if(userService.checkIfDuplicatedEmail(userDTO.email)){
-            bindingResult.addError(new FieldError("userDTO", "email", "이미 등록된 이메일입니다."));
-        }
-
-        if(bindingResult.hasErrors()){
-            HashMap<String, String> map = new HashMap<>();
-            bindingResult.getFieldErrors().forEach(err -> {
-                map.put(err.getField(), err.getDefaultMessage());
-            });
-
-            ResponseDTO responseDTO = new ResponseDTO(
-                    HttpStatus.FORBIDDEN,
-                    "Validation Error",
-                    map
-            );
-            return ResponseEntity.badRequest().body(responseDTO);
+            throw new IllegalArgumentException("이미 등록된 이메일입니다.");
         }
 
         userService.registerUser(userDTO);
