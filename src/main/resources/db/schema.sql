ea3b7d5a8b356d32bac206d4bd382db58c7b8452
minju26 <mjalswn26@gmail.com>
2024-07-05 17:59:14 +0900

    feat: member 테이블 생성

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,6 +1,14 @@
+-- products
 CREATE TABLE products (
     id BIGINT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     price INT NOT NULL,
     imageUrl VARCHAR(255)
+);
+
+-- members
+CREATE TABLE members (
+--       id LONG AUTO_INCREMENT PRIMARY KEY,
+         email VARCHAR(255) PRIMARY KEY,
+         password VARCHAR(255) NOT NULL
 );
