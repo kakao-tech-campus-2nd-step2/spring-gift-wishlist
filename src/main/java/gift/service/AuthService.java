59e53a02dc26622b0ba59f8e4351038dd92df268
suhyeon7497 <nucc1186@gmail.com>
2024-07-05 14:29:29 +0900

    feat: exception이 발생하면 UnAuthorization

--- src/main/java/gift/service/AuthService.java
+++ src/main/java/gift/service/AuthService.java
@@ -75,7 +75,7 @@ public class AuthService {
                 return false;
             }
             return true;
-        } catch (EmptyResultDataAccessException e) {
+        } catch (Exception e) {
             return false;
         }
     }
