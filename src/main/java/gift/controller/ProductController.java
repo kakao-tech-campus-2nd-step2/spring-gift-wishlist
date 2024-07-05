fbbd86711cbd20ed83e1d0cd609d026e0b0cdc52
Jeje <jjh4450jjh4450@gmail.com>
2024-07-05 15:09:53 +0900

    chore(ProductController.java): 주석 수정

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -17,27 +17,51 @@ public class ProductController {
         this.productDao = productDao;
     }
 
+    /**
+     * 새 상품을 생성하고 맵에 저장함
+     *
+     * @param product 저장할 상품 객체
+     */
     @PostMapping
     public Product createProduct(@Valid @RequestBody Product product) {
         return productDao.createProduct(product);
     }
 
+    /**
+     * 주어진 ID에 해당하는 상품을 반환함
+     *
+     * @param id 조회할 상품의 ID
+     */
     @GetMapping("/{id}")
     public Product getProduct(@PathVariable Long id) {
         Product product = productDao.getProduct(id);
         return product;
     }
 
+    /**
+     * 모든 상품을 반환함
+     */
     @GetMapping("/all")
     public List<Product> getProducts() {
         return productDao.getAllProducts();
     }
 
+    /**
+     * 주어진 ID에 해당하는 상품을 삭제함
+     *
+     * @param id 삭제할 상품의 ID
+     */
     @DeleteMapping("/{id}")
     public void deleteProduct(@PathVariable Long id) {
         productDao.deleteProduct(id);
     }
 
+    /**
+     * 주어진 ID에 해당하는 상품을 갱신함
+     *
+     * @param id      갱신할 상품의 ID
+     * @param product 갱신할 상품 객체
+     */
     @PutMapping("/{id}")
     public Product updateProduct(@PathVariable Long id, @Valid @RequestBody Product product) {
         return productDao.updateProduct(id, product);
