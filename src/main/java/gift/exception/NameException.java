656b421dbf58588edacf13745b2dff04afc605af
Kangboom <jkl6147@naver.com>
2024-07-05 13:10:28 +0900

    refactor(Exception): move NameException location

---
+++ src/main/java/gift/exception/NameException.java
@@ -0,0 +1,9 @@
+package gift.exception;
+
+import javax.naming.Name;
+
+public class NameException extends RuntimeException{
+    public NameException(String string){
+        super(string);
+    }
+}
