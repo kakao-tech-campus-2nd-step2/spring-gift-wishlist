d401edfbe09412084dd7aa7cf665644f6a6f1eff
Jeje <jjh4450jjh4450@gmail.com>
2024-07-05 15:42:34 +0900

    feat(AuthController.java):AuthController.java를 만듬

---
+++ src/main/java/gift/controller/AuthController.java
@@ -0,0 +1,14 @@
+package gift.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @PostMapping("/login")
+    public String login() {
+
+    }
+
+}
