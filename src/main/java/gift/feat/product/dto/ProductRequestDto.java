447c6cbe5c99961bfb21ddd6fb124abd97fd66f1
yooonwodyd <dbswodyd00@naver.com>
2024-07-05 16:29:45 +0900

    [refactor] 별도의 어노테이션으로 분리

--- src/main/java/gift/feat/product/dto/ProductRequestDto.java
+++ src/main/java/gift/feat/product/dto/ProductRequestDto.java
@@ -1,21 +1,16 @@
-package gift.dto;
+package gift.feat.product.dto;
 
-import gift.domain.Product;
+import gift.core.annotaions.NoKakao;
+import gift.core.annotaions.ValidCharset;
+import gift.feat.product.domain.Product;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record ProductRequestDto(
 	Long id,
-
-	@Pattern(
-		regexp = "^(?!.*카카오).*$",
-		message = "Product name cannot contain '카카오'"
-	)
 	@Size(max = 15, message = "Product name must be at most 15 characters long")
-	@Pattern(
-		regexp = "^[a-zA-Z0-9가-힣 \\(\\)\\[\\]\\+\\-\\&\\/\\_]*$",
-		message = "Product name contains invalid characters"
-	)
+	@ValidCharset
+	@NoKakao
 	String name,
 	Long price,
 	String imageUrl
