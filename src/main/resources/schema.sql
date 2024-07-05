c65c7155669d6ff85746c43a82c9ef68db61825c
안원모 <tommy45231@naver.com>
2024-07-05 15:20:09 +0900

    Refactor : 회원 테이블 생성 sql 스크립트

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,3 +5,9 @@ CREATE TABLE IF NOT EXISTS product (
     description VARCHAR(255),
     imageUrl VARCHAR(255)
 );
+
+CREATE TABLE IF NOT EXISTS member (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
