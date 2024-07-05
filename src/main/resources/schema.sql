728ba2d49b38b237bed1d80380ba772302db3aeb
Yongjin An <yongjin5352@gmail.com>
2024-07-05 12:23:05 +0900

    feat: Add users table definition to schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,16 @@
 DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE products (
     id LONG AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
     price INT,
     imageUrl TEXT(65535)
 );
+
+CREATE TABLE users (
+    id LONG AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255),
+    password VARCHAR(255)
+);
+
