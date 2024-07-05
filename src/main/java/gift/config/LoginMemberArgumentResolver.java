3929c7f65c3a1e910065367acd149677da5c2f6d
ajy4304 <ajy4304@naver.com>
2024-07-05 16:23:17 +0900

    feat(LoginMemberArgumentResolver): 사용자 인증을 위한 클래스

--- src/main/java/gift/config/LoginMemberArgumentResolver.java
+++ src/main/java/gift/config/LoginMemberArgumentResolver.java
@@ -1,2 +1,34 @@
-package gift.config;public class LoginMemberArgumentResolver {
+package gift.config;
+
+import gift.dto.UserRequestDTO;
+import gift.model.User;
+import gift.service.UserService;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+    private final UserService userService;
+
+    public LoginMemberArgumentResolver(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(LoginMember.class) != null;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        String token = webRequest.getHeader("Authorization").substring(7);
+        User user = userService.findByToken(token);
+
+        // User 객체를 UserRequestDTO로 변환
+        return new UserRequestDTO(user.getEmail(), user.getPassword());
+    }
 }
