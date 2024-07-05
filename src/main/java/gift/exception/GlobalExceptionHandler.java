6a9ad1892782d7bd30c64535c139f4aff7f8ffeb
Lucerna00 <pjs3859811@naver.com>
2024-07-05 13:03:24 +0900

    feat(InvalidPasswordException.java): add password mismatch exception

--- src/main/java/gift/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -31,7 +31,7 @@ public class GlobalExceptionHandler {
         return problemDetail;
     }
 
-    @ExceptionHandler({AlreadyExistMemberException.class, NoSuchMemberException.class})
+    @ExceptionHandler({AlreadyExistMemberException.class, NoSuchMemberException.class, InvalidPasswordException.class})
     public ProblemDetail handleAlreadyExistMemberException(RuntimeException runtimeException) {
         ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
         problemDetail.setDetail(runtimeException.getMessage());
