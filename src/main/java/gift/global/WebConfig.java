e437ee44965fe4cfba5924c4cdd6a5b528092061
momnpa333 <momnpa333@gmail.com>
2024-07-05 17:02:47 +0900

    feat: webConfig에서 interceptor,argumentResolver 추가

---
+++ src/main/java/gift/global/WebConfig.java
@@ -0,0 +1,29 @@
+package gift.global;
+
+import gift.auth.interceptor.AuthInterceptor;
+import gift.auth.resolver.LoginInfoArgumentResolver;
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+
+    public WebConfig(AuthInterceptor authInterceptor) {
+        this.authInterceptor = authInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginInfoArgumentResolver());
+    }
+}
