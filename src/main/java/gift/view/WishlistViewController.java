1b6426220bdc500655fbd786c5351117fa975af3
dong_yxxn <g0326kdy@gmail.com>
2024-07-05 14:47:36 +0900

    feat(WishlistViewController): wishlistViewController 생성

--- src/main/java/gift/view/WishlistViewController.java
+++ src/main/java/gift/view/WishlistViewController.java
@@ -1,27 +1,30 @@
 package gift.view;
 
 import gift.controller.ProductController;
+import gift.controller.WishlistController;
 import gift.model.Product;
+import gift.model.WishlistItem;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/withlist")
+@RequestMapping("/wishlist")
 public class WishlistViewController {
-    private final ProductController productController;
+    private WishlistController wishlistController;
 
-    public WishlistViewController(ProductController productController) {
-        this.productController = productController;
+    public WishlistViewController(WishlistController wishlistController) {
+        this.wishlistController = wishlistController;
     }
 
-    @GetMapping("")
-    public String getAllProducts(Model model) {
-        List<Product> products = productController.getAllProducts().getBody();
-        model.addAttribute("products", products);
-        return "withlist";
+    @GetMapping("{id}")
+    public String showWishlist(@PathVariable("id") Long userId, Model model) {
+        List<WishlistItem> wishlists = wishlistController.makeWishlist(userId).getBody();
+        model.addAttribute("wishlists", wishlists);
+        return "wishlist";
     }
 }
