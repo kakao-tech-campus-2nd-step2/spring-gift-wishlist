386e27e64befd5fff7d12822b956a028980b67d4
suhyeon7497 <nucc1186@gmail.com>
2024-07-05 12:04:52 +0900

    feat: members url pattern에 대해 interceptor X

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -16,6 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(authInterceptor);
+        registry.addInterceptor(authInterceptor)
+            .excludePathPatterns("/members/*");
     }
 }
