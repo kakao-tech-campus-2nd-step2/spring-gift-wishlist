9361c4927cab6923c169be7b4f2f4f8e43789e0b
suhyeon7497 <nucc1186@gmail.com>
2024-07-05 16:20:09 +0900

    feat: wishlist 목록 조회 skeleton code

--- src/main/java/gift/controller/WishController.java
+++ src/main/java/gift/controller/WishController.java
@@ -1,5 +1,23 @@
 package gift.controller;
 
-public class WishListController {
+import gift.model.dto.LoginUserDto;
+import gift.model.dto.ProductResponseDto;
+import gift.resolver.annotation.LoginUser;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WishController {
+
+    @GetMapping("/wishes")
+    public Map<ProductResponseDto, Long> getWishList(@LoginUser LoginUserDto loginUserDto){
+        Map<ProductResponseDto, Long> wishlist = new HashMap<>();
+        return wishlist;
+    }
+
 
 }
