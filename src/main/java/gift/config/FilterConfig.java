f0ff2a8c4a5ff0274024b8eb64fe635a64ec3522
Giheon Do <dgh0001@naver.com>
2024-07-05 16:02:51 +0900

    feat: middleware for /api/wishlist/*

--- src/main/java/gift/config/FilterConfig.java
+++ src/main/java/gift/config/FilterConfig.java
@@ -1,2 +1,18 @@
-package gift.config;public class FilterConfig {
+package gift.config;
+
+import gift.filter.JwtFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<JwtFilter> jwtFilter() {
+        FilterRegistrationBean<JwtFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new JwtFilter());
+        registrationBean.addUrlPatterns("/api/wishlist/*");
+        return registrationBean;
+    }
 }
--- src/main/java/gift/filter/JwtFilter.java
+++ src/main/java/gift/filter/JwtFilter.java
@@ -1,2 +1,40 @@
-package gift.filter;public class JwtFilter {
+package gift.filter;
+
+import gift.util.UserUtility;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class JwtFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            String token = authorizationHeader.substring(7);
+            Claims claims = UserUtility.tokenParser(token);
+            if (claims == null) {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid token");
+                return;
+            }
+            servletRequest.setAttribute("email", claims.get("email", String.class));
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
 }
