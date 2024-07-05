c943ef1da8e4729df591b79805e33b8e20834068
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-05 16:37:00 +0900

    refactor : wishlist 테이블 생성 sql 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS product;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS wishlist;
 CREATE TABLE product (
     id LONG AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
@@ -7,6 +8,14 @@ CREATE TABLE product (
     imageUrl VARCHAR(255)
 );
 CREATE TABLE users (
+    name VARCHAR(255),
     email VARCHAR(255) PRIMARY KEY ,
-    password VARCHAR(255)
+    password VARCHAR(255),
+    role VARCHAR(255)
+);
+CREATE TABLE wishlist (
+    id LONG AUTO_INCREMENT PRIMARY KEY,
+    userId LONG NOT NULL,
+    productId LONG NOT NULL,
+    quantity INT
 );
