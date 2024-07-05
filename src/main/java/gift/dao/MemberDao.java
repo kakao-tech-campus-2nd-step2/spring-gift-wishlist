2aaa98ce331ebce5d086e485c5235279d1cc3b9f
UiChan Lee <junglkls1234@gmail.com>
2024-07-05 16:32:32 +0900

    Feat: memberDao 메서드 추가

--- src/main/java/gift/dao/MemberDao.java
+++ src/main/java/gift/dao/MemberDao.java
@@ -4,6 +4,8 @@ import gift.model.member.Member;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public class MemberDao {
@@ -16,5 +18,26 @@ public class MemberDao {
     public void insertMemeber(Member member) {
         var sql = "insert into members (email, password, role) values (?, ?, ?)";
         jdbcTemplate.update(sql, member.email(), member.password(),member.role());
+    }
+    public void updateMember(Member member){
+        var sql = "update members set email = ? , password = ?, role = ? where email = ? ";
+        jdbcTemplate.update(sql,member.password(), member.role(), member.email());
+    }
+
+    public List<Member> selectAllMembers() {
+        var sql = "select email, password, role  from members";
+        return jdbcTemplate.query(
+                sql,
+                (resultSet, rowNum) -> new Member(
+                        resultSet.getString("email"),
+                        resultSet.getString("password"),
+                        resultSet.getString("role")
+                )
+        );
+    }
+    public boolean isMemberExist(String email) {
+        var sql = "select count(*) from members where email = ?";
+        int count = jdbcTemplate.queryForObject(sql, Integer.class, email);
+        return count > 0;
     }
 }
--- src/main/java/gift/service/MemberService.java
+++ src/main/java/gift/service/MemberService.java
@@ -1,4 +1,17 @@
 package gift.service;
 
+import gift.dao.MemberDao;
+import gift.model.member.Member;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MemberService {
+    private final MemberDao memberDao;
+    public MemberService(MemberDao memberDao){
+        this.memberDao = memberDao;
+    }
+
+    public void registerNewMember(Member member){
+        memberDao.insertMemeber(member);
+    }
 }
