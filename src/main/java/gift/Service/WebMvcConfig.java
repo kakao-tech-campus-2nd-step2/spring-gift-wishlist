997f8dc6e00e500b8034512165f8b6d9931e5306
pswaao88 <pswaao88@gmail.com>
2024-07-05 15:17:13 +0900

    Feat: WebMvcConfig 클래스 작성

--- src/main/java/gift/Service/WebMvcConfig.java
+++ src/main/java/gift/Service/WebMvcConfig.java
@@ -1,7 +1,22 @@
 package gift.Service;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+    private final BearerAuthInterceptor bearerAuthInterceptor;
 
+    public WebMvcConfig(BearerAuthInterceptor bearerAuthInterceptor) {
+        this.bearerAuthInterceptor = bearerAuthInterceptor;
+    }
+
+    public void addInterceptors(InterceptorRegistry registry){
+        //인터셉터 등록
+        System.out.println("");
+        registry.addInterceptor(bearerAuthInterceptor).addPathPatterns("/api/wish");
+        registry.addInterceptor(bearerAuthInterceptor).addPathPatterns("/api/wish/add/{id}");
+        registry.addInterceptor(bearerAuthInterceptor).addPathPatterns("/api/wish/delete/{id}");
+    }
 }
