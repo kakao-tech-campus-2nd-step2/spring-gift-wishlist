4505756d30166ac0a3a6affb303cffd42a251ea8
dong_yxxn <g0326kdy@gmail.com>
2024-07-05 12:30:07 +0900

    refactor(WishlistViewController): withlist html 나오도록 수정

--- src/main/java/gift/view/WishlistViewController.java
+++ src/main/java/gift/view/WishlistViewController.java
@@ -1,2 +1,27 @@
-package gift.view;public class WishlistViewController {
+package gift.view;
+
+import gift.controller.ProductController;
+import gift.model.Product;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/withlist")
+public class WishlistViewController {
+    private final ProductController productController;
+
+    public WishlistViewController(ProductController productController) {
+        this.productController = productController;
+    }
+
+    @GetMapping("")
+    public String getAllProducts(Model model) {
+        List<Product> products = productController.getAllProducts().getBody();
+        model.addAttribute("products", products);
+        return "withlist";
+    }
 }
