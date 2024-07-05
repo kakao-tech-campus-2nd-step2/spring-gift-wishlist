50e7c37180d73675b2ae9d6f2e3370edd4b18018
ariimo <159447466+ariimo@users.noreply.github.com>
2024-07-05 17:46:20 +0900

    feat: MemberService 생성

---
+++ src/main/java/gift/services/MemberService.java
@@ -0,0 +1,50 @@
+package gift.services;
+
+
+import gift.JWTUtil;
+import gift.Member;
+import gift.MemberDto;
+import gift.classes.Exceptions.RegisteringEmailIsAlreadyExists;
+import gift.repositories.MemberRepository;
+import java.util.NoSuchElementException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final JWTUtil jwtUtil;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository, JWTUtil jwtUtil) {
+        this.memberRepository = memberRepository;
+        this.jwtUtil = jwtUtil;
+    }
+
+    public void register(MemberDto memberDto) throws RegisteringEmailIsAlreadyExists {
+
+        Member member = new Member(memberDto.getEmail(), memberDto.getPassword());
+        Member existingMember = memberRepository.find(member);
+
+        if (existingMember != null) {
+            throw new RegisteringEmailIsAlreadyExists();
+        }
+        memberRepository.register(member);
+    }
+
+    public String login(MemberDto memberDto) {
+
+        Member member = new Member(memberDto.getEmail(), memberDto.getPassword());
+        Member existingMember = memberRepository.find(member);
+
+        if (existingMember == null) {
+            throw new NoSuchElementException("Email is not exist. ");
+        }
+
+        String token = jwtUtil.createJwt(member.getEmail());
+        return token;
+
+    }
+
+}
