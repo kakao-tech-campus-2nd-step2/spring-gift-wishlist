00b26524de96a144d632aaddef94dada20ef0d69
minjae <minjae4650@gmail.com>
2024-07-05 14:55:44 +0900

    feat: ForbiddenWordException 예외 처리 추가
    - ForbiddenWordException 예외를 처리하는 핸들러를 GlobalExceptionHandler에 추가
    - 예외 발생 시 400 BAD REQUEST 상태 코드를 반환하고, 예외 메시지를 응답 본문에 포함

--- src/main/java/gift/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -34,4 +34,10 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
+    @ExceptionHandler(ForbiddenWordException.class)
+    public ResponseEntity<Map<String, Object>> handleIllegalArgumentException(ForbiddenWordException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
 }
