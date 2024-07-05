941b259179418c17f39d410c6bf331a0de0b1e1d
minjae <minjae4650@gmail.com>
2024-07-05 16:40:13 +0900

    feat: MemberService 인터페이스 추가
    - 회원 가입 및 로그인을 위한 MemberService 인터페이스를 추가
    - registerMember 메소드: 회원 등록을 처리하고 결과를 Optional<String>으로 반환
    - login 메소드: 이메일과 비밀번호를 통해 로그인 처리하고 결과를 Optional<String>으로 반환

---
+++ src/main/java/gift/service/MemberService.java
@@ -0,0 +1,9 @@
+package gift.service;
+
+import gift.model.Member;
+import java.util.Optional;
+
+public interface MemberService {
+    Optional<String> registerMember(Member member);
+    Optional<String> login(String email, String password);
+}
