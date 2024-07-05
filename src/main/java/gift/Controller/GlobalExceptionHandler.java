2376e85b3c1bdae74338572d1e1b4d61466f67d4
rbm0524 <rbm0524@naver.com>
2024-07-05 17:50:05 +0900

    fix : ExceptionHandler 추가

--- src/main/java/gift/Controller/GlobalExceptionHandler.java
+++ src/main/java/gift/Controller/GlobalExceptionHandler.java
@@ -1,6 +1,10 @@
 package gift.Controller;
 
+import gift.Exception.ForbiddenException;
+import gift.Exception.UnauthorizedException;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -26,5 +30,15 @@ public class GlobalExceptionHandler {
             return "product_form"; // product_form 뷰로 포워딩
         }
         return "redirect:/api/products";
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<String> handleUnauthorizedException(UnauthorizedException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(ForbiddenException.class)
+    public ResponseEntity<String> handleForbiddenException(ForbiddenException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.FORBIDDEN);
     }
 }
