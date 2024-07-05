1fcd74060867219c6b875eaf023b6b9b0f50037f
rdme0 <we6610@gmail.com>
2024-07-06 01:28:17 +0900

    :sparkles: feat: ArgumentResolver 추가

---
+++ src/main/java/gift/controller/LoginMemberArgumentResolver.java
@@ -0,0 +1,38 @@
+package gift.controller;
+
+import gift.annotation.LoginMember;
+import gift.exception.BadRequestExceptions.UserNotFoundException;
+import gift.service.UserService;
+import gift.util.JwtUtil;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final UserService userService;
+
+    public LoginMemberArgumentResolver(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(LoginMember.class) != null;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        String token = webRequest.getHeader("Authorization").substring(7);
+        String email = JwtUtil.extractEmail(token);
+        try {
+            return userService.getUser(email);
+        } catch (UserNotFoundException e) {
+            throw new UserNotFoundException("회원이 아닙니다.");
+        }
+    }
+}
