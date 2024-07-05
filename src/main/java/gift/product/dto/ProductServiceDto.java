6b220578ec3c99432abab24bc8c9d914d7f4abb7
JaeBin <201924576@pusan.ac.kr>
2024-07-05 11:00:39 +0900

    refactor(product): rename serviceDto to ProductServiceDto

--- src/main/java/gift/product/dto/ProductServiceDto.java
+++ src/main/java/gift/product/dto/ProductServiceDto.java
@@ -4,7 +4,7 @@ import gift.product.domain.Product;
 import gift.product.domain.ProductName;
 import gift.product.domain.ProductPrice;
 
-public record ServiceDto(Long id, ProductName name, ProductPrice price, String imageUrl) {
+public record ProductServiceDto(Long id, ProductName name, ProductPrice price, String imageUrl) {
     public Product toProduct() {
         return new Product(id, name, price, imageUrl);
     }
