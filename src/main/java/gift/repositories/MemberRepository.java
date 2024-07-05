26a090d4f882ebddb619b705b136d20a88efccea
ariimo <159447466+ariimo@users.noreply.github.com>
2024-07-05 17:46:04 +0900

    feat: MemberRepository 생성

---
+++ src/main/java/gift/repositories/MemberRepository.java
@@ -0,0 +1,36 @@
+package gift.repositories;
+
+import gift.Member;
+import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public MemberRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+//    회원 찾기
+    public Member find(Member member) {
+        String email = member.getEmail();
+        List<Member> queried = jdbcTemplate.query("SELECT * FROM members where email=" + email + " limit 1", (resultSet, rowNum) ->
+            new Member(
+                resultSet.getString("email"),
+                resultSet.getString("password")
+            )
+        );
+        if (queried.size() == 0) {
+            return null;
+        }
+        return queried.get(0);
+    }
+
+//    회원 가입
+    public void register(Member member){
+        jdbcTemplate.execute("INSERT INTO members (email, password) VALUES('" + member.getEmail() + "', '" + member.getPassword() + "');");
+    }
+
+}
