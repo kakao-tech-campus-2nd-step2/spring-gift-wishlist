3e3042d97028cace136d2d53950464c4d623310e
KimSongMok <mok0618@naver.com>
2024-07-05 14:30:32 +0900

    fix: fix sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO products (name, price, IMAGE_URL) VALUES
+INSERT INTO product (name, price, IMAGE_URL) VALUES
     ('아이스 아메리카노', 4500, 'https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg');
