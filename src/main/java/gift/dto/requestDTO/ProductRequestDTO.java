56cd1a075771ec00d5282e56422f389044b7cb44
박민규 <pmq0101@naver.com>
2024-07-05 16:30:13 +0900

    feat(ProductRequestDTO): 검증 순서를 정할 수 있게 변경

--- src/main/java/gift/dto/requestDTO/ProductRequestDTO.java
+++ src/main/java/gift/dto/requestDTO/ProductRequestDTO.java
@@ -1,14 +1,11 @@
 package gift.dto.requestDTO;
 
+import gift.dto.common.valid.ValidProductName;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 
 public record ProductRequestDTO(
-    @Size(max = 15, message = "최대 15자까지 입력할 수 있습니다.")
-    @Pattern(regexp = "^(?!.*카카오)[\\w가-힣ㄱ-ㅎㅏ-ㅣ()\\[\\]+\\-&/_]*$" , message = "특수문자는 (), [], +, -, &, /, _ 만 가능합니다\n\"카카오\"가 포함된 문구는 담당 MD와 협의한 경우에만 사용할 수 있습니다\"")
-    @NotBlank
+    @ValidProductName
     String name,
     @Min(1)
     int price,
