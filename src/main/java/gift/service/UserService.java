f9c68331dec25eabfe152e34512479614b19aeae
geonny <kky8586696@naver.com>
2024-07-05 15:37:06 +0900

    refactor : jwt 토큰 기능 JwtUtil로 분리

--- src/main/java/gift/service/UserService.java
+++ src/main/java/gift/service/UserService.java
@@ -1,23 +1,21 @@
 package gift.service;
+import gift.util.JwtUtil;
 import gift.model.*;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class UserService {
 
-
     private UserRepository userRepository;
-    private final static String secretKey = "mySecretKey";
+    private JwtUtil jwtUtil;
+
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository,JwtUtil jwtUtil) {
         this.userRepository = userRepository;
+        this.jwtUtil = jwtUtil;
     }
 
     public boolean register(User user) {
@@ -31,24 +29,18 @@ public class UserService {
     public Optional<String> login(String email, String password) {
         Optional<User> user = userRepository.findByEmail(email);
         if (user.isPresent() && user.get().getPassword().equals(password)) {
-            String token = generateJWT(user.get());
+            String token = jwtUtil.generateJWT(user.get());
             return Optional.of(token);
         }
         return Optional.empty();
     }
     public boolean validateToken(String token) {
-        try {
-            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
+        return jwtUtil.checkValidateToken(token);
     }
 
     public Optional<User> getUserByToken(String token) {
         try {
-            Claims claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
-            String email = claims.getSubject();
+            String email = jwtUtil.getUserEmail(token);
             return userRepository.findByEmail(email);
         } catch (Exception e) {
             return Optional.empty();
@@ -64,19 +56,6 @@ public class UserService {
 
     public List<Gift> getGiftsForUser(Long userId) {
         return userRepository.getGiftsForUser(userId);
-    }
-
-    private String generateJWT(User user) {
-        long expirationTime = System.currentTimeMillis() + 3600000;
-        Date expirationDate = new Date(expirationTime);
-
-        String token = Jwts.builder()
-                .setSubject(user.getEmail())
-                .setExpiration(expirationDate)
-                .signWith(SignatureAlgorithm.HS512, secretKey)
-                .compact();
-
-        return token;
     }
 
 }
