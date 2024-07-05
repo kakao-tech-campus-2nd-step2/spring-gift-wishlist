910aa122e75cea031ec0f77cfb779f5bfe1cef61
anacnoda77 <sm1108shin@naver.com>
2024-07-05 15:06:06 +0900

    feat: 토큰 검증 인터셉터 구현

--- src/main/java/gift/auth/AuthInterceptor.java
+++ src/main/java/gift/auth/AuthInterceptor.java
@@ -1,24 +1,42 @@
 package gift.auth;
 
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+@Component
 public class AuthInterceptor implements HandlerInterceptor {
+
+    private final JwtTokenProvider tokenProvider;
+
+    public AuthInterceptor(JwtTokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
         Object handler) throws Exception {
 
-        HttpSession session = request.getSession(false);
+        String token = tokenProvider.extractJwtTokenFromHeader(request);
 
-        if (session == null || session.getAttribute("member") == null) {
-            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        if(ObjectUtils.isEmpty(token)) {
+            response.sendError(HttpStatus.UNAUTHORIZED.value());
             return false;
         }
+
+        if(tokenProvider.isExpired(token)) {
+            response.sendError(HttpStatus.UNAUTHORIZED.value());
+            return false;
+        }
+
+        Claims claims = tokenProvider.parseToken(token);
+        request.setAttribute("email", claims.getSubject());
 
         return true;
     }
