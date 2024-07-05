aea130d04cf9be6a8f5cd4f7dd5c03c61602200f
hyeonseo <69136047+hxunsc@users.noreply.github.com>
2024-07-05 15:33:23 +0900

    feat: Create Wishlist DTO

---
+++ src/main/java/gift/wishlist/dto/WishRequest.java
@@ -0,0 +1,13 @@
+package gift.wishlist.dto;
+
+public class WishRequest {
+    private String productName;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+}
