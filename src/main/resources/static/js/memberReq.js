29f38bc8659ad644c11451b91b5b7a30b3a0933e
5win <woozzoo7@gmail.com>
2024-07-05 17:36:43 +0900

    refact: Refactoring javascript function

--- src/main/resources/static/js/memberReq.js
+++ src/main/resources/static/js/memberReq.js
@@ -31,6 +31,7 @@ $(document).ready(function() {
     } else {
         updateUI(false);
     }
+    getWishlist();
 });
 
 function login() {
@@ -75,4 +76,61 @@ function updateUI(isLoggedIn) {
         $('#login-button').show();
         $('#logout-button').hide(); 
     }
+}
+
+
+
+function getWishlist() {
+    $.ajax({
+        url: "/api/members/member/wishlist",
+        type: "GET",
+        headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem('token')
+        },
+        success: function(data) {
+            var productList = $('#wish-list');
+            productList.empty();
+            $.each(data, function(index, object) {
+                var product = '<tr>';
+                product += '<td>' + object.id + '</td>';
+                product += '<td>' + object.name + '</td>';
+                product += '<td>' + object.price + '</td>';
+                product += '<td>';
+                product += '<a href="' + object.imageUrl + '">';
+                product += '<img src="' + object.imageUrl + '" alt="Not found image" style="width: 40px; height: 40px;" />';
+                product += '</a>';
+                product += '</td>';
+                product += '<td>';
+                product += '<button class="delete-button btn btn-danger" data-product-id="' + object.id + '">삭제</button>';
+                product += '</td>';
+                product += '</tr>';
+                productList.append(product);
+            });
+            
+        },
+        error: function(xhr, status, error) {
+            console.error("데이터 가져오기 실패:", error);
+        }
+    });
+}
+
+$(document).on('click', '.delete-button', function() {
+    var productId = $(this).data('product-id');
+    deleteWishlist(productId);
+});
+
+function deleteWishlist(productId) {
+    $.ajax({
+        url: `/api/members/member/wishlist/${productId}`,
+        type: 'DELETE',
+        headers: {
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+        },
+        success: function(response) {
+            alert(response);
+        },
+        error: function(xhr, status, error) {
+            console.error('Error deleting product:', error);
+        }
+    });
 }
