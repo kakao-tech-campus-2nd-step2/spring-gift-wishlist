b1d04d9e765d4baa799c1cb18df0ce8a8a607cc7
mingjuu <tisu79@naver.com>
2024-07-05 11:15:23 +0900

    fix : html 분리 - 등록 , 상품내역 확인 및 삭제

--- src/main/java/gift/controller/WishController.java
+++ src/main/java/gift/controller/WishController.java
@@ -1,3 +1,42 @@
+//package gift.controller;
+//
+//import gift.domain.Wish;
+//import gift.service.WishService;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@Controller
+//@RequestMapping("/wishes")
+//public class WishController {
+//    private final WishService wishService;
+//
+//    public WishController(WishService wishService) {
+//        this.wishService = wishService;
+//    }
+//
+//    @PostMapping
+//    @ResponseBody
+//    public void addWish(@RequestBody Wish wish) {
+//        wishService.addWish(wish.getMemberId(), wish.getProductId());
+//    }
+//
+//    @GetMapping
+//    public String getWishes(@RequestParam Long memberId, Model model) {
+//        List<Wish> wishes = wishService.getWishes(memberId);
+//        model.addAttribute("wishes", wishes);
+//        model.addAttribute("memberId", memberId);
+//        return "wishlist";
+//    }
+//
+//    @DeleteMapping("/remove")
+//    @ResponseBody
+//    public void removeWish(@RequestBody Wish wish) {
+//        wishService.removeWish(wish.getMemberId(), wish.getProductId());
+//    }
+//}
 package gift.controller;
 
 import gift.domain.Wish;
@@ -23,12 +62,17 @@ public class WishController {
         wishService.addWish(wish.getMemberId(), wish.getProductId());
     }
 
+    @GetMapping("/form")
+    public String showForm() {
+        return "wishlistForm";
+    }
+
     @GetMapping
     public String getWishes(@RequestParam Long memberId, Model model) {
         List<Wish> wishes = wishService.getWishes(memberId);
         model.addAttribute("wishes", wishes);
         model.addAttribute("memberId", memberId);
-        return "wishlist";
+        return "myWishlist";
     }
 
     @DeleteMapping("/remove")
