1bc42d1cd0a6cb0707e3abec5f8a5fa988bfc729
Profile-exe <kang67572346@gmail.com>
2024-07-05 15:47:18 +0900

    feat(AuthService): 회원가입 및 로그인 서비스 추가

---
+++ src/main/java/gift/auth/service/AuthService.java
@@ -0,0 +1,51 @@
+package gift.auth.service;
+
+import gift.auth.dto.LoginReqDto;
+import gift.auth.exception.InvalidGrantTypeException;
+import gift.auth.exception.LoginFailedException;
+import gift.auth.token.AuthToken;
+import gift.auth.token.AuthTokenGenerator;
+import gift.member.dto.MemberReqDto;
+import gift.member.dto.MemberResDto;
+import gift.member.service.MemberService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    private final MemberService memberService;
+    private final AuthTokenGenerator authTokenGenerator;
+
+    public AuthService(MemberService memberService, AuthTokenGenerator authTokenGenerator) {
+        this.memberService = memberService;
+        this.authTokenGenerator = authTokenGenerator;
+    }
+
+    public AuthToken register(MemberReqDto memberReqDto) {
+        MemberResDto memberResDto = memberService.addMember(memberReqDto);
+        return authTokenGenerator.generateToken(memberResDto);
+    }
+
+    public AuthToken login(String header, LoginReqDto loginReqDto) {
+        // 토큰 추출
+        String token = authTokenGenerator.extractToken(header);
+        Long memberId = authTokenGenerator.extractMemberId(token);
+
+        // 회원 정보 조회
+        MemberResDto member = memberService.getMember(memberId);
+        String password = memberService.getMemberPassword(memberId);
+
+        // 이메일과 비밀번호가 일치하지 않으면 예외 발생
+        if (!member.email().equals(loginReqDto.email()) || !password.equals(loginReqDto.password())) {
+            throw LoginFailedException.EXCEPTION;
+        }
+
+        // 올바른 권한이 아니면 예외 발생: ADMIN, USER 둘 만 존재
+        if (!member.role().equals("ADMIN") && !member.role().equals("USER")) {
+            throw InvalidGrantTypeException.EXCEPTION;
+        }
+
+        return authTokenGenerator.generateToken(member);
+    }
+
+}
