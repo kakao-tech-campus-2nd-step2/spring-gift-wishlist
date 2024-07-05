ff5ecee9f1576b1ade4803ae1c809534670ef4e6
SeoYeongU <westzeroright@gmail.com>
2024-07-06 04:53:36 +0900

    feat(WishlistDao): 장바구니 테이블 접근

---
+++ src/main/java/gift/wishlist/WishlistDao.java
@@ -0,0 +1,64 @@
+package gift.wishlist;
+
+import gift.member.Member;
+import gift.product.Product;
+import gift.product.ProductDao;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class WishlistDao {
+
+    private final ProductDao productDao;
+    private JdbcClient jdbcClient;
+
+    public WishlistDao(JdbcClient jdbcClient, ProductDao productDao) {this.jdbcClient = jdbcClient;
+        this.productDao = productDao;
+    }
+
+    public void insertWish(Member member, Long productId) {
+        var sql = """
+            insert into wishlist (member_id, product_id) 
+            values (?,?)
+            """;
+        jdbcClient.sql(sql)
+            .param(member.getId())
+            .param(productId)
+            .update();
+    }
+
+    public List<Product> findAllWish() {
+        var sql = """
+            select product_id
+            from wishlist
+            """;
+        List<Long> productIds = jdbcClient.sql(sql)
+            .query(Long.class)
+            .list();
+        return  productDao.findProductById(productIds);
+
+    }
+
+  public Optional<Long> findProductById(Long product_id) {
+        var sql = """
+            select product_id 
+            from wishlist 
+            where product_id = ?
+            """;
+        return jdbcClient.sql(sql).param(product_id).query(Long.class).optional();
+  }
+
+    public void deleteWish(Long productId) {
+        var sql = """
+            delete from wishlist 
+            where product_id = ?
+            """;
+        jdbcClient.sql(sql)
+            .param(productId)
+            .update();
+    }
+}
