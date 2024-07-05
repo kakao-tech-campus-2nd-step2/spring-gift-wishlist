6c1f3ddb543c8c08a9491517f70e19cc179f0dde
youcaslte03 <luckthb325@naver.com>
2024-07-05 14:14:07 +0900

    feat:WebConfig

---
+++ src/main/java/gift/config/WebConfig.java
@@ -0,0 +1,21 @@
+package gift.config;
+
+import gift.domain.JwtInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private JwtInterceptor jwtInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(jwtInterceptor).addPathPatterns("/api/products/**");
+    }
+
+}
