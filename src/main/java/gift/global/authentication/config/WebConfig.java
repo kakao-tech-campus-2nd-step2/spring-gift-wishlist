63426dea5c9b029669fde4716a919ec11bb0a8f7
sim-mer <gyu2781@gmail.com>
2024-07-05 12:27:37 +0900

    feat(authentication) : 토큰 검증 인터셉터 구현과 configuration

---
+++ src/main/java/gift/global/authentication/config/WebConfig.java
@@ -0,0 +1,21 @@
+package gift.global.authentication.config;
+
+import gift.global.authentication.interceptor.TokenCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final TokenCheckInterceptor tokenCheckInterceptor;
+
+    public WebConfig(TokenCheckInterceptor tokenCheckInterceptor) {
+        this.tokenCheckInterceptor = tokenCheckInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(tokenCheckInterceptor)
+            .addPathPatterns("/api/**");
+    }
+}
