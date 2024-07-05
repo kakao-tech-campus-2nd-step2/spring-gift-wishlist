e81a1ffb87cae07b52172fedbee200c526efb165
pinkbeenyeti <rbgusdn12@gmail.com>
2024-07-05 16:22:38 +0900

    feat: 각 페이지에서의 비즈니스 로직을 처리하는 계층

--- src/main/java/gift/service/AuthService.java
+++ src/main/java/gift/service/AuthService.java
@@ -1,2 +1,19 @@
-package gift.service;public class AuthService {
+package gift.service;
+
+import gift.dto.UserDTO;
+import gift.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    private final UserRepository userRepository;
+
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public boolean comparePassword(UserDTO userDTO) {
+        UserDTO realUser = userRepository.selectUser(userDTO.email());
+        return realUser.password().equals(userDTO.password());
+    }
 }
--- src/main/java/gift/service/UserService.java
+++ src/main/java/gift/service/UserService.java
@@ -1,2 +1,41 @@
-package gift.service;public class UserService {
+package gift.service;
+
+import gift.dto.TokenDTO;
+import gift.dto.UserDTO;
+import gift.model.User;
+import gift.repository.UserRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public void createUser(UserDTO userDTO) {
+        userRepository.insertUser(new User(
+                userDTO.email(),
+                userDTO.password()
+        ));
+    }
+
+    public boolean redundantUser(UserDTO user) {
+        return userRepository.countUsers(user.email())
+                .orElse(0) > 0;
+    }
+
+    public TokenDTO makeToken(UserDTO user) {
+        String secretKey = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
+        String accessToken = Jwts.builder()
+                .setSubject(user.email())
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .compact();
+
+        return new TokenDTO(accessToken);
+    }
 }
