57150a87686082e86376479eafe0e7dd803074eb
hyyyh0x <hyyyh0x@gmail.com>
2024-07-05 14:26:06 +0900

    refactor: delete codes

--- src/main/java/gift/auth/AuthController.java
+++ src/main/java/gift/auth/AuthController.java
@@ -1,16 +1,9 @@
 package gift.auth;
 
-import gift.user.User;
-import java.util.Collections;
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/login")
