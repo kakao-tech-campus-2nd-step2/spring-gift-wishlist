a9c327bb70e4c4598402c2066577e167547df7b9
mac <20j.code@gmail.com>
2024-07-05 15:30:05 +0900

    feat : 회원 모델 + 회원 등급 추가

---
+++ src/main/java/gift/model/Member.java
@@ -0,0 +1,33 @@
+package gift.model;
+
+public class Member {
+
+    private Long id;
+    private String email;
+    private String password;
+    private MemberRole role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
