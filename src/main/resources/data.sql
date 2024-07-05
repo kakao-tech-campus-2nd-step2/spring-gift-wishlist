1e2c7db1ed76d4963ebf55ce058a53a77ae8008f
bokyeong <wntjdals0412@gmail.com>
2024-07-05 14:59:52 +0900

    Feat: member 도메인 및 데이터

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,2 @@
 INSERT INTO product(name,price,img_url) values ('coffee',10000,'test.jpg');
+INSERT INTO member(email,name,password,role) values ('test@gmail.com','테스터','password','1');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,3 +4,11 @@ CREATE TABLE product (
     price INT NOT NULL,
     img_url VARCHAR(255)
 );
+
+CREATE TABLE member (
+     id BIGINT AUTO_INCREMENT PRIMARY KEY,
+     email VARCHAR(255) NOT NULL unique,
+     name VARCHAR(255) NOT NULL,
+     password VARCHAR(255) NOT NULL,
+     role INT NOT NULL
+);
