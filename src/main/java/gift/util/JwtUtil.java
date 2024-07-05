60ccac7eacfde8efe79e23ca5f300130a5e903c0
bokyeong <wntjdals0412@gmail.com>
2024-07-05 15:01:51 +0900

    Feat: jwt 토큰 생성을 위한 util 생성

---
+++ src/main/java/gift/util/JwtUtil.java
@@ -0,0 +1,26 @@
+package gift.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    @Value ("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiredMs}")
+    private Long expirationTime;
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+}
