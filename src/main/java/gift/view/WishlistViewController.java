5ca93cbc447269e62cb5387e20b6abb457bd8e64
dong_yxxn <g0326kdy@gmail.com>
2024-07-05 16:53:26 +0900

    refactor(WishlistViewController): saveWishlist() requestbody로 변경, redirect 추가

--- src/main/java/gift/view/WishlistViewController.java
+++ src/main/java/gift/view/WishlistViewController.java
@@ -4,10 +4,14 @@ import gift.controller.ProductController;
 import gift.controller.WishlistController;
 import gift.model.Product;
 import gift.model.WishlistItem;
+import javax.print.attribute.standard.PrinterURI;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -16,9 +20,11 @@ import java.util.List;
 @RequestMapping("/wishlist")
 public class WishlistViewController {
     private WishlistController wishlistController;
+    private ProductController productController;
 
-    public WishlistViewController(WishlistController wishlistController) {
+    public WishlistViewController(WishlistController wishlistController, ProductController productController) {
         this.wishlistController = wishlistController;
+        this.productController = productController;
     }
 
     @GetMapping("{id}")
@@ -27,5 +33,18 @@ public class WishlistViewController {
         model.addAttribute("wishlists", wishlists);
         model.addAttribute("id", userId);
         return "wishlist";
+    }
+
+    @GetMapping("{id}/new")
+    public String showAddProduct(@PathVariable("id") Long userId, Model model) {
+        List<Product> products = productController.getAllProducts().getBody();
+        model.addAttribute("products", products);
+        model.addAttribute("userId", userId);
+        return "add_wishlist";
+    }
+    @PostMapping("{id}/save")
+    public String saveWishlist(@PathVariable Long id, @RequestBody List<WishlistItem> wishlistItems) {
+        wishlistController.createWishlist(wishlistItems);
+        return "redirect:/wishlist/" + id;
     }
 }
