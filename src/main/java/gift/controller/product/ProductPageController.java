04858cadb18f42e4d5c765084deaf8838c2bab39
momnpa333 <momnpa333@gmail.com>
2024-07-05 17:00:17 +0900

    feat: Auth 어노테이션 추가

--- src/main/java/gift/controller/product/ProductPageController.java
+++ src/main/java/gift/controller/product/ProductPageController.java
@@ -1,11 +1,14 @@
-package gift.controller;
+package gift.controller.product;
 
+import gift.auth.Auth;
+import gift.model.user.Role;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ProductAdminController {
+public class ProductPageController {
 
+    @Auth(role = Role.ADMIN)
     @GetMapping("/admin")
     public String admin() {
         return "adminPage";
