5dcdda1416cc8d2f3ee6df5366f68ee8a369552c
wvssm <neoramiregi@gmail.com>
2024-07-05 13:20:56 +0900

    feat(WishService): Wish List 상품 목록 조회 Service 구현

--- src/main/java/gift/service/WishService.java
+++ src/main/java/gift/service/WishService.java
@@ -4,20 +4,28 @@ import gift.domain.Product;
 import gift.domain.User;
 import gift.domain.Wish;
 import gift.dto.WishRequestDto;
+import gift.dto.WishResponseDto;
 import gift.exception.ProductNotFoundException;
 import gift.exception.UserNotFoundException;
+import gift.exception.WishNotFoundException;
 import gift.repository.ProductRepository;
 import gift.repository.UserRepository;
 import gift.repository.WishRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class WishService {
     private final WishRepository wishRepository;
     private final UserRepository userRepository;
     private final ProductRepository productRepository;
+
+    private final String NOT_FOUND_USER_BY_EMAIL_MESSAGE = "해당 email의 유저가 존재하지 않습니다.";
+    private final String NOT_FOUND_PRODUCT_BY_NAME_MESSAGE = "해당 이름의 상품이 존재하지 않습니다.";
+    private final String NOT_FOUND_USER_WISHLIST_MESSAGE = "해당 유저의 위시리스트가 존재하지 않습니다.";
 
     public WishService(WishRepository wishRepository, UserRepository userRepository, ProductRepository productRepository) {
         this.wishRepository = wishRepository;
@@ -27,9 +35,32 @@ public class WishService {
 
     public void save(String userEmail, WishRequestDto request){
         User user = userRepository.findByEmail(userEmail)
-                .orElseThrow(() -> new UserNotFoundException("해당 email의 유저가 존재하지 않습니다."));
+                .orElseThrow(() -> new UserNotFoundException(NOT_FOUND_USER_BY_EMAIL_MESSAGE));
         Product product = productRepository.findByName(request.getProductName())
-                .orElseThrow(() -> new ProductNotFoundException("해당 이름의 상품이 존재하지 않습니다."));;
+                .orElseThrow(() -> new ProductNotFoundException(NOT_FOUND_PRODUCT_BY_NAME_MESSAGE));
         wishRepository.save(new Wish(user.getId(),product.getId(), request.getQuantity()));
+    }
+
+    public List<WishResponseDto> findByUserEmail(String userEmail){
+        User user = userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new UserNotFoundException(NOT_FOUND_USER_BY_EMAIL_MESSAGE));
+        return wishRepository.findByUserId(user.getId())
+                .orElseThrow(() -> new WishNotFoundException(NOT_FOUND_USER_WISHLIST_MESSAGE))
+                .stream()
+                .map(this::convertToWishDto)
+                .collect(Collectors.toList());
+    }
+
+    private WishResponseDto convertToWishDto(Wish wish) {
+        Product product = productRepository.findById(wish.getProductId())
+                .orElseThrow(() -> new ProductNotFoundException(NOT_FOUND_PRODUCT_BY_NAME_MESSAGE));
+        return new WishResponseDto(
+                wish.getId(),
+                wish.getProductId(),
+                product.getName(),
+                product.getPrice(),
+                product.getImageUrl(),
+                wish.getQuantity()
+        );
     }
 }
