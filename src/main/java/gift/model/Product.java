d1cda0beea05658897f84d4f70dbc931830669a9
Jeje <jjh4450jjh4450@gmail.com>
2024-07-05 14:23:32 +0900

    fix(Product.java): 알파벳에 대해서만 허용하는 현상과 카카오에 대해서 판별하지 않던 부분을 해결함

--- src/main/java/gift/model/Product.java
+++ src/main/java/gift/model/Product.java
@@ -13,7 +13,8 @@ public record Product(
 
     @NotNull(message = "이름은 null이 될 수 없습니다.")
     @Size(min = 1, max = 15, message = "이름은 1자에서 15자 사이여야 합니다.")
-    @Pattern(regexp = "^[a-zA-Z0-9\\s\\(\\)\\[\\]\\+\\-\\&\\/\\_]*$", message = "이름은 알파벳, 숫자, 공백 및 특수 문자 ( ) [ ] + - & / _ 만 포함할 수 있습니다.")
+    @Pattern(regexp = "^[\\p{L}\\p{N}\\s\\(\\)\\[\\]\\+\\-\\&\\/\\_]*$", message = "이름은 문자, 숫자, 공백 및 특수 문자 ( ) [ ] + - & / _ 만 포함할 수 있습니다.")
+    @Pattern(regexp = "^(?!.*카카오).*$", message = "카카오가 들어가는 품목명은 담당 MD와 협의한 경우에만 사용 가능합니다.")
     String name,
 
     @NotNull(message = "가격은 null이 될 수 없습니다.")
