17da19dfaf854256e9dfbbbc6502291cf71480eb
hyunseo Jeong <wjdgustj2877@naver.com>
2024-07-05 15:49:21 +0900

    feat: 위시리스트 항목 객체 클래스 작성

---
+++ src/main/java/gift/domain/wishlist/entity/WishItem.java
@@ -0,0 +1,32 @@
+package gift.domain.wishlist.entity;
+
+import gift.domain.product.entity.Product;
+import gift.domain.user.entity.User;
+
+public class WishItem {
+    Long id;
+    User user;
+    Product product;
+
+    public WishItem(Long id, User user, Product product) {
+        this.id = id;
+        this.user = user;
+        this.product = product;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
