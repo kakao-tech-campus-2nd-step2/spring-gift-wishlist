b051b7db0969a06748a03d7ca88d6ace7eda44dc
us4c0d3 <jwseok312@gmail.com>
2024-07-05 09:52:55 +0900

    refactor(ExceptionHandler): Map으로 body를 직접 만드는 방식에서 ProblemDetail 클래스를 사용하도록 변경

--- src/main/java/gift/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -1,50 +1,27 @@
 package gift.exception;
 
-import java.util.HashMap;
-import java.util.Map;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, Object>> handleMethodArgumentNotValidException(
-        MethodArgumentNotValidException ex, WebRequest request) {
-        HashMap<String, Object> body = new HashMap<>();
-        HttpStatus status = HttpStatus.BAD_REQUEST;
+    public ProblemDetail handleMethodArgumentNotValidException (MethodArgumentNotValidException ex) {
+        String message = ex.getBindingResult()
+                            .getAllErrors()
+                            .getFirst()
+                            .getDefaultMessage();
 
-        body.put("timestamp", System.currentTimeMillis());
-        body.put("status", status.value());
-        body.put("error", status.getReasonPhrase());
-        body.put("message", ex.getBindingResult()
-                                .getAllErrors()
-                                .getFirst()
-                                .getDefaultMessage());
-        body.put("path", request.getDescription(false)
-                                .replace("uri=", ""));
-
-        return ResponseEntity.status(status).body(body);
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, message);
     }
 
     @ExceptionHandler(value = ProductNotFoundException.class)
-    public ResponseEntity<Map<String, Object>> handleProductNotFoundException(
-        ProductNotFoundException ex, WebRequest request) {
-        HashMap<String, Object> body = new HashMap<>();
-        HttpStatus status = HttpStatus.NOT_FOUND;
-
-        body.put("timestamp", System.currentTimeMillis());
-        body.put("status", status.value());
-        body.put("error", status.getReasonPhrase());
-        body.put("message", ex.getMessage());
-        body.put("path", request.getDescription(false)
-                                .replace("uri=", ""));
-
-        return ResponseEntity.status(status).body(body);
+    public ProblemDetail handleProductNotFoundException (ProductNotFoundException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
     }
 
 }
