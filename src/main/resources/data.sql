96db2a7674e72c8b55d42df3155bfc666bee43b0
hyunseo <wndlthsk@naver.com>
2024-07-05 16:05:23 +0900

    feat : member테이블 추가 및 초기화

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,2 @@
 INSERT INTO product (name, price, imageUrl) VALUES ('iced americano', 4500, 'https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg');
+INSERT INTO member (email, password) VALUES ('testemail@email', 'testpassword');
