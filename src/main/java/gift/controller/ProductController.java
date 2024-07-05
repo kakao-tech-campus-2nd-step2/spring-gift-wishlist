c3a9fdff36eecbb60ba36f78a37ce55d0468cc92
Yongjin An <yongjin5352@gmail.com>
2024-07-05 14:12:13 +0900

    refactor: Rename model package to entity package

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -1,9 +1,8 @@
 package gift.controller;
 
-import gift.model.ProductRecord;
+import gift.entity.ProductRecord;
 import gift.repository.ProductDAO;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
---
+++ src/main/java/gift/entity/User.java
@@ -0,0 +1,7 @@
+package gift.entity;
+
+public class User {
+    private int id;
+    private String email;
+    private String password;
+}
