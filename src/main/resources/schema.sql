62d0fba96ff194d4c2eee5a6f5ce6c28f4547d33
sim-mer <gyu2781@gmail.com>
2024-07-06 14:35:27 +0900

    fix(schema.slq) : member email에 unique 제약조건 설정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE product (
 
 CREATE TABLE member (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
 
