dbe45a119999a0e201e5218c23542fa62fbb08c8
sanghee0820 <lsh277604@gmail.com>
2024-07-07 00:09:50 +0900

    feat: Wishes ddl 추가

--- src/main/resources/static/ddl.sql
+++ src/main/resources/static/ddl.sql
@@ -1,14 +1,26 @@
-CREATE TABLE IF NOT EXISTS products (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    price int NOT NULL,
-    image_url VARCHAR(255),
+CREATE TABLE IF NOT EXISTS products
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name       VARCHAR(255) NOT NULL,
+    price      int          NOT NULL,
+    image_url  VARCHAR(255),
     is_deleted BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE IF NOT EXISTS users(
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS users
+(
+    id       BIGINT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role VARCHAR(255) NOT NULL
+    role     VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS wishes
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    user_id    BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    amount     INT    NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (product_id) REFERENCES products (id)
 );
