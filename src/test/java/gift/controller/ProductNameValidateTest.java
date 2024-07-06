9d329e27a3840e94cde400cc8e79ef4cd54008a8
Seonghun Jeong <tjdgns5506@gmail.com>
2024-07-06 23:50:08 +0900

    Revert "Merge branch 'zzoe2346' into step2"
    This reverts commit 63e222325640749a9e0a0d050344dde0e6641036, reversing
    changes made to 829964aa1dd9a57dc57ad38f5876234cc9b98fe7.

--- src/test/java/gift/controller/ProductNameValidateTest.java
+++ src/test/java/gift/controller/ProductNameValidateTest.java
@@ -1,9 +1,9 @@
 package gift.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import gift.controller.api.ProductRestController;
-import gift.repository.ProductRepository;
+import gift.controller.product.ProductController;
 import gift.dto.Product;
+import gift.service.ProductService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -17,23 +17,23 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(ProductRestController.class)
+@WebMvcTest(ProductController.class)
 class ProductNameValidateTest {
 
     @MockBean
-    ProductRepository productDB;
+    private ProductService productDB;
     private MockMvc mockMvc;
-    ObjectMapper objectMapper;
+    private ObjectMapper objectMapper;
 
     @BeforeEach
     void setUp() {
-        this.mockMvc = MockMvcBuilders.standaloneSetup(new ProductRestController(productDB)).build();
+        this.mockMvc = MockMvcBuilders.standaloneSetup(new ProductController(productDB)).build();
         this.objectMapper = new ObjectMapper();
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"a", "ab", "abc", "abcd", "15자aaaaaaaaaaaa", "    햇반      "})
-    @DisplayName("상품 이름이 15자 이하인 경우에 Created 상태반환")
+    @ValueSource(strings = {"a", "ab", "abc", "abcd", "15자aaaaaaaaaaaa", "    햇반      ", "[단독] 고급 지갑", "커피&우유", "1+1 제품", "/바로/구매___", "-_- 안사면 후회"})
+    @DisplayName("유효한 상품 이름 검증 TEST")
     void lengthTest(String name) throws Exception {
         Product product = new Product(name, 10000, "imageUrl");
         String json = objectMapper.writeValueAsString(product);
@@ -45,48 +45,9 @@ class ProductNameValidateTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"[단독] 고급 지갑", "커피&우유", "1+1 제품", "/바로/구매___", "-_- 안사면 후회"})
-    @DisplayName("상품 이름에 허용된 특수문자가 포함된 경우에 Created 상태반환")
-    void allowedSpeiclCharTest(String name) throws Exception {
-        Product product = new Product(name, 10000, "imageUrl");
-        String json = objectMapper.writeValueAsString(product);
-        productDB.addProduct(product);
-
-        mockMvc.perform(MockMvcRequestBuilders
-                        .post("/api/products").contentType(MediaType.APPLICATION_JSON).content(json))
-                .andExpect(status().isCreated());
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"공백              포함               ", "aaaaaaaaaaaaaaaa", "aaaaaaaaaaaaaaaaa", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"})
-    @DisplayName("상품 이름이 15자 초과인 경우에 BadRequest 상태반환")
+    @ValueSource(strings = {"!!!", "저렴한 우유!!", "@멘션", "#더샵", "진짜~~~~", "카카오톡", "리얼 카카오 우유", "카카오카카오", "진짜카카오100", "공백              포함               ", "aaaaaaaaaaaaaaaa", "aaaaaaaaaaaaaaaaa", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"})
+    @DisplayName("비유효한 상품 이름 검증 TEST")
     void t2(String name) throws Exception {
-        Product product = new Product(name, 10000, "imageUrl");
-        String json = objectMapper.writeValueAsString(product);
-        productDB.addProduct(product);
-
-        mockMvc.perform(MockMvcRequestBuilders
-                        .post("/api/products").contentType(MediaType.APPLICATION_JSON).content(json))
-                .andExpect(status().isBadRequest());
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"카카오톡", "리얼 카카오 우유", "카카오카카오", "진짜카카오100"})
-    @DisplayName("상품 이름에 '카카오'포함 경우에 BadRequest 상태반환")
-    void kakaoWordTest(String name) throws Exception {
-        Product product = new Product(name, 10000, "imageUrl");
-        String json = objectMapper.writeValueAsString(product);
-        productDB.addProduct(product);
-
-        mockMvc.perform(MockMvcRequestBuilders
-                        .post("/api/products").contentType(MediaType.APPLICATION_JSON).content(json))
-                .andExpect(status().isBadRequest());
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"!!!", "저렴한 우유!!", "@멘션", "#더샵", "진짜~~~~"})
-    @DisplayName("상품 이름에 비허용된 특수문자가 포함된 경우에 BadRequest 상태반환(허용: ( ) [ ] + - & / _ ")
-    void containsSpeciaCharTest(String name) throws Exception {
         Product product = new Product(name, 10000, "imageUrl");
         String json = objectMapper.writeValueAsString(product);
         productDB.addProduct(product);
