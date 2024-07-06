f6b110e7fda3c215794437688a82566ed5dea4af
suhyeon7497 <nucc1186@gmail.com>
2024-07-06 15:56:49 +0900

    chores: 불필요한 기능 제거

--- src/main/java/gift/service/AuthService.java
+++ src/main/java/gift/service/AuthService.java
@@ -62,14 +62,6 @@ public class AuthService {
                 .parseSignedClaims(token);
             Claims payload = jws.getPayload();
             User user = userDao.selectUserById(Long.parseLong(payload.getSubject()));
-            if (payload.get("name", String.class) == null || !user.getName()
-                .equals(payload.get("name", String.class))) {
-                return false;
-            }
-            if (payload.get("role", String.class) == null || !user.getRole()
-                .equals(payload.get("role", String.class))) {
-                return false;
-            }
             return true;
         } catch (Exception e) {
             return false;
