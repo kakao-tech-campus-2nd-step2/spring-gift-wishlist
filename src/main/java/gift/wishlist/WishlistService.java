1a3013ba4297c2ae6931f0757e657f859dc60142
geist9110 <geist9110@gmail.com>
2024-07-07 05:12:24 +0900

    feat(wishlist): Add wishlist service layer

---
+++ src/main/java/gift/wishlist/WishlistService.java
@@ -0,0 +1,42 @@
+package gift.wishlist;
+
+import gift.member.Member;
+import gift.product.Product;
+import gift.product.ProductRepository;
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WishlistService {
+
+    private final WishlistRepository wishlistRepository;
+    private final ProductRepository productRepository;
+
+    public WishlistService(
+        WishlistRepository wishlistRepository,
+        ProductRepository productRepository
+    ) {
+        this.wishlistRepository = wishlistRepository;
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllWishlists(Member member) {
+        return wishlistRepository.getAllWishlists(member);
+    }
+
+    public void addWishlist(Member member, long productId) {
+        hasProductByProductID(productId);
+        wishlistRepository.addWishlist(new Wishlist(productId, member.email()));
+    }
+
+    public void deleteWishlist(Member member, long productId) {
+        hasProductByProductID(productId);
+        wishlistRepository.deleteWishlist(new Wishlist(productId, member.email()));
+    }
+
+    private void hasProductByProductID(long productId) {
+        if (!productRepository.existProduct(productId)) {
+            throw new IllegalArgumentException("Product does not exist");
+        }
+    }
+}
