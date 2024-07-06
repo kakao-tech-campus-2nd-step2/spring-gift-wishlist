9b2a1e785f76ad627fcfb49f9b64735ce4668973
Seonghun Jeong <tjdgns5506@gmail.com>
2024-07-07 00:14:53 +0900

    test: 멤버 컨트롤러 테스트 리팩토링

--- src/test/java/gift/controller/member/MemberControllerTest.java
+++ src/test/java/gift/controller/member/MemberControllerTest.java
@@ -5,6 +5,7 @@ import gift.dto.request.MemberRequest;
 import gift.repository.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -18,15 +19,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class MemberControllerTest {
 
+    @Autowired
     private MemberRepository memberRepository;
+    @Autowired
     private MockMvc mockMvc;
     ObjectMapper objectMapper = new ObjectMapper();
     MemberRequest newMember = new MemberRequest("zzoe2346@github.com", "1234");
-
-    public MemberControllerTest(MemberRepository memberRepository, MockMvc mockMvc) {
-        this.memberRepository = memberRepository;
-        this.mockMvc = mockMvc;
-    }
 
     @Test
     @DisplayName("정상 회원가입 테스트")
