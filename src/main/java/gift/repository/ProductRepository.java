5ffd3cee457ecd14a22e816bca352bc2c05033d5
pinkbeenyeti <rbgusdn12@gmail.com>
2024-07-07 03:30:06 +0900

    feat: ProductRepository 생성 컨트톨러가 하는 역할이 너무 많아 분리하기 위해 생성하였다. DB와 직접적인 상호 작용을 하고 서비스와 상호작용 한다.

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -1,2 +1,49 @@
-package gift.repository;public class ProductRepository {
+package gift.repository;
+
+import gift.dto.ProductDTO;
+import gift.model.Product;
+import gift.model.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ProductRepository {
+    private final JdbcTemplate jdbcTemplate;
+    private SimpleJdbcInsert simpleJdbcInsert;
+
+    private static final String productInsertSql = "INSERT INTO PRODUCTS (id, name, price, imageurl) VALUES (?, ?, ?, ?)";
+    private static final String allProductsSelectSql = "SELECT * FROM PRODUCTS";
+    private static final String productUpdateSql = "UPDATE PRODUCTS SET name = ?, price = ?, imageurl = ? WHERE id = ?";
+    private static final String productDeleteSql = "DELETE FROM PRODUCTS WHERE id = ?";
+
+    public ProductRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void insertToTable(Product product) {
+        jdbcTemplate.update(productInsertSql, product.getId(), product.getName(), product.getPrice(), product.getImageUrl());
+    }
+
+    public List<Product> selectAllProducts() {
+        return jdbcTemplate.query(
+                allProductsSelectSql,
+                (resultSet, rowNum) -> new Product(
+                        resultSet.getLong("id"),
+                        resultSet.getString("name"),
+                        resultSet.getInt("price"),
+                        resultSet.getString("imageurl")
+                )
+        );
+    }
+
+    public void updateToTable(Long id, Product product) {
+        jdbcTemplate.update(productUpdateSql, product.getName(), product.getPrice(), product.getImageUrl(), id);
+    }
+
+    public void deleteToTable(Long id) {
+        jdbcTemplate.update(productDeleteSql, id);
+    }
 }
