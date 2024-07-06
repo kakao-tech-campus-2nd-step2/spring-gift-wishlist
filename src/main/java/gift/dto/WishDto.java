bdfde7a59db75a76bfba2333a3efe8b0f1ab632c
UiChan Lee <junglkls1234@gmail.com>
2024-07-07 00:47:26 +0900

    Feat: wishDto 생성

--- src/main/java/gift/dto/WishDto.java
+++ src/main/java/gift/dto/WishDto.java
@@ -1,6 +1,25 @@
 package gift.dto;
 
 public class WishDto {
-    private String productId;
+    private Long productId;
+    private String memberEmail;
+    private int amount;
 
+    public WishDto(Long productId, String memberEmail, int amount){
+        this.productId = productId;
+        this.memberEmail = memberEmail;
+        this.amount = amount;
+    }
+
+    public Long getProductId(){
+        return productId;
+    }
+
+    public String getMemberEmail(){
+        return memberEmail;
+    }
+
+    public int getAmount(){
+        return amount;
+    }
 }
