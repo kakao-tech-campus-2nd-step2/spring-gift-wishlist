72371aee226468c35c00f2dc9eeb80571602823a
dkswoals <a4445656@gmail.com>
2024-07-06 17:27:56 +0900

    feat: 이메일 중복, 없음, 비밀번호 불일치 예외 구현

---
+++ src/main/java/wishlist/exception/CustomException/DuplicateEmailException.java
@@ -0,0 +1,12 @@
+package wishlist.exception.CustomException;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+public class DuplicateEmailException extends MethodArgumentNotValidException {
+    public DuplicateEmailException(MethodParameter parameter,
+        BindingResult bindingResult) {
+        super(parameter, bindingResult);
+    }
+}
