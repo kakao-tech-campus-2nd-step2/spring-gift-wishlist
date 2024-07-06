66fe3191fa688b501a36a7db13ccffdfd2a1d65e
J-1ac <tkdwns26@naver.com>
2024-07-07 00:49:24 +0900

    feat: UserReqeustDto 제약사항 추가

--- src/main/java/gift/domain/model/UserRequestDto.java
+++ src/main/java/gift/domain/model/UserRequestDto.java
@@ -1,8 +1,15 @@
 package gift.domain.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class UserRequestDto {
 
+    @NotBlank
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
     private String email;
+
+    @NotBlank
     private String password;
 
     public String getEmail() {
