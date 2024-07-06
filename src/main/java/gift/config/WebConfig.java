1fc6c3e5a1fabbf4c86212ba8542a6f65dd1af32
suhyeon7497 <nucc1186@gmail.com>
2024-07-06 15:58:32 +0900

    feat: add URL /wishes in authinterceptor range

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -22,7 +22,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor)
-            .addPathPatterns("/api/**", "/admin/**");
+            .addPathPatterns("/api/**", "/admin/**","/wishes");
     }
 
     @Override
