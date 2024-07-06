db1c78337068cec3f27af5dc161a4f142c73447a
pinkbeenyeti <rbgusdn12@gmail.com>
2024-07-07 03:33:27 +0900

    feat: 프로덕스 서비스 계층 생성

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -1,4 +1,42 @@
 package gift.service;
 
-public class ProducteService {
+import gift.dto.ProductDTO;
+import gift.dto.ProductsDTO;
+import gift.model.Product;
+import gift.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public void createProduct(ProductDTO productDTO) {
+        productRepository.insertToTable(new Product(
+                productDTO.id(),
+                productDTO.name(),
+                productDTO.price(),
+                productDTO.imageUrl()
+        ));
+    }
+
+    public ProductsDTO getAllProducts() {
+        return new ProductsDTO(productRepository.selectAllProducts());
+    }
+
+
+    public void updateProduct(Long id, ProductDTO productDTO) {
+        productRepository.updateToTable(id, new Product(
+                productDTO.id(),
+                productDTO.name(),
+                productDTO.price(),
+                productDTO.imageUrl()));
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteToTable(id);
+    }
 }
