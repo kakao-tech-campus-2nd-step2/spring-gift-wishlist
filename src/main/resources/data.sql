192c26b917a8bfa6bc92d0774c5429b5213750aa
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-07-07 02:22:06 +0900

    feat : data.sql WishLists 테이블 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,3 +12,8 @@ INSERT INTO PRODUCTS (name, price, imageUrl)
 values ('바닐라라떼',
         6000,
         '이미지URL3.....');
+
+INSERT INTO USERS (email, password, nickname)
+values ('yoonsu0325@gmail.com',
+        'thisIsPassword1234',
+        'yoonsu0325')
