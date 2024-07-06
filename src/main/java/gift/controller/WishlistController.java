66719035b4d5f1b134f450c0a4281c35db5980ca
GamJaDo <kim0615157@gmail.com>
2024-07-07 07:06:16 +0900

    style: WishlistController.java
    Controller를 최대한 얇게 변경

--- src/main/java/gift/controller/WishlistController.java
+++ src/main/java/gift/controller/WishlistController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,10 +14,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import gift.exception.InvalidProductException;
-import gift.model.User;
 import gift.model.Wishlist;
-import gift.service.AuthService;
 import gift.service.WishlistService;
 
 @RestController
@@ -26,13 +24,9 @@ public class WishlistController {
 	@Autowired
 	private WishlistService wishlistService;
 	
-	@Autowired
-	private AuthService authService;
-	
 	@GetMapping
-	public ResponseEntity<List<Wishlist>> getWishlist(@RequestHeader("Authorization") String token){
-		User user = authService.getUser(token);
-		List<Wishlist> wishlist = wishlistService.getWishlist(user);
+	public ResponseEntity<List<Wishlist>> getWishlist(@RequestHeader("Authorization") String token, BindingResult bindingResult){
+		List<Wishlist> wishlist = wishlistService.getWishlist(token, bindingResult);
 		if(wishlist.isEmpty()) {
 			return ResponseEntity.noContent().build();
 		}
@@ -40,32 +34,23 @@ public class WishlistController {
 	}
 
 	@PostMapping
-	public ResponseEntity<String> addWishlist(@RequestHeader("Authorization") String token, @RequestBody Wishlist wishlist){
-		User user = authService.getUser(token);
-		int rowsAffected = wishlistService.addWishlist(user, wishlist.getProductName(), wishlist.getQuantity());
-		if(rowsAffected == 0) {
-			throw new InvalidProductException("Product could not be added to wishlist.");
-		}
+	public ResponseEntity<String> addWishlist(@RequestHeader("Authorization") String token, @RequestBody Wishlist wishlist
+			, BindingResult bindingResult){
+		wishlistService.addWishlist(token, wishlist, bindingResult);
 		return ResponseEntity.ok("Product added to wishlist successfully.");
 	}
 	
 	@DeleteMapping
-	public ResponseEntity<String> removeWishlist(@RequestHeader("Authorization") String token, @RequestBody Wishlist wishlist){
-		User user = authService.getUser(token);
-		int rowsAffected = wishlistService.removeWishlist(user, wishlist.getProductName());
-		if(rowsAffected == 0) {
-			throw new InvalidProductException("Product could not be removed from wishlist.");
-		}
+	public ResponseEntity<String> removeWishlist(@RequestHeader("Authorization") String token, @RequestBody Wishlist wishlist
+			, BindingResult bindingResult){
+		wishlistService.removeWishlist(token, wishlist, bindingResult);
 		return ResponseEntity.ok("Product removed from wishlist successfully.");
 	}
 	
 	@PutMapping
-	public ResponseEntity<String> updateWishlist(@RequestHeader("Authorization") String token, @RequestBody  Wishlist wishlist){
-		User user = authService.getUser(token);
-		int rowsAffected = wishlistService.updateWishlistQuantity(user, wishlist.getProductName(), wishlist.getQuantity());
-		if(rowsAffected == 0) {
-			throw new InvalidProductException("Product quantity could not be updated.");
-		}
+	public ResponseEntity<String> updateWishlist(@RequestHeader("Authorization") String token, @RequestBody  Wishlist wishlist
+			, BindingResult bindingResult){
+		wishlistService.updateWishlistQuantity(token, wishlist, bindingResult);
 		return ResponseEntity.ok("Product quantity updated successfully.");
 	}
 }
