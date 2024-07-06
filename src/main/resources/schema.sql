04f23b7b467288848b0f5ab1efc41a261a0ed064
bokyeong <wntjdals0412@gmail.com>
2024-07-06 19:07:35 +0900

    Feat: wish 테이블 생성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -12,3 +12,13 @@ CREATE TABLE member (
      password VARCHAR(255) NOT NULL,
      role INT NOT NULL
 );
+
+CREATE TABLE wish (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    member_id BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    quantity INT NOT NULL,
+
+    FOREIGN KEY (member_id) REFERENCES member (id),
+    FOREIGN KEY (product_id) REFERENCES product (id)
+);
