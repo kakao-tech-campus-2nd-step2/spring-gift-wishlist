ae72f25e1e880c727323f97dbddb3f521c0dc25f
hjinshin <gudwls818@gmail.com>
2024-07-07 00:07:05 +0900

    refactor: createdBy, updatedBy 필드 추가

--- src/main/java/gift/model/WishDao.java
+++ src/main/java/gift/model/WishDao.java
@@ -17,16 +17,16 @@ public class WishDao {
     }
 
     public void save(WishInsertRequest request, Long memberId) {
-        var sql = "insert into wish(product_id, product_count, member_id) values(?, ?, ?)";
+        var sql = "insert into wish(product_id, product_count, member_id, created_by, updated_by) values(?, ?, ?, ?, ?)";
         jdbcClient.sql(sql)
-                .params(request.productId(), 1, memberId)
+                .params(request.productId(), 1, memberId, memberId, memberId)
                 .update();
     }
 
     public void update(WishPatchRequest request, Long memberId) {
-        var sql = "update wish set product_count = ? where member_id = ? and product_id = ?";
+        var sql = "update wish set product_count = ? and updated_by = ? where member_id = ? and product_id = ?";
         jdbcClient.sql(sql)
-                .params(request.productCount(), memberId, request.productId())
+                .params(request.productCount(), memberId, memberId, request.productId())
                 .update();
     }
 
