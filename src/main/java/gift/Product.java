fbd0adb456086e68ab1f2074ce236a2780c4ceb2
seojiu <seojiu5598@gmail.com>
2024-07-07 00:15:54 +0900

    fix(Product.java): setter 추가

--- src/main/java/gift/Product.java
+++ src/main/java/gift/Product.java
@@ -1,22 +1,23 @@
 package gift;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 
 public class Product {
     private Long id;
+
     @NotBlank(message = "이름은 필수 입력 값입니다.")
-    @Size(max=15, message = "이름은 공백을 포함하여 최대 15자까지 입력 가능합니다.")
+    @Size(max = 15, message = "이름은 최대 15자까지 입력 가능합니다.")
     @Pattern(regexp = "^[a-zA-Z0-9가-힣_()+&/ ]*$",
-            message = "이름에 특수 문자는 (, ), [, ], +, -, &, /, _ 만 사용 가능합니다.")
+            message = "이름에는 특수 문자는 (, ), [, ], +, -, &, /, _ 만 사용 가능합니다.")
     private String name;
-    @Min(value = 0, message = "가격은 0원 이상의 값만 가능합니다.")
+
     private int price;
     private String imageUrl;
 
-    public Product() {}
+    public Product() {
+    }
 
     public Product(Long id, String name, int price, String imageUrl) {
         this.id = id;
@@ -29,15 +30,31 @@ public class Product {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getPrice() {
         return price;
     }
 
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
     public String getImageUrl() {
         return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 }
