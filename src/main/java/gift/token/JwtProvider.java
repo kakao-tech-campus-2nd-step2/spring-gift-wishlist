a2d7efe31cdbb1dc8e418cc817c6f9d95ef167ff
geist9110 <geist9110@gmail.com>
2024-07-07 04:59:22 +0900

    feat(wishlist): Add getMemberFromToken method in JwtProvider

--- src/main/java/gift/token/JwtProvider.java
+++ src/main/java/gift/token/JwtProvider.java
@@ -1,9 +1,11 @@
 package gift.token;
 
 import gift.user.Member;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import java.security.Key;
+import javax.crypto.SecretKey;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -19,5 +21,17 @@ public class JwtProvider {
             .claim("password", member.password())
             .signWith(KEY)
             .compact();
+    }
+
+    public Member getMemberFromToken(String token) {
+        Claims claims = Jwts.parser()
+            .verifyWith((SecretKey) KEY)
+            .build()
+            .parseSignedClaims(token)
+            .getPayload();
+        return new Member(
+            claims.get("email", String.class),
+            claims.get("password", String.class)
+        );
     }
 }
