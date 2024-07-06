011ab35a96c73835ba68ee07d4109a11a2b45f54
seojiu <seojiu5598@gmail.com>
2024-07-06 19:26:46 +0900

    fix(JwtUtil.java): 토큰 만료시간 변경

--- src/main/java/gift/JwtUtil.java
+++ src/main/java/gift/JwtUtil.java
@@ -20,7 +20,7 @@ public class JwtUtil {
                 .setClaims(claims)
                 .setSubject(email)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30 ))
                 .signWith(secretKey)
                 .compact();
     }
