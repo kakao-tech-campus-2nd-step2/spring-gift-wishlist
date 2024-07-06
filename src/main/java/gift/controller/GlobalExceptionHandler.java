bb744b9e2bca96af7879fd22ef33e017ee2d8da8
박정우 <114933967+doubleclip118@users.noreply.github.com>
2024-07-07 00:28:45 +0900

    부산대 BE_박정우 2주차 과제 (2단계,3단계) (#213)
    * feat : User repository,dto,domain
    
    * feat : gradle, AuthFilter,FilterConfig,JwtTokenProvider
    
    * feat : UserService, User Exception
    
    * feat : UserController
    
    * fix : UserDTO
    
    * fix : 수정사항 적용
    
    * fix : 서비스 메서드 이름 변경
    
    * feat : step3내용 추가
    
    * fix : 리뷰 사항 수정

--- src/main/java/gift/controller/GlobalExceptionHandler.java
+++ src/main/java/gift/controller/GlobalExceptionHandler.java
@@ -3,6 +3,13 @@ package gift.controller;
 import gift.utils.error.NotpermitNameException;
 import gift.utils.error.ProductAlreadyExistException;
 import gift.utils.error.ProductNotFoundException;
+import gift.utils.error.TokenAuthException;
+import gift.utils.error.UserAlreadyExistsException;
+import gift.utils.error.UserNotFoundException;
+import gift.utils.error.UserPasswordNotFoundException;
+import gift.utils.error.WishListAddFailedException;
+import gift.utils.error.WishListChangeFailedException;
+import gift.utils.error.WishListNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -30,4 +37,40 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.FORBIDDEN);
     }
 
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<?> handleUserAlreadyExistsException(UserAlreadyExistsException ex,
+        WebRequest request) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<?> handleUserNotFoundException(UserNotFoundException ex,
+        WebRequest request){
+        return new ResponseEntity<>(ex.getMessage(),HttpStatus.UNAUTHORIZED);
+    }
+    @ExceptionHandler(TokenAuthException.class)
+    public ResponseEntity<?> handleTokenAuthException(TokenAuthException ex,
+        WebRequest request){
+        return new ResponseEntity<>(ex.getMessage(),HttpStatus.UNAUTHORIZED);
+    }
+    @ExceptionHandler(UserPasswordNotFoundException.class)
+    public ResponseEntity<?> handleUserPasswordNotFoundException(UserPasswordNotFoundException ex,
+        WebRequest request){
+        return new ResponseEntity<>(ex.getMessage(),HttpStatus.UNAUTHORIZED);
+    }
+    @ExceptionHandler(WishListAddFailedException.class)
+    public ResponseEntity<?> handleWishListAddFailedException(WishListAddFailedException ex,
+        WebRequest request){
+        return new ResponseEntity<>(ex.getMessage(),HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(WishListNotFoundException.class)
+    public ResponseEntity<?> handleWishListNotFoundException(WishListNotFoundException ex,
+        WebRequest request){
+        return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(WishListChangeFailedException.class)
+    public ResponseEntity<?> handleWishListChangeFailedException(WishListChangeFailedException ex,
+        WebRequest request){
+        return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND);
+    }
 }
