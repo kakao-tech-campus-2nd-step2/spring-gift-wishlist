37f90d0038136a69e8c2241825cd04621430c6a5
sanghee0820 <lsh277604@gmail.com>
2024-07-07 00:23:53 +0900

    feat: wishRequest 추가

---
+++ src/main/java/gift/controller/dto/request/WishRequest.java
@@ -0,0 +1,17 @@
+package gift.controller.dto.request;
+
+import gift.model.Wish;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public record WishRequest(
+        @NotNull
+        Long productId,
+        @NotNull
+        @Min(value = 1, message = "수량은 1개 이상이어야 합니다.")
+        Integer amount
+) {
+    public Wish toModel(Long userId) {
+        return new Wish(productId(), userId, amount());
+    }
+}
