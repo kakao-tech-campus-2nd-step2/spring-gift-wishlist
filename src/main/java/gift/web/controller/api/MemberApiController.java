57e1d26887d8a86427721e194ddb748c1c90fe99
nove1080 <skwpqjq@gmail.com>
2024-07-07 02:08:07 +0900

    feat: MemberApiController updateWishProduct API 추가

--- src/main/java/gift/web/controller/api/MemberApiController.java
+++ src/main/java/gift/web/controller/api/MemberApiController.java
@@ -6,9 +6,11 @@ import gift.service.MemberService;
 import gift.service.WishProductService;
 import gift.web.dto.request.LoginRequest;
 import gift.web.dto.request.member.CreateMemberRequest;
+import gift.web.dto.request.wishproduct.UpdateWishProductRequest;
 import gift.web.dto.response.LoginResponse;
 import gift.web.dto.response.member.CreateMemberResponse;
 import gift.web.dto.response.wishproduct.ReadAllWishProductsResponse;
+import gift.web.dto.response.wishproduct.UpdateWishProductResponse;
 import java.net.URI;
 import java.net.URISyntaxException;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,7 +37,8 @@ public class MemberApiController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<CreateMemberResponse> createMember(@Validated @RequestBody CreateMemberRequest request)
+    public ResponseEntity<CreateMemberResponse> createMember(
+        @Validated @RequestBody CreateMemberRequest request)
         throws URISyntaxException {
         CreateMemberResponse response = memberService.createMember(request);
 
@@ -51,6 +55,15 @@ public class MemberApiController {
     @GetMapping("/wishlist")
     public ResponseEntity<ReadAllWishProductsResponse> readWishProduct(@LoginMember Member member) {
         ReadAllWishProductsResponse response = wishProductService.readAllWishProducts(member.getId());
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/wishlist/{wishProductId}")
+    public ResponseEntity<UpdateWishProductResponse> updateWishProduct(
+        @PathVariable Long wishProductId,
+        @Validated @RequestBody UpdateWishProductRequest request) {
+        UpdateWishProductResponse response = wishProductService.updateWishProduct(
+            wishProductId, request);
         return ResponseEntity.ok(response);
     }
 
