977c5505cd7aa91899ab11f224b7aeda26b16ddc
UiChan Lee <junglkls1234@gmail.com>
2024-07-07 01:53:41 +0900

    Refactor: wishDto 데이터 추가

--- src/main/java/gift/dto/WishDto.java
+++ src/main/java/gift/dto/WishDto.java
@@ -3,11 +3,13 @@ package gift.dto;
 public class WishDto {
     private Long productId;
     private String memberEmail;
+    private String productName;
     private int amount;
 
-    public WishDto(Long productId, String memberEmail, int amount){
+    public WishDto(Long productId, String memberEmail,String productName, int amount){
         this.productId = productId;
         this.memberEmail = memberEmail;
+        this.productName = productName;
         this.amount = amount;
     }
 
@@ -17,6 +19,10 @@ public class WishDto {
 
     public String getMemberEmail(){
         return memberEmail;
+    }
+
+    public String getProductName(){
+        return productName;
     }
 
     public int getAmount(){
