3caaae5eda705d954ea67af60740e20419d9de33
dkswoals <a4445656@gmail.com>
2024-07-06 18:31:40 +0900

    feat: 중복되는 wishitem 추가 예외 처리 헨들러 추가

--- src/main/java/wishlist/exception/GlobalExceptionHandler.java
+++ src/main/java/wishlist/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package wishlist.exception;
 import io.jsonwebtoken.JwtException;
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -32,6 +34,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(JwtException.class)
     public ResponseEntity<String> handleAuthException(JwtException e) {
         return ResponseEntity.status(ErrorCode.INVALID_TOKEN.getStatus()).body(e.getMessage());
+    }
+
+    @ExceptionHandler(DuplicateKeyException.class)
+    public ResponseEntity<?> handleDuplicateKeyException(DuplicateKeyException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     public ResponseEntity<ErrorResponseDTO> handleException(ErrorCode errorCode,
