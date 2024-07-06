7c853a99dbc717e0d54950ca5c92c2ea2589e79c
Shsin9797 <aaa8141@naver.com>
2024-07-06 18:52:12 +0900

    feat : 1주차 step1 이름 유효성 검사 기능 구현 및 http 테스트 코드작성 완료

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -4,10 +4,15 @@ import gift.dto.CreateProduct;
 import gift.dto.EditProduct;
 import gift.entity.Product;
 import gift.repository.ProductDao;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import java.util.List;
 
+@ControllerAdvice
 @Service
 public class ProductService {
 
@@ -18,6 +23,7 @@ public class ProductService {
     }
 
     public Long createProduct(CreateProduct.Request request) {
+        checkValidProductName(request.getName());
         return productDao.insert(request);
     }
 
@@ -30,12 +36,99 @@ public class ProductService {
     }
 
     public void update(int id, EditProduct.Request request) {
+        checkValidProductName(request.getName());
         productDao.update(id, request);
     }
 
     public void delete(int id) {
         productDao.delete(id);
     }
+
+
+    // 이름 유효성 검사 코드
+    public boolean checkValidProductName(String name) {
+        boolean result = false;
+        if (!(checkValidLength(name, 1, 15))) {
+            throw new IllegalProductNameLengthException();
+        }
+        if (!(checkValidSpecialCharacter(name))) {
+            throw new IllegalProductNameCharacterException();
+        }
+        if (!(checkNotContainKeyword(name, "카카오"))) {
+            throw new IllegalProductNameKeywordException();
+        }
+        result = true;
+        return result;
+
+    }
+
+    private boolean checkValidLength(String name, int start, int end) {
+        return (name.length() >= 1 && name.length() <= end);
+    }
+
+    private boolean checkValidSpecialCharacter(String name) {
+        boolean result = true;
+
+        char[] nameWords = name.toCharArray();
+        for (char word : nameWords) {
+            if (checkSpecialCharacter(word) && (!checkAllowedSpecialCharacter(word))) {
+                result = false;
+            }
+        }
+        return result;
+    }
+
+    private boolean checkAllowedSpecialCharacter(char word) {
+        char[] allowedSpecialCharacters = {'(', ')', '[', ']', '+', '-', '&', '/', '_'};
+        for (int allowedSpecialCharacter : allowedSpecialCharacters) {
+            if (word == allowedSpecialCharacter) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkSpecialCharacter(char word) {
+        boolean result = true;
+        if ((word >= 'a' && word <= 'z') ||
+                (word >= 'A' && word <= 'Z') ||
+                (word >= '0' && word <= '9') ||
+                (word >= '가' && word <= '힣') ||
+                (word == ' ')) {
+            result = false;
+        }
+        return result;
+    }
+
+    private boolean checkNotContainKeyword(String name, String keyword) {
+        return !(name.contains(keyword));
+    }
+
+    public class IllegalProductNameLengthException extends IllegalArgumentException {
+        public IllegalProductNameLengthException() {
+            super("상품 이름은 1 ~ 15 글자로 입력해주세요.");
+        }
+    }
+
+    public class IllegalProductNameCharacterException extends IllegalArgumentException {
+        public IllegalProductNameCharacterException() {
+            super("상품 이름에서 특수문자는 ()[] + - & / _ 만 사용 가능합니다.");
+        }
+    }
+
+    public class IllegalProductNameKeywordException extends IllegalArgumentException {
+        public IllegalProductNameKeywordException() {
+            super("상품이름에 \"카카오\" 키워드를 포함시키고 싶으신 경우, 담당MD와의 협의가 필요합니다. 고객센터로 문의주세요.");
+        }
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({IllegalProductNameLengthException.class, IllegalProductNameCharacterException.class, IllegalProductNameKeywordException.class})
+    public ResponseEntity<String> handleProductNameExceptions(RuntimeException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+
 //    public Map<Long,ProductDTO> getAllProducts() {
 //        Map<Long,Product> allProducts = collectionDB.findAll();
 //        Map<Long,ProductDTO> allProductsDTO= new HashMap<>();
@@ -67,91 +160,5 @@ public class ProductService {
 //        return  productDetailDTO;
 //    }
 //
-//    public boolean checkValidProductName(String name) {
-//        boolean result = false;
-//        if (!(checkValidLength(name,1,15))){
-//            throw new IllegalProductNameLengthException(); // 여기에 "상품 이름은 1 ~ 15 글자로 입력해주세요." 값 넣으면 argument 개수가 맞지 않는다고 뜹니다...줌 강의랑 똑같이 했는데 그러네요 ㅡㅜ
-//        }
-//        if (!(checkValidSpecialCharacter(name))){
-//            throw new IllegalProductNameCharacterException();
-//        }
-//        if (!(checkNotContainKeyword(name,"카카오"))){ //이렇게 이중부정 써서 구현하는거 별로인가요.. 이렇게한이유 : 카카오 포함하지 않는게 정상 -> 정상이 아닌상황에는 예외 발생
-//            throw new IllegalProductNameKeywordException();
-//        }
-//        result =true;
-//        return result;
-//
-//    }
-//
-//    private boolean checkValidLength(String name,int start, int end) {
-//        return (name.length()>=1 && name.length()<=end);
-//    }
-//
-//    private boolean checkValidSpecialCharacter(String name) {
-//        boolean result = true;
-//
-//        char [] nameWords = name.toCharArray();
-//        for (char word : nameWords) {
-//            if(checkSpecialCharacter(word) && (!checkAllowedSpecialCharacter(word))){
-//                result = false;
-//            }
-//        }
-//        return result;
-//    }
-//
-//    private boolean checkAllowedSpecialCharacter(char word) {
-//        char [] allowedSpecialCharacters = {'(',')','[',']','+','-','&','/','_'};
-//        for (int allowedSpecialCharacter: allowedSpecialCharacters) {
-//            if (word == allowedSpecialCharacter) {
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
-//
-//    private boolean checkSpecialCharacter(char word) {
-//        boolean result = true;
-//        if ((word >='a' && word <='z') ||
-//                (word >='A' && word <='Z') ||
-//                (word >='0' && word <='9') ||
-//                (word >='가' && word <='힣')) {
-//            result= false;
-//        }
-//        return result;
-//    }
-//
-//    private boolean checkNotContainKeyword(String name, String keyword) {
-//        return !(name.contains(keyword));
-//    }
-//
-//
-//
-//    private class IllegalProductNameLengthException extends RuntimeException {
-//
-//    }
-//
-//    private class IllegalProductNameCharacterException extends RuntimeException {
-//    }
-//
-//    private class IllegalProductNameKeywordException extends RuntimeException {
-//    }
-//
-//    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "상품 이름 길이가 부적절합니다.")
-//    @ExceptionHandler(IllegalProductNameLengthException.class)
-//    public String handleIllegalProductNameLengthException(IllegalProductNameLengthException e) {
-//        return "상품 이름은 1 ~ 15 글자로 입력해주세요.";
-//    }
-//
-//    @ResponseStatus(value=HttpStatus.BAD_REQUEST, reason = "상품이름에 부적절한 특수문자가 포함되어있습니다." )
-//    @ExceptionHandler(IllegalProductNameCharacterException.class)
-//    public String handleIllegalProductNameCharacterException(IllegalProductNameCharacterException e) {
-//        return "상품 이름에서 특수문자는 ()[] + - & / _ 만 사용 가능합니다.";
-//    }
-//
-//    @ResponseStatus(value=HttpStatus.BAD_REQUEST,reason = "상품 이름에 부적절한 키워드가 포함되어 있습니다.")
-//    @ExceptionHandler(IllegalProductNameKeywordException.class)
-//    public String handleIllegalProductNameKeywordException(IllegalProductNameKeywordException e) {
-//        return "상품이름에 \"카카오\" 키워드를 포함시키고 싶으신 경우, 담당MD와의 협의가 필요합니다. 고객센터로 문의주세요.";
-//    }
-//
+
 }
