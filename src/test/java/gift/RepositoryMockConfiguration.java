829964aa1dd9a57dc57ad38f5876234cc9b98fe7
SeongHun <tjdgns5506@gmail.com>
2024-07-06 16:36:06 +0900

    test: 전역 테스트시 충돌 방지위한 Mock설정파일

---
+++ src/test/java/gift/RepositoryMockConfiguration.java
@@ -0,0 +1,21 @@
+package gift;
+
+import gift.repository.MemberRepository;
+import gift.repository.ProductRepository;
+import gift.repository.TokenRepository;
+import gift.repository.WishListRepository;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@TestConfiguration
+public class RepositoryMockConfiguration {
+
+    @MockBean
+    MemberRepository memberRepository;
+    @MockBean
+    ProductRepository productRepository;
+    @MockBean
+    TokenRepository tokenRepository;
+    @MockBean
+    WishListRepository wishListRepository;
+}
