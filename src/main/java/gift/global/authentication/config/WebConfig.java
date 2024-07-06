72ec021334149defd3a2467acd7b54fc8173205a
sim-mer <gyu2781@gmail.com>
2024-07-06 15:12:07 +0900

    fix(authentication) : 리졸버 등록 코드 추가

--- src/main/java/gift/global/authentication/config/WebConfig.java
+++ src/main/java/gift/global/authentication/config/WebConfig.java
@@ -1,16 +1,21 @@
 package gift.global.authentication.config;
 
+import gift.global.authentication.annotation.MemberIdResolver;
 import gift.global.authentication.interceptor.TokenCheckInterceptor;
+import java.util.List;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     private final TokenCheckInterceptor tokenCheckInterceptor;
+    private final MemberIdResolver memberIdResolver;
 
-    public WebConfig(TokenCheckInterceptor tokenCheckInterceptor) {
+    public WebConfig(TokenCheckInterceptor tokenCheckInterceptor, MemberIdResolver memberIdResolver) {
         this.tokenCheckInterceptor = tokenCheckInterceptor;
+        this.memberIdResolver = memberIdResolver;
     }
 
     @Override
@@ -22,5 +27,10 @@ public class WebConfig implements WebMvcConfigurer {
                 "/api/members/login",
                 "/api/members/reissue"
             );
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(memberIdResolver);
     }
 }
