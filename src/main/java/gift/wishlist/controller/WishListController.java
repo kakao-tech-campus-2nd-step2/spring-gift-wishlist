3ba5269bd0e698843d16cfd0228004a781762852
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-07-07 00:44:02 +0900

    feat: wishlist controller 생성

---
+++ src/main/java/gift/wishlist/controller/WishListController.java
@@ -0,0 +1,18 @@
+package gift.wishlist.controller;
+
+import gift.user.model.dto.User;
+import gift.user.resolver.LoginUser;
+import gift.wishlist.model.WishListResponse;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/wishes")
+public class WishListController {
+    @GetMapping("")
+    public List<WishListResponse> getWishListForUser(@LoginUser User loginUser) {
+        return wishListRepository.getWishList(loginUser.id());
+    }
+}
