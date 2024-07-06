de7a7361f5d9ba5ce196187f46bd7eaa4e79e7b2
Giheon Do <dgh0001@naver.com>
2024-07-06 15:18:48 +0900

    test: add JwtFilterTest

--- src/test/java/gift/filter/JwtFilterTest.java
+++ src/test/java/gift/filter/JwtFilterTest.java
@@ -1,2 +1,49 @@
-package gift.filter;public class JwtFilterTest {
+package gift.filter;
+
+import gift.util.Vars;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class JwtFilterTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testRequestWithValidToken() throws Exception {
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRlc3RAbmF2ZXIuY29tIn0.KImiqe-VpxLzKmgLyrUBnI2m2hikArHpV3NAYvdu86Y";
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/wishlist")
+                        .header("Authorization", Vars.TokenPrefix + token))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testRequestWithInvalidToken() throws Exception {
+        String token = "abcdefg.hijklmnopqr.stuvwxyz";
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/wishlist")
+                        .header("Authorization", Vars.TokenPrefix + token))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void testRequestWithEmptyToken() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/wishlist"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void testRequestToPermittedUrlWithoutPermisson() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/products"))
+                .andExpect(status().isOk());
+    }
 }
+
+
