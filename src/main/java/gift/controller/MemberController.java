adc5f6e9866355133aef5c8c603305b239293f2a
HaegyeongKim01 <haegyeong.kim01@gmail.com>
2024-07-06 15:38:44 +0900

    refactor: 변경@Controller to @RestController

--- src/main/java/gift/controller/MemberController.java
+++ src/main/java/gift/controller/MemberController.java
@@ -6,14 +6,14 @@ import gift.service.MemberService;
 import gift.vo.Member;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@RestController
 public class MemberController {
 
     private final MemberService service;
