75f3e4ef69e3c1618160eab3c5d61fad6c1f9a97
us4c0d3 <jwseok312@gmail.com>
2024-07-07 01:46:42 +0900

    fix(WishlistController): 잘못된 requestmapping 수정

--- src/main/java/gift/controller/WishlistController.java
+++ src/main/java/gift/controller/WishlistController.java
@@ -12,9 +12,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/wishes")
+@RestController
+@RequestMapping("/api/wishes")
 public class WishlistController {
     private final WishlistService wishlistService;
 
