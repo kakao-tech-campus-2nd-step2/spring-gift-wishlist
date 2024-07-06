49b6edf6f25dcdf6a7f4dd647fe5b5cc435cef9b
sim-mer <gyu2781@gmail.com>
2024-07-06 13:54:00 +0900

    feat(member.business) : 멤버 id로 위시리스트 가져오는 로직, dto 작성

--- src/main/java/gift/member/business/service/MemberService.java
+++ src/main/java/gift/member/business/service/MemberService.java
@@ -5,8 +5,15 @@ import gift.global.authentication.jwt.JwtToken;
 import gift.global.exception.ErrorCode;
 import gift.global.exception.LoginException;
 import gift.member.business.dto.MemberRegisterDto;
+import gift.member.business.dto.WishListDto;
+import gift.member.persistence.entity.Wishlist;
 import gift.member.persistence.repository.MemberRepository;
 import gift.member.business.dto.MemberLoginDto;
+import gift.member.persistence.repository.WishlistRepository;
+import gift.product.persistence.entity.Product;
+import gift.product.persistence.repository.ProductRepository;
+import java.util.List;
+import java.util.Map;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,10 +21,15 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final JwtGenerator jwtGenerator;
+    private final WishlistRepository wishlistRepository;
+    private final ProductRepository productRepository;
 
-    public MemberService(MemberRepository memberRepository, JwtGenerator jwtGenerator) {
+    public MemberService(MemberRepository memberRepository, JwtGenerator jwtGenerator,
+        WishlistRepository wishlistRepository, ProductRepository productRepository) {
         this.memberRepository = memberRepository;
         this.jwtGenerator = jwtGenerator;
+        this.wishlistRepository = wishlistRepository;
+        this.productRepository = productRepository;
     }
 
     public JwtToken registerMember(MemberRegisterDto memberRegisterDto) {
@@ -32,5 +44,23 @@ public class MemberService {
             throw new LoginException(ErrorCode.LOGIN_ERROR, "패스워드가 이메일과 일치하지 않습니다.");
         }
         return jwtGenerator.createToken(member.getId());
+    }
+
+    public List<WishListDto> getWishLists(Long memberId) {
+        var wishLists = wishlistRepository.getWishListByMemberId(memberId);
+
+        var productIds = wishLists.stream()
+            .map(Wishlist::getProductId)
+            .toList();
+
+        Map<Long, Product> products = productRepository.getProductsByIds(productIds);
+
+        return wishLists.stream()
+            .map(wishlist -> WishListDto.of(
+                wishlist.getId(),
+                products.get(wishlist.getProductId()),
+                wishlist.getCount()
+            ))
+            .toList();
     }
 }
