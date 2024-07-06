e18eb3e5f83c0b4100b318ce346b26b4efbec510
sim-mer <gyu2781@gmail.com>
2024-07-06 13:59:36 +0900

    feat(jwt) : 액세스 토큰 재발급 메서드 작성

--- src/main/java/gift/global/authentication/jwt/JwtGenerator.java
+++ src/main/java/gift/global/authentication/jwt/JwtGenerator.java
@@ -19,11 +19,18 @@ public class JwtGenerator {
 
     public JwtToken createToken(Long id) {
         long current = System.currentTimeMillis();
-        var accessExpireTime = new java.util.Date(current + ACCESS_TOKEN_EXPIRE_TIME);
-        var refreshExpireTime = new java.util.Date(current + REFRESH_TOKEN_EXPIRE_TIME);
-        var accessToken = generateToken(id, accessExpireTime, java.util.Map.of("type", TokenType.ACCESS));
-        var refreshToken = generateToken(id, refreshExpireTime, java.util.Map.of("type", TokenType.REFRESH));
+        var accessExpireTime = new Date(current + ACCESS_TOKEN_EXPIRE_TIME);
+        var refreshExpireTime = new Date(current + REFRESH_TOKEN_EXPIRE_TIME);
+        var accessToken = generateToken(id, accessExpireTime, Map.of("type", TokenType.ACCESS));
+        var refreshToken = generateToken(id, refreshExpireTime, Map.of("type", TokenType.REFRESH));
         return new JwtToken(accessToken, refreshToken);
+    }
+
+    public String reissueAccessToken(String refreshToken) {
+        Long id = jwtValidator.validate(refreshToken, TokenType.REFRESH);
+        long current = System.currentTimeMillis();
+        var accessExpireTime = new Date(current + ACCESS_TOKEN_EXPIRE_TIME);
+        return generateToken(id, accessExpireTime, java.util.Map.of("type", TokenType.ACCESS));
     }
 
     private String generateToken(Long id, Date expireTime, Map<String, Object> claims) {
