eaf9431793bf779a8a1744699f64a867668ca9d4
nove1080 <skwpqjq@gmail.com>
2024-07-07 02:09:31 +0900

    feat: ApiExceptionHandler NoSuchElementException 핸들러 추가

--- src/main/java/gift/web/validation/handler/ApiExceptionHandler.java
+++ src/main/java/gift/web/validation/handler/ApiExceptionHandler.java
@@ -2,6 +2,7 @@ package gift.web.validation.handler;
 
 import gift.web.dto.response.ErrorResponse;
 import gift.web.validation.exception.InvalidCredentialsException;
+import java.util.NoSuchElementException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -20,10 +21,19 @@ public class ApiExceptionHandler {
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ErrorResponse> handleNoSuchElementException(NoSuchElementException e) {
+        ErrorResponse errorResponse = new ErrorResponse("not_found", "resource", e.getMessage());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
     @ExceptionHandler(InvalidCredentialsException.class)
     public ResponseEntity<ErrorResponse> handleInvalidCredentialsException(InvalidCredentialsException e) {
         ErrorResponse errorResponse = new ErrorResponse("unauthorized", "memberInfo", e.getMessage());
 
         return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
     }
+
+    //todo 예상치 못한 에러(HttpStatusCode 500) 핸들링
 }
