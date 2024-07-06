5b9f77031b72b32a4cb027f55c00809b377c34ce
Profile-exe <kang67572346@gmail.com>
2024-07-06 16:49:33 +0900

    feat(JwtInterceptor): 인가를 위한 인터셉터 추가
    `WebConfig`에서 제외한 엔드포인트가 아니라면 토큰이 없을 때 접근 불가

---
+++ src/main/java/gift/config/JwtInterceptor.java
@@ -0,0 +1,40 @@
+package gift.config;
+
+import static gift.utils.JwtTokenProvider.BEARER_PREFIX_LENGTH;
+
+import gift.utils.JwtTokenProvider;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class JwtInterceptor implements HandlerInterceptor {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtInterceptor(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (token == null || token.length() <= BEARER_PREFIX_LENGTH) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return false;
+        }
+
+        token = token.substring(BEARER_PREFIX_LENGTH);
+
+        if (!jwtTokenProvider.validateToken(token)) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return false;
+        }
+
+        return true;
+    }
+}
