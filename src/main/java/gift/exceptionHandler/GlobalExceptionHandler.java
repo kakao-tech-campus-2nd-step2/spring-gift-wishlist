ed2a37a7e1e0be03347285b9df3532fbdf4439be
geist9110 <geist9110@gmail.com>
2024-07-07 05:15:29 +0900

    refactor(wishlist): Rename member exception handler to global exception handler

--- src/main/java/gift/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/gift/exceptionHandler/GlobalExceptionHandler.java
@@ -1,24 +1,23 @@
 package gift.exceptionHandler;
 
 import gift.exception.FailedLoginException;
-import gift.member.MemberController;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice(assignableTypes = MemberController.class)
-public class MemberExceptionHandler {
+@RestControllerAdvice
+public class GlobalExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
-    public String handleIllegalArgumentException(IllegalArgumentException ex) {
-        return ex.getMessage();
+    public String handleIllegalArgumentException(IllegalArgumentException exception) {
+        return exception.getMessage();
     }
 
     @ResponseStatus(HttpStatus.FORBIDDEN)
     @ExceptionHandler(FailedLoginException.class)
-    public String handleFailedLoginException(FailedLoginException ex) {
-        return ex.getMessage();
+    public String handleFailedLoginException(FailedLoginException exception) {
+        return exception.getMessage();
     }
 }
