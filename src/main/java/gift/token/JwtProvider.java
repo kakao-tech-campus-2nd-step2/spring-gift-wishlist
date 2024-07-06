2b1be55a7d25349534a8245d0be100bc1f53216f
geist9110 <geist9110@gmail.com>
2024-07-07 05:14:19 +0900

    feat(wishlist): Add Jwt validation login in getMemberFromToken method

--- src/main/java/gift/token/JwtProvider.java
+++ src/main/java/gift/token/JwtProvider.java
@@ -1,6 +1,7 @@
 package gift.token;
 
 import gift.member.Member;
+import gift.member.MemberRepository;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
@@ -10,6 +11,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class JwtProvider {
+
+    private final MemberRepository memberRepository;
+
+    public JwtProvider(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     private final String SECRET_KEY = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
     private final Key KEY = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
@@ -27,11 +34,17 @@ public class JwtProvider {
         Claims claims = Jwts.parser()
             .verifyWith((SecretKey) KEY)
             .build()
-            .parseSignedClaims(token)
+            .parseSignedClaims(token.replace(PREFIX, ""))
             .getPayload();
-        return new Member(
+        Member member = new Member(
             claims.get("email", String.class),
             claims.get("password", String.class)
         );
+
+        if (!memberRepository.existMemberByEmail(member.email())) {
+            throw new IllegalArgumentException("Member does not exist");
+        }
+        member.isAuthentication(memberRepository.findMemberByEmail(member.email()));
+        return member;
     }
 }
