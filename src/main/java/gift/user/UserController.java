fa00fc03de2e36df1aefacc139c38b7f29e9edf9
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-07-07 02:21:17 +0900

    fix : UserController UserService UserRepository 수정으로 인한 변경사항 수정

--- src/main/java/gift/user/UserController.java
+++ src/main/java/gift/user/UserController.java
@@ -28,9 +28,8 @@ public class UserController {
         if(!userService.validateUser(loginDTO)){
             throw new IllegalArgumentException("이메일이 존재하지 않거나 비밀번호가 일치하지 않습니다.");
         }
-
-        UserDTO userDTO = userService.getUserDTOByLoginDTO(loginDTO);
-        String accessToken = jwtService.generateAccessToken(userDTO);
+        User user = userService.getUserByEmail(loginDTO.getEmail());
+        String accessToken = jwtService.generateAccessToken(user);
 
         return ResponseEntity.ok(new Token(accessToken));
     }
@@ -42,8 +41,8 @@ public class UserController {
             throw new IllegalArgumentException("이미 등록된 이메일입니다.");
         }
 
-        userService.registerUser(userDTO);
-        String accessToken = jwtService.generateAccessToken(userDTO);
+        User user = userService.registerUser(userDTO);
+        String accessToken = jwtService.generateAccessToken(user);
         return ResponseEntity.ok(new Token(accessToken));
     }
 
