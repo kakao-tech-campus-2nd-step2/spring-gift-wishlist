35914afe77d3f4100629441004b7d5ba484a0984
minju26 <mjalswn26@gmail.com>
2024-07-06 22:12:30 +0900

    feat: Member default constructor 추가

--- src/main/java/gift/domain/Member.java
+++ src/main/java/gift/domain/Member.java
@@ -4,6 +4,9 @@ public class Member {
     private String email;
     private String password;
 
+    public Member() {
+    }
+
     public Member(String email, String password) {
         this.email = email;
         this.password = password;
