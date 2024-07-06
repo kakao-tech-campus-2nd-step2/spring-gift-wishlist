7a29f7a0d6170b1549ff7091a74cc04dfcfb972c
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-07-07 00:32:58 +0900

    feat: wish 테이블 초기 데이터 삽입 스크립트 추가

--- src/main/resources/script/data.sql
+++ src/main/resources/script/data.sql
@@ -7,3 +7,14 @@ INSERT INTO AppUser (email, password, role)
 VALUES ('yoo@example.com', '1234', 'ADMIN'),
        ('kyeong@example.com', '1234', 'USER'),
        ('miiii@example.com', '1234', 'USER');
+
+INSERT INTO Wish (userId, productId, quantity)
+VALUES (1, 1, 2);
+INSERT INTO Wish (userId, productId, quantity)
+VALUES (1, 2, 1);
+INSERT INTO Wish (userId, productId, quantity, status)
+VALUES (2, 3, 3, FALSE);
+INSERT INTO Wish (userId, productId, quantity)
+VALUES (2, 1, 1);
+INSERT INTO Wish (userId, productId, quantity)
+VALUES (3, 1, 1);
