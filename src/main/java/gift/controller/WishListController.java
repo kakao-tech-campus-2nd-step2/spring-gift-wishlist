1c98b7a29de7768930a0c601f338990c09758f25
UiChan Lee <junglkls1234@gmail.com>
2024-07-07 01:53:17 +0900

    Feat: wishController 구현

--- src/main/java/gift/controller/WishListController.java
+++ src/main/java/gift/controller/WishListController.java
@@ -1,4 +1,46 @@
 package gift.controller;
 
+import gift.dto.WishDto;
+import gift.model.member.Member;
+import gift.model.wish.Wish;
+import gift.service.WishListService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/wishlist")
 public class WishListController {
+
+    private final WishListService wishListService;
+
+    public WishListController(WishListService wishListService) {
+        this.wishListService = wishListService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Wish>> getAllWishes() {
+        List<Wish> wishes = wishListService.getAllWishes();
+        return ResponseEntity.ok(wishes);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insertWish(@RequestBody WishDto wishDto) {
+        wishListService.insertWish(wishDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> removeWish(@PathVariable Long id) {
+        wishListService.deleteWish(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> updateWish(@RequestBody WishDto wishDto) {
+        wishListService.updateWish(wishDto);
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/gift/service/WishListService.java
+++ src/main/java/gift/service/WishListService.java
@@ -1,5 +1,35 @@
 package gift.service;
 
-public class WishListService {
+import gift.dao.WishDao;
+import gift.dto.WishDto;
+import gift.model.wish.Wish;
 
+import java.util.List;
+
+public class WishListService {
+    private final WishDao wishDao;
+    private final ProductService productService;
+
+    public WishListService(WishDao wishDao, ProductService productService){
+        this.wishDao = wishDao;
+        this.productService = productService;
+    }
+
+    public List<Wish> getAllWishes() {
+        return wishDao.getAllWishes();
+    }
+
+    public void insertWish(WishDto wishDto) {
+        Wish wish = new Wish(wishDto.getProductId(),wishDto.getMemberEmail(),wishDto.getProductName(), wishDto.getAmount());
+        wishDao.insertWish(wish);
+    }
+
+    public void deleteWish(Long productId) {
+        wishDao.deleteWish(productId);
+    }
+
+    public void updateWish(WishDto wishDto){
+        Wish wish = new Wish(wishDto.getProductId(),wishDto.getMemberEmail(),wishDto.getProductName(), wishDto.getAmount());
+        wishDao.updateWish(wish);
+    }
 }
