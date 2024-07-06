bc1e06ede873714b9adb1c902a783b0625f0f5ed
geist9110 <geist9110@gmail.com>
2024-07-07 05:01:25 +0900

    feat(wishlist): Add memberResolver

---
+++ src/main/java/gift/user/HandlerMemberArgumentResolver.java
@@ -0,0 +1,38 @@
+package gift.user;
+
+
+import com.github.dockerjava.api.exception.UnauthorizedException;
+import gift.token.JwtProvider;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class HandlerMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final JwtProvider jwtProvider;
+
+    public HandlerMemberArgumentResolver(JwtProvider jwtProvider) {
+        this.jwtProvider = jwtProvider;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterType().equals(Member.class);
+    }
+
+    @Override
+    public Member resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+        NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+        String token = request.getHeader("Authorization");
+        if (token == null) {
+            throw new UnauthorizedException("Unauthorized");
+        }
+        return jwtProvider.getMemberFromToken(token);
+    }
+}
