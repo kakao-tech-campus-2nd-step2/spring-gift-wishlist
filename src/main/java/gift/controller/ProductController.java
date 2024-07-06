3711cdcdeada07ff2825269714ec8ab0cb667e85
pinkbeenyeti <rbgusdn12@gmail.com>
2024-07-07 03:32:07 +0900

    fix: 컨트롤러와 서비스 계층 분리 서비스 계층을 생성하였다. 비즈니스 로직을 처리한다.

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -1,97 +1,58 @@
 package gift.controller;
 
-import gift.exceptions.KakaoContainException;
+import gift.exceptions.InvalidNameException;
+import gift.service.ProductService;
 import jakarta.validation.Valid;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import gift.model.Product;
 import gift.dto.*;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 
 @Controller
 @Validated
-@RequestMapping("/")
+@RequestMapping("/v3/products")
 public class ProductController {
-    private JdbcTemplate jdbcTemplate;
-    private final SimpleJdbcInsert simpleJdbcInsert;
 
-    public ProductController(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-        this.simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
-                .withTableName("products")
-                .usingGeneratedKeyColumns("id");
-    }
+    @Autowired
+    private ProductService productService;
 
-    @GetMapping("v3/products")
+    @GetMapping
     public String getAllProducts(Model model) {
-        List<Product> products = getProducts();
+        ProductsDTO products = productService.getAllProducts();
         model.addAttribute("products", products);
         return "manage";
     }
 
-    @PostMapping("v3/products")
+    @PostMapping
     public String addProduct(@Valid  @RequestBody ProductDTO productDTO) {
         validateProductName(productDTO.name());
-
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put("name", productDTO.name());
-        parameters.put("price", productDTO.price());
-        parameters.put("imageurl", productDTO.imageUrl());
-
-        simpleJdbcInsert.execute(parameters);
+        productService.createProduct(productDTO);
 
         return "redirect:/v3/products";
     }
 
-    @PostMapping("/v3/products/{id}")
+    @PostMapping("/{id}")
     public String modifyProduct(@PathVariable Long id, @Valid @RequestBody ProductDTO productDTO) {
         validateProductName(productDTO.name());
-
-        updateProduct(id, productDTO);
+        productService.updateProduct(id, productDTO);
 
         return "redirect:/v3/products";
     }
 
-    @DeleteMapping("v3/products/{id}")
+    @DeleteMapping("/{id}")
     public String DeleteProduct(@PathVariable("id") Long id) {
-        deleteProductById(id);
+        productService.deleteProduct(id);
 
         return "redirect:/v3/products";
-    }
-
-    private List<Product> getProducts() {
-        String sql = "SELECT id, name, price, imageurl FROM products";
-        return jdbcTemplate.query(
-                sql, (resultSet, rowNum) -> new Product(
-                        resultSet.getLong("id"),
-                        resultSet.getString("name"),
-                        resultSet.getInt("price"),
-                        resultSet.getString("imageurl")
-                ));
-    }
-
-    private void updateProduct(Long id, ProductDTO productDTO) {
-        String sql = "UPDATE products SET name = ?, price = ?, imageurl = ? WHERE id = ?";
-        jdbcTemplate.update(sql, productDTO.name(), productDTO.price(), productDTO.imageUrl(), id);
-    }
-
-    private void deleteProductById(Long id) {
-        String sql = "DELETE FROM products WHERE id = ?";
-        jdbcTemplate.update(sql, id);
     }
 
     private void validateProductName(String name) {
         if (name.contains("카카오")) {
-            throw new KakaoContainException("이름에 카카오는 포함할 수 없습니다. 수정해 주세요");
+            throw new InvalidNameException("이름에 카카오는 포함할 수 없습니다. 수정해 주세요");
         }
     }
 }
