e562f2d4799ed0dae4a144c5f6293e0d7f6cb987
Kangboom <jkl6147@naver.com>
2024-07-06 22:12:32 +0900

    refactor(domain): add exception handling in Member constructor

--- src/main/java/gift/domain/Product.java
+++ src/main/java/gift/domain/Product.java
@@ -8,12 +8,15 @@ public class Product {
     private static final Pattern ALLOWED_SPECIAL_CHARACTERS = Pattern.compile("[\\w\\s\\(\\)\\[\\]\\+\\-\\&\\/가-힣]*");
     private Long id;
     private String name;
-    private Integer price;
+    private int price;
     private String imageUrl;
 
     public Product(){
     }
-    public Product(Long id, String name, Integer price, String imageUrl){
+    public Product(Long id, String name, int price, String imageUrl){
+        findNameLengthException(name);
+        findAllowedCharactersException(name);
+        findSpecificCharacterException(name);
         this.id = id;
         this.name = name;
         this.price = price;
@@ -31,7 +34,7 @@ public class Product {
 
     public void setName(String name) {this.name = name;}
 
-    public Integer getPrice() { return this.price;}
+    public int getPrice() { return this.price;}
 
     public void setPrice(Integer price) {
         this.price = price;
@@ -45,25 +48,19 @@ public class Product {
         this.imageUrl = imageUrl;
     }
 
-    public void validate() throws NameException {
-        findNameLengthException();
-        findAllowedCharactersException();
-        findSpecificCharacterException();
-    }
-
-    private void findNameLengthException() throws NameException{
-        if (this.name.length() > 15){
+    private void findNameLengthException(String name){
+        if (name.length() > 15){
             throw new NameException("최대 15자리까지 입력하실 수 있습니다.");
         }
     }
-    private void findAllowedCharactersException() throws NameException{
-        if (!ALLOWED_SPECIAL_CHARACTERS.matcher(this.name).matches()) {
+    private void findAllowedCharactersException(String name){
+        if (!ALLOWED_SPECIAL_CHARACTERS.matcher(name).matches()) {
             throw new NameException("특수 문자는 '(), [], +, -, &, /, _ '만 사용가능 합니다.");
         }
     }
 
-    private void findSpecificCharacterException() throws NameException{
-        if (this.name.contains("카카오")){
+    private void findSpecificCharacterException(String name){
+        if (name.contains("카카오")){
             throw new NameException("담당 MD와 협의해 주세요.");
         }
     }
