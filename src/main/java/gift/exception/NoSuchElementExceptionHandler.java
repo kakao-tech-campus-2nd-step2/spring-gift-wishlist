baff65c13789ce61292e35d692c31ba759a0b8ed
alsswl <alswl4223@naver.com>
2024-07-07 03:09:18 +0900

    refactor : @RestControllerAdvice로 변경

--- src/main/java/gift/exception/NoSuchElementExceptionHandler.java
+++ src/main/java/gift/exception/NoSuchElementExceptionHandler.java
@@ -3,14 +3,14 @@ package gift.exception;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
 
-@ControllerAdvice
+@RestControllerAdvice
 public class NoSuchElementExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationExceptions(
