ae8671ce2f82e6db329b31072a63f15dee135ce4
HaegyeongKim01 <haegyeong.kim01@gmail.com>
2024-07-06 15:40:02 +0900

    refactor: Exception handler 수정

--- src/main/java/gift/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -12,30 +12,52 @@ import java.util.Objects;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    // 예외 메시지를 이용해 ResponseEntity를 생성하는 메소드
+    /**
+     * 예외 메시지를 이용해 ResponseEntity 생성
+     * @param errorMessage String
+     * @param status HttpSatus
+     * @return ResponseEntity(errorMessage, status)
+     */
     protected ResponseEntity<String> buildResponseEntity(String errorMessage, HttpStatus status) {
         return new ResponseEntity<>(errorMessage, status);
     }
 
-    // 일반적인 예외를 처리하는 핸들러
+    /**
+     * 일반적 예외 처리
+     * @param ex Exception
+     * @return ResponseEntity
+     */
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGenericException(Exception ex) {
         return buildResponseEntity("서버 오류 발생.", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    // RuntimeException을 처리하는 핸들러
+    /**
+     * RuntimeException 처리하는 핸들러
+     * @param ex RuntimeException
+     * @return ResponseEntity
+     */
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
         return buildResponseEntity("인증 실패: " + ex.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 
-    // Validation 예외를 처리하는 핸들러
+    /**
+     * Validation 예외 처리하는 핸들러
+     * @param ex ConstraintViolationException
+     * @return ResponseEntity
+     */
     @ExceptionHandler({ConstraintViolationException.class})
     public ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException ex) {
         String errorMessage = ex.getConstraintViolations().iterator().next().getMessage();
         return buildResponseEntity(errorMessage, HttpStatus.BAD_REQUEST);
     }
 
+    /**
+     * Validation 예외 처리하는 핸들러 - service
+     * @param ex MethodArgumentNotValidException
+     * @return
+     */
     @ExceptionHandler({MethodArgumentNotValidException.class})
     public ResponseEntity<String> handleMethodArgumentValidException(MethodArgumentNotValidException ex) {
         String errorMessage = Objects.requireNonNull(ex.getBindingResult().getFieldError()).getDefaultMessage();
