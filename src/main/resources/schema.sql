d641bbe1af486eb796902ad3dabace95642ce1ce
sim-mer <gyu2781@gmail.com>
2024-07-06 13:49:39 +0900

    feat(schema.sql) : 위시리스트 테이블 작성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,3 +11,12 @@ CREATE TABLE member (
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE wishlist (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    member_id INT NOT NULL,
+    product_id INT NOT NULL,
+    count INT NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member(id),
+    FOREIGN KEY (product_id) REFERENCES product(id)
+);
