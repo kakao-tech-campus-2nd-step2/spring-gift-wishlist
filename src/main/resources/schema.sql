94ffd24bc4a582be0356e46a54c9f46c75fb3ca4
geist9110 <geist9110@gmail.com>
2024-07-07 04:57:43 +0900

    feat(wishlist): Add withlist table in schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,3 +11,12 @@ CREATE TABLE member
     email    VARCHAR(255) PRIMARY KEY,
     password VARCHAR(255)
 );
+
+CREATE Table wishlist
+(
+    product_id   INT,
+    member_email VARCHAR(255),
+    PRIMARY KEY (product_id, member_email),
+    FOREIGN KEY (product_id) REFERENCES product (id),
+    FOREIGN KEY (member_email) REFERENCES member (email)
+)
