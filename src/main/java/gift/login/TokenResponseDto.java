0a9f5155ee04c20103b3bdf9dd7ed777c5b42493
SeoYeongU <westzeroright@gmail.com>
2024-07-06 21:31:11 +0900

    chore: 패키지 구조 수정

--- src/main/java/gift/login/TokenResponseDto.java
+++ src/main/java/gift/login/TokenResponseDto.java
@@ -1,5 +1,23 @@
 package gift.login;
 
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 public record TokenResponseDto(String token) {
 
+    @Configuration
+    public static class WebConfig implements WebMvcConfigurer {
+        private final LoginMember.LoginMemberArgumentResolver loginMemberArgumentResolver;
+
+        public WebConfig(LoginMember.LoginMemberArgumentResolver loginMemberArgumentResolver) {
+            this.loginMemberArgumentResolver = loginMemberArgumentResolver;
+        }
+
+        @Override
+        public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+            argumentResolvers.add(loginMemberArgumentResolver);
+        }
+    }
 }
