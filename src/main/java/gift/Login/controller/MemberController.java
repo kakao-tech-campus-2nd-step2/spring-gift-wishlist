20c6bc4f20c4460a3721dd646e8b322086741411
pykido <animoto1@naver.com>
2024-07-06 20:01:50 +0900

    refactor : 전체적인 리팩토링

--- src/main/java/gift/Login/controller/MemberController.java
+++ src/main/java/gift/Login/controller/MemberController.java
@@ -45,9 +45,4 @@ public class MemberController {
             return ResponseEntity.status(401).build();
         }
     }
-=======
-
-        return ResponseEntity.ok(responseToken);
-    }
->>>>>>> origin/step2
 }
