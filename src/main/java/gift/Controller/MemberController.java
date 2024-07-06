1676e05469eaf50d48b2a21f6db6c4c6b290c125
HyeJiJUN <jhj910421@gmail.com>
2024-07-07 02:32:31 +0900

    feat: get user id by authorization token

--- src/main/java/gift/Controller/MemberController.java
+++ src/main/java/gift/Controller/MemberController.java
@@ -2,9 +2,13 @@ package gift.Controller;
 
 import gift.model.Member;
 import gift.model.MemberDao;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.naming.AuthenticationException;
 import java.util.Objects;
 
 @RestController
@@ -28,6 +32,7 @@ public class MemberController {
         return token;
     }
 
+
     @PostMapping("/login")
     public String login(@RequestBody Member member){
         if (MemberDao.selectMember(member.getEmail()) == null) {
@@ -45,9 +50,44 @@ public class MemberController {
         }
     }
 
+    static String secretKey = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
+
+    // 토큰으로 멤버 id 가져옴
+    @GetMapping("/getMemberId")
+    public Long getIdByToken(HttpServletRequest request) throws AuthenticationException {
+        Long id = 0L;
+        // 요청 헤더에서 Authorization 헤더 값을 가져옴
+        String authHeader = request.getHeader("Authorization");
+        System.out.println(authHeader);
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            // Authorization 헤더에서 Bearer 토큰을 추출
+            String token = authHeader.substring(7);
+            System.out.println(token);
+            // 토큰을 파싱하여 클레임(Claims)을 추출
+            Claims claims = Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            id = claims.get("id", Long.class);
+            System.out.println(id);
+        }
+        else{
+            throw new AuthenticationException("헤더 혹은 토큰이 유효하지 않습니다.");
+        }
+        System.out.println(id);
+        return id;
+    }
+
     @ResponseStatus(value = HttpStatus.FORBIDDEN)
     @ExceptionHandler(IllegalArgumentException.class)
     public String handleIllegalArgumentException(IllegalArgumentException e) {
+        return e.getMessage();
+    }
+
+    @ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(AuthenticationException.class)
+    public String handleAuthenticationException(AuthenticationException e) {
         return e.getMessage();
     }
 }
