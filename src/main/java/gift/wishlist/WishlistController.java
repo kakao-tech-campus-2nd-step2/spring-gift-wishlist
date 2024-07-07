9e27436ebed323b557a79daf523dd206b2848ca8
SeoYeongU <westzeroright@gmail.com>
2024-07-07 14:44:56 +0900

    style: 표현 통일

--- src/main/java/gift/wishlist/WishlistController.java
+++ src/main/java/gift/wishlist/WishlistController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/wishes")
+@RequestMapping("/api/wishlist")
 public class WishlistController {
 
     private final WishlistDao wishlistDao;
