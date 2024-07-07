2c262b681f29eca97cf07e2d0044b6da7b52b2bf
yoong-saks <wjdghtjd06@gmail.com>
2024-07-08 01:39:58 +0900

    feat: members 테이블 생성

--- src/main/resources/initDB/schema.sql
+++ src/main/resources/initDB/schema.sql
@@ -5,3 +5,11 @@ create table products (
     image_url varchar(255),
     primary key (id)
 );
+
+creat table members (
+      id bigint auto_increment,
+      email varchar(255),
+      password varchar(255),
+      role varchar(255) default "user",
+      primary key (id)
+)
