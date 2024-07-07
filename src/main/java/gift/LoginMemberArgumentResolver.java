67113c26b4334cc2e1ea61ca0e2e67a462d0084e
ariimo <159447466+ariimo@users.noreply.github.com>
2024-07-07 13:48:02 +0900

    feat: LoginMemberArgumentResolver 생성

---
+++ src/main/java/gift/LoginMemberArgumentResolver.java
@@ -0,0 +1,33 @@
+package gift;
+
+import gift.services.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+    private MemberService memberService;
+
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean hasLoginUserAnnotation = parameter.hasParameterAnnotation(LoginMember.class);
+        boolean isUserType = MemberDto.class.isAssignableFrom(parameter.getParameterType());
+        return hasLoginUserAnnotation && isUserType;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        String token = request.getHeader("Authorization");
+        if (token == null) {
+            return null;
+        }
+        return memberService.getLoginUser(token);
+    }
+}
