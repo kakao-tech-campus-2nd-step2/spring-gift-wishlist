5193b635a89df19f889d6f077ed838797e9bd892
Awhn <ahnshwan@outlook.com>
2024-07-07 21:30:39 +0900

    feat : member 테이블에 접근하는 DAO

---
+++ src/main/java/member/MemberDAO.java
@@ -0,0 +1,43 @@
+package member;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class MemberDAO {
+    private final JdbcTemplate jdbcTemplate;
+
+    public MemberDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void create() {
+        var sql = """
+                create table member (
+                    email varchar(255),
+                    password varchar(255),
+                    token varchar(255)
+                )
+                """;
+        jdbcTemplate.execute(sql);
+    }
+
+
+    public String insert(Member member, String token) {
+        var sql = "INSERT INTO member (email, password, token) VALUES (?, ?, ?)";
+        jdbcTemplate.update(sql, member.email(), member.password(), token);
+        return token;
+    }
+
+    public String selectTokenbyMember(Member member){
+        var sql = "SELECT token FROM member WHERE email = ? AND password = ?";
+        return jdbcTemplate.queryForObject(sql, String.class, member.email(), member.password());
+    }
+
+    public List selectTokenbyToken(String token){
+        var sql = "SELECT token FROM member WHERE token = ?";
+        return jdbcTemplate.queryForList(sql, token);
+    }
+}
