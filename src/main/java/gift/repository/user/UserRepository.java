01453f8a02c55764dcd0bff1e696eabc2a58c0b0
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-07 13:53:52 +0900

    refactor : save 메서드의 update 파라미터 추가

--- src/main/java/gift/repository/user/UserRepository.java
+++ src/main/java/gift/repository/user/UserRepository.java
@@ -2,6 +2,7 @@ package gift.repository.user;
 
 import gift.domain.Product;
 import gift.domain.User;
+import gift.utils.JwtUtil;
 import java.util.List;
 import org.springframework.boot.autoconfigure.batch.BatchProperties.Jdbc;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -11,8 +12,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class UserRepository {
     private final JdbcTemplate jdbcTemplate;
-    public UserRepository(JdbcTemplate jdbcTemplate) {
+    private final JwtUtil jwtUtil;
+
+    public UserRepository(JdbcTemplate jdbcTemplate, JwtUtil jwtUtil) {
         this.jdbcTemplate = jdbcTemplate;
+        this.jwtUtil = jwtUtil;
     }
 
     public User findByEmail(String email) {
@@ -21,13 +25,18 @@ public class UserRepository {
         return users.isEmpty() ? null : users.get(0);
     }
 
+    public String registerUser(String name, String email, String password) {
+        User user = new User(name, email, password, "ROLE_USER");
+        save(user);
+        return jwtUtil.generateToken(email); // 회원 가입 후 토큰 생성하여 반환
+    }
+
     public void save(User user) {
         var sql = "insert into users (name, email, password, role) values (?, ?, ?, ?)";
-        jdbcTemplate.update(sql, user.getEmail(), user.getPassword());
+        jdbcTemplate.update(sql, user.getName(),user.getEmail(), user.getPassword(), user.getRole());
     }
 
     private final RowMapper<User> userRowMapper = (rs, rowNum) -> new User(
-        rs.getLong("id"),
         rs.getString("name"),
         rs.getString("email"),
         rs.getString("password"),
