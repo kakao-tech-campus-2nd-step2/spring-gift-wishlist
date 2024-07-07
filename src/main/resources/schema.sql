09de8fa0279d495a0337078fa30ccdb458667f52
peacefullyquietly <pjh73015@gmail.com>
2024-07-07 19:21:59 +0900

    feat : wishlist 테이블 생성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -10,5 +10,12 @@ CREATE TABLE members (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    token_issued TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE wishlist (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    userid BIGINT NOT NULL,
+    product_name VARCHAR(255) NOT NULL,
+    product_price INT NOT NULL,
+    FOREIGN KEY (userid) REFERENCES members(id)
 );
