b0ecc25bd6e5411028d6d3e71991b80da56da2da
SeoYeongU <westzeroright@gmail.com>
2024-07-07 14:20:43 +0900

    add: 삭제된 파일 복구

--- src/main/java/gift/login/LoginMember.java
+++ src/main/java/gift/login/LoginMember.java
@@ -16,33 +16,4 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 @Target(ElementType.PARAMETER)
 public @interface LoginMember {
 
-    @Component
-    class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
-
-        private final MemberDao memberDao;
-        private final JwtTokenUtil jwtTokenUtil;
-
-        public LoginMemberArgumentResolver(MemberDao memberDao, JwtTokenUtil jwtTokenUtil) {
-            this.memberDao = memberDao;
-            this.jwtTokenUtil = jwtTokenUtil;
-        }
-
-        @Override
-        public boolean supportsParameter(MethodParameter parameter) {
-            boolean hashLoginUserAnnotation = parameter.hasParameterAnnotation(LoginMember.class);
-            return hashLoginUserAnnotation;
-        }
-
-        @Override
-        public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
-            NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-
-            String token = webRequest.getHeader("Authorization").substring(7);
-            System.out.println("토큰"+token);
-            String userEmail = jwtTokenUtil.decodeJWT(token).getSubject();
-            System.out.println("아이디"+userEmail);
-
-            return memberDao.findMemberById(userEmail);
-        }
-    }
 }
