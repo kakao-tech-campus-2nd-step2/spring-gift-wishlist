bf53bb715c3df88509623b143f0fe0fe5e87827d
dkswoals <a4445656@gmail.com>
2024-07-07 18:17:13 +0900

    feat: 유저식별값 email -> userId

--- src/main/java/wishlist/interceptor/AuthInterceptor.java
+++ src/main/java/wishlist/interceptor/AuthInterceptor.java
@@ -3,6 +3,7 @@ package wishlist.interceptor;
 import io.jsonwebtoken.JwtException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import wishlist.exception.ErrorCode;
@@ -12,24 +13,26 @@ import wishlist.service.JwtProvider;
 public class AuthInterceptor implements HandlerInterceptor {
 
     private final JwtProvider jwtProvider;
+    private final String authType;
 
-    public AuthInterceptor(JwtProvider jwtProvider) {
+    public AuthInterceptor(JwtProvider jwtProvider,@Value("${jwt.authType}")String authType) {
         this.jwtProvider = jwtProvider;
+        this.authType = authType;
     }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
         Object handler) throws Exception {
         String authHeader = request.getHeader("Authorization");
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+        if (authHeader == null || !authHeader.startsWith(authType)) {
             throw new JwtException(ErrorCode.ACCESS_DENIED.getMessage());
         }
-        String token = authHeader.substring(7);
+        String token = authHeader.substring(authType.length());
         if (!jwtProvider.validateToken(token)) {
             return false;
         }
-        String email = jwtProvider.getEmailFromToken(token);
-        request.setAttribute("email", email);
+        Long userId = Long.parseLong(jwtProvider.getUserIdFromToken(token));
+        request.setAttribute("userId", userId);
         return true;
     }
 
