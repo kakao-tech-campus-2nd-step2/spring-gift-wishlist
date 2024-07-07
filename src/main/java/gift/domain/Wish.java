c4141cfe73b76b4238bf63c2679a5f840b848606
박민규 <pmq0101@naver.com>
2024-07-07 14:57:42 +0900

    feat(Wish, WishRqeustDTO): wish 추가

---
+++ src/main/java/gift/domain/Wish.java
@@ -0,0 +1,8 @@
+package gift.domain;
+
+public class Wish {
+    private Long id;
+    private Long userId;
+    private Long productId;
+    private int count;
+}
---
+++ src/main/java/gift/dto/requestDTO/WishRequestDTO.java
@@ -0,0 +1,8 @@
+package gift.dto.requestDTO;
+
+public class WishRequestDTO {
+    private Long id;
+    private Long userId;
+    private Long productId;
+    private int count;
+}
