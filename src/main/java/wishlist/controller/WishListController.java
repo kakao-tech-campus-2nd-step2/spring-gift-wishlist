52f5ce83c8101bcb6fb437b58855734e8c7388f2
dkswoals <a4445656@gmail.com>
2024-07-07 18:18:55 +0900

    feat: addToWishList api 수정
    path: "/wishes/add{id}" -> "/wishes/{id}"
    method: get -> post

--- src/main/java/wishlist/controller/WishListController.java
+++ src/main/java/wishlist/controller/WishListController.java
@@ -4,33 +4,31 @@ import java.util.List;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RestController;
 import wishlist.model.item.ItemDTO;
-import wishlist.service.UserService;
 import wishlist.service.WishListService;
 
 @RestController
 public class WishListController {
     private final WishListService wishListService;
-    private final UserService userService;
-    public WishListController(WishListService wishListService,UserService userService) {
+    public WishListController(WishListService wishListService) {
         this.wishListService = wishListService;
-        this.userService = userService;
     }
 
     @GetMapping("/wishes")
-    public List<ItemDTO> getWishList(@RequestAttribute("email") String email) {
-        return wishListService.getList(userService.findByEmail(email).getId());
+    public List<ItemDTO> getWishList(@RequestAttribute("userId") Long userId) {
+        return wishListService.getList(userId);
     }
 
-    @GetMapping("/wishes/add/{id}")
-    public void addToWishList(@PathVariable("id") Long itemId ,@RequestAttribute("email") String email) {
-        wishListService.addToWishList(userService.findByEmail(email).getId(), itemId);
+    @PostMapping("/wishes/{id}")
+    public void addToWishList(@PathVariable("id") Long itemId ,@RequestAttribute("userId") Long userId) {
+        wishListService.addToWishList(userId,itemId);
     }
 
     @DeleteMapping("/wishes/{id}")
-    public void deleteFromWishList(@PathVariable("id") Long itemId, @RequestAttribute("email") String email) {
-        wishListService.deleteFromWishList(userService.findByEmail(email).getId(), itemId);
+    public void deleteFromWishList(@PathVariable("id") Long itemId, @RequestAttribute("userId") Long userId) {
+        wishListService.deleteFromWishList(userId,itemId);
     }
 }
