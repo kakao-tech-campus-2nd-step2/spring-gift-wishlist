6966dfb4134db8c6d70dbb3c7bcfd97b19760132
Shsin9797 <aaa8141@naver.com>
2024-07-07 22:19:55 +0900

    fix: 위시리스트에 상품 추가 기능 수정 - 어노테이션 추가

--- src/main/java/gift/controller/WishController.java
+++ src/main/java/gift/controller/WishController.java
@@ -4,11 +4,14 @@ import gift.dto.WishDto;
 import gift.service.WishService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.net.CacheRequest;
 import java.util.Map;
 
+@RequestMapping("/wish")
 @Controller
 public class WishController {
     private final WishService wishService;
@@ -18,7 +21,11 @@ public class WishController {
     }
 
     //insert
+    @PostMapping()
     public ResponseEntity<Map<String,Object>> insert(@RequestBody WishDto.Request request) {
         return wishService.insert(request);
     }
+
+    //getAll
+
 }
