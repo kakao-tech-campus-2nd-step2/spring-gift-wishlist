a7fac69c520d83c289a0e95a700fc37e99ecb4b2
dong_yxxn <g0326kdy@gmail.com>
2024-07-07 18:09:33 +0900

    refactor(GlobalExceptionHandler): Exception 변경

--- src/main/java/gift/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -1,20 +1,21 @@
 package gift.exception;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import gift.model.Product;
+import jakarta.validation.ConstraintViolationException;
+
 import org.springframework.ui.Model;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(NotValidProductNameException.class)
-    public String handleNotValidProductNameException(NotValidProductNameException e, Model model) {
-        model.addAttribute("errorMessage", e.getMessage());
-        model.addAttribute("product", e.getProduct());
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public String handleConstraintViolationException(ConstraintViolationException ex, Model model) {
+        String errorMessage = ex.getMessage(); // 이 부분은 ConstraintViolationException의 메시지를 가져오는 것으로 변경
+        model.addAttribute("errorMessage", errorMessage);
+        model.addAttribute("product", new Product(0,"", 0, ""));
         return "add_product";
     }
+
 }
