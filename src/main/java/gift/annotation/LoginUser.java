ff6139e43045aaaa55cf698137a71059c0974623
pinkbeenyeti <rbgusdn12@gmail.com>
2024-07-08 00:41:37 +0900

    feat: jwt 토큰 사용 사용자 구분 기능 구현

--- src/main/java/gift/annotation/LoginUser.java
+++ src/main/java/gift/annotation/LoginUser.java
@@ -1,2 +1,11 @@
-package gift.annotation;public interface LoginUser {
+package gift.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginUser {
 }
