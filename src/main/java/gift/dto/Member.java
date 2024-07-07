2b8c4e4e2c0bb91df365da7c7d773c1203da771e
5win <woozzoo7@gmail.com>
2024-07-07 20:46:36 +0900

    test: Add member controller and DAO test codes - 이 과정에서 테스트를 위해 Member DTO의 디폴트 생성자를 추가함.

--- src/main/java/gift/dto/Member.java
+++ src/main/java/gift/dto/Member.java
@@ -2,8 +2,14 @@ package gift.dto;
 
 public class Member {
 
-    private final String email;
-    private final String password;
+    private String email;
+    private String password;
+
+    /**
+     * MockMvc 테스트를 위해 선언
+     */
+    public Member() {
+    }
 
     public Member(String email, String password) {
         this.email = email;
