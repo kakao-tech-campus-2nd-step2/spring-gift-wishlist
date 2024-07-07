4a946e86d5eb73f36193a8177b76ed8ea4cc29fa
yoong-saks <wjdghtjd06@gmail.com>
2024-07-08 06:57:40 +0900

    feat: [step3] wishController 생성 및 getApi 구현

---
+++ src/main/java/gift/web/WishController.java
@@ -0,0 +1,26 @@
+package gift.web;
+
+import gift.web.dto.MemberDto;
+import gift.web.dto.WishDto;
+import gift.web.jwt.AuthUser;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/wishes")
+public class WishController {
+    private final WishService wishService;
+
+    public WishController(WishService wishService) {
+        this.wishService = wishService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<WishDto>> getWishes(@AuthUser MemberDto member) {
+        return wishService.findAllWishes()
+    }
+
+}
