a09cf4e39a0e6ac31c6c2ae1f4aae5b6e63bcdf4
설빈 <138611481+seolbin96@users.noreply.github.com>
2024-07-08 02:48:25 +0900

    전남대 BE_조설빈 2주차 과제 (2단계) (#329)
    * docs : README 파일 추가
    
    * feat : 의존성 추가
    
    * feat : 유효성 검사 추가
    
    * feat : 유효성 검사를 위한 ProductController 수정
    
    * feat : GlobalExceptionHandler 추가
    
    * test : 테스트 코드 추가
    
    * fix : 한글을 유효하지 않은 값이라고 처리하지 않도록 수정
    
    * fix : 한글을 유효하지 않은 값이라고 처리하지 않도록 수정
    
    * feat : 불필요한 메소드 삭제
    
    * feat : 어노테이션 메세지 수정
    
    * feat : 어노테이션 메세지 수정
    
    * fix : 불필요한 코드 삭제
    
    * feat : @Min 어노테이션 추가
    
    * feat : build.gradle 파일 수정
    
    * feat : 불필요한 코드 삭제
    
    * feat : 서비스 레이어 추가
    
    * feat : 서비스 레이어 호출하도록 수정
    
    * fix : 의존성 추가
    
    * feat: schema에 user 테이블 추가
    
    * feat : users 초기값 설정
    
    * feat : UserService 클래스 작성
    
    * feat : UserRepository 클래스 생성
    
    * feat : User 클래스 생성
    
    * fix : GlobalExceptionHandler 수정
    
    * fix : GlobalExceptionHandler 오타 수정
    
    * feat : 토큰 값을 json으로 반환하기 위한 TokenResponse 클래스 생성
    
    * feat : UserController 생성
    
    * docs : README 파일 수정
    
    * feat : application.properties 수정
    
    * fix : gitignore에 app.log 추가
    
    * feat : request.http에회원가입, 로그인 추가

--- src/main/java/gift/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,5 +27,19 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleAllExceptions(Exception ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        body.put("error", "Internal Server Error");
+        body.put("message", ex.getMessage());
+        body.put("path", "");
+
+        ex.printStackTrace();
+
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
