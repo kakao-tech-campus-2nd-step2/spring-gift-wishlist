1061f8e3ec84a1f191b20d4565a52f81d7ff0ad6
Seonghun Jeong <tjdgns5506@gmail.com>
2024-07-07 12:47:44 +0900

    refactor: 생성자 의존성 주입으로 리팩토링

--- src/main/java/gift/validation/UniqueEmailValidator.java
+++ src/main/java/gift/validation/UniqueEmailValidator.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class UniqueEmailValidator implements ConstraintValidator<UniqueEmail, String> {
 
-    @Autowired
-    MemberService memberService;
+    private final MemberService memberService;
+
+    public UniqueEmailValidator(MemberService memberService) {
+        this.memberService = memberService;
+    }
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
