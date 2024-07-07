6675de7521e054f87532227b9bc51c39292347c9
sanghee0820 <lsh277604@gmail.com>
2024-07-07 11:14:48 +0900

    refactor: 삭제되지 않은 wish 반환으로 쿼리 수정

--- src/main/java/gift/model/dao/WishQuery.java
+++ src/main/java/gift/model/dao/WishQuery.java
@@ -3,6 +3,6 @@ package gift.model.dao;
 public class WishQuery {
     public static final String INSERT_WISH = "INSERT INTO wishes (user_id, product_id, amount) VALUES (?, ?, ?)";
     public static final String UPDATE_WISH = "UPDATE wishes SET amount = ? WHERE id = ?";
-    public static final String SELECT_WISH_BY_ID_AND_USER_ID = "SELECT * FROM wishes WHERE id = ? AND user_id = ?";
-    public static final String SELECT_WISH_BY_USER_ID = "SELECT * FROM wishes WHERE user_id = ?";
+    public static final String SELECT_WISH_BY_ID_AND_USER_ID = "SELECT * FROM wishes WHERE id = ? AND user_id = ? where is_deleted = false";
+    public static final String SELECT_WISH_BY_USER_ID = "SELECT * FROM wishes WHERE user_id = ? where is_deleted = false";
 }
