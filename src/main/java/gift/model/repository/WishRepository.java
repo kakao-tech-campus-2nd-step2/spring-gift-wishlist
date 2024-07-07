cd605534aabe02148573e77fedb97289daf1fa03
sanghee0820 <lsh277604@gmail.com>
2024-07-07 10:45:00 +0900

    feat: userId로 Wish찾는 함수 추가

--- src/main/java/gift/model/repository/WishRepository.java
+++ src/main/java/gift/model/repository/WishRepository.java
@@ -1,6 +1,8 @@
 package gift.model.repository;
 
 import gift.model.Wish;
+import java.util.Optional;
 
 public interface WishRepository extends RepositoryInterface<Wish, Long> {
+    Optional<Wish> findByIdAndUserId(Long id, Long userId);
 }
