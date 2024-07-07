a0a733c382409a8a655930407f8782ed2926e5be
Seonghun Jeong <tjdgns5506@gmail.com>
2024-07-07 12:48:17 +0900

    refactor: 임포트 정리

--- src/main/java/gift/validation/UniqueEmailValidator.java
+++ src/main/java/gift/validation/UniqueEmailValidator.java
@@ -3,7 +3,6 @@ package gift.validation;
 import gift.service.MemberService;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class UniqueEmailValidator implements ConstraintValidator<UniqueEmail, String> {
 
