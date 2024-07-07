b1f8cf04780a97a0b1a69ded812aa5af6b2f180b
alsswl <alswl4223@naver.com>
2024-07-07 14:17:56 +0900

    refactor : 충돌 해결

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -4,3 +4,13 @@ CREATE TABLE menus (
                        price INT NOT NULL,
                        imageUrl VARCHAR(2048) NOT NULL
 );
+CREATE TABLE members (
+                         id VARCHAR(255),
+                         passwd VARCHAR(255)
+);
+
+CREATE TABLE wishList (
+                          id INT AUTO_INCREMENT PRIMARY KEY,
+                          menuId INT,
+                          memberId VARCHAR(255)
+);
