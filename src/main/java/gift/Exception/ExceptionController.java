4dcc7ea9fe9bcad537f50ab41029a2bdd1f578c7
unknown <dn06023@naver.com>
2024-07-07 15:22:55 +0900

    reafactor : ExceptionHandler 변경

--- src/main/java/gift/Exception/ExceptionController.java
+++ src/main/java/gift/Exception/ExceptionController.java
@@ -3,15 +3,16 @@ package gift.Exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class ExceptionHandler {
-    @org.springframework.web.bind.annotation.ExceptionHandler(LoginException.class)
+public class ExceptionController {
+    @ExceptionHandler(LoginException.class)
     public ResponseEntity<?> handleLoginException(LoginException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(AuthorizedException.class)
+    @ExceptionHandler(AuthorizedException.class)
     public ResponseEntity<?> handleAuthorizedException(LoginException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
     }
