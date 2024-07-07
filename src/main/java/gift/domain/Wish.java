dff12de65b0bb06d4f5846c3710280cf0de0ee68
박민규 <pmq0101@naver.com>
2024-07-07 18:06:36 +0900

    feat(User, Wish): getter 추가

--- src/main/java/gift/domain/Wish.java
+++ src/main/java/gift/domain/Wish.java
@@ -5,4 +5,26 @@ public class Wish {
     private Long userId;
     private Long productId;
     private int count;
+
+    public Wish(Long userId, Long productId, int count) {
+        this.userId = userId;
+        this.productId = productId;
+        this.count = count;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public int getCount() {
+        return count;
+    }
 }
