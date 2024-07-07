a336f88edf5e69de1cf5a9afcefaa57a20114a56
sanghee0820 <lsh277604@gmail.com>
2024-07-07 10:47:58 +0900

    feat: wish 수정 API 추가

--- src/main/java/gift/controller/WishController.java
+++ src/main/java/gift/controller/WishController.java
@@ -11,6 +11,7 @@ import gift.model.repository.WishRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -27,20 +28,32 @@ public class WishController {
         this.wishRepository = wishRepository;
     }
 
-    @PostMapping("/{productId}")
+    @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
-    public void addWish(@PathVariable("productId") Long productId,
-                        @RequestBody WishRequest wishRequest,
+    public void addWish(@RequestBody WishRequest wishRequest,
                         @LoginUser User loginUser
     ) {
-        if (!wishRequest.productId().equals(productId)) {
-            throw new IllegalArgumentException("요청 URL과 상품 ID가 일치하지 않습니다.");
-        }
-
-        Product product = productRepository.find(productId)
-                .orElseThrow(() -> ProductNotFoundException.of(productId));
+        Product product = productRepository.find(wishRequest.productId())
+                .orElseThrow(() -> ProductNotFoundException.of(wishRequest.productId()));
 
         Wish wish = wishRequest.toModel(loginUser.getId());
         wishRepository.save(wish);
+    }
+
+    @PutMapping("/{wishId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateWish(@PathVariable("wishId") Long wishId,
+                           @RequestBody WishRequest wishRequest,
+                           @LoginUser User loginUser
+    ) {
+        Wish wish = wishRepository.findByIdAndUserId(loginUser.getId(), wishId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 위시리스트를 찾을 수 없습니다."));
+
+        if (!wish.getProductId().equals(wishRequest.productId())) {
+            throw new IllegalArgumentException("위시리스트의 상품을 변경할 수 없습니다.");
+        }
+
+        Wish newWish = wishRequest.toModel(wishId, loginUser.getId());
+        wishRepository.save(newWish);
     }
 }
