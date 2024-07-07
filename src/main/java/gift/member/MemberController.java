737d9a682e6104e9b601bc5800eda750b724b3c1
geist9110 <geist9110@gmail.com>
2024-07-07 17:27:46 +0900

    fix(wishlist): Fix login and register method not contained authorization header

--- src/main/java/gift/member/MemberController.java
+++ src/main/java/gift/member/MemberController.java
@@ -1,5 +1,6 @@
 package gift.member;
 
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,12 +18,12 @@ public class MemberController {
     }
 
     @PostMapping("/register")
-    public void register(@Valid @RequestBody Member member) {
-        memberService.register(member);
+    public void register(@Valid @RequestBody Member member, HttpServletResponse response) {
+        response.setHeader("Authorization", memberService.register(member));
     }
 
     @PostMapping("/login")
-    public void login(@Valid @MemberResolver Member member) {
-        memberService.login(member);
+    public void login(@Valid @MemberResolver Member member, HttpServletResponse response) {
+        response.setHeader("Authorization", memberService.login(member));
     }
 }
