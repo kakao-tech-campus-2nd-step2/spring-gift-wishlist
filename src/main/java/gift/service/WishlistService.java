b966d74c004436597aa706b71acecba6146aabc0
NextrPlue <p.plue1881@gmail.com>
2024-07-07 19:19:06 +0900

    feat(service): add WishlistService

---
+++ src/main/java/gift/service/WishlistService.java
@@ -0,0 +1,37 @@
+// WishlistService.java
+package gift.service;
+
+import gift.entity.Product;
+import gift.entity.Wish;
+import gift.repository.ProductRepository;
+import gift.repository.WishlistRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WishlistService {
+    private final WishlistRepository wishlistRepository;
+    private final ProductRepository productRepository;
+
+    public WishlistService(WishlistRepository wishlistRepository, ProductRepository productRepository) {
+        this.wishlistRepository = wishlistRepository;
+        this.productRepository = productRepository;
+    }
+
+    public Wish addProduct(Wish product) {
+        return wishlistRepository.addOrUpdateProduct(product);
+    }
+
+    public List<Wish> getProductsByMemberId(Long memberId) {
+        return wishlistRepository.getProductsByMemberId(memberId);
+    }
+
+    public void deleteItem(Long wishId) {
+        wishlistRepository.deleteItem(wishId);
+    }
+
+    public Product getProductById(Long productId) {
+        return productRepository.findById(productId).orElseThrow(() -> new IllegalArgumentException("Product not found"));
+    }
+}
