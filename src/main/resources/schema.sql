400ed4e83306784f111de37230f630cf1cb8f13c
GitJIHO <jiho99322@gmail.com>
2024-07-07 22:02:06 +0900

    feat: wishes테이블에 NOT NULL 제약조건 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE product (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    price INT,
+    name VARCHAR(255) NOT NULL,
+    price INT NOT NULL,
     img VARCHAR(255)
 );
 
@@ -15,9 +15,9 @@ CREATE TABLE users (
 CREATE TABLE wishes
 (
     id         BIGINT AUTO_INCREMENT PRIMARY KEY,
-    user_id    BIGINT,
-    product_id BIGINT,
-    number     BIGINT,
+    user_id    BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    number     BIGINT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (product_id) REFERENCES product (id)
 );
