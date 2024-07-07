466ffeac04e0c08de539bfbd0bba8558ef10346c
hyeonseo <69136047+hxunsc@users.noreply.github.com>
2024-07-07 21:43:14 +0900

    refactor: Remove Setter

--- src/main/java/gift/member/dto/MemberRequest.java
+++ src/main/java/gift/member/dto/MemberRequest.java
@@ -8,15 +8,7 @@ public class MemberRequest {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 }
