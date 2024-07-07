c69db497e20caefa80af90c1916432088d8259cf
dong_yxxn <g0326kdy@gmail.com>
2024-07-07 18:09:10 +0900

    refactor(ProductController): 임시로 @Validated 부여

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -6,12 +6,14 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
+@Validated
 public class ProductController {
 
     private final ProductRepository productRepository;
