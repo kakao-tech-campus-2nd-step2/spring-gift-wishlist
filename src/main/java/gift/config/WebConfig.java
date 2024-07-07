4b89b892e893151913c123ba56c9892aa40194c1
5win <woozzoo7@gmail.com>
2024-07-07 17:33:08 +0900

    fix: Add excluded path into interceptor - 로그인, 회원가입 경로를 제외시킴.

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(jwtInterceptor).addPathPatterns("/api/products")
-            .addPathPatterns("/api/member/**");
+        registry.addInterceptor(jwtInterceptor).addPathPatterns("/api/member/**")
+            .excludePathPatterns("/api/member/login", "/api/member/register");
     }
 }
