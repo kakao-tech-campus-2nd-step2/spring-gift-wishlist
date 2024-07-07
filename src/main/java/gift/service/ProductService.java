b97050d5ce03494f27bbbd14289ba9e59305f7f1
us4c0d3 <jwseok312@gmail.com>
2024-07-07 22:01:12 +0900

    refactor(Product): ProductNotFoundException 로직 service 계층으로 이동

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -1,5 +1,6 @@
 package gift.service;
 
+import gift.exception.product.ProductNotFoundException;
 import gift.model.Product;
 import gift.repository.ProductRepository;
 import java.util.List;
@@ -18,6 +19,7 @@ public class ProductService {
     }
 
     public Product getProductById(Long id) {
+        checkProductExist(id);
         return productRepository.findById(id);
     }
 
@@ -26,11 +28,20 @@ public class ProductService {
     }
 
     public void updateProduct(Long id, Product product) {
+        checkProductExist(id);
         productRepository.update(id, product);
     }
 
     public void deleteProduct(Long id) {
+        checkProductExist(id);
         productRepository.delete(id);
     }
 
+    public void checkProductExist(Long id) {
+        Product product = productRepository.findById(id);
+
+        if(product == null) {
+            throw new ProductNotFoundException("해당 ID의 상품을 찾을 수 없습니다.");
+        }
+    }
 }
