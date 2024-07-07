1ef8f4af8f084981807f73d56c914c42a8519c8a
hyunseo <wndlthsk@naver.com>
2024-07-07 15:07:47 +0900

    feat : wish 테이블 추가 및 초기화

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -10,3 +10,11 @@ CREATE TABLE member (
                         email VARCHAR(255) NOT NULL,
                         password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE wish (
+                      id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                      member_id BIGINT NOT NULL,
+                      product_id BIGINT NOT NULL,
+                      FOREIGN KEY (member_id) REFERENCES member(id),
+                      FOREIGN KEY (product_id) REFERENCES product(id)
+);
