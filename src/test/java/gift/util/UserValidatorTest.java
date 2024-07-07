326ed56bbd0c4351ab4bf1e0b0dac6a86c1c4c5b
Giheon Do <dgh0001@naver.com>
2024-07-07 09:56:33 +0900

    test: add userEmailValidator test

--- src/test/java/gift/util/UserValidatorTest.java
+++ src/test/java/gift/util/UserValidatorTest.java
@@ -1,4 +1,44 @@
 package gift.util;
 
-public class UserValidator {
+
+import gift.model.ProductDTO;
+import gift.model.User;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Set;
+
+@SpringBootTest
+public class UserValidatorTest {
+
+    @Autowired
+    private Validator validator;
+
+    @Test
+    public void save_emailSuccess() {
+        //given
+        User user = new User("test@naver.com", "abc");
+
+        //when
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+
+        //then
+        Assertions.assertThat(violations).isEmpty();
+    }
+
+    @Test
+    public void save_emailFailure() {
+        //given
+        User user = new User("test@123@naver,com", "abc");
+
+        //when
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+
+        //then
+        Assertions.assertThat(violations).isNotEmpty();
+    }
 }
