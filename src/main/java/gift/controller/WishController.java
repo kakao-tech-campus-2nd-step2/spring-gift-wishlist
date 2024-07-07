4c5fc12e9a77835d6a7f96987b7b256657b0ca7c
donghyun kim <gimdonghyun5434@gmail.com>
2024-07-07 14:47:23 +0900

    feat : Controller에 addWish 추가

---
+++ src/main/java/gift/controller/WishController.java
@@ -0,0 +1,39 @@
+package gift.controller;
+
+import gift.dto.WishPatchDto;
+import gift.dto.WishRequestDto;
+import gift.dto.WishResponseDto;
+import gift.service.JwtProvider;
+import gift.service.WishService;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/wishes")
+public class WishController {
+    private final JwtProvider jwtProvider;
+    private final WishService wishService;
+    public WishController(JwtProvider jwtProvider, WishService wishService) {
+        this.jwtProvider = jwtProvider;
+        this.wishService = wishService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> addWish(@RequestHeader("Authorization") String fullToken, @RequestBody WishRequestDto wishRequestDto){
+        String userEmail = jwtProvider.getUserEmail(fullToken.substring(7));
+        wishService.addWish(userEmail,wishRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+
+}
