8834328fc6b117c030f945269b239319c85bc2f9
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-07-07 18:55:06 +0900

    refactor : 코딩 컨벤션 준수

--- src/main/java/gift/controller/WishlistController.java
+++ src/main/java/gift/controller/WishlistController.java
@@ -7,9 +7,15 @@ import gift.model.User;
 import gift.annotation.LoginUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/wishlist")
