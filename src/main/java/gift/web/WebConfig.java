0e800f23d80a4e1c291e5f0dc47e6e1c1d787d95
yoong-saks <wjdghtjd06@gmail.com>
2024-07-08 06:25:50 +0900

    feat: [step3] WebMvcConfigurer 구현

---
+++ src/main/java/gift/web/WebConfig.java
@@ -0,0 +1,22 @@
+package gift.web;
+
+import gift.service.member.MemberService;
+import gift.web.jwt.AuthUserArgumentResolver;
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final MemberService memberService;
+
+    public WebConfig(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new AuthUserArgumentResolver(memberService));
+    }
+}
