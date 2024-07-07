72df5edbd60b91ac3e821a80048299577954c015
UiChan Lee <junglkls1234@gmail.com>
2024-07-07 14:14:20 +0900

    Feat: webConfig resolver연결

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -1,22 +1,18 @@
 package gift.config;
 
-/*import gift.jwt.JwtUtil;
-import gift.service.MemberService;
-import jakarta.servlet.Filter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    private final JwtUtil jwtUtil;
-    private final MemberService memberService;
+    private LoginMemberArgumentResolver loginMemberArgumentResolver;
 
-    @Autowired
-    public WebConfig(JwtUtil jwtUtil, MemberService memberService) {
-        this.jwtUtil = jwtUtil;
-        this.memberService = memberService;
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginMemberArgumentResolver);
     }
-}*/
+
+}
