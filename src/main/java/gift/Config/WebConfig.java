fcdc4d95d5e27b03287b6305d54ba41b12960382
rbm0524 <rbm0524@naver.com>
2024-07-07 16:12:59 +0900

    feat : WebConfig 작성

---
+++ src/main/java/gift/Config/WebConfig.java
@@ -0,0 +1,25 @@
+package gift.Config;
+
+import gift.Annotation.WishListResolverHandlerMethodArgumentResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final WishListResolverHandlerMethodArgumentResolver wishListResolverHandlerMethodArgumentResolver;
+
+    @Autowired
+    public WebConfig(WishListResolverHandlerMethodArgumentResolver wishListResolverHandlerMethodArgumentResolver) {
+        this.wishListResolverHandlerMethodArgumentResolver = wishListResolverHandlerMethodArgumentResolver;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(wishListResolverHandlerMethodArgumentResolver);
+    }
+}
