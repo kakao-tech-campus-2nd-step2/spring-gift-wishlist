ce3a15c7588edc50c99543aef6ed380b8791caa8
ez23re <ez23re@gmail.com>
2024-07-07 12:58:27 +0900

    feat : 3단계 위시리스트 mvc 구현

--- src/main/java/gift/service/WishListService.java
+++ src/main/java/gift/service/WishListService.java
@@ -1,9 +1,10 @@
 package gift.service;
 
+import gift.entity.Product;
+import gift.entity.WishList;
+import gift.repository.WishListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class WishListService {
@@ -11,14 +12,18 @@ public class WishListService {
     @Autowired
     private WishListRepository wishListRepository;
 
-    public List<Product> getWishList(Long userId) {
+    // id로 위시리스트 찾기
+    public WishList findByUserId(Long userId) {
         return wishListRepository.findByUserId(userId);
     }
 
+    // 위시리스트에 상품 추가
     public void addProductToWishList(Long userId, Product product) {
-
+        wishListRepository.addProductToWishList(userId, product);
     }
 
+    // 위시리스트에 상품 삭제
     public void removeProductFromWishList(Long userId, Long productId) {
+        wishListRepository.removeProductFromWishList(userId, productId);
     }
 }
