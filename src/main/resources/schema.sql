ed37c9430ee5aec6a6a052ab08f30b1563e6d293
Kim Sanghae <pelikan@pusan.ac.kr>
2024-07-07 22:16:46 +0900

    chore: 위시 리스트 테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,5 +9,20 @@ CREATE TABLE IF NOT EXISTS products(
 CREATE TABLE IF NOT EXISTS members (
     id IDENTITY PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE wish_lists (
+                            id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                            member_id BIGINT,
+                            FOREIGN KEY (member_id) REFERENCES members (id)
+);
+
+CREATE TABLE wish_list_items (
+     id BIGINT AUTO_INCREMENT PRIMARY KEY,
+     wish_list_id BIGINT,
+     product_id BIGINT,
+     quantity INT,
+     FOREIGN KEY (wish_list_id) REFERENCES wish_lists(id),
+     FOREIGN KEY (product_id) REFERENCES products(id)
 );
