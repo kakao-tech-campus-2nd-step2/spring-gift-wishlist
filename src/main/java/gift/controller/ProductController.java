b87d5e9a3c068fb6990b8c34d418c31dbb0090c6
dong_yxxn <g0326kdy@gmail.com>
2024-07-07 15:24:38 +0900

    refactor(ProductController): @Validated 삭제 - controller에서는 필요없음

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -5,7 +5,6 @@ import gift.repository.ProductRepository;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 
 import org.springframework.web.bind.annotation.*;
 
@@ -13,7 +12,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
-@Validated
 public class ProductController {
 
     private final ProductRepository productRepository;
