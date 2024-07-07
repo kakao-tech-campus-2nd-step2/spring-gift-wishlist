7f4533734a52c4cb1e8ebfd97064c701b7897c88
dong_yxxn <g0326kdy@gmail.com>
2024-07-07 18:25:21 +0900

    refactor(ProductController): 비즈니스 조직 제거 및 @Validated 제거

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -1,36 +1,33 @@
 package gift.controller;
 
 import gift.model.Product;
-import gift.repository.ProductRepository;
+import gift.service.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
-@Validated
 public class ProductController {
 
-    private final ProductRepository productRepository;
+    private final ProductService productService;
 
-    public ProductController(ProductRepository productRepository) {
-        this.productRepository = productRepository;
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
 
     @GetMapping("")
     public ResponseEntity<List<Product>> getAllProducts() {
-        List<Product> products = productRepository.findAll();
+        List<Product> products = productService.getAllProducts();
         return new ResponseEntity<>(products, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Product> findById(@PathVariable Long id) {
-        Product product = productRepository.findById(id);
+        Product product = productService.getProductById(id);
         if (product == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
@@ -39,27 +36,19 @@ public class ProductController {
 
     @PostMapping("")
     public ResponseEntity<Void> createProduct(@Valid @RequestBody Product product) {
-        productRepository.save(product);
+        productService.createProduct(product);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Product> updateProduct(@PathVariable("id") Long id, @Valid @RequestBody Product product) {
-        Product existingProduct = productRepository.findById(id);
-        if (existingProduct == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        productRepository.update(id, product);
+        productService.updateProduct(id, product);
         return new ResponseEntity<>(product, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Long> deleteProduct(@PathVariable Long id) {
-        Product existingProduct = productRepository.findById(id);
-        if (existingProduct == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        productRepository.deleteById(id);
+        productService.deleteProduct(id);
         return new ResponseEntity<>(id, HttpStatus.OK);
     }
 }
--- src/main/java/gift/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -12,7 +12,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ConstraintViolationException.class)
     public String handleConstraintViolationException(ConstraintViolationException ex, Model model) {
-        String errorMessage = ex.getMessage(); // 이 부분은 ConstraintViolationException의 메시지를 가져오는 것으로 변경
+        String errorMessage = ex.getMessage();
         model.addAttribute("errorMessage", errorMessage);
         model.addAttribute("product", new Product(0,"", 0, ""));
         return "add_product";
