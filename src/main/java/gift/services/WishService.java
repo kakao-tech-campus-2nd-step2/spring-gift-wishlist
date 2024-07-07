39353fd27641d02e3b18ce1c2a39b5a21d98a1a0
ariimo <159447466+ariimo@users.noreply.github.com>
2024-07-07 13:45:33 +0900

    feat: WishService 생성

---
+++ src/main/java/gift/services/WishService.java
@@ -0,0 +1,41 @@
+package gift.services;
+
+
+import gift.Wish;
+import gift.WishDto;
+import gift.repositories.WishRepository;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WishService {
+    private final WishRepository wishRepository;
+
+    public WishService(WishRepository wishRepository) {
+        this.wishRepository = wishRepository;
+    }
+
+//    Wishlist 조회
+    public List<WishDto> getWishListById (Long memberId){
+        List<Wish> wishList = wishRepository.findWishListById(memberId);
+        List<WishDto> wishDtoList = wishList.stream().map(wish -> new WishDto(
+            wish.getMemberId(),
+            wish.getProductId(),
+            wish.getProductName()
+        )).toList();
+
+        return wishDtoList;
+    }
+
+//    Wish 추가
+    public void addWish(WishDto wishDto){
+        Wish wish = new Wish(wishDto.getMemberId(), wishDto.getProductId(), null);
+        wishRepository.insertWish(wish);
+    }
+
+//    Wish 삭제
+    public void deleteWish(Long memberId, Long productId){
+        wishRepository.deleteWish(memberId, productId);
+    }
+
+}
