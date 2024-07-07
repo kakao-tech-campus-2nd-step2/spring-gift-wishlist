0764f463e1871b5b50c089019cef588fd05589a1
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-07-07 16:51:14 +0900

    feat : LoginDTO 생성 로그인을 할 때는 이메일과 비밀 번호만 필요하다고 생각하여 이렇게 작성했습니다.

---
+++ src/main/java/gift/Controller/LoginDTO.java
@@ -0,0 +1,13 @@
+package gift.Controller;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginDTO(
+    @NotBlank(message = "이메일을 입력하세요.")
+    @Email(message = "유효한 이메일을 입력하세요.")
+    String email,
+
+    @NotBlank(message = "비밀번호를 입력하세요.")
+    String password
+) {}
