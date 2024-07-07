5fa153adf321275574f0d73efdbd1f7e60854f57
rladbrua0207 <48901587+rladbrua0207@users.noreply.github.com>
2024-07-07 19:15:52 +0900

    refact: Webconfig 생성자 1개일때 @Autowired 어노테이션 제거

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -1,6 +1,5 @@
 package gift.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,7 +9,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final AuthInterceptor authInterceptor;
 
-    @Autowired
     public WebConfig(AuthInterceptor authInterceptor) {
         this.authInterceptor = authInterceptor;
     }
