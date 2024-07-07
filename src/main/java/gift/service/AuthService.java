3e5cabfad97cb5785f836493237755b5dc766756
박민규 <pmq0101@naver.com>
2024-07-07 18:08:03 +0900

    feat(AuthService): 인가 구현

--- src/main/java/gift/service/AuthService.java
+++ src/main/java/gift/service/AuthService.java
@@ -29,4 +29,16 @@ public class AuthService {
         String token = jwtUtil.createToken(user.getEmail(), user.getRole());
         return new UserResponseDTO(token);
     }
+
+    public void authorizeUser(User user, Long userId){
+        if (!user.getId().equals(userId)){
+            throw new IllegalStateException("권한이 없습니다.");
+        }
+    }
+
+    public void authorizeAdminUser(User user){
+        if (!user.getRole().equals("관리자")){
+            throw new IllegalStateException("권한이 없습니다.");
+        }
+    }
 }
