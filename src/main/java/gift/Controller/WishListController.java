fa113bd8890028e46751c1548232ee1c868f7e10
rbm0524 <rbm0524@naver.com>
2024-07-07 16:11:38 +0900

    fix : getWishlist JWT 사용으로 수정

--- src/main/java/gift/Controller/WishListController.java
+++ src/main/java/gift/Controller/WishListController.java
@@ -1,19 +1,23 @@
 package gift.Controller;
 
+import gift.Annotation.WishListResolver;
 import gift.Model.User;
 import gift.Model.WishListItem;
 import gift.Service.UserService;
 import gift.Service.WishListService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.WebUtils;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/wishlist")
 public class WishListController {
     private final WishListService wishlistService;
     private final UserService userService;
@@ -24,28 +28,34 @@ public class WishListController {
         this.userService = userService;
     }
 
-    @GetMapping
+    @GetMapping("/wishlist")
     public String getWishlist(Model model, HttpServletRequest request) {
-        String userEmail = (String) request.getAttribute("userEmail");
+        Cookie jwtCookie = WebUtils.getCookie(request, "token");
+        String token = jwtCookie.getValue();
+        Claims claims = Jwts.parser()
+                .setSigningKey(getSigningKey().getBytes())
+                .parseClaimsJws(token)
+                .getBody();
+        String userEmail = claims.getSubject();
         User user = userService.findByEmail(userEmail);
         List<WishListItem> wishlist = wishlistService.getWishlist(user.getId());
         model.addAttribute("wishlist", wishlist);
-        return "wishlist"; // 이 부분이 변경되었습니다.
+        return "wishlist";
     }
 
-    @PostMapping("/add")
-    public String addWishlistItem(HttpServletRequest request, @RequestParam int productId) {
-        String userEmail = (String) request.getAttribute("userEmail");
-        User user = userService.findByEmail(userEmail);
-        wishlistService.addWishlistItem(user.getId(), productId);
-        return "redirect:/wishlist";
+    public String getSigningKey() {
+        return "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
     }
 
-    @PostMapping("/remove/{productId}")
-    public String removeWishlistItem(HttpServletRequest request, @PathVariable int productId) {
-        String userEmail = (String) request.getAttribute("userEmail");
-        User user = userService.findByEmail(userEmail);
-        wishlistService.removeWishlistItem(user.getId(), productId);
+    @PostMapping("/wishlist/add")
+    public String addWishlistItem(@WishListResolver WishListItem wishlistItem) {
+        wishlistService.addWishlistItem(wishlistItem);
+        return "redirect:/products";
+    }
+
+    @PostMapping("/wishlist/remove/{productId}")
+    public String removeWishlistItem(@WishListResolver WishListItem wishListItem) {
+        wishlistService.removeWishlistItem(wishListItem);
         return "redirect:/wishlist";
     }
 }
