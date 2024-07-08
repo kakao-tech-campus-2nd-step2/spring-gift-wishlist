fc37956fc81b843abb8cd0c81c0f3f29856a5e2e
SeoYeongU <westzeroright@gmail.com>
2024-07-08 18:56:05 +0900

    test(ProductControllerTest): POST 테스트 코드

---
+++ src/test/java/gift/product/ProductControllerTest.java
@@ -0,0 +1,41 @@
+package gift.product;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URI;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.RequestEntity;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpHeaders.LOCATION;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
+
+@SpringBootTest(webEnvironment =  WebEnvironment.RANDOM_PORT)
+class ProductControllerTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    //private ProductDao productDao;
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void port() {
+        assertThat(port).isNotZero();
+    }
+
+    @Test
+    void create() {
+        var url = "http://localhost:" + port + "/api/products";
+        var request = new ProductRequestDto("사과",2000,"www");
+        var requestEntity = new RequestEntity<>(request, POST, URI.create(url));
+        var actual = restTemplate.exchange(requestEntity, String.class);
+        assertThat(actual.getStatusCode()).isEqualTo(OK);
+    }
+}
