db5128536b9dc0a87080c5d64fff7c00717b0863
Kim Sanghae <pelikan@pusan.ac.kr>
2024-07-09 05:14:13 +0900

    feat: Wish 테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,36 +1,19 @@
-CREATE TABLE IF NOT EXISTS products(
-
-    id BIGINT PRIMARY KEY,
-    name VARCHAR(255),
-    price INT,
-    image_url VARCHAR(255)
+CREATE TABLE IF NOT EXISTS product (
+id BIGINT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(255) NOT NULL,
+price INTEGER NOT NULL,
+imageUrl VARCHAR(255)
 );
-
 
 CREATE TABLE IF NOT EXISTS members (
-    id IDENTITY PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+id BIGINT AUTO_INCREMENT PRIMARY KEY,
+email VARCHAR(255) NOT NULL UNIQUE,
+password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE wish_lists (
-                            id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                            member_id BIGINT,
-                            FOREIGN KEY (member_id) REFERENCES members (id)
+CREATE TABLE IF NOT EXISTS wishes (
+id BIGINT AUTO_INCREMENT PRIMARY KEY,
+member_id BIGINT NOT NULL,
+product_name VARCHAR(255) NOT NULL,
+FOREIGN KEY (member_id) REFERENCES members(id)
 );
-
-CREATE TABLE wish_list_items (
-     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-     wish_list_id BIGINT,
-     product_id BIGINT,
-     quantity INT,
-     FOREIGN KEY (wish_list_id) REFERENCES wish_lists(id),
-     FOREIGN KEY (product_id) REFERENCES products(id)
-);
-
-id BIGINT PRIMARY KEY,
-name VARCHAR(255),
-price INT,
-image_url VARCHAR(255)
-);
-
