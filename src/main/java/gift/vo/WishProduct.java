2f9545a8c0d8ce221e3643bacb3ca86328774ef4
HaegyeongKim01 <haegyeong.kim01@gmail.com>
2024-07-08 12:01:30 +0900

    style: 주석 보충 설명

--- src/main/java/gift/vo/WishProduct.java
+++ src/main/java/gift/vo/WishProduct.java
@@ -3,7 +3,7 @@ package gift.vo;
 public class WishProduct {
 
     private Long wishProductId;
-    private String memberId; //Member 객체의 pk는 email로 사용된다. 여기서 사용하는 memberId 역시 Member의 email이다.
+    private String memberId; //Member pk는 String-email. Member 데이터 구조가 변경된다면 이 필드 타입도 변경 필요
     private Long productId;
 
     public WishProduct(String memberId, Long productId) {
