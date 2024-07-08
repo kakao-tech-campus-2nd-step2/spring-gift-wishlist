61fd3977bbceb39126337111249477e95b90c9ba
seojiu <seojiu5598@gmail.com>
2024-07-09 05:31:35 +0900

    fix(Member.java): 유효성 검사 중복 삭제

--- src/main/java/gift/model/Member.java
+++ src/main/java/gift/model/Member.java
@@ -5,10 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 
 public class Member {
     private Long id;
-    @NotBlank(message = "이메일은 필수 입력값입니다.")
-    @Email(message = "유효하지 않은 이메일 형식입니다.")
     private String email;
-    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     private String password;
     private String activeToken;
 
