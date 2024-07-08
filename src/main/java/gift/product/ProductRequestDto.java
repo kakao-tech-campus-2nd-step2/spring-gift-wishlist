2fdf6a40e4bf2cacb4cbd3ede33127b9e2f9be96
SeoYeongU <westzeroright@gmail.com>
2024-07-08 15:43:18 +0900

    refactor: kakao, Kakao, KAKAO 등과 같은 표현 역시 검증

--- src/main/java/gift/product/ProductRequestDto.java
+++ src/main/java/gift/product/ProductRequestDto.java
@@ -9,7 +9,7 @@ public record ProductRequestDto(
     @NotBlank
     @Size(max = 15, message = "상품 이름은 공백을 포함하여 최대 15자까지 입력할 수 있습니다")
     @Pattern(regexp = "^[a-zA-Z0-9가-힣()*\\[\\]+\\-&/_]*$", message = "허용되지 않은 특수문자입니다")
-    @Pattern(regexp = "^(?!.*카카오).*$", message = "'카카오'가 포함된 문구는 담당 MD와 협의한 경우에만 사용 가능합니다")
+    @Pattern(regexp = "^(?i)(?!.*(?:카카오|kakao)).*$", message = "'카카오' 또는 'kakao'가 포함된 문구는 담당 MD와 협의한 경우에만 사용 가능합니다")
     String name,
     @Min(value = 1,message = "상품 가격을 입력해주세요")
     int price,
