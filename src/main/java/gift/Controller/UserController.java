1cad9a72fb62cf7e769773d28d9754c476a4d390
Woojae-Jeong <83636428+Woojae-Jeong@users.noreply.github.com>
2024-07-09 08:52:20 +0900

    전남대 BE_정우재 2주차 과제 (3단계) (#442)
    * chore : 1주차 코드 복사 완료
    
    * docs : README 정리(구현할 기능 정리)
    
    * feat : 상품을 추가하고 수정하는 경우 잘못된 값인지 검사하는 기능 구현
    
    * feat : 잘못된 값이 전달된 경우 적절히 예외를 처리하는 기능 구현
    
    * docs : step2 README에 구현할 기능 정리
    
    * refactor : 서비스 레이어를 추가하기 위한 리팩토링
    
    Pproduct에 대한 Controller와 Service 레이어를 분리함. 이를 위한 코드 리팩토링을 진행
    
    * style : 리팩토링으로 인해 필요없는 주석 삭제
    
    * style : 서비스로 인식할 수 있게끔 Service 이노테이션 추가
    
    * feat : 회원가입 기능 구현
    
    * sytle : @Autorwired 어노테이션 추가
    
    * feat : 로그인 기능 구현
    
    * docs : jwt 관련 문서 수정
    
    * style : 안쓰는 import문 정리
    
    * feat : 토큰 발행 기능 구현
    
    * feat : 토큰 발행 관련 예외 추가
    
    * docs : step3 README 업데이트
    
    * feats : 올바르지 않은 토큰에 대한 예외 구현
    
    * feats : 올바르지 않은 토큰에 대한 예외 구현
    
    * feats : 올바르지 않은 토큰에 대한 예외 구현
    
    * feats : 요청에 들어있는 토큰을 바탕으로 토큰이 유효한지 검사하는 기능 구현
    
    * feats : 위시 리스트에 상품을 등록하는 기능 구현
    
    * feats : 위시 리스트에 등록된 상품 목록을 조회하는 기능 구현
    
    * feats : 위시 리스트에 등록된 상품을 수정하는 기능 구현
    
    * feats : 위시 리스트에 등록된 상품을 삭제하는 기능 구현
    
    * style : 참고했던 코드 흔적 삭제

---
+++ src/main/java/gift/Controller/UserController.java
@@ -0,0 +1,33 @@
+package gift.Controller;
+
+import gift.Model.User;
+import gift.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/members")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody User user){
+        userService.signUpUser(user);
+        return "signUp successed";
+    }
+
+
+    @PostMapping("/login")
+    public String loginUser(@RequestBody User user) {
+        return "{\"token\": \"" + userService.loginUser(user) + "\"}";
+    }
+
+
+
+
+}
---
+++ src/main/java/gift/Repository/ProductRepository.java
@@ -0,0 +1,58 @@
+package gift.Repository;
+
+import gift.Model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ProductRepository {
+    private final JdbcTemplate jdbctemplate;
+
+    @Autowired
+    public ProductRepository(JdbcTemplate jdbctemplate) {
+        this.jdbctemplate = jdbctemplate;
+    }
+
+    public void createProductTable() {
+        var sql = """
+                create table Products(
+                id bigint auto_increment,
+                name varchar(255),
+                price bigint,
+                imageUrl varchar(2000),
+                primary key(id)
+                )
+                """;
+        jdbctemplate.execute(sql);
+    }
+
+    public void insertProduct(Product product) {
+        var sql = "insert into Products (name, price, imageUrl) values (?,?,?)";
+        jdbctemplate.update(sql, product.getName(), product.getPrice(), product.getImageUrl());
+    }
+
+    public List<Product> selectProduct() {
+        var sql = "select * from products";
+        return jdbctemplate.query(sql, new BeanPropertyRowMapper<>(Product.class));
+    }
+
+    public Product selectProductById(Long id) {
+        var sql = "SELECT * FROM products WHERE id = ?";
+        return jdbctemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Product.class), id);
+
+    }
+
+    public void updateProduct(Long id, Product product) {
+        var sql = "UPDATE products SET name = ?, price = ?, imageUrl = ? WHERE id = ?";
+        jdbctemplate.update(sql, product.getName(), product.getPrice(), product.getImageUrl(), id);
+    }
+
+    public void deleteProduct(Long id) {
+        var sql = "DELETE FROM products WHERE id = ?";
+        jdbctemplate.update(sql, id);
+    }
+}
