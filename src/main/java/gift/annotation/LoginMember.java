36dae4e220e5eb2c383a5f01d872070137c6381c
Kim Sanghae <pelikan@pusan.ac.kr>
2024-07-08 18:49:55 +0900

    chore: Documented 어노테이션 추가

--- src/main/java/gift/annotation/LoginMember.java
+++ src/main/java/gift/annotation/LoginMember.java
@@ -1,5 +1,6 @@
-package gift.model.annotation;
+package gift.annotation;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -7,5 +8,6 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface LoginMember {
 }
