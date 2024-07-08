fe612f26f8766ff9a23c16c91dc9eeab7b2c558a
SeoYeongU <westzeroright@gmail.com>
2024-07-08 14:37:25 +0900

    refactor(Member): 사용하지 않는 setter 제거

--- src/main/java/gift/member/Member.java
+++ src/main/java/gift/member/Member.java
@@ -15,22 +15,6 @@ public class Member {
         this.role = role;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public void setRole(boolean role) {
         this.role = role;
     }
