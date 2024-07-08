4a652b6dfddac21ac0f9900fafdae10a48f5b9eb
ariimo <159447466+ariimo@users.noreply.github.com>
2024-07-08 22:03:55 +0900

    refactor: LoginMemberArgumentResolver 수정

--- src/main/java/gift/LoginMemberArgumentResolver.java
+++ src/main/java/gift/LoginMemberArgumentResolver.java
@@ -1,8 +1,10 @@
 package gift;
 
+import gift.classes.Exceptions.AuthException;
 import gift.services.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.core.MethodParameter;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -11,8 +13,11 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 
 @Component
 public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
-    private MemberService memberService;
+    private final MemberService memberService;
 
+    public LoginMemberArgumentResolver(MemberService memberService) {
+        this.memberService = memberService;
+    }
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -22,12 +27,23 @@ public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolve
     }
 
     @Override
-    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+    public MemberDto resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+
         HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
-        String token = request.getHeader("Authorization");
-        if (token == null) {
-            return null;
-        }
+        String auth = request.getHeader("authorization");
+        if (auth == null)
+            throw new AuthException("Authorization header is missing");
+
+        String[] splitted = auth.split(" ");
+        if (splitted.length < 2)
+            throw new AuthException("Invalid token format");
+
+        if (!splitted[0].equals("beareer"))
+            throw new AuthException("Invalid token type");
+        String token = splitted[1];
+
         return memberService.getLoginUser(token);
+
+
     }
 }
