a1f3471957036a4fe351c1d5d1e45e15ea84fc6c
ariimo <159447466+ariimo@users.noreply.github.com>
2024-07-08 21:58:10 +0900

    refactor: HTTP Method 수정

--- src/main/java/gift/controller/WishController.java
+++ src/main/java/gift/controller/WishController.java
@@ -1,14 +1,19 @@
 package gift.controller;
 
 import gift.LoginMember;
+import gift.Member;
 import gift.MemberDto;
 import gift.RequestWishDto;
 import gift.WishDto;
+import gift.services.MemberService;
 import gift.services.WishService;
 import java.util.List;
+import java.util.Map;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +21,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/wishlist")
 public class WishController {
     public final WishService wishService;
+    public final MemberService memberService;
 
-    public WishController(WishService wishService) {
+    public WishController(WishService wishService, MemberService memberService) {
         this.wishService = wishService;
+        this.memberService = memberService;
     }
 
 //    Wish 추가
@@ -35,8 +42,9 @@ public class WishController {
     }
 
 //    Wish 삭제
-    @GetMapping("/delete")
+    @DeleteMapping
     public void deleteWish(@RequestBody RequestWishDto requestWishDto, @LoginMember MemberDto memberDto){
         wishService.deleteWish(memberDto.getMemberId(), requestWishDto.getProductId());
+
     }
 }
