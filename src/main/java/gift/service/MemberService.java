0a38520e4fcbec2b087b9d512708cdbf92766a0f
nove1080 <skwpqjq@gmail.com>
2024-07-09 03:54:36 +0900

    refactor: MemberService.java login 중 예외 발생 세분화

--- src/main/java/gift/service/MemberService.java
+++ src/main/java/gift/service/MemberService.java
@@ -4,14 +4,13 @@ import gift.authentication.token.JwtProvider;
 import gift.authentication.token.Token;
 import gift.domain.Member;
 import gift.domain.vo.Email;
-import gift.domain.vo.Password;
 import gift.repository.MemberRepository;
 import gift.web.dto.request.LoginRequest;
 import gift.web.dto.request.member.CreateMemberRequest;
 import gift.web.dto.response.LoginResponse;
 import gift.web.dto.response.member.CreateMemberResponse;
 import gift.web.dto.response.member.ReadMemberResponse;
-import gift.web.validation.exception.InvalidCredentialsException;
+import gift.web.validation.exception.IncorrectEmailException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -37,15 +36,11 @@ public class MemberService {
         return ReadMemberResponse.fromEntity(member);
     }
 
-    //todo: 이메일로 회원 조회 후 Password.matches()로 비밀번호 검증 -> 디테일한 예외를 알려줄 수 있다
-    //이메일 조회가 안되면 -> 아이디 입력 잘못
-    //패스워드가 맞지 않으면 -> 패스워드를 잘못 입력
     public LoginResponse login(LoginRequest request) {
         Email email = Email.from(request.getEmail());
-        Password password = Password.from(request.getPassword());
+        Member member = memberRepository.findByEmail(email).orElseThrow(IncorrectEmailException::new);
 
-        Member member = memberRepository.findByEmailAndPassword(email, password)
-            .orElseThrow(InvalidCredentialsException::new);
+        member.matchPassword(request.getPassword());
 
         Token token = jwtProvider.generateToken(member);
 
