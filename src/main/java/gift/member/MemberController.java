7cd20b4df771bf7535453f4535d0b61435690d82
SeoYeongU <westzeroright@gmail.com>
2024-07-08 14:46:56 +0900

    refacor: url 리소스 표기 변경 - api/복수형

--- src/main/java/gift/member/MemberController.java
+++ src/main/java/gift/member/MemberController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/member")
+@RequestMapping("api/members")
 public class MemberController {
 
     private final MemberDao memberDao;
