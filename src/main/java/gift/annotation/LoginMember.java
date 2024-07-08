fd74c8ff268c6cf8997bbbbff60b6495314b0461
seoyoung-dev <seoyoung.dev@gmail.com>
2024-07-08 09:45:32 +0900

    feat: LoginUserArgumentResolver 등록

--- src/main/java/gift/annotation/LoginMember.java
+++ src/main/java/gift/annotation/LoginMember.java
@@ -1,4 +1,4 @@
-package gift.annotaion;
+package gift.annotation;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
--- src/main/java/gift/resolver/LoginUserArgumentResolver.java
+++ src/main/java/gift/resolver/LoginUserArgumentResolver.java
@@ -1,2 +1,44 @@
-package gift.resolver;public class LoginUserArgumentResolver {
+package gift.resolver;
+
+import gift.exception.TokenException;
+import gift.service.MemberService;
+import gift.util.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final MemberService memberService;
+    public LoginUserArgumentResolver(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(gift.annotation.LoginMember.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, org.springframework.web.bind.support.WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+        String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            return null;
+        }
+
+        String token = authorizationHeader.substring(7);
+        System.out.println(token);
+        JwtUtil.verifyToken(token);
+        String memberEmail = JwtUtil.getClaims(token).getSubject();
+        System.out.println(memberEmail);
+
+        return memberService.getMemberIdByEmail(memberEmail);
+    }
 }
