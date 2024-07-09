5e32aeeeb57814179096147994df28c3140b32f8
안원모 <tommy45231@naver.com>
2024-07-09 15:00:45 +0900

    Feat : Wish 도메인 생성

--- src/main/java/gift/GlobalExceptionHandler.java
+++ src/main/java/gift/GlobalExceptionHandler.java
@@ -1,11 +1,8 @@
 package gift;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
